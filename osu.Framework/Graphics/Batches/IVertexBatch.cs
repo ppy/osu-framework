f3d00712fd6eceb0c999ccc5e1aecf69e91525b3
Dean Herbert <pe@ppy.sh>
2016-08-26 13:14:57 +0900

    Initial commit.

---
+++ osu.Framework/Graphics/Batches/IVertexBatch.cs
@@ -0,0 +1,10 @@
+﻿//Copyright (c) 2007-2016 ppy Pty Ltd <contact@ppy.sh>.
+//Licensed under the MIT License - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
+
+namespace osu.Framework.Graphics.Batches
+{
+    public interface IVertexBatch
+    {
+        void Draw();
+    }
+}
---
+++ osu.Framework/Input/Handlers/IKeyboardInputHandler.cs
@@ -0,0 +1,15 @@
+﻿//Copyright (c) 2007-2016 ppy Pty Ltd <contact@ppy.sh>.
+//Licensed under the MIT License - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenTK.Input;
+
+namespace osu.Framework.Input.Handlers
+{
+    interface IKeyboardInputHandler
+    {
+        List<Key> PressedKeys { get; }
+    }
+}
