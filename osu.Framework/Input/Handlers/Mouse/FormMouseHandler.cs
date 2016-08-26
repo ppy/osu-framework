16f8982811293efbf99bb409da97ecb591553b70
Dean Herbert <pe@ppy.sh>
2016-08-26 17:26:09 +0900

    License -> Licence.

--- osu.Framework/Input/Handlers/Mouse/FormMouseHandler.cs
+++ osu.Framework/Input/Handlers/Mouse/FormMouseHandler.cs
@@ -1,5 +1,5 @@
 ﻿//Copyright (c) 2007-2016 ppy Pty Ltd <contact@ppy.sh>.
-//Licensed under the MIT License - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
+//Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
 
 using System.Collections.Generic;
 using System.Windows.Forms;
