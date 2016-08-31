4de834bd5c74ac6eb16aecae60056fe4eaf4d79d
Dean Herbert <pe@ppy.sh>
2016-08-31 16:03:31 +0900

    Add base web request classes and nuget dependencies.

---
+++ osu.Framework/IO/Network/FileWebRequest.cs
@@ -0,0 +1,37 @@
+//Copyright (c) 2007-2016 ppy Pty Ltd <contact@ppy.sh>.
+//Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
+
+using System;
+using System.IO;
+
+namespace osu.Framework.IO.Network
+{
+    /// <summary>
+    /// Downloads a file from the internet to a specified location
+    /// </summary>
+    public class FileWebRequest : WebRequest
+    {
+        public string Filename;
+
+        protected override Stream CreateOutputStream()
+        {
+            string path = Path.GetDirectoryName(Filename);
+            if (!string.IsNullOrEmpty(path)) Directory.CreateDirectory(path);
+
+            return new FileStream(Filename, FileMode.Create, FileAccess.Write, FileShare.Write, 32768);
+        }
+
+        public FileWebRequest(string filename, string url) : base(url)
+        {
+            Timeout *= 2;
+            Filename = filename;
+        }
+
+        protected override void Complete(Exception e = null)
+        {
+            ResponseStream?.Close();
+            if (e != null) FileSafety.FileDelete(Filename);
+            base.Complete(e);
+        }
+    }
+}
