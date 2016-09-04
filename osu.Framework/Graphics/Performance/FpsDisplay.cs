59ab223f21f2b10c69c07b2fdc842cfb7f345386
Dean Herbert <pe@ppy.sh>
2016-09-04 17:45:31 +0900

    Add basic FPS display for update/draw loops.

---
+++ osu.Framework/Graphics/Performance/FpsDisplay.cs
@@ -0,0 +1,47 @@
+﻿//Copyright (c) 2007-2016 ppy Pty Ltd <contact@ppy.sh>.
+//Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Timing;
+using OpenTK;
+
+namespace osu.Framework.Graphics.Performance
+{
+    class FpsDisplay : AutoSizeContainer
+    {
+        SpriteText counter;
+
+        private readonly string name;
+        private ThrottledFrameClock clock;
+
+        public FpsDisplay(string name, ThrottledFrameClock clock)
+        {
+            Position = new Vector2(5, 5);
+            this.name = name;
+            this.clock = clock;
+        }
+
+        public override void Load()
+        {
+            base.Load();
+
+            Add(counter = new SpriteText()
+            {
+                Text = @"...",
+            });
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+
+            counter.Text = $@"{name}: " + (1000 / clock.AverageFrameTime).ToString(@"0");
+        }
+    }
+}
