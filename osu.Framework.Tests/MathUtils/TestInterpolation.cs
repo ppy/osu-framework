261b3517e847d3396be33e6f3dc268815568f20b
Drew DeVault <sir@cmpwn.com>
2016-09-05 08:43:18 -0400

    Add test suite and some simple tests
    Now based on NUnit

---
+++ osu.Framework.Tests/MathUtils/TestInterpolation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+using osu.Framework.MathUtils;
+
+namespace osu.Framework.Tests.MathUtils
+{
+    [TestFixture]
+    public class TestInterpolation
+    {
+        [Test]
+        public void TestLerp()
+        {
+            Assert.AreEqual(5, Interpolation.Lerp(0, 10, 0.5f));
+            Assert.AreEqual(0, Interpolation.Lerp(0, 10, 0));
+            Assert.AreEqual(10, Interpolation.Lerp(0, 10, 1));
+        }
+    }
+}
+
