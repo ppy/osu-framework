b5fc3cad2a5bdafbd679ac25f814f67c2ccc7024
Dean Herbert <pe@ppy.sh>
2016-09-07 19:06:56 +0900

    Add base VisualTests project.

--- SampleGame/SampleGame.cs
+++ SampleGame/SampleGame.cs
@@ -9,7 +9,6 @@ using osu.Framework.Graphics.Drawables;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
---
+++ osu.Framework.VisualTests/VisualTestGame.cs
@@ -0,0 +1,16 @@
+﻿//Copyright (c) 2007-2016 ppy Pty Ltd <contact@ppy.sh>.
+//Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
+
+using OpenTK;
+
+namespace osu.Framework.VisualTests
+{
+    class VisualTestGame : Game
+    {
+        public override void Load()
+        {
+            base.Load();
+            Host.Size = new Vector2(1366, 768);
+        }
+    }
+}
