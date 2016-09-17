48f4d28979c65f3a34118003183c5b4a8d180c3d
Dean Herbert <pe@ppy.sh>
2016-09-17 23:53:05 +0900

    Use Cached invalidation logic in FlowContainer.

--- osu.Framework/Graphics/Containers/FlowContainer.cs
+++ osu.Framework/Graphics/Containers/FlowContainer.cs
@@ -5,6 +5,7 @@ using System;
 using OpenTK;
 using osu.Framework.Graphics.Transformations;
 using System.Linq;
+using osu.Framework.Cached;
 
 namespace osu.Framework.Graphics.Containers
 {
@@ -13,7 +14,11 @@ namespace osu.Framework.Graphics.Containers
         internal event Action OnLayout;
 
         public EasingTypes LayoutEasing;
-        public int LayoutDuration = 0;
+        public int LayoutDuration
+        {
+            get { return layout.RefreshInterval; }
+            set { layout.RefreshInterval = value; }
+        }
 
         private FlowDirection direction = FlowDirection.Full;
         public FlowDirection Direction
@@ -24,19 +29,12 @@ namespace osu.Framework.Graphics.Containers
                 if (value == direction) return;
                 direction = value;
 
-                requiresLayout = true;
+                layout.Invalidate();
             }
         }
 
-        private double lastLayout;
-        private bool requiresLayout = true;
-
-        /// <summary>
-        /// When we are using a transformation for layouts, we want to ensure we don't trigger re-flow due to our own actions.
-        /// </summary>
-        private double nextLayout => lastLayout + LayoutDuration;
-
         Vector2 maximumSize;
+
         /// <summary>
         /// Optional maximum dimensions for this container.
         /// </summary>
@@ -68,7 +66,7 @@ namespace osu.Framework.Graphics.Containers
         public override bool Invalidate(Invalidation invalidation = Invalidation.All, Drawable source = null, bool shallPropagate = true)
         {
             if ((invalidation & (Invalidation.ScreenSpaceQuad | Invalidation.Visibility)) > 0)
-                requiresLayout = true;
+                layout.Invalidate();
 
             return base.Invalidate(invalidation, source, shallPropagate);
         }
@@ -76,71 +74,75 @@ namespace osu.Framework.Graphics.Containers
         public override Drawable Add(Drawable drawable)
         {
             //let's force an instant re-flow on adding a new drawable for now.
-            lastLayout = 0;
+            layout.Invalidate();
 
             return base.Add(drawable);
         }
+
+        private Cached<Vector2> layout = new Cached<Vector2>();
 
         protected override void UpdateLayout()
         {
             base.UpdateLayout();
 
-            if (!requiresLayout || (nextLayout > 0 && Time < nextLayout)) return;
+            if (layout.IsValid) return;
 
-            OnLayout?.Invoke();
-
-            lastLayout = Time;
-            requiresLayout = false;
-
-            if (Children.FirstOrDefault() == null) return;
-
-            Vector2 current = new Vector2(Math.Max(0, Padding.X), Math.Max(0, Padding.Y));
-
-            Vector2 max = maximumSize;
-            if (direction == FlowDirection.Full && maximumSize == Vector2.Zero)
+            layout.Refresh(delegate
             {
-                var actual = ActualSize;
+                OnLayout?.Invoke();
 
-                //If we are autosize and haven't specified a maximum size, we should allow infinite expansion.
-                //If we are inheriting then we need to use the parent size (our ActualSize).
-                max.X = (SizeMode & InheritMode.X) == 0 ? float.MaxValue : actual.X;
-                max.Y = (SizeMode & InheritMode.Y) == 0 ? float.MaxValue : actual.Y;
-            }
+                if (Children.FirstOrDefault() == null) return Vector2.Zero;
 
-            float rowMaxHeight = 0;
-            foreach (Drawable d in Children)
-            {
-                if (d.SizeMode != InheritMode.None) continue;
+                Vector2 current = new Vector2(Math.Max(0, Padding.X), Math.Max(0, Padding.Y));
 
-                Vector2 size = Vector2.Zero;
-
-                if (d.IsVisible)
+                Vector2 max = maximumSize;
+                if (direction == FlowDirection.Full && maximumSize == Vector2.Zero)
                 {
-                    size = d.ActualSize * d.Scale * ContentScale;
+                    var actual = ActualSize;
 
-                    if (Direction != FlowDirection.HorizontalOnly && current.X + size.X > max.X)
+                    //If we are autosize and haven't specified a maximum size, we should allow infinite expansion.
+                    //If we are inheriting then we need to use the parent size (our ActualSize).
+                    max.X = (SizeMode & InheritMode.X) == 0 ? float.MaxValue : actual.X;
+                    max.Y = (SizeMode & InheritMode.Y) == 0 ? float.MaxValue : actual.Y;
+                }
+
+                float rowMaxHeight = 0;
+                foreach (Drawable d in Children)
+                {
+                    if (d.SizeMode != InheritMode.None) continue;
+
+                    Vector2 size = Vector2.Zero;
+
+                    if (d.IsVisible)
                     {
-                        current.X = Math.Max(0, Padding.X);
-                        current.Y += rowMaxHeight;
+                        size = d.ActualSize * d.Scale * ContentScale;
 
-                        rowMaxHeight = 0;
+                        if (Direction != FlowDirection.HorizontalOnly && current.X + size.X > max.X)
+                        {
+                            current.X = Math.Max(0, Padding.X);
+                            current.Y += rowMaxHeight;
+
+                            rowMaxHeight = 0;
+                        }
+
+                        //todo: check this is correct
+                        if (size.X > 0) size.X = Math.Max(0, size.X + Padding.X);
+                        if (size.Y > 0) size.Y = Math.Max(0, size.Y + Padding.Y);
+
+                        if (size.Y > rowMaxHeight) rowMaxHeight = size.Y;
                     }
 
-                    //todo: check this is correct
-                    if (size.X > 0) size.X = Math.Max(0, size.X + Padding.X);
-                    if (size.Y > 0) size.Y = Math.Max(0, size.Y + Padding.Y);
+                    if (current != d.Position)
+                    {
+                        d.MoveTo(current, LayoutDuration, LayoutEasing);
+                        d.UpdateSubTree();
+                    }
 
-                    if (size.Y > rowMaxHeight) rowMaxHeight = size.Y;
+                    current.X += size.X;
                 }
 
-                if (current != d.Position)
-                {
-                    d.MoveTo(current, LayoutDuration, LayoutEasing);
-                    d.UpdateSubTree();
-                }
-
-                current.X += size.X;
-            }
+                return current;
+            });
         }
     }
 
