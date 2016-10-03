f6fcb9c56fc04408e642c7acddfa9dd3c116ee76
Drew DeVault <sir@cmpwn.com>
2016-10-03 12:03:29 -0400

    Use RGBA for JPGs

--- osu.Framework/Graphics/Textures/RawTextureLoaderStore.cs
+++ osu.Framework/Graphics/Textures/RawTextureLoaderStore.cs
@@ -34,15 +34,27 @@ namespace osu.Framework.Graphics.Textures
             RawTexture t = new RawTexture();
             using (var bmp = (Bitmap)Image.FromStream(stream))
             {
-                t.Pixels = new byte[bmp.Width * bmp.Height * 3];
+                t.Pixels = new byte[bmp.Width * bmp.Height * 4];
                 t.Width = bmp.Width;
                 t.Height = bmp.Height;
-                t.PixelFormat = OpenTK.Graphics.ES20.PixelFormat.Rgb;
+                t.PixelFormat = OpenTK.Graphics.ES20.PixelFormat.Rgba;
                 var pixels = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height),
                     ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
                 try
                 {
-                    Marshal.Copy(pixels.Scan0, t.Pixels, 0, t.Pixels.Length);
+                    unsafe
+                    {
+                        byte* p = (byte*)pixels.Scan0;
+                        for (int i = 0; i < bmp.Width * bmp.Height; i++)
+                        {
+                            int desti = i * 4;
+                            int srci = i * 3;
+                            t.Pixels[desti] = p[srci];
+                            t.Pixels[desti + 1] = p[srci + 1];
+                            t.Pixels[desti + 2] = p[srci + 2];
+                            t.Pixels[desti + 3] = 255;
+                        }
+                    }
                 }
                 finally
                 {
