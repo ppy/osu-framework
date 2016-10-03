2e553411935d02d51b4774cb18f650cc19a86c24
Drew DeVault <sir@cmpwn.com>
2016-10-03 10:37:22 -0400

    Fixes for RawTextureLoaderStore

--- osu.Framework/Graphics/Textures/RawTextureLoaderStore.cs
+++ osu.Framework/Graphics/Textures/RawTextureLoaderStore.cs
@@ -1,5 +1,9 @@
-﻿using osu.Framework.Graphics.Textures.Png;
+﻿using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+using osu.Framework.Graphics.Textures.Png;
 using osu.Framework.IO.Stores;
+using System.Runtime.InteropServices;
 
 namespace osu.Framework.Graphics.Textures
 {
@@ -10,20 +14,53 @@ namespace osu.Framework.Graphics.Textures
         public RawTextureLoaderStore(IResourceStore<byte[]> store)
         {
             Store = store;
+            (store as ResourceStore<byte[]>)?.AddExtension(@"png");
+            (store as ResourceStore<byte[]>)?.AddExtension(@"jpg");
+        }
+
+        private RawTexture LoadPng(Stream stream)
+        {
+            RawTexture t = new RawTexture();
+            var reader = new PngReader();
+            t.Pixels = reader.Read(stream);
+            t.PixelFormat = OpenTK.Graphics.ES20.PixelFormat.Rgba;
+            t.Width = reader.Width;
+            t.Height = reader.Height;
+            return t;
+        }
+
+        private RawTexture LoadOther(Stream stream)
+        {
+            RawTexture t = new RawTexture();
+            using (var bmp = (Bitmap)Image.FromStream(stream))
+            {
+                t.Pixels = new byte[bmp.Width * bmp.Height * 3];
+                t.Width = bmp.Width;
+                t.Height = bmp.Height;
+                t.PixelFormat = OpenTK.Graphics.ES20.PixelFormat.Rgb;
+                var pixels = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height),
+                    ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+                try
+                {
+                    Marshal.Copy(pixels.Scan0, t.Pixels, 0, t.Pixels.Length);
+                }
+                finally
+                {
+                    bmp.UnlockBits(pixels);
+                }
+            }
+            return t;
         }
 
         public override RawTexture Get(string name)
         {
-            RawTexture t = new RawTexture();
+            
             using (var stream = Store.GetStream(name))
             {
-                var reader = new PngReader();
-                t.Pixels = reader.Read(stream);
-                t.PixelFormat = OpenTK.Graphics.ES20.PixelFormat.Rgba;
-                t.Width = reader.Width;
-                t.Height = reader.Height;
+                if (PngReader.IsPngImage(stream))
+                    return LoadPng(stream);
+                return LoadOther(stream);
             }
-            return t;
         }
     }
 }
