5763e3cca50f48acf66f2ee65efd4f4f62a2b8e0
Dean Herbert <pe@ppy.sh>
2016-10-06 16:44:30 +0900

    Make Benchmark class a bit more flexible.

--- osu.Framework.VisualTests/Benchmark.cs
+++ osu.Framework.VisualTests/Benchmark.cs
@@ -1,13 +1,16 @@
 ﻿// Copyright (c) 2007-2016 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
 
+using System;
+using System.Diagnostics;
 using osu.Framework.GameModes.Testing;
-using osu.Framework.VisualTests.Tests;
 
 namespace osu.Framework.VisualTests
 {
     public class Benchmark : Game
     {
+        private double timePerTest = 1000;
+
         public override void Load()
         {
             base.Load();
@@ -18,13 +21,19 @@ namespace osu.Framework.VisualTests
             TestBrowser f = new TestBrowser();
             Add(f);
 
+            Console.WriteLine($@"{Time}: Running {f.TestCount} tests for {timePerTest}ms each...");
+
             for (int i = 1; i < f.TestCount; i++)
             {
                 int loadableCase = i;
-                Scheduler.AddDelayed(delegate { f.LoadTest(loadableCase); }, loadableCase * 1000);
+                Scheduler.AddDelayed(delegate
+                {
+                    f.LoadTest(loadableCase);
+                    Console.WriteLine($@"{Time}: Switching to test #{loadableCase}");
+                }, loadableCase * timePerTest);
             }
 
-            Scheduler.AddDelayed(Exit, f.TestCount * 1000);
+            Scheduler.AddDelayed(Exit, f.TestCount * timePerTest);
         }
     }
 }
