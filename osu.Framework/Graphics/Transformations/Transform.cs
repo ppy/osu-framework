38f603c6f4a84e6d9e11a1a622df723677ca4297
Dean Herbert <pe@ppy.sh>
2016-10-08 12:51:56 +0900

    Add Shift method to move transforms in time.

--- osu.Framework/Graphics/Transformations/Transform.cs
+++ osu.Framework/Graphics/Transformations/Transform.cs
@@ -1,6 +1,7 @@
 // Copyright (c) 2007-2016 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
 
+using System;
 using System.Diagnostics;
 using osu.Framework.Timing;
 
@@ -104,6 +105,12 @@ namespace osu.Framework.Graphics.Transformations
 
         public void Load()
         {
+        }
+
+        public void Shift(double offset)
+        {
+            StartTime += offset;
+            EndTime += offset;
         }
     }
 }
