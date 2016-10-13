89cfb62441e61d327669ed78cda646a02b35e684
Remie Richards <remierichards@gmail.com>
2016-10-13 02:05:47 +0100

    Adds CheckBoxes and a TestCase

---
+++ osu.Framework.VisualTests/Tests/TestCaseCheckBoxes.cs
@@ -0,0 +1,115 @@
+﻿// Copyright (c) 2007-2016 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
+
+using OpenTK;
+using OpenTK.Graphics;
+using osu.Framework.GameModes.Testing;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Drawables;
+using osu.Framework.Graphics.Primitives;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Graphics.UserInterface;
+
+namespace osu.Framework.VisualTests.Tests
+{
+    class TestCaseCheckBox : TestCase
+    {
+        public override string Name => @"Checkboxes";
+
+        public override string Description => @"CheckBoxes with clickable labels";
+
+        public CheckBox BasicCheckBox;
+        public CheckBox ResizingWidthCheckBox;
+        public CheckBox ResizingHeightCheckBox;
+        public CheckBox ActionsCheckBox;
+
+        public override void Reset()
+        {
+            base.Reset();
+
+            Children = new Drawable[]
+            {
+                new FlowContainer
+                {
+                    Anchor = Anchor.TopLeft,
+                    Origin = Anchor.TopLeft,
+                    Spacing = new Vector2(0,10),
+                    Padding = new MarginPadding(10),
+                    Direction = FlowDirection.VerticalOnly,
+                    Children = new Drawable[]
+                    {
+                        BasicCheckBox = new CheckBox
+                        {
+                            Labels = new Drawable[]
+                            {
+                                new SpriteText
+                                {
+                                    Padding = new MarginPadding
+                                    {
+                                        Left = 20
+                                    },
+                                    Text = @"Basic Test"
+                                }
+                            }
+                        },
+                        ResizingWidthCheckBox = new CheckBox
+                        {
+                            CheckedDrawable = new Box
+                            {
+                                Size = new Vector2(50, 20),
+                                Colour = Color4.Cyan
+                            },
+                            Labels = new Drawable[]
+                            {
+                                new SpriteText
+                                {
+                                    Padding = new MarginPadding
+                                    {
+                                        Left = 20
+                                    },
+                                    Text = @"Resizing Width Test"
+                                },
+                            }
+                        },
+                        ResizingHeightCheckBox = new CheckBox
+                        {
+                            CheckedDrawable = new Box
+                            {
+                                Size = new Vector2(20, 50),
+                                Colour = Color4.Cyan
+                            },
+                            Labels = new Drawable[]
+                            {
+                                new SpriteText
+                                {
+                                    Padding = new MarginPadding
+                                    {
+                                        Left = 20
+                                    },
+                                    Text = @"Resizing Height Test"
+                                }
+                            }
+                        },
+                        ActionsCheckBox = new CheckBox
+                        {
+                            CheckedAction = () => ActionsCheckBox.RotateTo(45, 100),
+                            UncheckedAction = () => ActionsCheckBox.RotateTo(0, 100),
+                            Labels = new Drawable[]
+                            {
+                                new SpriteText
+                                {
+                                    Padding = new MarginPadding
+                                    {
+                                        Left = 20
+                                    },
+                                    Text = @"Enabled/Disabled Actions Test"
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+        }
+    }
+}
---
+++ osu.Framework/Graphics/UserInterface/CheckBox.cs
@@ -0,0 +1,119 @@
+﻿// Copyright (c) 2007-2016 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
+
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Drawables;
+using osu.Framework.Input;
+using OpenTK.Graphics;
+using System;
+using OpenTK;
+using System.Collections.Generic;
+
+namespace osu.Framework.Graphics.UserInterface
+{
+    public class CheckBox : FlowContainer, IStateful<CheckBoxState>
+    {
+
+        public Drawable CheckedDrawable = new Box { Size = new Vector2(20, 20), Colour = Color4.Cyan };
+        public Drawable UncheckedDrawable = new Box { Size = new Vector2(20, 20) };
+        private AutoSizeContainer visualsCont;
+
+        private List<Drawable> labels = new List<Drawable>();
+        public IEnumerable<Drawable> Labels
+        {
+            get { return labels; }
+            set
+            {
+                if (IsLoaded)
+                {
+                    foreach (Drawable d in value)
+                        AddLabel(d);
+                }
+                else
+                {
+                    foreach (Drawable d in value)
+                        labels.Add(d);
+                }
+            }
+        }
+
+        private CheckBoxState state = CheckBoxState.Unchecked;
+        public CheckBoxState State
+        {
+            get { return state; }
+            set
+            {
+                if (state == value)
+                    return;
+
+                state = value;
+                switch (state)
+                {
+                    case CheckBoxState.Checked:
+                        CheckedAction?.Invoke();
+                        visualsCont.Remove(UncheckedDrawable);
+                        visualsCont.Add(CheckedDrawable);
+                        break;
+                    case CheckBoxState.Unchecked:
+                        UncheckedAction?.Invoke();
+                        visualsCont.Remove(CheckedDrawable);
+                        visualsCont.Add(UncheckedDrawable);
+                        break;
+                }
+            }
+        }
+
+        public Action CheckedAction;
+        public Action UncheckedAction;
+
+        public CheckBox()
+        {
+            Direction = FlowDirection.HorizontalOnly;
+            Children = new Drawable[]
+            {
+                visualsCont = new AutoSizeContainer()
+            };
+        }
+
+        public override void Load(BaseGame game)
+        {
+            base.Load(game);
+            visualsCont.Add(UncheckedDrawable);
+            foreach (Drawable d in labels)
+                Add(d);
+        }
+
+        protected override bool OnClick(InputState state)
+        {
+            if (State == CheckBoxState.Checked)
+            {
+                State = CheckBoxState.Unchecked;
+            }
+            else
+            {
+                State = CheckBoxState.Checked;
+            }
+            base.OnClick(state);
+            return true;
+        }
+
+        public void AddLabel(Drawable d)
+        {
+            Add(d);
+            labels.Add(d);
+        }
+
+        public void RemoveLabel(Drawable d)
+        {
+            Remove(d);
+            labels.Remove(d);
+        }
+    }
+
+    public enum CheckBoxState
+    {
+        Checked,
+        Unchecked
+    }
+
+}
