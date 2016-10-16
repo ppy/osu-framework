0816faf5e1ef7cec29a7aa8eff97759ed8e2bca8
Thomas Müller <thomas94@gmx.net>
2016-10-16 09:25:11 +0200

    Spaces instead of tabs.

--- osu.Framework/Resources/Shaders/sh_TextureRounded.fs
+++ osu.Framework/Resources/Shaders/sh_TextureRounded.fs
@@ -42,37 +42,37 @@ bool contains(vec2 pos, vec2 p0, vec2 p1, vec2 p2)
 bool contains(vec2 pos)
 {
     return
-		contains(pos, g_MaskingBottomRight, g_MaskingBottomLeft, g_MaskingTopRight) ||
+        contains(pos, g_MaskingBottomRight, g_MaskingBottomLeft, g_MaskingTopRight) ||
         contains(pos, g_MaskingTopLeft, g_MaskingTopRight, g_MaskingBottomLeft);
 }
 
 vec2 max3(vec2 a, vec2 b, vec2 c)
 {
-	return max(max(a, b), c);
+    return max(max(a, b), c);
 }
 
 float distanceFromRoundedRect()
 {
-	vec2 topLeftOffset = g_TexSize * (g_TexRect.xy - v_TexCoord);
-	vec2 bottomRightOffset = g_TexSize * (v_TexCoord - g_TexRect.zw);
+    vec2 topLeftOffset = g_TexSize * (g_TexRect.xy - v_TexCoord);
+    vec2 bottomRightOffset = g_TexSize * (v_TexCoord - g_TexRect.zw);
 
-	vec2 distanceFromShrunkRect = max3(vec2(0.0), bottomRightOffset + vec2(g_Radius), topLeftOffset + vec2(g_Radius));
-	return length(distanceFromShrunkRect);
+    vec2 distanceFromShrunkRect = max3(vec2(0.0), bottomRightOffset + vec2(g_Radius), topLeftOffset + vec2(g_Radius));
+    return length(distanceFromShrunkRect);
 }
 
 void main(void)
 {
-	if (!contains(v_Position))
-	{
-		gl_FragColor = vec4(0.0);
-		return;
-	}
+    if (!contains(v_Position))
+    {
+        gl_FragColor = vec4(0.0);
+        return;
+    }
 
-	float dist = g_Radius == 0.0 ? 0.0 : distanceFromRoundedRect();
-	gl_FragColor = v_Colour * texture2D(m_Sampler, v_TexCoord, -0.9);
+    float dist = g_Radius == 0.0 ? 0.0 : distanceFromRoundedRect();
+    gl_FragColor = v_Colour * texture2D(m_Sampler, v_TexCoord, -0.9);
 
-	// This correction is needed to avoid fading of the alpha value for radii below 1px.
-	float radiusCorrection = max(0.0, 1.0 - g_Radius);
-	if (dist > g_Radius - 1.0 + radiusCorrection)
-		gl_FragColor.a *= max(0.0, g_Radius - dist + radiusCorrection);
+    // This correction is needed to avoid fading of the alpha value for radii below 1px.
+    float radiusCorrection = max(0.0, 1.0 - g_Radius);
+    if (dist > g_Radius - 1.0 + radiusCorrection)
+        gl_FragColor.a *= max(0.0, g_Radius - dist + radiusCorrection);
 }
