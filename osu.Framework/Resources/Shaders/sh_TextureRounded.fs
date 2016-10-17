854f3a5ec98671daade9718560e59174891a563d
Tom94 <thomas94@gmx.net>
2016-10-17 17:30:15 +0200

    Fix tabs.

--- osu.Framework/Resources/Shaders/sh_TextureRounded.fs
+++ osu.Framework/Resources/Shaders/sh_TextureRounded.fs
@@ -15,42 +15,42 @@ uniform vec4 g_BorderColour;
 
 float distanceFromRoundedRect()
 {
-	// Compute offset distance from masking rect in masking space.
+    // Compute offset distance from masking rect in masking space.
     vec2 topLeftOffset = g_MaskingRect.xy - v_MaskingPosition;
     vec2 bottomRightOffset = v_MaskingPosition - g_MaskingRect.zw;
 
     vec2 distanceFromShrunkRect = max(bottomRightOffset + vec2(g_CornerRadius), topLeftOffset + vec2(g_CornerRadius));
-	float maxDist = max(distanceFromShrunkRect.x, distanceFromShrunkRect.y);
+    float maxDist = max(distanceFromShrunkRect.x, distanceFromShrunkRect.y);
 
-	if (maxDist <= 0.0)
-		return maxDist;
-	else
-		return length(max(vec2(0.0), distanceFromShrunkRect));
+    if (maxDist <= 0.0)
+        return maxDist;
+    else
+        return length(max(vec2(0.0), distanceFromShrunkRect));
 }
 
 void main(void)
 {
     float dist = distanceFromRoundedRect();
 
-	// This correction is needed to avoid fading of the alpha value for radii below 1px.
+    // This correction is needed to avoid fading of the alpha value for radii below 1px.
     float radiusCorrection = max(0.0, 1.0 - g_CornerRadius);
-	float fadeStart = g_CornerRadius + radiusCorrection;
-	float alphaFactor = min(fadeStart - dist, 1.0);
-	if (alphaFactor <= 0.0)
-	{
-		gl_FragColor = vec4(0.0);
-		return;
-	}
+    float fadeStart = g_CornerRadius + radiusCorrection;
+    float alphaFactor = min(fadeStart - dist, 1.0);
+    if (alphaFactor <= 0.0)
+    {
+        gl_FragColor = vec4(0.0);
+        return;
+    }
 
-	float borderStart = fadeStart - g_BorderThickness + 1.0;
-	float colourWeight = min(borderStart - dist, 1.0);
-	if (colourWeight <= 0.0)
-	{
-		gl_FragColor = vec4(g_BorderColour.r, g_BorderColour.g, g_BorderColour.b, g_BorderColour.a * alphaFactor);
-		return;
-	}
+    float borderStart = fadeStart - g_BorderThickness + 1.0;
+    float colourWeight = min(borderStart - dist, 1.0);
+    if (colourWeight <= 0.0)
+    {
+        gl_FragColor = vec4(g_BorderColour.r, g_BorderColour.g, g_BorderColour.b, g_BorderColour.a * alphaFactor);
+        return;
+    }
 
     gl_FragColor =
-		colourWeight * vec4(v_Colour.r, v_Colour.g, v_Colour.b, v_Colour.a * alphaFactor) * texture2D(m_Sampler, v_TexCoord, -0.9) +
-		(1.0 - colourWeight) * g_BorderColour;
+        colourWeight * vec4(v_Colour.r, v_Colour.g, v_Colour.b, v_Colour.a * alphaFactor) * texture2D(m_Sampler, v_TexCoord, -0.9) +
+        (1.0 - colourWeight) * g_BorderColour;
 }
