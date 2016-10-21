ab2ee0cb9b744ba831d342c34677f8e647a2003f
Dean Herbert <pe@ppy.sh>
2016-10-21 12:24:46 +0900

    Return a more specific DesktopGameHost.

--- osu.Framework.Desktop/Host.cs
+++ osu.Framework.Desktop/Host.cs
@@ -1,16 +1,16 @@
 ﻿// Copyright (c) 2007-2016 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
 
+using osu.Framework.Desktop.Platform;
 using osu.Framework.Desktop.Platform.Linux;
 using osu.Framework.Desktop.Platform.Windows;
-using osu.Framework.Platform;
 using OpenTK.Graphics;
 
 namespace osu.Framework.Desktop
 {
     public static class Host
     {
-        public static BasicGameHost GetSuitableHost(string gameName)
+        public static DesktopGameHost GetSuitableHost(string gameName)
         {
             GraphicsContextFlags flags = GraphicsContextFlags.Default;
             if (RuntimeInfo.IsUnix)
