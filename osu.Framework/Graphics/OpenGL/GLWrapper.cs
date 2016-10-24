e0f3825a7076699ac64e8073f200c3cf7dc9ae23
Dean Herbert <pe@ppy.sh>
2016-10-24 18:18:03 +0900

    Add missing gl initialisation statements.

--- osu.Framework/Graphics/OpenGL/GLWrapper.cs
+++ osu.Framework/Graphics/OpenGL/GLWrapper.cs
@@ -46,7 +46,10 @@ namespace osu.Framework.Graphics.OpenGL
 
             MaxTextureSize = Math.Min(2048, GL.GetInteger(GetPName.MaxTextureSize));
 
+            GL.Disable(EnableCap.DepthTest);
+            GL.Disable(EnableCap.StencilTest);
             GL.Enable(EnableCap.Blend);
+            GL.Enable(EnableCap.ScissorTest);
 
             IsInitialized = true;
         }
