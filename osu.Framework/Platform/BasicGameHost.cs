588e6e53ed6ff16aed1278c4edc3f56bde15e439
Drew DeVault <sir@cmpwn.com>
2016-10-25 10:42:52 -0400

    Tweak things to support osu!'s TestStorage

--- osu.Framework/Platform/BasicGameHost.cs
+++ osu.Framework/Platform/BasicGameHost.cs
@@ -80,7 +80,7 @@ namespace osu.Framework.Platform
             throw new NotImplementedException("This platform does not implement IPC.");
         }
 
-        public virtual BasicStorage Storage { get; protected set; }
+        public virtual BasicStorage Storage { get; set; }
 
         public override bool IsVisible => true;
 
