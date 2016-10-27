9658ff21a54c4c18af532576c9b2ceb76f5871c5
Thomas Müller <thomas94@gmx.net>
2016-10-27 22:12:29 +0200

    Add test case for buffered container (by hijacking masking test for now).

---
+++ osu.Framework.VisualTests/Tests/TestCaseBufferedContainer.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) 2007-2016 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
+
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Primitives;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Graphics.Transformations;
+using osu.Framework.Input;
+using OpenTK;
+using OpenTK.Graphics;
+using osu.Framework.GameModes.Testing;
+using System;
+
+namespace osu.Framework.VisualTests.Tests
+{
+    class TestCaseBufferedContainer : TestCaseMasking
+    {
+        public override string Name => @"BufferedContainer";
+        public override string Description => @"Various scenarios which potentially challenge buffered containers calculations.";
+
+        public override void Reset()
+        {
+            base.Reset();
+
+            Remove(TestContainer);
+
+            Add(new BufferedContainer()
+            {
+                RelativeSizeAxes = Axes.Both,
+                Children = new[] { TestContainer }
+            });
+        }
+    }
+
+}
