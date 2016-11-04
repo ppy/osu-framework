75819351c8334bcd7cee8d3b96c9a03f3755d28a
TocoToucan <TocoToucanMS@gmail.com>
2016-11-04 23:27:40 +0300

    Add Sliderbar + TestCase

---
+++ osu.Framework.VisualTests/Tests/TestCaseSliderbar.cs
@@ -0,0 +1,45 @@
+﻿using osu.Framework.Configuration;
+using osu.Framework.GameModes.Testing;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Graphics.UserInterface;
+using OpenTK;
+using OpenTK.Graphics;
+
+namespace osu.Framework.VisualTests.Tests
+{
+    public class TestCaseSliderbar : TestCase
+    {
+        public override string Name => @"Sliderbar";
+        public override string Description => @"Sliderbar tests.";
+        private Sliderbar positiveAndNegativeSliderbar;
+        private BindableDouble positiveAndNegativeBindableDouble;
+        private SpriteText positiveAndNegativeSliderbarText;
+
+        public override void Reset()
+        {
+            base.Reset();
+
+            positiveAndNegativeBindableDouble=new BindableDouble(8);
+            positiveAndNegativeBindableDouble.ValueChanged += PositiveAndNegativeBindableDouble_ValueChanged;
+
+            positiveAndNegativeSliderbarText = new SpriteText
+            {
+                Text = $"Selected value: {positiveAndNegativeBindableDouble.Value}",
+                Position = new Vector2(25,0)
+            };
+            positiveAndNegativeSliderbar = new Sliderbar(-10, 10, positiveAndNegativeBindableDouble, Color4.White, Color4.Pink)
+            {
+                Size = new Vector2(200, 10),
+                Position = new Vector2(25, 25)
+            };
+
+            Add(positiveAndNegativeSliderbar);
+            Add(positiveAndNegativeSliderbarText);
+        }
+
+        private void PositiveAndNegativeBindableDouble_ValueChanged(object sender, System.EventArgs e)
+        {
+            positiveAndNegativeSliderbarText.Text = $"Selected value: {positiveAndNegativeBindableDouble.Value:N}";
+        }
+    }
+}
---
+++ osu.Framework/Graphics/UserInterface/Sliderbar.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using osu.Framework.Configuration;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Graphics.Transformations;
+using osu.Framework.Input;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Input;
+
+namespace osu.Framework.Graphics.UserInterface
+{
+    public class Sliderbar : Container
+    {
+        private readonly double keyboardStep;
+        private readonly double minValue, maxValue;
+        private readonly BindableDouble selectedValue;
+        private readonly Color4 color, selectedRangeColor;
+        private readonly Box selectedRangeBox;
+        private readonly Box sliderbarBox;
+        private double valuesRange => maxValue - minValue;
+        private double selectedRange;
+
+        public Sliderbar(double minValue, double maxValue, BindableDouble selectedValue, Color4 color, Color4 selectedRangeColor)
+        {
+            this.minValue = minValue;
+            this.maxValue = maxValue;
+            this.selectedValue = selectedValue;
+            this.color = color;
+            this.selectedRangeColor = selectedRangeColor;
+            keyboardStep = 0.01;
+            selectedRange = this.selectedValue - this.minValue;
+            selectedValue.ValueChanged += SelectedValue_ValueChanged;
+
+            Children = new Drawable[]
+            {
+                sliderbarBox = new Box
+                {
+                    Colour = color,
+                    RelativeSizeAxes = Axes.Both,
+                },
+                selectedRangeBox = new Box
+                {
+                    Colour = selectedRangeColor,
+                    RelativeSizeAxes = Axes.Both,
+                    Origin = Anchor.CentreLeft,
+                    Anchor = Anchor.CentreLeft
+                }
+            };
+        }
+
+        #region Disposal
+
+        ~Sliderbar()
+        {
+            selectedValue.ValueChanged -= SelectedValue_ValueChanged;
+        }
+
+        #endregion
+
+        protected override void Load(BaseGame game)
+        {
+            base.Load(game);
+            updateVisualization();
+        }
+
+        protected override bool OnClick(InputState state)
+        {
+            handleMouseInput(state);
+            return true;
+        }
+
+        protected override bool OnDrag(InputState state)
+        {
+            handleMouseInput(state);
+            return true;
+        }
+
+        protected override bool OnDragStart(InputState state)
+        {
+            return true;
+        }
+
+        protected override bool OnDragEnd(InputState state)
+        {
+            return true;
+        }
+
+        protected override bool OnKeyDown(InputState state, KeyDownEventArgs args)
+        {
+            if (args.Key == Key.Right)
+                selectedValue.Value += keyboardStep;
+            else if (args.Key == Key.Left)
+                selectedValue.Value -= keyboardStep;
+            return true;
+        }
+
+        private void SelectedValue_ValueChanged(object sender, EventArgs e)
+        {
+            if (selectedValue.Value > maxValue)
+            {
+                selectedValue.Value = maxValue;
+                return;
+            }
+
+            if (selectedValue.Value < minValue)
+            {
+                selectedValue.Value = minValue;
+                return;
+            }
+            updateVisualization();
+        }
+
+        private void handleMouseInput(InputState state)
+        {
+            var xPosition = state.Mouse.Position.X;
+            if (xPosition < 0)
+                xPosition = 0;
+            if (xPosition > sliderbarBox.DrawWidth)
+                xPosition = sliderbarBox.DrawWidth;
+            double percentage = xPosition / sliderbarBox.DrawWidth;
+            selectedRange = valuesRange * percentage;
+            selectedValue.Value = minValue + selectedRange;
+        }
+
+        private void updateVisualization()
+        {
+            var percentage = selectedRange / valuesRange;
+            selectedRangeBox.ScaleTo(new Vector2((float)percentage, 1), 300, EasingTypes.OutQuint);
+        }
+    }
+}
