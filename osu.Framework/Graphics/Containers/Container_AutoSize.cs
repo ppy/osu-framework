21054da2af8ceee035add8f2ed1a5a79e40da2dc
Dean Herbert <pe@ppy.sh>
2016-11-08 17:21:50 +0900

    Attempt to fix compiling on mono.

--- osu.Framework/Graphics/Containers/Container_AutoSize.cs
+++ osu.Framework/Graphics/Containers/Container_AutoSize.cs
@@ -104,7 +104,7 @@ namespace osu.Framework.Graphics.Containers
                 Vector2 maxBoundSize = Vector2.Zero;
 
                 // Find the maximum width/height of children
-                foreach (Drawable c in AliveChildren)
+                foreach (T c in AliveChildren)
                 {
                     if (!c.IsVisible)
                         continue;
