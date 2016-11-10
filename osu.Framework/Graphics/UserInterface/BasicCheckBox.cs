c5e9d450124e9d6184c1c546bb88084437689a6a
TocoToucan <TocoToucanMS@gmail.com>
2016-11-10 20:01:30 +0300

    Simplify BasicCheckBox

--- osu.Framework/Graphics/UserInterface/BasicCheckBox.cs
+++ osu.Framework/Graphics/UserInterface/BasicCheckBox.cs
@@ -1,79 +1,62 @@
 ﻿// Copyright (c) 2007-2016 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
 
-using OpenTK;
-using OpenTK.Graphics;
 using osu.Framework.Graphics.Primitives;
 using osu.Framework.Graphics.Sprites;
+using OpenTK;
+using OpenTK.Graphics;
 
 namespace osu.Framework.Graphics.UserInterface
 {
-
     public class BasicCheckBox : CheckBox
     {
-        protected virtual Drawable CreateCheckedDrawable() => new Box { Size = new Vector2(20, 20), Colour = Color4.Cyan };
-        protected virtual Drawable CreateUncheckedDrawable() => new Box { Size = new Vector2(20, 20) };
-        private Drawable checkedDrawable;
-        private Drawable uncheckedDrawable;
-        private SpriteText labelSpriteText;
-        private string labelText = string.Empty;
+        public Color4 CheckedColor { get; set; } = Color4.Cyan;
+        public Color4 UncheckedColor { get; set; } = Color4.White;
+
         public string LabelText
         {
-            get { return labelText; }
+            get { return labelSpriteText?.Text; }
             set
             {
-                labelText = value;
                 if (labelSpriteText != null)
-                    labelSpriteText.Text = labelText;
+                    labelSpriteText.Text = value;
             }
         }
-        private MarginPadding labelPadding;
+
         public MarginPadding LabelPadding
         {
-            get { return labelPadding; }
+            get { return labelSpriteText?.Padding ?? new MarginPadding(); }
             set
             {
-                labelPadding = value;
                 if (labelSpriteText != null)
-                    labelSpriteText.Padding = labelPadding;
+                    labelSpriteText.Padding = value;
             }
         }
+
+        private Box box;
+        private SpriteText labelSpriteText;
 
         public BasicCheckBox()
         {
-            LabelPadding = new MarginPadding
-            {
-                Left = 20
-            };
             Children = new Drawable[]
             {
                 labelSpriteText = new SpriteText
                 {
-                    Padding = LabelPadding,
-                    Text = labelText,
+                    Padding = new MarginPadding
+                    {
+                        Left = 20
+                    },
                     Depth = float.MaxValue
+                },
+                box = new Box
+                {
+                    Size = new Vector2(20, 20)
                 }
             };
         }
 
-        protected override void Load(BaseGame game)
-        {
-            base.Load(game);
-            checkedDrawable = CreateCheckedDrawable();
-            uncheckedDrawable = CreateUncheckedDrawable();
-            Add(uncheckedDrawable);
-        }
+        protected override void OnUnchecked() => box.Colour = UncheckedColor;
 
-        protected override void OnUnchecked()
-        {
-            Remove(checkedDrawable);
-            Add(uncheckedDrawable);
-        }
-
-        protected override void OnChecked()
-        {
-            Remove(uncheckedDrawable);
-            Add(checkedDrawable);
-        }
+        protected override void OnChecked() => box.Colour = CheckedColor;
     }
 }
