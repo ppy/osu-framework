4859cf90f4eb0b194a1bc3089a9384cac7a63026
Dean Herbert <pe@ppy.sh>
2016-11-12 19:04:47 +0900

    Fixed borked assert.

--- osu.Framework/Allocation/DependencyContainer.cs
+++ osu.Framework/Allocation/DependencyContainer.cs
@@ -28,11 +28,7 @@ namespace osu.Framework.Allocation
 
         private void Register(Type type, bool lazy)
         {
-            if (activators.ContainsKey(type) || cache.ContainsKey(type))
-            {
-                Debug.Assert(false, $@"Type {type.FullName} should not be registered twice");
-                return;
-            }
+            Debug.Assert(!activators.ContainsKey(type) && !cache.ContainsKey(type), $@"Type {type.FullName} should not be registered twice");
 
             var initialize = GetLoaderMethod(type);
             var constructor = type.GetConstructors().SingleOrDefault(c => c.GetParameters().Length == 0);
