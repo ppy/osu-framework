d0ee1cf6e08fe42daad067831717f9375a05fab6
Dean Herbert <pe@ppy.sh>
2016-11-23 12:30:12 +0900

    Add fill modes to Sprite.

--- osu.Framework/Graphics/Sprites/Sprite.cs
+++ osu.Framework/Graphics/Sprites/Sprite.cs
@@ -117,7 +117,7 @@ namespace osu.Framework.Graphics.Sprites
                     $@"May not smooth more than {MAX_EDGE_SMOOTHNESS} or will leak neighboring textures in atlas.");
 
                 Vector3 scale = DrawInfo.MatrixInverse.ExtractScale();
-                
+
                 inflationAmount = new Vector2(scale.X * EdgeSmoothness.X, scale.Y * EdgeSmoothness.Y);
                 return ToScreenSpace(DrawRectangle.Inflate(inflationAmount));
             }
@@ -135,5 +135,47 @@ namespace osu.Framework.Graphics.Sprites
         {
             return base.ToString() + $" tex: {texture?.AssetName}";
         }
+
+        private FillMode fillMode;
+
+        public FillMode FillMode
+        {
+            get { return fillMode; }
+            set
+            {
+                fillMode = value;
+            }
+        }
+
+        protected override Vector2 DrawScale
+        {
+            get
+            {
+                Vector2 modifier = Vector2.One;
+
+                switch (FillMode)
+                {
+                    case FillMode.Fill:
+                        Scale = new Vector2(Math.Max(Parent.DrawSize.X / Texture.Width, Parent.DrawSize.Y / Texture.Height));
+                        break;
+                    case FillMode.Fit:
+                        Scale = new Vector2(Math.Min(Parent.DrawSize.X / Texture.Width, Parent.DrawSize.Y / Texture.Height));
+                        break;
+                    case FillMode.Stretch:
+                        Scale = new Vector2(Parent.DrawSize.X / Texture.Width, Parent.DrawSize.Y / Texture.Height);
+                        break;
+                }
+
+                return base.DrawScale * modifier;
+            }
+        }
+    }
+
+    public enum FillMode
+    {
+        None,
+        Fill,
+        Fit,
+        Stretch
     }
 }
