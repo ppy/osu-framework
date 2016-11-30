2af082bbab48024ef73a4f9cc7266c16b5e3b920
Dean Herbert <pe@ppy.sh>
2016-11-30 18:25:39 +0900

    Add a basic password textbox.

---
+++ osu.Framework/Graphics/UserInterface/PasswordTextBox.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) 2007-2016 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osu.Framework.Graphics.UserInterface
+{
+    public class PasswordTextBox : TextBox
+    {
+        protected virtual char MaskCharacter => '*';
+
+        protected override Drawable AddCharacterToFlow(char c) => base.AddCharacterToFlow(MaskCharacter);
+    }
+}
