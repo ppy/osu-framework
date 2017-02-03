617bd893c720bb3d5c81f94532dd711b510612ee
Dean Herbert <pe@ppy.sh>
2017-02-03 14:13:23 +0900

    Add comment.

--- osu.Framework/Graphics/Containers/Container_AutoSize.cs
+++ osu.Framework/Graphics/Containers/Container_AutoSize.cs
@@ -63,6 +63,7 @@ namespace osu.Framework.Graphics.Containers
 
             set
             {
+                //transform check here is to allow AutoSizeDuration to work below.
                 Debug.Assert(Transforms.Find(t => t is TransformSize) != null || (AutoSizeAxes & Axes.Both) == 0, @"The Size of an AutoSizeContainer should only be manually set if it is relative to its parent.");
                 base.Size = value;
             }
