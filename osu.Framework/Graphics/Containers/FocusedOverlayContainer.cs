05a1b8e48e96ed230e8687cc75baf097e47eaf2b
Dean Herbert <pe@ppy.sh>
2017-02-08 15:09:02 +0900

    Add the concept of a FocusedOverlayContainer.

---
+++ osu.Framework/Graphics/Containers/FocusedOverlayContainer.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) 2007-2016 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
+
+using System.Linq;
+using osu.Framework.Input;
+using OpenTK.Input;
+using System;
+
+namespace osu.Framework.Graphics.Containers
+{
+    /// <summary>
+    /// An overlay container that eagerly holds keyboard focus.
+    /// </summary>
+    public abstract class FocusedOverlayContainer : OverlayContainer
+    {
+        public override bool RequestingFocus => IsPresent && State == Visibility.Visible;
+
+        public override bool IsPresent => base.IsPresent || State == Visibility.Visible;
+
+        protected override bool OnFocus(InputState state) => true;
+
+        protected override void OnFocusLost(InputState state)
+        {
+            if (state.Keyboard.Keys.Contains(Key.Escape))
+                Hide();
+            base.OnFocusLost(state);
+        }
+
+        protected override void PopIn()
+        {
+            TriggerFocusContention();
+        }
+
+        protected override bool OnHover(InputState state) => true;
+
+        protected override bool OnMouseDown(InputState state, MouseDownEventArgs args) => true;
+
+        protected override bool OnClick(InputState state) => true;
+
+        protected override bool OnKeyDown(InputState state, KeyDownEventArgs args)
+        {
+            switch (args.Key)
+            {
+                case Key.Escape:
+                    if (State == Visibility.Hidden) return false;
+                    Hide();
+                    return true;
+            }
+            return base.OnKeyDown(state, args);
+        }
+    }
+}
