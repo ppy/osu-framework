bb0ff40ae43e3c5f50e4eee08d865e0697fd096d
Dean Herbert <pe@ppy.sh>
2017-02-09 16:01:28 +0900

    Add missing license header.

--- osu.Framework/Audio/IBassAudio.cs
+++ osu.Framework/Audio/IBassAudio.cs
@@ -1,4 +1,7 @@
-﻿namespace osu.Framework.Audio
+﻿// Copyright (c) 2007-2016 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
+
+namespace osu.Framework.Audio
 {
     internal interface IBassAudio
     {
