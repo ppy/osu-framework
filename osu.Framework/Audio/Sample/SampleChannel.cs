112d8187aa891bb202359ed545f7ddce592a17de
Thomas Müller <thomas94@gmx.net>
2017-02-18 11:10:49 +0100

    Ensure samples are never null passed.

--- osu.Framework/Audio/Sample/SampleChannel.cs
+++ osu.Framework/Audio/Sample/SampleChannel.cs
@@ -1,6 +1,8 @@
 // Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
 
+using System.Diagnostics;
+
 namespace osu.Framework.Audio.Sample
 {
     public abstract class SampleChannel : AdjustableAudioComponent, IHasCompletedState
@@ -11,6 +13,7 @@ namespace osu.Framework.Audio.Sample
 
         public SampleChannel(Sample sample)
         {
+            Debug.Assert(sample != null, "Can not use a null sample.");
             Sample = sample;
         }
 
