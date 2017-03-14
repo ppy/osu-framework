4982585e5621c22d231f38360d4a38aa46334928
DrabWeb <sethunity@gmail.com>
2017-03-14 11:03:04 -0300

    T -> in T

--- osu.Framework/Graphics/IHasDrawableRepresentation.cs
+++ osu.Framework/Graphics/IHasDrawableRepresentation.cs
@@ -3,7 +3,7 @@
 
 namespace osu.Framework.Graphics
 {
-    public interface IHasDrawableRepresentation<T> where T : Drawable
+    public interface IHasDrawableRepresentation<in T> where T : Drawable
     {
         T CreateDrawable();
     }
