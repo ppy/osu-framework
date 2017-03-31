574cc19f24b6b77732210c9ea3e6390ce74ea313
Dean Herbert <pe@ppy.sh>
2017-03-31 17:28:46 +0900

    Add licence header.

--- osu.Framework/Input/MouseUpEventArgs.cs
+++ osu.Framework/Input/MouseUpEventArgs.cs
@@ -1,4 +1,7 @@
-﻿namespace osu.Framework.Input
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
+
+namespace osu.Framework.Input
 {
     public class MouseUpEventArgs : MouseEventArgs
     {
