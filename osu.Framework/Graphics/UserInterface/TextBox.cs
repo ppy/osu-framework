e51249b2050fbf45c3672547dcace33e1876b75f
Dean Herbert <pe@ppy.sh>
2017-04-19 18:43:41 +0900

    Better ReadOnly handling for TextBox.

--- osu.Framework/Graphics/UserInterface/TextBox.cs
+++ osu.Framework/Graphics/UserInterface/TextBox.cs
@@ -443,7 +443,7 @@ namespace osu.Framework.Graphics.UserInterface
         protected bool HandlePendingText(InputState state)
         {
             string str = textInput?.GetPendingText();
-            if (string.IsNullOrEmpty(str))
+            if (string.IsNullOrEmpty(str) || ReadOnly)
                 return false;
 
             if (state.Keyboard.ShiftPressed)
@@ -465,6 +465,8 @@ namespace osu.Framework.Graphics.UserInterface
                 return false;
 
             if (HandlePendingText(state)) return true;
+
+            if (ReadOnly) return true;
 
             switch (args.Key)
             {
