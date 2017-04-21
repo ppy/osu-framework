aae2002fd8932388b7194643207e8484387ee98e
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2017-04-21 14:25:18 +0800

    Check locale hierarchically.

--- osu.Framework/Localization/LocaleEngine.cs
+++ osu.Framework/Localization/LocaleEngine.cs
@@ -5,18 +5,26 @@ using osu.Framework.Allocation;
 using osu.Framework.Configuration;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 
 namespace osu.Framework.Localization
 {
     public class LocalisationEngine
     {
         private Bindable<bool> preferUnicode;
+        private Bindable<string> locale;
+
+        public virtual string[] SupportedLocales => new[] { "en" };
 
         [BackgroundDependencyLoader]
         private void load(FrameworkConfigManager config)
         {
             preferUnicode = config.GetBindable<bool>(FrameworkConfig.ShowUnicode);
             preferUnicode.ValueChanged += updateUnicodeStrings;
+            locale = config.GetBindable<string>(FrameworkConfig.Locale);
+            locale.ValueChanged += checkLocale;
+            locale.TriggerChange();
         }
 
         private List<WeakReference<UnicodeBindableString>> unicodeBindings = new List<WeakReference<UnicodeBindableString>>();
@@ -44,6 +52,31 @@ namespace osu.Framework.Localization
                 else
                     unicodeBindings.Remove(w);
             }
+        }
+
+        private void checkLocale(string newValue)
+        {
+            CultureInfo culture;
+            if (newValue == null) //means use current locale
+                culture = CultureInfo.CurrentCulture;
+            else
+                culture = new CultureInfo(newValue);
+
+            var locales = SupportedLocales.ToList();
+            string validLocale=null;
+            for (var c = culture; !c.IsNeutralCulture; c = c.Parent)
+                if (locales.Contains(c.Name))
+                {
+                    validLocale = c.Name;
+                    break;
+                }
+
+            if (validLocale == null)
+                validLocale = locales[0];
+            if (validLocale != newValue)
+                locale.Value = validLocale;
+            else
+                CultureInfo.DefaultThreadCurrentCulture = new CultureInfo(validLocale);
         }
 
         public class UnicodeBindableString : Bindable<string>
