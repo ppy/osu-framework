7a2a999fc97b78aeafb2cb98daed27d1625d9770
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2017-04-21 15:02:56 +0800

    Add LocalisedSpriteText.

---
+++ osu.Framework/Graphics/Sprites/LocalisedSpriteText.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
+
+using osu.Framework.Configuration;
+
+namespace osu.Framework.Graphics.Sprites
+{
+    public class LocalisedSpriteText : SpriteText
+    {
+        private Bindable<string> bindable;
+        public Bindable<string> Bindable
+        {
+            get { return bindable; }
+            set
+            {
+                if (bindable != null)
+                    bindable.ValueChanged -= bindableChanged;
+                if (value != null)
+                    value.ValueChanged += bindableChanged;
+
+                bindable = value;
+                value.TriggerChange();
+            }
+        }
+
+        private void bindableChanged(string newValue)
+        {
+            Text = newValue;
+        }
+    }
+}
