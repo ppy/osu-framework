86bbd6b6247f50aeff58fafd69bd8f8a459039a4
Thomas Tan <thomastanck@gmail.com>
2017-04-24 01:46:27 +0800

    Add license header.

--- osu.Framework/Graphics/UserInterface/CircularProgress.cs
+++ osu.Framework/Graphics/UserInterface/CircularProgress.cs
@@ -1,5 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
+
+using System;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Sprites;
 using osu.Framework.Configuration;
