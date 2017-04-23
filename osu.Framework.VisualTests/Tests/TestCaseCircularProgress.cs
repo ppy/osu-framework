a7bd214930cc8ff0b4bdfc86399a1ac2d3f73ad8
Thomas Tan <thomastanck@gmail.com>
2017-04-24 07:03:08 +0800

    Add CircularProgress VisualTest (WIP)

---
+++ osu.Framework.VisualTests/Tests/TestCaseCircularProgress.cs
@@ -0,0 +1,81 @@
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
+
+using OpenTK;
+using OpenTK.Graphics;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.UserInterface;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Testing;
+
+namespace osu.Framework.VisualTests.Tests
+{
+    internal class TestCaseCircularProgress : TestCase
+    {
+        public override string Description => @"Circular progress bar";
+
+        private CircularProgress clock1;
+        private CircularProgress clock2;
+
+        public override void Reset()
+        {
+            base.Reset();
+
+            Children = new Drawable[]
+            {
+                new Container
+                {
+                    Depth = 3,
+
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+
+                    Width = 320,
+                    Height = 320,
+                    CornerRadius = 8,
+
+                    Masking = true,
+
+                    Children = new Drawable[]
+                    {
+                        new Box
+                        {
+                            Anchor = Anchor.Centre,
+                            Origin = Anchor.Centre,
+                            RelativeSizeAxes = Axes.Both,
+                            Colour = new Color4(100, 100, 100, 255),
+                        },
+                        clock1 = new CircularProgress
+                        {
+                            Anchor = Anchor.Centre,
+                            Origin = Anchor.Centre,
+
+                            Width = 300,
+                            Height = 300,
+                            Colour = new Color4(128, 255, 128, 255),
+                        },
+                    },
+                },
+                clock2 = new CircularProgress
+                {
+                    Anchor = Anchor.TopLeft,
+                    Origin = Anchor.TopLeft,
+                    Position = new Vector2(120, 20),
+
+                    Width = -80,
+                    Height = 100,
+                },
+            };
+
+            //AddStep("Right to left", () => graph.Direction = BarDirection.RightToLeft);
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+            clock1.Current.Value = (Time.Current % 500) / 500;
+            clock2.Current.Value = (Time.Current % 730) / 730;
+        }
+    }
+}
