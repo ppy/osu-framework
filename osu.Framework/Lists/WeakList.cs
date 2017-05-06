2fb2e8d7efd22a83dce5a031c7b5d2f72c9993e0
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2017-05-06 22:19:30 +0800

    Encapsule a list of WeakReference.

---
+++ osu.Framework/Lists/WeakList.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
+
+using System;
+using System.Collections.Generic;
+
+namespace osu.Framework.Lists
+{
+    public class WeakList<T> : List<WeakReference<T>>
+        where T : class
+    {
+        public void Add(T obj) => Add(new WeakReference<T>(obj));
+
+        public void ForEachAlive(Action<T> action)
+        {
+            int index = 0;
+            while (index < Count)
+            {
+                T obj;
+                if (this[index].TryGetTarget(out obj))
+                {
+                    action(obj);
+                    index++;
+                }
+                else
+                    RemoveAt(index);
+            }
+        }
+    }
+}
