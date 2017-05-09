ec5161e24c44a36eb98a8c6cd0c69e432725b8dd
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2017-05-09 15:29:12 +0800

    Add xmldoc for WeakList.

--- osu.Framework/Lists/WeakList.cs
+++ osu.Framework/Lists/WeakList.cs
@@ -6,11 +6,18 @@ using System.Collections.Generic;
 
 namespace osu.Framework.Lists
 {
+    /// <summary>
+    /// A list maintaining weak reference of objects.
+    /// </summary>
+    /// <typeparam name="T">Type of items tracked by weak reference.</typeparam>
     public class WeakList<T> : List<WeakReference<T>>
         where T : class
     {
         public void Add(T obj) => Add(new WeakReference<T>(obj));
 
+        /// <summary>
+        /// Iterate on alive items, and remove non-alive references.
+        /// </summary>
         public void ForEachAlive(Action<T> action)
         {
             int index = 0;
