773d60eb6b811f395e32a22dc66bb4d2e63a6dbc
smoogipooo <smoogipooo@gmail.com>
2017-05-22 20:14:01 +0900

    Move TrackAmplitudes to separate file.

---
+++ osu.Framework/Audio/Track/TrackAmplitudes.cs
@@ -0,0 +1,11 @@
+// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
+
+namespace osu.Framework.Audio.Track
+{
+    public struct TrackAmplitudes
+    {
+        public float LeftChannel;
+        public float RightChannel;
+    }
+}
