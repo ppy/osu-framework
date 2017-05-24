81d2bd0de74980ec21b72c8b84ca0ae021075d4b
MrTheMake <marc.stephan96@hotmail.de>
2017-05-24 17:03:37 +0200

    Added condition

--- osu.Framework/Audio/Track/TrackBass.cs
+++ osu.Framework/Audio/Track/TrackBass.cs
@@ -185,7 +185,9 @@ namespace osu.Framework.Audio.Track
 
                 if (clamped != CurrentTime)
                 {
-                    Interlocked.Exchange(ref currentTime, clamped);
+                    if (clamped == Length)
+                        Interlocked.Exchange(ref currentTime, clamped);
+
                     long pos = Bass.ChannelSeconds2Bytes(activeStream, clamped / 1000d);
                     Bass.ChannelSetPosition(activeStream, pos);
                 }
