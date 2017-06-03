40c10e923f0395ccac71d253f0fd65a912791533
EVAST9919 <megaman9919@gmail.com>
2017-06-03 21:09:03 +0300

    Context menu implementation

---
+++ osu.Framework/Graphics/UserInterface/ContextMenuContainer.cs
@@ -0,0 +1,115 @@
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
+
+using OpenTK;
+using OpenTK.Graphics;
+using osu.Framework.Caching;
+using osu.Framework.Extensions.Color4Extensions;
+using osu.Framework.Graphics.Containers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace osu.Framework.Graphics.UserInterface
+{
+    public class ContextMenuContainer<TItem> : Container
+        where TItem : ContextMenuItem
+    {
+        private readonly ContextMenu contextMenu;
+
+        public MenuState State => contextMenu?.State ?? MenuState.Closed;
+
+        public IEnumerable<TItem> Items
+        {
+            set
+            {
+                if (contextMenu != null)
+                {
+                    contextMenu.ItemsContainer.Children = value;
+
+                    foreach (var item in Items)
+                        item.Action += Close;
+                }
+            }
+            get
+            {
+                return contextMenu.ItemsContainer.Children;
+            }
+        }
+
+        public ContextMenuContainer()
+        {
+            AlwaysReceiveInput = true;
+            AutoSizeAxes = Axes.Y;
+            Add(contextMenu = new ContextMenu());
+        }
+
+        protected override void UpdateAfterChildren()
+        {
+            base.UpdateAfterChildren();
+            if (!menuWidth.IsValid)
+            {
+                menuWidth.Refresh(() =>
+                {
+                    float width = 0;
+
+                    foreach (var item in Items)
+                        width = Math.Max(width, item.DrawWidth);
+                    return width;
+                });
+
+                Width = menuWidth.Value;
+            }
+        }
+
+        private Cached<float> menuWidth = new Cached<float>();
+
+        public override void InvalidateFromChild(Invalidation invalidation)
+        {
+            menuWidth.Invalidate();
+            base.InvalidateFromChild(invalidation);
+        }
+
+        public void Open()
+        {
+            if (contextMenu == null)
+                return;
+            contextMenu.State = MenuState.Opened;
+        }
+
+        public void Close()
+        {
+            if (contextMenu == null)
+                return;
+            contextMenu.State = MenuState.Closed;
+        }
+
+        public class ContextMenu : Menu<TItem>
+        {
+            private const int fade_duration = 250;
+
+            public ContextMenu()
+            {
+                CornerRadius = 5;
+                ItemsContainer.Padding = new MarginPadding { Vertical = ContextMenuItem.MARGIN_VERTICAL };
+                Masking = true;
+                EdgeEffect = new EdgeEffect
+                {
+                    Type = EdgeEffectType.Shadow,
+                    Colour = Color4.Black.Opacity(0.25f),
+                    Radius = 4,
+                };
+            }
+
+            protected override void AnimateOpen() => FadeIn(fade_duration, EasingTypes.OutQuint);
+
+            protected override void AnimateClose() => FadeOut(fade_duration, EasingTypes.OutQuint);
+
+            protected override void UpdateContentHeight()
+            {
+                var actualHeight = (RelativeSizeAxes & Axes.Y) > 0 ? 1 : ContentHeight;
+                ResizeTo(new Vector2(1, State == MenuState.Opened ? actualHeight : 0), fade_duration, EasingTypes.OutQuint);
+            }
+        }
+    }
+}
---
+++ osu.Framework/Graphics/UserInterface/ContextMenuItem.cs
@@ -0,0 +1,135 @@
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
+
+using OpenTK.Graphics;
+using osu.Framework.Allocation;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Input;
+
+namespace osu.Framework.Graphics.UserInterface
+{
+    public class ContextMenuItem : MenuItem
+    {
+        private const int transition_length = 200;
+        private const int margin_horizontal = 15;
+        public const int MARGIN_VERTICAL = 5;
+
+        private readonly SpriteText text;
+        private readonly SpriteText textBold;
+
+        private readonly Container contentContainer;
+
+        private ContextMenuType type;
+
+        private bool enabled = true;
+        public new bool Enabled
+        {
+            set
+            {
+                enabled = value;
+
+                if(IsLoaded)
+                    updateTextColour();
+            }
+            get { return enabled; }
+        }
+
+
+        public new float DrawWidth
+        {
+            get
+            {
+                return contentContainer.DrawWidth;
+            }
+        }
+
+        public ContextMenuItem(string title, ContextMenuType type = ContextMenuType.Standard)
+        {
+            this.type = type;
+
+            Children = new Drawable[]
+            {
+                contentContainer = new Container
+                {
+                    AutoSizeAxes = Axes.Both,
+                    Anchor = Anchor.CentreLeft,
+                    Origin = Anchor.CentreLeft,
+                    Children = new Drawable[]
+                    {
+                        text = new SpriteText
+                        {
+                            Anchor = Anchor.CentreLeft,
+                            Origin = Anchor.CentreLeft,
+                            TextSize = 17,
+                            Text = title,
+                            Margin = new MarginPadding{ Horizontal = margin_horizontal, Vertical = MARGIN_VERTICAL },
+                        },
+                        textBold = new SpriteText
+                        {
+                            AlwaysPresent = true,
+                            Alpha = 0,
+                            Anchor = Anchor.CentreLeft,
+                            Origin = Anchor.CentreLeft,
+                            TextSize = 17,
+                            Text = title,
+                            Font = @"Exo2.0-Bold",
+                            Margin = new MarginPadding{ Horizontal = margin_horizontal, Vertical = MARGIN_VERTICAL },
+                        }
+                    }
+                }
+            };
+        }
+
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            BackgroundColour = Color4.Transparent;
+            BackgroundColourHover = Color4.Blue;
+
+            updateTextColour();
+        }
+
+        private void updateTextColour()
+        {
+            if (Enabled)
+            {
+                switch (type)
+                {
+                    case ContextMenuType.Standard:
+                        text.Colour = textBold.Colour = Color4.White;
+                        break;
+                    case ContextMenuType.Destructive:
+                        text.Colour = textBold.Colour = Color4.Red;
+                        break;
+                    case ContextMenuType.Highlighted:
+                        text.Colour = textBold.Colour = Color4.Yellow;
+                        break;
+                }
+            }
+            else
+            {
+                text.Colour = textBold.Colour = Color4.Gray;
+            }
+        }
+
+        protected override bool OnHover(InputState state)
+        {
+            textBold.FadeIn(transition_length, EasingTypes.OutQuint);
+            text.FadeOut(transition_length, EasingTypes.OutQuint);
+            return base.OnHover(state);
+        }
+
+        protected override void OnHoverLost(InputState state)
+        {
+            textBold.FadeOut(transition_length, EasingTypes.OutQuint);
+            text.FadeIn(transition_length, EasingTypes.OutQuint);
+            base.OnHoverLost(state);
+        }
+
+        protected override bool OnClick(InputState state)
+        {
+            return enabled ? base.OnClick(state) : false;
+        }
+    }
+}
