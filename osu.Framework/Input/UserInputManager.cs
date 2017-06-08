27d48da8ab2a37d6e5ae91090e6cc20375a93335
Dean Herbert <pe@ppy.sh>
2017-06-08 17:53:33 +0900

    Make InputManager abstract
    - Makes InputHandler list a property, and removes the ability to add/remove by default.
    - Moves initialisation on InputHandlers to GameHost.
    - Adds a method to reset to a sane default by completely disposing and recreating handlers.
    - Fixes UserInputManager not getting InputHandler changes (new added or existing removed) from its underlying GameHost.

--- osu.Framework/Input/UserInputManager.cs
+++ osu.Framework/Input/UserInputManager.cs
@@ -1,8 +1,8 @@
 ﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
 
+using System.Collections.Generic;
 using osu.Framework.Input.Handlers;
-using osu.Framework.Allocation;
 
 namespace osu.Framework.Input
 {
@@ -13,14 +13,6 @@ namespace osu.Framework.Input
             AlwaysReceiveInput = true;
         }
 
-        [BackgroundDependencyLoader]
-        private void load()
-        {
-            if (Host != null)
-            {
-                foreach (InputHandler h in Host.AvailableInputHandlers)
-                    AddHandler(h);
-            }
-        }
+        protected override IEnumerable<InputHandler> InputHandlers => Host.AvailableInputHandlers;
     }
 }
