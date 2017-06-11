d29f615aed69986914512d7da435ce9b6242d35d
default0 <default3@web.de>
2017-06-11 12:17:42 +0200

    Add EdgeEffect

---
+++ osu.Framework/Graphics/Effects/EdgeEffect.cs
@@ -0,0 +1,34 @@
+﻿using osu.Framework.Graphics.Containers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osu.Framework.Graphics.Effects
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class EdgeEffect : IEffect<Container>
+    {
+        public EdgeEffectParameters Parameters { get; set; }
+
+        public float CornerRadius { get; set; }
+
+        public Container ApplyTo(Drawable drawable)
+        {
+            return new Container
+            {
+                Masking = true,
+                EdgeEffect = Parameters,
+                CornerRadius = CornerRadius,
+                Anchor = drawable.Anchor,
+                Origin = drawable.Origin,
+                RelativeSizeAxes = drawable.RelativeSizeAxes,
+                AutoSizeAxes = Axes.Both & ~drawable.RelativeSizeAxes,
+                Children = new[] { drawable }
+            };
+        }
+    }
+}
