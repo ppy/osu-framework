336606ae912d9ded32d8e1b10c248dead101cdac
default0 <default3@web.de>
2017-06-11 12:43:09 +0200

    Add License Header

--- osu.Framework/Graphics/Effects/EdgeEffect.cs
+++ osu.Framework/Graphics/Effects/EdgeEffect.cs
@@ -1,4 +1,7 @@
-﻿using osu.Framework.Graphics.Containers;
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
+
+using osu.Framework.Graphics.Containers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
