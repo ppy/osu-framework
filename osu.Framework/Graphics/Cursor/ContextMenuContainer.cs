15d06463b02dfd42c69003ecde484ab4b15acb2d
Dean Herbert <pe@ppy.sh>
2017-06-12 15:32:41 +0900

    Improve clarity of comment

--- osu.Framework/Graphics/Cursor/ContextMenuContainer.cs
+++ osu.Framework/Graphics/Cursor/ContextMenuContainer.cs
@@ -13,7 +13,7 @@ namespace osu.Framework.Graphics.Cursor
 {
     /// <summary>
     /// A container which manages a <see cref="ContextMenu{TItem}"/>.
-    /// If a right-click happens on a <see cref="Drawable"/> that implements <see cref="IHasContextMenu"/> which lies below this container,
+    /// If a right-click happens on a <see cref="Drawable"/> that implements <see cref="IHasContextMenu"/> and exists as a child of the same <see cref="InputManager"/> as this container,
     /// a <see cref="ContextMenu{TItem}"/> will be displayed with bottom-right origin at the right-clicked position.
     /// </summary>
     public class ContextMenuContainer : Container
