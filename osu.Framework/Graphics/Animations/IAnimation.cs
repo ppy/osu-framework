48d3ce66591ce86bd87432f3bccbda792d1b0166
default0 <default3@web.de>
2017-06-17 21:23:24 +0200

    Better formatting for IAnimation

--- osu.Framework/Graphics/Animations/IAnimation.cs
+++ osu.Framework/Graphics/Animations/IAnimation.cs
@@ -13,10 +13,12 @@ namespace osu.Framework.Graphics.Animations
         /// The number of frames this animation has.
         /// </summary>
         int FrameCount { get; }
+
         /// <summary>
         /// True if the animation is playing, false otherwise.
         /// </summary>
         bool IsPlaying { get; set; }
+
         /// <summary>
         /// True if the animation should start over from the first frame after finishing. False if it should stop playing and keep displaying the last frame when finishing.
         /// </summary>
@@ -27,11 +29,13 @@ namespace osu.Framework.Graphics.Animations
         /// </summary>
         /// <param name="frameIndex">The zero-based index of the frame to display.</param>
         void GotoFrame(int frameIndex);
+
         /// <summary>
         /// Displays the frame with the given zero-based frame index and stops the animation at that frame.
         /// </summary>
         /// <param name="frameIndex">The zero-based index of the frame to display.</param>
         void GotoAndStop(int frameIndex);
+
         /// <summary>
         /// Displays the frame with the given zero-based frame index amd plays the animation from that frame.
         /// </summary>
