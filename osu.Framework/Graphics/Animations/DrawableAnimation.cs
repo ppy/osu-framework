910ca31ec35b07df3983ed6c55a0648525000bda
default0 <default3@web.de>
2017-06-17 19:59:01 +0200

    Add License headers

--- osu.Framework/Graphics/Animations/DrawableAnimation.cs
+++ osu.Framework/Graphics/Animations/DrawableAnimation.cs
@@ -1,4 +1,7 @@
-﻿namespace osu.Framework.Graphics.Animations
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
+
+namespace osu.Framework.Graphics.Animations
 {
     /// <summary>
     /// An animation that switches the displayed drawable when a new frame is displayed.
