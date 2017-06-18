aade1805f44bd22c18aa642816f7575daa7b861a
default0 <default3@web.de>
2017-06-18 23:22:19 +0200

    Remove unnecessary using

--- osu.Framework/Graphics/Animations/FrameData.cs
+++ osu.Framework/Graphics/Animations/FrameData.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
 
-using osu.Framework.Graphics.Containers;
-
 namespace osu.Framework.Graphics.Animations
 {
     /// <summary>
