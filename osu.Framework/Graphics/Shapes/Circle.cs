94f637bbd40636069f71c17289ea95a0cc9710ef
paparony03 <paparony03@gmail.com>
2017-06-19 16:17:30 +1000

    Add xmldoc

--- osu.Framework/Graphics/Shapes/Circle.cs
+++ osu.Framework/Graphics/Shapes/Circle.cs
@@ -5,6 +5,9 @@ using osu.Framework.Graphics.Containers;
 
 namespace osu.Framework.Graphics.Shapes
 {
+    /// <summary>
+    /// A simple <see cref="CircularContainer"/> with a fill using a <see cref="Box"/>. Can be coloured using the <see cref="Drawable.Colour"/> property.
+    /// </summary>
     public class Circle : CircularContainer
     {
         public Circle()
