b6ef95143f32b445191bcbb942894bef80e25945
ColdVolcano <mineflamecraft@hotmail.com>
2017-06-18 19:29:50 -0500

    Improve xml-doc

--- osu.Framework/Audio/Track/TrackAmplitudes.cs
+++ osu.Framework/Audio/Track/TrackAmplitudes.cs
@@ -15,8 +15,7 @@ namespace osu.Framework.Audio.Track
         public float Average => (LeftChannel + RightChannel) / 2;
 
         /// <summary>
-        /// 256 length array containing bins after combining channels.
-        /// Each bin represents about 78 Hz of the audible spectrum (0Hz - 20,000Hz).
+        /// 256 length array of bins containing the average frequency of both channels at every ~78Hz step of the audible spectrum (0Hz - 20,000Hz).
         /// </summary>
         public float[] FrequencyAmplitudes;
     }
