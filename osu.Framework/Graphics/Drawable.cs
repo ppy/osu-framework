f8d4ef2b264bb4cc4a11605be2defc70210d3677
Thomas Müller <thomas94@gmx.net>
2017-07-02 13:04:50 +0300

    Fix other CI concerns

--- osu.Framework/Graphics/Drawable.cs
+++ osu.Framework/Graphics/Drawable.cs
@@ -1301,6 +1301,9 @@ namespace osu.Framework.Graphics
                 di.Colour.ApplyChild(colour);
             else
             {
+                Debug.Assert(Parent != null,
+                    $"The {nameof(di)} of null parents should always have the single colour white, and therefore this branch should never be hit.");
+
                 // Cannot use ToParentSpace here, because ToParentSpace depends on DrawInfo to be completed
                 Quad interp = Quad.FromRectangle(DrawRectangle) * (di.Matrix * Parent.DrawInfo.MatrixInverse);
                 Vector2 parentSize = Parent.DrawSize;
