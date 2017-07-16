4c0bfd8b416f06a73cce0fe5611bbb3917090dfa
Thomas Müller <thomas94@gmx.net>
2017-07-16 13:33:25 +0300

    Incorporate delay in TestCaseTransformSequence

--- osu.Framework.VisualTests/Tests/TestCaseTransformSequence.cs
+++ osu.Framework.VisualTests/Tests/TestCaseTransformSequence.cs
@@ -117,8 +117,7 @@ namespace osu.Framework.VisualTests.Tests
                 b => b.ScaleTo(2)
             )
             .Then(
-                b => b.Loop(500, 2, d => d.RotateTo(0).RotateTo(360, 1000)),
-                b => b.ScaleTo(0.5f, 500)
+                b => b.Loop(500, 2, d => d.RotateTo(0).RotateTo(360, 1000)).Delay(500).ScaleTo(0.5f, 500)
             )
             .Then().FadeEdgeEffectTo(Color4.Red, 1000).ScaleTo(2, 500);
 
