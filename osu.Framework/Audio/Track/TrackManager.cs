7d51a5915ea02ed050e2febb39426ead9cbb476e
Dean Herbert <pe@ppy.sh>
2017-07-20 16:09:38 +0900

    Remove SetExclusive logic
    This was very flaky in the first place. Will be reimplemented in the osu! project.

--- osu.Framework/Audio/Track/TrackManager.cs
+++ osu.Framework/Audio/Track/TrackManager.cs
@@ -11,9 +11,6 @@ namespace osu.Framework.Audio.Track
     {
         private readonly IResourceStore<byte[]> store;
 
-        private Track exclusiveTrack;
-        private readonly object exclusiveMutex = new object();
-
         public TrackManager(IResourceStore<byte[]> store)
         {
             this.store = store;
@@ -24,51 +21,6 @@ namespace osu.Framework.Audio.Track
             TrackBass track = new TrackBass(store.GetStream(name));
             AddItem(track);
             return track;
-        }
-
-        /// <summary>
-        /// Specify an AudioTrack which should get exclusive playback over everything else.
-        /// Will pause all other tracks and throw away any existing exclusive track.
-        /// </summary>
-        public void SetExclusive(Track track)
-        {
-            if (track == null)
-                throw new ArgumentNullException(nameof(track));
-
-            Track last;
-            lock (exclusiveMutex)
-            {
-                if (exclusiveTrack == track) return;
-                last = exclusiveTrack;
-                exclusiveTrack = track;
-            }
-
-            PendingActions.Enqueue(() =>
-            {
-                foreach (var item in Items)
-                    if (!item.HasCompleted)
-                        item.Stop();
-
-                last?.Dispose();
-                AddItem(track);
-            });
-        }
-
-        public override void Update()
-        {
-            if (exclusiveTrack?.HasCompleted != false)
-                lock (exclusiveMutex)
-                    // We repeat the if-check inside the lock to make sure exclusiveTrack
-                    // has not been overwritten prior to us taking the lock.
-                    if (exclusiveTrack?.HasCompleted != false)
-                    {
-                        if (exclusiveTrack != null)
-                            UnregisterItem(exclusiveTrack);
-
-                        exclusiveTrack = Items.FirstOrDefault();
-                    }
-
-            base.Update();
         }
     }
 }
