e843173c51d716f09b1915d0d57c8f0c78ffb414
default0 <default3@web.de>
2017-08-20 00:27:32 +0200

    Add License header

--- osu.Framework/Graphics/Containers/CustomizableTextContainer.cs
+++ osu.Framework/Graphics/Containers/CustomizableTextContainer.cs
@@ -1,4 +1,7 @@
-﻿using osu.Framework.Graphics.Sprites;
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
+
+using osu.Framework.Graphics.Sprites;
 using System;
 using System.Collections.Generic;
 using System.Linq;
