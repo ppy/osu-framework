3b0209f58182235101114cbfddc4caa38d54a59f
smoogipooo <smoogipooo@gmail.com>
2017-08-31 22:26:27 +0900

    Re-make TestCaseMenuBar.

--- osu.Framework.Desktop.Tests/Visual/TestCaseMenuBar.cs
+++ osu.Framework.Desktop.Tests/Visual/TestCaseMenuBar.cs
@@ -1,51 +1,91 @@
 ﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
 
+using System;
+using System.Collections.Generic;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.UserInterface;
 using osu.Framework.Testing;
 
 namespace osu.Framework.Desktop.Tests.Visual
 {
     internal class TestCaseMenuBar : TestCase
     {
+        private const int max_depth = 5;
+        private const int max_count = 5;
+
+        private readonly Random rng;
+
         public TestCaseMenuBar()
         {
-            //MenuItem second;
-            //Add(new MenuBar
-            //{
-            //    Anchor = Anchor.Centre,
-            //    Origin = Anchor.Centre,
-            //    Items = new[]
-            //    {
-            //        new MenuItem("First")
-            //        {
-            //            Items = new[]
-            //            {
-            //                new MenuItem("First #1"),
-            //                new MenuItem("First #2"),
-            //                new MenuItem("First #3"),
-            //                new MenuItem("First #4"),
-            //                new MenuItem("First #5"),
-            //            }
-            //        },
-            //        second = new MenuItem("Second")
-            //        {
-            //            Items = new[]
-            //            {
-            //                new MenuItem("Second #1"),
-            //                new MenuItem("Second #2"),
-            //                new MenuItem("Second #3"),
-            //                new MenuItem("Second #4"),
-            //                new MenuItem("Second #5"),
-            //            }
-            //        }
-            //    }
-            //});
+            rng = new Random(1337);
 
-            //AddStep("Change text", () =>
-            //{
-            //    second.Text.Value = "Third";
-            //    second.Items.ForEach(i => i.Text.Value = i.Text.Value.Replace("Second", "Third"));
-            //});
+            Add(new StyledMenuBar
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                State = MenuState.Opened,
+                AlwaysOpen = true,
+                Items = new[]
+                {
+                    generateRandomMenu("First"),
+                    generateRandomMenu("Second"),
+                    generateRandomMenu("Third"),
+                }
+            });
+        }
+
+        private MenuItem generateRandomMenu(string name = "Menu Item", int currDepth = 1)
+        {
+            var item = new MenuItem(name);
+
+            if (currDepth == max_depth)
+                return item;
+
+            int subCount = rng.Next(0, max_count);
+            var subItems = new List<MenuItem>();
+            for (int i = 0; i < subCount; i++)
+                subItems.Add(generateRandomMenu(item.Text + $" #{i + 1}", currDepth + 1));
+
+            item.Items = subItems;
+            return item;
+        }
+
+        private class StyledMenuBar : Menu
+        {
+            public StyledMenuBar()
+                : base(Direction.Horizontal)
+            {
+            }
+
+            protected override DrawableMenuItem CreateDrawableMenuItem(MenuItem item) => new StyledMenuBarItem(item);
+
+            private class StyledMenuBarItem : DrawableMenuItem
+            {
+                public StyledMenuBarItem(MenuItem item)
+                    : base(item)
+                {
+                }
+
+                private class StyledMenuBarItemMenu : Menu
+                {
+                    public StyledMenuBarItemMenu()
+                        : base(Direction.Vertical)
+                    {
+                        Anchor = Anchor.BottomLeft;
+                    }
+
+                    protected override DrawableMenuItem CreateDrawableMenuItem(MenuItem item) => new StyledMenuItem(item);
+
+                    private class StyledMenuItem : DrawableMenuItem
+                    {
+                        public StyledMenuItem(MenuItem item)
+                            : base(item)
+                        {
+                        }
+                    }
+                }
+            }
         }
     }
 }
