e9c093eea6b4ee3bdf6455853fd0465b3c990feb
Miterosan <miterosan@gmail.com>
2017-09-13 20:12:33 +0200

    Add Moq
    Add one small RNG.Next Unittest.

---
+++ osu.Framework.Tests/MathUtils/TestRNG.cs
@@ -0,0 +1,25 @@
+﻿using Moq;
+using NUnit.Framework;
+using osu.Framework.MathUtils;
+using System;
+
+namespace osu.Framework.Tests.MathUtils
+{
+    [TestFixture]
+    class TestRNG
+    {
+        [Test]
+        public void TestNext()
+        {
+            Mock<IRandomProvider> mockRandomProvider = new Mock<IRandomProvider>();
+            int expectedRandomNumber = 134123;
+            mockRandomProvider.Setup((mock) => mock.Next()).Returns(expectedRandomNumber);
+            RNG.Random = mockRandomProvider.Object;
+
+            int actualRandomNumber = RNG.Next();
+
+            Assert.AreEqual(expectedRandomNumber, actualRandomNumber);
+            mockRandomProvider.Verify((mock) => mock.Next(), Times.Once);
+        }
+    }
+}
