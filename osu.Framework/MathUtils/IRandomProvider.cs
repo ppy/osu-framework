2f9ac3ff193e380d99b59fd19955999a6b43683b
Miterosan <miterosan@gmail.com>
2017-09-13 20:10:00 +0200

    Make RNG mockable

---
+++ osu.Framework/MathUtils/IRandomProvider.cs
@@ -0,0 +1,11 @@
+﻿namespace osu.Framework.MathUtils
+{
+    public interface IRandomProvider
+    {
+        int Next();
+        int Next(int minValue, int maxValue);
+        int Next(int maxValue);
+        void NextBytes(byte[] buffer);
+        double NextDouble();
+    }
+}
---
+++ osu.Framework/MathUtils/RandomProvider.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace osu.Framework.MathUtils
+{
+    internal class RandomProvider : IRandomProvider
+    {
+
+        public Random _random;
+
+        public RandomProvider(Random random = null)
+        {
+            _random = random ?? new Random();
+        }
+
+        public int Next()
+        {
+            return _random.Next();
+        }
+
+        public int Next(int minValue, int maxValue)
+        {
+             return _random.Next(minValue, maxValue);
+        }
+
+        public int Next(int maxValue)
+        {
+            return _random.Next(maxValue);
+        }
+
+        public void NextBytes(byte[] buffer)
+        {
+            _random.NextBytes(buffer);
+        }
+
+        public double NextDouble()
+        {
+            return _random.NextDouble();
+        }
+    }
+}
