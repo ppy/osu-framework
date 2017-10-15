f0b4c3fd9e917d45f33abd17669bc1885d7251f1
Shane Woolcock <samahnub@gmail.com>
2017-10-15 15:02:15 +1030

    Create namespace for macOS, based on the current Linux implementation

---
+++ osu.Framework/Platform/MacOS/MacOSGameHost.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
+
+namespace osu.Framework.Platform.MacOS
+{
+    public class MacOSGameHost : DesktopGameHost
+    {
+        internal MacOSGameHost(string gameName, bool bindIPC = false)
+            : base(gameName, bindIPC)
+        {
+            Window = new DesktopGameWindow();
+            Window.WindowStateChanged += (sender, e) =>
+            {
+                if (Window.WindowState != OpenTK.WindowState.Minimized)
+                    OnActivated();
+                else
+                    OnDeactivated();
+            };
+            Dependencies.Cache(Storage = new MacOSStorage(gameName));
+        }
+
+        public override Clipboard GetClipboard()
+        {
+            return new MacOSClipboard();
+        }
+    }
+}
---
+++ osu.Framework/Platform/MacOS/MacOSStorage.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
+
+using System;
+using System.IO;
+
+namespace osu.Framework.Platform.MacOS
+{
+    public class MacOSStorage : DesktopStorage
+    {
+        public MacOSStorage(string baseName)
+            : base(baseName)
+        {
+        }
+
+        protected override string LocateBasePath()
+        {
+            string home = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            string xdg = Environment.GetEnvironmentVariable("XDG_DATA_HOME");
+            string[] paths =
+            {
+                xdg ?? Path.Combine(home, ".local", "share"),
+                Path.Combine(home)
+            };
+
+            foreach (string path in paths)
+            {
+                if (Directory.Exists(path))
+                    return path;
+            }
+
+            return paths[0];
+        }
+    }
+}
