d15cc9a192a492f96aa91f479aec730f571f79d3
VperuS <vperus@gmail.com>
2017-10-19 06:53:28 +0300

    Delete usings

--- osu.Framework/Platform/Windows/WindowsGameWindow.cs
+++ osu.Framework/Platform/Windows/WindowsGameWindow.cs
@@ -1,11 +1,6 @@
 ﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
 
-using System;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Windows.Forms;
 using OpenTK.Input;
 
 namespace osu.Framework.Platform.Windows
