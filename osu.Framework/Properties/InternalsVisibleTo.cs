2da6512bf77038d3a3f09b7631420eee64b6528a
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2017-10-23 17:49:32 +0800

    Migrate osu.Framework. Seems every package resolves successfully.

---
+++ osu.Framework/Properties/InternalsVisibleTo.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
+
+using System.Runtime.CompilerServices;
+
+// We publish our internal attributes to other sub-projects of the framework.
+// Note, that we omit visual tests as they are meant to test the framework
+// behavior "in the wild".
+
+[assembly: InternalsVisibleTo("osu.Framework.Tests")]
