1eeecc9cafd07dd6a340424c445f1ac91c4fb879
Dean Herbert <pe@ppy.sh>
2017-11-09 23:31:47 +0900

    Disallow Pushing to a non-current screen

--- osu.Framework/Screens/Screen.cs
+++ osu.Framework/Screens/Screen.cs
@@ -134,6 +134,9 @@ namespace osu.Framework.Screens
         /// <param name="screen">The new Screen.</param>
         public virtual bool Push(Screen screen)
         {
+            if (!IsCurrentScreen)
+                throw new InvalidOperationException("Cannot push a child screen to a non-current screen");
+
             if (ChildScreen != null)
                 throw new InvalidOperationException("Can not push more than one child screen.");
 
