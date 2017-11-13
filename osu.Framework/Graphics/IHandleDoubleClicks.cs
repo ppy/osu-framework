c05f0090178a8baa9213bfe9a7bff2bb43665fee
TocoToucan <TocoToucanMS@gmail.com>
2017-11-13 21:58:39 +0300

    Add missing License headers

--- osu.Framework/Graphics/IHandleDoubleClicks.cs
+++ osu.Framework/Graphics/IHandleDoubleClicks.cs
@@ -1,4 +1,7 @@
-﻿using osu.Framework.Input;
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
+
+using osu.Framework.Input;
 
 namespace osu.Framework.Graphics
 {
