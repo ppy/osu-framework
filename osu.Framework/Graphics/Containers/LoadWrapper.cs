8641a396badb8da7e7066cc15fb869375ee1f08d
FreezyLemon <freezestyleman@gmx.de>
2017-11-21 17:08:01 +0100

    Added license header to the new class (file).

--- osu.Framework/Graphics/Containers/LoadWrapper.cs
+++ osu.Framework/Graphics/Containers/LoadWrapper.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
+
+using System;
 using System.Threading.Tasks;
 using osu.Framework.Allocation;
 using osu.Framework.Caching;
