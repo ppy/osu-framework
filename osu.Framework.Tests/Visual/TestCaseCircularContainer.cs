539c146944e00079e0def72ae32e034a8012f225
FreezyLemon <freezestyleman@gmx.de>
2017-11-23 07:16:15 +0100

    Added new test case for the "rectangular slider follow circle" bug

---
+++ osu.Framework.Tests/Visual/TestCaseCircularContainer.cs
@@ -0,0 +1,71 @@
+﻿using OpenTK;
+using OpenTK.Graphics;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
+using osu.Framework.Testing;
+using osu.Framework.Timing;
+
+namespace osu.Framework.Tests.Visual
+{
+    class TestCaseCircularContainer : TestCase
+    {
+        private ThrottledFrameClock throttledClock;
+
+        public override string Description => "Checking for bugged corner radius (LOW FPS)";
+
+        public TestCaseCircularContainer()
+        {
+            CircularContainer circularContainer;
+
+            throttledClock = new ThrottledFrameClock() { MaximumUpdateHz = 15 };
+
+            Add(circularContainer = new TestCircularContainer()
+            {
+                Masking = true,
+                AutoSizeAxes = Axes.Both,
+                Blending = BlendingMode.Additive,
+                Origin = Anchor.Centre,
+                Anchor = Anchor.Centre,
+                BorderThickness = 5,
+                BorderColour = Color4.White,
+                Children = new[]
+                {
+                    new Box
+                    {
+                        Origin = Anchor.Centre,
+                        Anchor = Anchor.Centre,
+                        Width = 128f,
+                        Height = 128f,
+                        Colour = Color4.Green,
+                    }
+                },
+            });
+        }
+
+        private class TestCircularContainer : CircularContainer
+        {
+            private int i = 0;
+            private bool isExpanded = false;
+            public bool IsExpanded
+            {
+                get { return isExpanded; }
+                set
+                {
+                    isExpanded = value;
+
+                    this.ScaleTo(isExpanded ? 2f : 1, 0);
+                    Child.FadeTo(isExpanded ? 1f : 0, 0);
+                }
+            }
+
+            protected override void Update()
+            {
+                base.Update();
+
+                if (i++ % 10 == 0)
+                    IsExpanded = !IsExpanded;
+            }
+        }
+    }
+}
