b2a8ab2d6df0a84c9c122a1bb42c1fa49b867621
Dean Herbert <pe@ppy.sh>
2017-12-01 14:05:17 +0900

    Fix regression in consecutive double-click behaviour

--- osu.Framework/Input/InputManager.cs
+++ osu.Framework/Input/InputManager.cs
@@ -465,9 +465,11 @@ namespace osu.Framework.Input
                             if (handleMouseDoubleClick(state))
                                 //when we handle a double-click we want to block a normal click from firing.
                                 isValidClick = false;
+                                
+                            lastClickTime = 0;
                         }
-
-                        lastClickTime = Time.Current;
+                        else
+                            lastClickTime = Time.Current;
                     }
                 }
 
