173c4c19aa8f4ebff2ebee286c8fa7a150be4f4e
Dean Herbert <pe@ppy.sh>
2017-12-13 17:52:23 +0900

    Ensure assert failures in TestBrowser don't cause hard crash (in interactive mode)

--- osu.Framework/Testing/Drawables/Steps/AssertButton.cs
+++ osu.Framework/Testing/Drawables/Steps/AssertButton.cs
@@ -3,6 +3,8 @@
 
 using System;
 using System.Diagnostics;
+using osu.Framework.Allocation;
+using osu.Framework.Platform;
 using OpenTK.Graphics;
 
 namespace osu.Framework.Testing.Drawables.Steps
@@ -13,10 +15,18 @@ namespace osu.Framework.Testing.Drawables.Steps
         public string ExtendedDescription;
         public StackTrace CallStack;
 
+        private bool interactive;
+
         public AssertButton()
         {
             BackgroundColour = Color4.OrangeRed;
             Action += checkAssert;
+        }
+
+        [BackgroundDependencyLoader]
+        private void load(GameHost host)
+        {
+            interactive = !(host is HeadlessGameHost);
         }
 
         private void checkAssert()
@@ -27,7 +37,12 @@ namespace osu.Framework.Testing.Drawables.Steps
                 BackgroundColour = Color4.YellowGreen;
             }
             else
-                throw new TracedException($"{Text} {ExtendedDescription}", CallStack);
+            {
+                if (interactive)
+                    BackgroundColour = Color4.Red;
+                else
+                    throw new TracedException($"{Text} {ExtendedDescription}", CallStack);
+            }
         }
 
         public override string ToString() => "Assert: " + base.ToString();
