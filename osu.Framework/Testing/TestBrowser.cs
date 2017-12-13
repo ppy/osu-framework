e7bc33d95f078f7521abe826f512499e23ffe19c
Dean Herbert <pe@ppy.sh>
2017-12-13 18:04:30 +0900

    Make "run all tests" a checkbox, rather than a button
    Allows easier dynamic compilation testing

--- osu.Framework/Testing/TestBrowser.cs
+++ osu.Framework/Testing/TestBrowser.cs
@@ -156,7 +156,6 @@ namespace osu.Framework.Testing
                             RelativeSizeAxes = Axes.X,
                             Height = 50,
                             Depth = -1,
-                            RunAllSteps = () => CurrentTest.RunAllSteps(() => toolbar.RunComplete(), toolbar.RunComplete),
                         },
                         testContentContainer = new Container
                         {
@@ -211,6 +210,7 @@ namespace osu.Framework.Testing
                 toolbar.AssemblyDropdown.AddDropdownItem(asm.GetName().Name, asm);
 
             toolbar.AssemblyDropdown.Current.ValueChanged += updateList;
+            toolbar.RunAllSteps.Current.ValueChanged += v => runTests(null);
             toolbar.RateAdjustSlider.Current.BindTo(rateBindable);
 
             rateBindable.ValueChanged += v => rateAdjustClock.Rate = v;
@@ -271,10 +271,9 @@ namespace osu.Framework.Testing
 
         public void LoadTest(int testIndex) => LoadTest(TestTypes[testIndex]);
 
+
         public void LoadTest(Type testType = null, Action onCompletion = null)
         {
-            toolbar.RunComplete();
-
             if (testType == null && TestTypes.Count > 0)
                 testType = TestTypes[0];
 
@@ -326,13 +325,19 @@ namespace osu.Framework.Testing
 
                     backgroundCompiler.Checkpoint(CurrentTest);
 
-                    if (!interactive)
-                        loaded.RunAllSteps(onCompletion);
-                    else
-                        CurrentTest.RunFirstStep();
+                    runTests(onCompletion);
+
                     updateButtons();
                 });
             }
+        }
+
+        private void runTests(Action onCompletion)
+        {
+            if (!interactive || toolbar.RunAllSteps.Current)
+                CurrentTest.RunAllSteps(onCompletion);
+            else
+                CurrentTest.RunFirstStep();
         }
 
         private void updateButtons()
@@ -347,15 +352,7 @@ namespace osu.Framework.Testing
 
             public BasicDropdown<Assembly> AssemblyDropdown;
 
-            private Button runAllButton;
-
-            public Action RunAllSteps;
-
-            public void RunComplete(Exception error = null)
-            {
-                runAllButton.Enabled.Value = true;
-                runAllButton.BackgroundColour = error != null ? Color4.Red : Color4.MediumPurple;
-            }
+            public BasicCheckbox RunAllSteps;
 
             [BackgroundDependencyLoader]
             private void load()
@@ -401,18 +398,13 @@ namespace osu.Framework.Testing
                                             {
                                                 Width = 300,
                                             },
-                                            runAllButton = new Button
+                                            RunAllSteps = new BasicCheckbox
                                             {
-                                                Text = "Run all steps",
-                                                BackgroundColour = Color4.MediumPurple,
-                                                Action = delegate
-                                                {
-                                                    runAllButton.Enabled.Value = false;
-                                                    runAllButton.BackgroundColour = Color4.DimGray;
-                                                    RunAllSteps?.Invoke();
-                                                },
+                                                LabelText = "Run all steps",
+                                                AutoSizeAxes = Axes.Y,
                                                 Width = 140,
-                                                RelativeSizeAxes = Axes.Y,
+                                                Anchor = Anchor.CentreLeft,
+                                                Origin = Anchor.CentreLeft,
                                             },
                                         }
                                     },
