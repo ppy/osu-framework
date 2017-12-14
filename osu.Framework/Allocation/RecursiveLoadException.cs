4e056f16b767cb8d716fd29b6721639b31b02db1
smoogipoo <smoogipoo@smgi.me>
2017-12-14 19:09:57 +0900

    Remove unused using

--- osu.Framework/Allocation/RecursiveLoadException.cs
+++ osu.Framework/Allocation/RecursiveLoadException.cs
@@ -5,7 +5,6 @@ using System;
 using System.Linq;
 using System.Reflection;
 using System.Text;
-using osu.Framework.Extensions.TypeExtensions;
 using osu.Framework.Graphics.Containers;
 
 namespace osu.Framework.Allocation
