6a08332ff9cd87c4edad5509b1a8d89ca5e995c9
Dean Herbert <pe@ppy.sh>
2018-01-04 20:58:09 +0900

    Fix CI issues

--- osu.Framework/Configuration/BindableNumberWithPrecision.cs
+++ osu.Framework/Configuration/BindableNumberWithPrecision.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
 
 using System;
@@ -50,7 +50,7 @@ namespace osu.Framework.Configuration
 
             if (!propagateToBindings)
                 return;
-            
+
             Bindings?.ForEachAlive(b =>
             {
                 if (b is BindableNumberWithPrecision<T> other)
