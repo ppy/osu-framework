672c7cedf05757a3dfc303a00219967ab9fe0a4f
smoogipoo <smoogipoo@smgi.me>
2018-01-25 19:55:04 +0900

    Add Bindable nunit tests

---
+++ osu.Framework.Tests/Bindables/BindableEnumTest.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
+
+using NUnit.Framework;
+using osu.Framework.Configuration;
+
+namespace osu.Framework.Tests.Bindables
+{
+    [TestFixture]
+    public class BindableEnumTest
+    {
+        [TestCase(TestEnum.Value1)]
+        [TestCase(TestEnum.Value2)]
+        [TestCase(TestEnum.Value1 - 1)]
+        [TestCase(TestEnum.Value2 + 1)]
+        public void TestSet(TestEnum value)
+        {
+            var bindable = new Bindable<TestEnum> { Value = value };
+            Assert.AreEqual(value, bindable.Value);
+        }
+
+        [TestCase("Value1", TestEnum.Value1)]
+        [TestCase("Value2", TestEnum.Value2)]
+        [TestCase("-1", TestEnum.Value1 - 1)]
+        [TestCase("2", TestEnum.Value2 + 1)]
+        public void TestParsingString(string value, TestEnum expected)
+        {
+            var bindable = new Bindable<TestEnum>();
+            bindable.Parse(value);
+
+            Assert.AreEqual(expected, bindable.Value);
+        }
+
+        [TestCase(TestEnum.Value1)]
+        [TestCase(TestEnum.Value2)]
+        [TestCase(TestEnum.Value1 - 1)]
+        [TestCase(TestEnum.Value2 + 1)]
+        public void TestParsingEnum(TestEnum value)
+        {
+            var bindable = new Bindable<TestEnum>();
+            bindable.Parse(value);
+
+            Assert.AreEqual(value, bindable.Value);
+        }
+
+        public enum TestEnum
+        {
+            Value1 = 0,
+            Value2 = 1
+        }
+    }
+}
---
+++ osu.Framework.Tests/Bindables/BindableFloatTest.cs
@@ -0,0 +1,66 @@
+﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
+
+using NUnit.Framework;
+using osu.Framework.Configuration;
+
+namespace osu.Framework.Tests.Bindables
+{
+    [TestFixture]
+    public class BindableFloatTest
+    {
+        [TestCase(0)]
+        [TestCase(-0)]
+        [TestCase(1)]
+        [TestCase(-105.123f)]
+        [TestCase(105.123f)]
+        [TestCase(float.MinValue)]
+        [TestCase(float.MaxValue)]
+        public void TestSet(float value)
+        {
+            var bindable = new BindableFloat { Value = value };
+            Assert.AreEqual(value, bindable.Value);
+        }
+
+        [TestCase("0", 0f)]
+        [TestCase("1", 1f)]
+        [TestCase("-0", 0f)]
+        [TestCase("-105.123", -105.123f)]
+        [TestCase("105.123", 105.123f)]
+        public void TestParsingString(string value, float expected)
+        {
+            var bindable = new BindableFloat();
+            bindable.Parse(value);
+
+            Assert.AreEqual(expected, bindable.Value);
+        }
+
+        [TestCase("0", -10, 10, 0)]
+        [TestCase("1", -10, 10, 1)]
+        [TestCase("-0", -10, 10, 0)]
+        [TestCase("-105.123", -10, 10, -10)]
+        [TestCase("105.123", -10, 10, 10)]
+        public void TestParsingStringWithRange(string value, float minValue, float maxValue, float expected)
+        {
+            var bindable = new BindableFloat { MinValue = minValue, MaxValue = maxValue };
+            bindable.Parse(value);
+
+            Assert.AreEqual(expected, bindable.Value);
+        }
+
+        [TestCase(0)]
+        [TestCase(-0)]
+        [TestCase(1)]
+        [TestCase(-105.123f)]
+        [TestCase(105.123f)]
+        [TestCase(float.MinValue)]
+        [TestCase(float.MaxValue)]
+        public void TestParsingFloat(float value)
+        {
+            var bindable = new BindableFloat();
+            bindable.Parse(value);
+
+            Assert.AreEqual(value, bindable.Value);
+        }
+    }
+}
