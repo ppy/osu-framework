111e938f8049ef59d840b7c448e6dbd1dc606747
smoogipoo <smoogipoo@smgi.me>
2018-02-07 14:04:58 +0900

    Add back TestTestCase with documentation

---
+++ osu.Framework/Testing/Tests/TestTestCase.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
+
+using NUnit.Framework;
+
+namespace osu.Framework.Testing.Tests
+{
+    [TestFixture]
+    internal class TestTestCase : TestCase
+    {
+        // This TestCase is required for nunit to not throw errors
+        // See: https://github.com/nunit/nunit/issues/1118
+    }
+}
