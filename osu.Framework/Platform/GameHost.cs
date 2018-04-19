73a9a37573e1cab13ef4899613f931716f084379
Alexandre Padovan <pro@alexpado.fr>
2018-04-19 14:02:12 +0200

    Added storage.ini file

--- osu.Framework/Platform/GameHost.cs
+++ osu.Framework/Platform/GameHost.cs
@@ -155,7 +155,7 @@ namespace osu.Framework.Platform
             FileSafety.DeleteCleanupDirectory();
 
             Dependencies.CacheAs(this);
-            Dependencies.CacheAs(Storage = GetStorage(gameName));
+            Dependencies.CacheAs(Storage = new UserStorage());
 
             Name = gameName;
             Logger.GameIdentifier = gameName;
@@ -177,6 +177,22 @@ namespace osu.Framework.Platform
             var path = Path.GetDirectoryName(FullPath);
             if (path != null)
                 Environment.CurrentDirectory = path;
+        }
+
+        private class UserStorage : DesktopStorage
+        {
+            private readonly StorageOverriderConfigManager configManager = new StorageOverriderConfigManager();
+
+            protected override string LocateBasePath()
+            {
+                return configManager.Get<string>(StorageConfig.Path);
+            }
+
+            public UserStorage()
+                : base(string.Empty)
+            {
+                configManager.Load();
+            }
         }
 
         private void exceptionHandler(object sender, UnhandledExceptionEventArgs e)
