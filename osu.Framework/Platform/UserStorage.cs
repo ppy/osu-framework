849e4124af27db3449354221ba7285793c7ec17a
Alexandre Padovan <pro@alexpado.fr>
2018-04-23 13:49:39 +0200

    Forgot Licence header

--- osu.Framework/Platform/UserStorage.cs
+++ osu.Framework/Platform/UserStorage.cs
@@ -1,4 +1,7 @@
-﻿using osu.Framework.Configuration;
+﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using osu.Framework.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Text;
