1e84ab2e5e1a6a88c12036fa2720e32b3ced2bd3
Dean Herbert <pe@ppy.sh>
2018-05-24 16:39:53 +0900

    Fix PassThroughInputManager not handling wheel events

--- osu.Framework/Input/PassThroughInputManager.cs
+++ osu.Framework/Input/PassThroughInputManager.cs
@@ -67,6 +67,8 @@ namespace osu.Framework.Input
 
         protected override bool OnKeyDown(InputState state, KeyDownEventArgs args) => acceptState(state);
 
+        protected override bool OnWheel(InputState state) => acceptState(state);
+
         protected override bool OnKeyUp(InputState state, KeyUpEventArgs args) => acceptState(state);
 
         protected override bool OnJoystickPress(InputState state, JoystickEventArgs args) => acceptState(state);
@@ -83,6 +85,7 @@ namespace osu.Framework.Input
                 Mouse = (state.Mouse.NativeState as MouseState)?.Clone();
                 Keyboard = (state.Keyboard as KeyboardState)?.Clone();
                 Joystick = (state.Joystick as JoystickState)?.Clone();
+                Last = state.Last;
             }
         }
     }
