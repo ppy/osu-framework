026571bc55f419212dbc3737ec376bdd398a55a6
Shane Woolcock <samahnub@gmail.com>
2018-06-10 13:48:48 +0930

    Add xmldoc to UpdateableContainer

--- osu.Framework/Graphics/Containers/UpdateableContainer.cs
+++ osu.Framework/Graphics/Containers/UpdateableContainer.cs
@@ -5,21 +5,48 @@ using System;
 
 namespace osu.Framework.Graphics.Containers
 {
-    public abstract class UpdateableContainer<T> : Container
-        where T : class
+    /// <summary>
+    /// An abstract class that manages dynamically displaying a custom Drawable based on a "source" object.
+    /// Useful for replacing Drawables on the fly.
+    /// </summary>
+    public abstract class UpdateableContainer<T> : Container where T : class
     {
         private Drawable displayedDrawable;
         private readonly Drawable placeholderDrawable;
 
+        /// <summary>
+        /// Determines whether the current Drawable should fade out straight away when switching to a new source,
+        /// or whether it should wait until the new Drawable has finished loading.
+        /// </summary>
         protected virtual bool FadeOutImmediately => false;
 
+        /// <summary>
+        /// Override to instantiate a placeholder Drawable that will be displayed when no source is set.
+        /// May be null to indicate no placeholder.
+        /// </summary>
         protected virtual Drawable CreatePlaceholder() => null;
 
+        /// <summary>
+        /// Override to instantiate a custom Drawable based on the passed source item.
+        /// May be null to indicate that the source item has no visual representation,
+        /// in which case the placeholder will be used if it exists.
+        /// </summary>
+        /// <param name="item">The source item that the Drawable should represent.</param>
         protected virtual Drawable CreateDrawable(T item) => null;
 
+        /// <summary>
+        /// Override to perform a custom comparison of two source items.  By default an object reference comparison
+        /// is used, but it may be desirable to compare based on properties of the items.
+        /// </summary>
+        /// <returns><c>true</c>, if the items are logically equivalent, <c>false</c> otherwise.</returns>
         protected virtual bool CompareItems(T lhs, T rhs) => lhs == rhs;
 
         private T source;
+
+        /// <summary>
+        /// Gets or sets the source item, potentially triggering the current Drawable to update.
+        /// Subclasses should expose this via a nicer property name to better represent the data being set.
+        /// </summary>
         protected T Source
         {
             get => source;
