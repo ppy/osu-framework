6d45ff36966e48f626444afb500772be246d6ae5
Dean Herbert <pe@ppy.sh>
2018-06-11 17:36:15 +0900

    Add zoom ability to test case

--- osu.Framework.Tests/Visual/TestCaseWaveform.cs
+++ osu.Framework.Tests/Visual/TestCaseWaveform.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using osu.Framework.Allocation;
 using osu.Framework.Audio;
 using osu.Framework.Audio.Track;
+using osu.Framework.Configuration;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Audio;
 using osu.Framework.Graphics.Containers;
@@ -29,6 +30,8 @@ namespace osu.Framework.Tests.Visual
 
         private Button button;
         private TrackBass track;
+        private SliderBar<float> zoomSlider;
+        private readonly Bindable<float> zoom = new BindableFloat(1) { MinValue = 0.1f, MaxValue = 20 };
 
         [BackgroundDependencyLoader]
         private void load(Game game, AudioManager audio)
@@ -46,12 +49,35 @@ namespace osu.Framework.Tests.Visual
                 Direction = FillDirection.Vertical,
                 Children = new Drawable[]
                 {
-                    button = new Button
+                    new FillFlowContainer
                     {
-                        Text = "Start",
-                        Size = new Vector2(100, 50),
-                        BackgroundColour = Color4.DarkSlateGray,
-                        Action = startStop
+                        AutoSizeAxes = Axes.Y,
+                        RelativeSizeAxes = Axes.X,
+                        Spacing = new Vector2(10),
+                        Children = new Drawable[]
+                        {
+                            button = new Button
+                            {
+                                Text = "Start",
+                                Size = new Vector2(100, 50),
+                                BackgroundColour = Color4.DarkSlateGray,
+                                Anchor = Anchor.CentreLeft,
+                                Origin = Anchor.CentreLeft,
+                                Action = startStop
+                            },
+                            new SpriteText
+                            {
+                                Text = "Zoom Level:",
+                                Origin = Anchor.CentreLeft,
+                                Anchor = Anchor.CentreLeft,
+                            },
+                            zoomSlider = new BasicSliderBar<float>
+                            {
+                                Anchor = Anchor.CentreLeft,
+                                Origin = Anchor.CentreLeft,
+                                Size = new Vector2(200, 40)
+                            },
+                        },
                     },
                     flow = new FillFlowContainer
                     {
@@ -64,6 +90,9 @@ namespace osu.Framework.Tests.Visual
 
             for (int i = 1; i <= 16; i *= 2)
                 flow.Add(new TestWaveform(track, 1f / i) { Waveform = waveform });
+
+            zoomSlider.Current.BindTo(zoom);
+            zoomSlider.Current.ValueChanged += v => flow.Width = v;
         }
 
         private void startStop()
@@ -102,7 +131,7 @@ namespace osu.Framework.Tests.Visual
                         Colour = new Color4(232, 78, 6, 255),
                         LowColour = new Color4(255, 232, 100, 255),
                         MidColour = new Color4(255, 153, 19, 255),
-                        HighColour = new Color4(255, 46, 7, 255)
+                        HighColour = new Color4(255, 46, 7, 255),
                     },
                     new Container
                     {
