935836ae54953a113452f69b6aad45e5048c8a76
smoogipoo <smoogipoo@smgi.me>
2018-06-19 22:03:20 +0900

    Fix decoupled clock running one frame behind

--- osu.Framework/Timing/DecoupleableInterpolatingFramedClock.cs
+++ osu.Framework/Timing/DecoupleableInterpolatingFramedClock.cs
@@ -57,7 +57,6 @@ namespace osu.Framework.Timing
             base.ProcessFrame();
 
             decoupledStopwatch.Rate = adjustableSource?.Rate ?? 1;
-            decoupledClock.ProcessFrame();
 
             bool sourceRunning = SourceClock?.IsRunning ?? false;
 
@@ -83,6 +82,8 @@ namespace osu.Framework.Timing
                 Start();
                 decoupledStopwatch.Seek(CurrentTime);
             }
+
+            decoupledClock.ProcessFrame();
         }
 
         public override void ChangeSource(IClock source)
