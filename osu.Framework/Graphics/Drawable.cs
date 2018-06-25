4f035c9431e8c310e56267dc9ffa0e7fcfd57884
Roman Kapustin <TocoToucanMS@gmail.com>
2018-06-26 00:12:49 +0300

    Perhaps the '??' operator works in a different way than it was expected. Its priority is lower than priority of other operators in its left part.

--- osu.Framework/Graphics/Drawable.cs
+++ osu.Framework/Graphics/Drawable.cs
@@ -1070,7 +1070,7 @@ namespace osu.Framework.Graphics
         /// to the <see cref="Parent"/> in the coordinate system with origin at the top
         /// left corner of the <see cref="Parent"/>'s <see cref="DrawRectangle"/>.
         /// </summary>
-        public Vector2 AnchorPosition => RelativeAnchorPosition * Parent?.ChildSize ?? Vector2.Zero;
+        public Vector2 AnchorPosition => RelativeAnchorPosition * (Parent?.ChildSize ?? Vector2.Zero);
 
         /// <summary>
         /// Helper function to compute an absolute position given an absolute size and
