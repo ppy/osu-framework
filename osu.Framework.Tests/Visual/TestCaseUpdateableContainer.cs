93e9f2556a9d85e3187e2b0ec78901123062a0b6
Shane Woolcock <samahnub@gmail.com>
2018-07-02 18:46:22 +0930

    Add more tests for UpdateableContainer

--- osu.Framework.Tests/Visual/TestCaseUpdateableContainer.cs
+++ osu.Framework.Tests/Visual/TestCaseUpdateableContainer.cs
@@ -19,35 +19,62 @@ namespace osu.Framework.Tests.Visual
         {
             TestUpdateableContainer updateContainer;
             PlaceholderTestUpdateableContainer placeholderContainer;
+            DelayedTestUpdateableContainer delayedContainer;
 
             AddRange(new Drawable[]
             {
                 updateContainer = new TestUpdateableContainer
                 {
                     Position = new Vector2(50, 50),
-                    Size = new Vector2(250, 250)
+                    Size = new Vector2(100, 100)
                 },
                 placeholderContainer = new PlaceholderTestUpdateableContainer
                 {
-                    Position = new Vector2(50, 350),
-                    Size = new Vector2(250, 250)
+                    Position = new Vector2(50, 250),
+                    Size = new Vector2(100, 100)
+                },
+                delayedContainer = new DelayedTestUpdateableContainer
+                {
+                    Position = new Vector2(50, 450),
+                    Size = new Vector2(100, 100)
                 }
             });
 
-            AddStep("Set null no PH", () => updateContainer.Item = null);
-            AddStep("Set item 0 no PH", () => updateContainer.Item = new TestItem(0));
-            AddUntilStep(() => updateContainer.NextDrawable == null, "wait until loaded");
-            AddStep("Set item 1 no PH", () => updateContainer.Item = new TestItem(1));
-            AddUntilStep(() => updateContainer.NextDrawable == null, "wait until loaded");
-            AddStep("Set null no PH", () => updateContainer.Item = null);
-            AddAssert("Check no placeholder", () => updateContainer.PlaceholderDrawable == null);
 
-            AddStep("Set item 0 with PH", () => placeholderContainer.Item = new TestItem(0));
-            AddUntilStep(() => placeholderContainer.NextDrawable == null, "wait until loaded");
-            AddStep("Set item 1 with PH", () => placeholderContainer.Item = new TestItem(1));
-            AddUntilStep(() => placeholderContainer.NextDrawable == null, "wait until loaded");
-            AddStep("Set null with PH", () => placeholderContainer.Item = null);
-            AddAssert("Check placeholder is visible", () => (placeholderContainer.PlaceholderDrawable?.Alpha ?? 0) > 0);
+            addNullTest("no PH", updateContainer, false);
+            addItemTest("no PH", updateContainer, 0);
+            addItemTest("no PH", updateContainer, 1);
+            addNullTest("no PH", updateContainer, false);
+
+            addNullTest("PH", placeholderContainer, true);
+            addItemTest("PH", placeholderContainer, 0);
+            addItemTest("PH", placeholderContainer, 1);
+            addNullTest("PH", placeholderContainer, true);
+
+            AddStep("Set item null", () => delayedContainer.Item = null);
+            AddStep("Set item with delay", () => delayedContainer.Item = new TestItem(0));
+            AddAssert("Test next drawable not null", () => delayedContainer.NextDrawable != null);
+            AddWaitStep(5);
+            AddAssert("Test next drawable null", () => delayedContainer.NextDrawable == null);
+        }
+
+        private void addNullTest(string prefix, TestUpdateableContainer container, bool expectPlaceholder)
+        {
+            AddStep($"{prefix} Set null", () => container.Item = null);
+            if (expectPlaceholder)
+                AddAssert($"{prefix} Check null with PH", () => container.DisplayedDrawable == null && (container.PlaceholderDrawable?.Alpha ?? 0) > 0);
+            else
+            {
+                AddUntilStep(() => container.NextDrawable == null, $"{prefix} wait until loaded");
+                AddAssert($"{prefix} Check non-null no PH", () => container.VisibleItemId == -1 && container.PlaceholderDrawable == null);
+            }
+        }
+
+        private void addItemTest(string prefix, TestUpdateableContainer container, int itemNumber)
+        {
+            AddStep($"{prefix} Set item {itemNumber}", () => container.Item = new TestItem(itemNumber));
+            AddUntilStep(() => container.NextDrawable == null, $"{prefix} wait until loaded");
+            AddAssert($"{prefix} Check item {itemNumber}", () => container.VisibleItemId == itemNumber);
         }
 
         private class TestItem
@@ -62,9 +89,12 @@ namespace osu.Framework.Tests.Visual
 
         private class TestItemDrawable : SpriteText
         {
+            public readonly int ItemId;
+
             public TestItemDrawable(TestItem item)
             {
-                Position = new Vector2(50, 50);
+                ItemId = item?.ItemId ?? -1;
+                Position = new Vector2(10, 10);
                 Text = item == null ? "No Item" : $"Item {item.ItemId}";
             }
 
@@ -80,26 +110,35 @@ namespace osu.Framework.Tests.Visual
         {
             public TestItem Item { get => Source; set => Source = value; }
 
+            public int VisibleItemId => (DisplayedDrawable as TestItemDrawable)?.ItemId ?? -1;
+
             public TestUpdateableContainer()
                 : base((lhs, rhs) => lhs?.ItemId == rhs?.ItemId ? 0 : -1)
             {
+                Add(new Box
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    Alpha = 0,
+                    AlwaysPresent = true
+                });
+                BorderColour = Color4.White;
+                BorderThickness = 2;
+                Masking = true;
             }
 
             protected override Drawable CreateDrawable(TestItem item) => new TestItemDrawable(item);
         }
 
-        private class PlaceholderTestUpdateableContainer : UpdateableContainer<TestItem>
+        private class PlaceholderTestUpdateableContainer : TestUpdateableContainer
         {
-            public TestItem Item { get => Source; set => Source = value; }
-
-            public PlaceholderTestUpdateableContainer()
-                : base((lhs, rhs) => lhs?.ItemId == rhs?.ItemId ? 0 : -1)
-            {
-            }
-
             protected override Drawable CreateDrawable(TestItem item) => item == null ? null : new TestItemDrawable(item);
 
             protected override Drawable CreatePlaceholder() => new Box { Colour = Color4.Blue };
+        }
+
+        private class DelayedTestUpdateableContainer : PlaceholderTestUpdateableContainer
+        {
+            protected override double LoadDelay => 500 / Clock.Rate;
         }
     }
 }
