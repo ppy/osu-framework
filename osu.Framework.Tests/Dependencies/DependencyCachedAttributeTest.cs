082082136054ffd1f96e4f9dc7a33cbcb0abfb8f
smoogipoo <smoogipoo@smgi.me>
2018-07-18 20:21:08 +0900

    Add more tests

--- osu.Framework.Tests/Dependencies/DependencyCachedAttributeTest.cs
+++ osu.Framework.Tests/Dependencies/DependencyCachedAttributeTest.cs
@@ -1,6 +1,7 @@
 // Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
 
+using System;
 using NUnit.Framework;
 using osu.Framework.Allocation;
 
@@ -20,7 +21,7 @@ namespace osu.Framework.Tests.Dependencies
         }
 
         [Test]
-        public void TestCacheTypeAsParentClass()
+        public void TestCacheTypeAsParentType()
         {
             var provider = new Provider2();
 
@@ -30,7 +31,7 @@ namespace osu.Framework.Tests.Dependencies
         }
 
         [Test]
-        public void TestCacheTypeOverrideParent()
+        public void TestCacheTypeOverrideParentCache()
         {
             var provider = new Provider3();
 
@@ -38,6 +39,73 @@ namespace osu.Framework.Tests.Dependencies
 
             Assert.AreEqual(provider, dependencies.Get<Provider1>());
             Assert.AreEqual(null, dependencies.Get<Provider3>());
+        }
+
+        [Test]
+        public void TestAttemptToCacheStruct()
+        {
+            var provider = new Provider4();
+
+            Assert.Throws<ArgumentException>(() => DependencyActivator.BuildDependencies(provider, new DependencyContainer()));
+        }
+
+        [Test]
+        public void TestCacheMultipleFields()
+        {
+            var provider = new Provider5();
+
+            var dependencies = DependencyActivator.BuildDependencies(provider, new DependencyContainer());
+
+            Assert.IsNotNull(dependencies.Get<ProvidedType1>());
+            Assert.IsNotNull(dependencies.Get<ProvidedType2>());
+        }
+
+        [Test]
+        public void TestCacheFieldsOverrideBaseFields()
+        {
+            var provider = new Provider6();
+
+            var dependencies = DependencyActivator.BuildDependencies(provider, new DependencyContainer());
+
+            Assert.AreEqual(provider.Provided3, dependencies.Get<ProvidedType1>());
+        }
+
+        [Test]
+        public void TestCacheFieldsAsMultipleTypes()
+        {
+            var provider = new Provider7();
+
+            var dependencies = DependencyActivator.BuildDependencies(provider, new DependencyContainer());
+
+            Assert.IsNotNull(dependencies.Get<object>());
+            Assert.IsNotNull(dependencies.Get<ProvidedType1>());
+        }
+
+        [Test]
+        public void TestCacheTypeAsMultipleTypes()
+        {
+            var provider = new Provider8();
+
+            var dependencies = DependencyActivator.BuildDependencies(provider, new DependencyContainer());
+
+            Assert.IsNotNull(dependencies.Get<object>());
+            Assert.IsNotNull(dependencies.Get<Provider8>());
+        }
+
+        [Test]
+        public void TestAttemptToCacheBaseAsDerived()
+        {
+            var provider = new Provider9();
+
+            Assert.Throws<ArgumentException>(() => DependencyActivator.BuildDependencies(provider, new DependencyContainer()));
+        }
+
+        private class ProvidedType1
+        {
+        }
+
+        private class ProvidedType2
+        {
         }
 
         [DependencyCached]
@@ -53,6 +121,50 @@ namespace osu.Framework.Tests.Dependencies
         [DependencyCached(typeof(Provider1))]
         private class Provider3 : Provider1
         {
+        }
+
+        private class Provider4
+        {
+            [DependencyCached]
+            private int fail;
+        }
+
+        private class Provider5
+        {
+            [DependencyCached]
+            private ProvidedType1 provided1 = new ProvidedType1();
+
+            public ProvidedType1 Provided1 => provided1;
+
+            [DependencyCached]
+            private ProvidedType2 provided2 = new ProvidedType2();
+        }
+
+        private class Provider6 : Provider5
+        {
+            [DependencyCached]
+            private ProvidedType1 provided3 = new ProvidedType1();
+
+            public ProvidedType1 Provided3 => provided3;
+        }
+
+        private class Provider7
+        {
+            [DependencyCached]
+            [DependencyCached(typeof(object))]
+            private ProvidedType1 provided1 = new ProvidedType1();
+        }
+
+        [DependencyCached]
+        [DependencyCached(typeof(object))]
+        private class Provider8
+        {
+        }
+
+        private class Provider9
+        {
+            [DependencyCached(typeof(ProvidedType1))]
+            private object provided1 = new object();
         }
     }
 }
