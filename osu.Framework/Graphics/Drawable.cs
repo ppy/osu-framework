7286889e215f2c298820ca8711a61278dd7cec9c
Dean Herbert <pe@ppy.sh>
2018-08-15 17:47:35 +0900

    Remove unnecessary catch

--- osu.Framework/Graphics/Drawable.cs
+++ osu.Framework/Graphics/Drawable.cs
@@ -228,13 +228,7 @@ namespace osu.Framework.Graphics
                 // get our dependencies from our parent, but allow local overriding of our inherited dependency container
                 Dependencies = CreateChildDependencies(dependencies);
 
-                try
-                {
-                    dependencies.Inject(this);
-                }
-                catch (OperationCanceledException)
-                {
-                }
+                dependencies.Inject(this);
 
                 LoadAsyncComplete();
 
