68e3b479f448bf783b700edfc737fbdfea645943
Dean Herbert <pe@ppy.sh>
2018-08-18 23:50:06 +0900

    FPS -> FrameTime

--- osu.Framework/Graphics/Performance/FrameTimeDisplay.cs
+++ osu.Framework/Graphics/Performance/FrameTimeDisplay.cs
@@ -1,26 +1,27 @@
 ﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
 
+using System;
+using System.Globalization;
 using OpenTK.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
 using osu.Framework.Graphics.Sprites;
 using osu.Framework.MathUtils;
 using osu.Framework.Timing;
-using System;
+using OpenTK;
 
 namespace osu.Framework.Graphics.Performance
 {
-    internal class FpsDisplay : Container
+    internal class FrameTimeDisplay : Container
     {
         private readonly SpriteText counter;
 
-        private readonly IFrameBasedClock clock;
-        private double displayFps;
+        private readonly ThrottledFrameClock clock;
 
         public bool Counting = true;
 
-        public FpsDisplay(IFrameBasedClock clock)
+        public FrameTimeDisplay(ThrottledFrameClock clock)
         {
             this.clock = clock;
 
@@ -39,6 +40,7 @@ namespace osu.Framework.Graphics.Performance
                 {
                     Anchor = Anchor.TopRight,
                     Origin = Anchor.TopRight,
+                    Spacing = new Vector2(-1, 0),
                     Text = @"...",
                     FixedWidth = true,
                 }
@@ -47,13 +49,13 @@ namespace osu.Framework.Graphics.Performance
 
         private float aimWidth;
 
+        private double displayFps;
+
         protected override void Update()
         {
             base.Update();
 
             if (!Counting) return;
-
-            displayFps = Interpolation.Damp(displayFps, clock.FramesPerSecond, 0.01, Math.Max(clock.ElapsedFrameTime, 0) / 1000);
 
             if (counter.DrawWidth != aimWidth)
             {
@@ -69,7 +71,10 @@ namespace osu.Framework.Graphics.Performance
                 aimWidth = counter.DrawWidth;
             }
 
-            counter.Text = displayFps.ToString(@"0");
+            displayFps = Interpolation.Damp(displayFps, clock.FramesPerSecond, 0.01, Math.Max(clock.ElapsedFrameTime, 0) / 1000);
+
+            counter.Text = $"{displayFps:0}fps({clock.AverageFrameTime:0.00}ms)"
+                           + $"{(clock.MaximumUpdateHz < 10000 ? clock.MaximumUpdateHz.ToString(CultureInfo.InvariantCulture) : "∞").PadLeft(4)}hz";
         }
     }
 }
