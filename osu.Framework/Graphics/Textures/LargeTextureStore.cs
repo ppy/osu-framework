6ad9f71f958a794c8aa0895297f9fd64c4f54192
Dean Herbert <pe@ppy.sh>
2018-09-03 12:41:51 +0900

    Add xmldoc explaining LargeTextureStore retrieval

--- osu.Framework/Graphics/Textures/LargeTextureStore.cs
+++ osu.Framework/Graphics/Textures/LargeTextureStore.cs
@@ -15,6 +15,13 @@ namespace osu.Framework.Graphics.Textures
         {
         }
 
+        /// <summary>
+        /// Retrieves a texture.
+        /// This texture should only be assigned once, as reference counting is being used internally.
+        /// If you wish to use the same texture multiple times, call this method an equal number of times.
+        /// </summary>
+        /// <param name="name">The name of the texture.</param>
+        /// <returns>The texture.</returns>
         public override Texture Get(string name)
         {
             var baseTex = base.Get(name);
