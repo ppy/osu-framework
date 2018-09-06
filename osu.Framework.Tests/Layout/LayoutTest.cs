a3e3cf4d85d1c94735a030739672c11de72ef7a3
smoogipoo <smoogipoo@smgi.me>
2018-09-06 10:41:06 +0900

    Improve xmldocs

--- osu.Framework.Tests/Layout/LayoutTest.cs
+++ osu.Framework.Tests/Layout/LayoutTest.cs
@@ -1,16 +1,30 @@
 // Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
 
-using System;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Platform;
 
 namespace osu.Framework.Tests.Layout
 {
+    /// <summary>
+    /// Base class for a test that runs a game until required.
+    /// </summary>
     public abstract class LayoutTest
     {
-        public static void Run(Drawable child, Func<int, bool> testAction)
+        /// <summary>
+        /// A delegate invoked every <see cref="Drawable.UpdateSubTree"/>. Returning true will cause the game to exit.
+        /// </summary>
+        /// <param name="frameIndex">The index of invocation of <see cref="Drawable.UpdateSubTree"/>.
+        /// A value of 0 indicates the callback happened prior to the first <see cref="Drawable.UpdateSubTree"/>.</param>
+        public delegate bool TestDelegate(int frameIndex);
+
+        /// <summary>
+        /// Runs the game.
+        /// </summary>
+        /// <param name="child">The child to add to the hierarchy.</param>
+        /// <param name="testAction">A delegate to be executed every <see cref="Drawable.UpdateSubTree"/>. Returning true will cause the game to exit.</param>
+        public static void Run(Drawable child, TestDelegate testAction)
         {
             using (var host = new HeadlessGameHost())
             {
@@ -21,9 +35,9 @@ namespace osu.Framework.Tests.Layout
         private class LayoutTestGame : Game
         {
             private readonly Drawable child;
-            private readonly Func<int, bool> callback;
+            private readonly TestDelegate callback;
 
-            public LayoutTestGame(Drawable child, Func<int, bool> callback)
+            public LayoutTestGame(Drawable child, TestDelegate callback)
             {
                 this.child = child;
                 this.callback = callback;
