7459b8938e79937db59667be265c41b7f159da3e
smoogipoo <smoogipoo@smgi.me>
2018-09-14 15:44:41 +0900

    Make LocalisableString a struct

--- osu.Framework/Localisation/LocalisableString.cs
+++ osu.Framework/Localisation/LocalisableString.cs
@@ -9,34 +9,22 @@ namespace osu.Framework.Localisation
     /// <summary>
     /// A class representing text that can be localised and formatted.
     /// </summary>
-    public class LocalisableString
+    public struct LocalisableString : IEquatable<LocalisableString>
     {
         /// <summary>
         /// The text to be used for localisation and/or formatting.
         /// </summary>
-        public Bindable<string> Text { get; } = new Bindable<string>();
+        public Bindable<string> Text { get; }
 
         /// <summary>
         /// Whether <see cref="Text"/> should be localised.
         /// </summary>
-        public Bindable<bool> Localised { get; } = new Bindable<bool>();
+        public Bindable<bool> Localised { get; }
 
         /// <summary>
         /// The arguments to format <see cref="Text"/> with.
         /// </summary>
-        public Bindable<object[]> Args { get; } = new Bindable<object[]>();
-
-        /// <summary>
-        /// Creates a new <see cref="LocalisableString"/>. This localises by default.
-        /// </summary>
-        /// <param name="text">The text.</param>
-        /// <param name="args">The arguments to format the text with.</param>
-        public LocalisableString([NotNull] string text, params object[] args)
-        {
-            Text.Value = text;
-            Localised.Value = true;
-            Args.Value = args;
-        }
+        public Bindable<object[]> Args { get; }
 
         /// <summary>
         /// Creates a new <see cref="LocalisableString"/>.
@@ -44,11 +32,23 @@ namespace osu.Framework.Localisation
         /// <param name="text">The text.</param>
         /// <param name="localised">Whether the text should be localised.</param>
         /// <param name="args">The arguments to format the text with.</param>
-        public LocalisableString([NotNull] string text, bool localised = true, params object[] args)
+        public LocalisableString(string text, bool localised = true, params object[] args)
         {
-            Text.Value = text;
-            Localised.Value = localised;
-            Args.Value = args;
+            Text = new Bindable<string>(text ?? string.Empty);
+            Localised = new Bindable<bool>(localised);
+            Args = new Bindable<object[]>(args);
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="LocalisableString"/>. This localises by default.
+        /// </summary>
+        /// <param name="text">The text.</param>
+        /// <param name="args">The arguments to format the text with.</param>
+        public LocalisableString(string text, params object[] args)
+            : this(text, true, args)
+        {
+        }
+        {
         }
     }
 }
