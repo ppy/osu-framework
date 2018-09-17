e415fa500c6f815cdb2fad2fa51b6c4f0584777f
AtomCrafty <atomcrafty@frucost.net>
2018-09-17 10:28:47 +0200

    Undid changes to SampleGame

--- SampleGame/SampleGame.cs
+++ SampleGame/SampleGame.cs
@@ -1,19 +1,12 @@
 ﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
 
-using System;
-using System.Drawing;
 using osu.Framework;
 using osu.Framework.Graphics;
 using OpenTK;
 using OpenTK.Graphics;
 using osu.Framework.Graphics.Shapes;
 using osu.Framework.Allocation;
-using osu.Framework.Configuration;
-using osu.Framework.Input.EventArgs;
-using osu.Framework.Input.States;
-using osu.Framework.Platform;
-using OpenTK.Input;
 
 namespace SampleGame {
     internal class SampleGame : Game {
@@ -27,39 +20,11 @@ namespace SampleGame {
                 Size = new Vector2(150, 150),
                 Colour = Color4.Tomato
             });
-            //((DesktopGameWindow)Window).Position = new Vector2(0.5f, 0.5f);
         }
 
         protected override void Update() {
             base.Update();
             box.Rotation += (float)Time.Elapsed / 10;
-        }
-
-        protected override bool OnMouseDown(InputState state, MouseDownEventArgs args) {
-            var win = (DesktopGameWindow)Window;
-
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("Bounds:   " + win.Bounds);
-            Console.WriteLine("Client:   " + win.ClientSize);
-            Console.WriteLine("Location: " + win.Location);
-            Console.WriteLine("Size:     " + win.Size);
-            Console.WriteLine("Mode:     " + win.WindowMode);
-            Console.WriteLine("State:    " + win.WindowState);
-            Console.ResetColor();
-
-            switch(args.Button) {
-
-                case MouseButton.Left:
-                    win.WindowMode.Value = win.WindowMode.Value == WindowMode.Borderless ? WindowMode.Windowed : WindowMode.Borderless;
-                    break;
-
-                case MouseButton.Right:
-                    win.WindowMode.Value = WindowMode.Windowed;
-                    win.ClientSize = new Size(1280, 720);
-                    win.Position = new Vector2(1, 1);
-                    break;
-            }
-            return true;
         }
     }
 }
