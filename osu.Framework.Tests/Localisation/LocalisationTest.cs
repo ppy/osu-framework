8cd22c3cf5c2c840fcb1f02d79b7f65367ec2c16
smoogipoo <smoogipoo@smgi.me>
2018-09-20 10:44:51 +0900

    Add empty string tests

--- osu.Framework.Tests/Localisation/LocalisationTest.cs
+++ osu.Framework.Tests/Localisation/LocalisationTest.cs
@@ -129,6 +129,23 @@ namespace osu.Framework.Tests.Localisation
             Assert.AreEqual(unicode_2, text.Value);
         }
 
+        [Test]
+        public void TestEmptyStringFallback([Values("", null)] string emptyValue)
+        {
+            const string non_unicode_fallback = "non unicode";
+            const string unicode_fallback = "unicode";
+
+            var text = manager.GetLocalisedString(new LocalisedString((unicode_fallback, emptyValue)));
+
+            config.Set(FrameworkSetting.ShowUnicode, false);
+            Assert.AreEqual(unicode_fallback, text.Value);
+
+            text = manager.GetLocalisedString(new LocalisedString((emptyValue, non_unicode_fallback)));
+
+            config.Set(FrameworkSetting.ShowUnicode, true);
+            Assert.AreEqual(non_unicode_fallback, text.Value);
+        }
+
         private class FakeFrameworkConfigManager : FrameworkConfigManager
         {
             protected override string Filename => null;
