0d4e4301733bd2389c89444e6e85cb671ee5970d
smoogipoo <smoogipoo@smgi.me>
2018-10-05 15:45:25 +0900

    Fix license headers

--- osu.Framework/Graphics/Lines/TexturedPath.cs
+++ osu.Framework/Graphics/Lines/TexturedPath.cs
@@ -1,5 +1,5 @@
 // Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
-// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
 
 using osu.Framework.Graphics.Textures;
 
