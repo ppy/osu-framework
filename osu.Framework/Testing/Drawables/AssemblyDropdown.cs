b19510d03602d6c62cec7df6751b1b2811f30a28
AtomCrafty <atomcrafty@frucost.net>
2018-10-09 14:09:33 +0200

    Add license header

--- osu.Framework/Testing/Drawables/AssemblyDropdown.cs
+++ osu.Framework/Testing/Drawables/AssemblyDropdown.cs
@@ -1,9 +1,12 @@
-﻿using System.Reflection;
+﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using System.Reflection;
 using osu.Framework.Graphics.UserInterface;
 
 namespace osu.Framework.Testing.Drawables
 {
-    class AssemblyDropdown : BasicDropdown<Assembly>
+    public class AssemblyDropdown : BasicDropdown<Assembly>
     {
         public void AddAssembly(string name, Assembly assembly) => AddDropdownItem(name, assembly);
     }
