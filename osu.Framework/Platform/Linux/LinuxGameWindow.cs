db5e972bcf7b96f863bf7ab37230e14412cfccb9
WebFreak001 <gh@webfreak.org>
2018-10-13 18:52:45 +0200

    Add support for SDL clipboard on linux fix #1179
    This only adds support for when the SDL backend is used as the X11
    backend would require some of event loop changes.

--- osu.Framework/Platform/Linux/LinuxGameWindow.cs
+++ osu.Framework/Platform/Linux/LinuxGameWindow.cs
@@ -1,9 +1,31 @@
 ﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
 
+using System;
+using System.Linq;
+using System.Reflection;
+using OpenTK;
+
 namespace osu.Framework.Platform.Linux
 {
     public class LinuxGameWindow : DesktopGameWindow
     {
+        private bool isSDL;
+
+        public bool IsSDL => isSDL;
+
+        public LinuxGameWindow()
+        {
+            Load += OnLoad;
+        }
+
+        private void OnLoad(object sender, EventArgs e)
+        {
+            var implementationField = typeof(NativeWindow).GetRuntimeFields().Single(x => x.Name == "implementation");
+
+            var windowImpl = implementationField.GetValue(Implementation);
+
+            isSDL = windowImpl.GetType().Name == "Sdl2NativeWindow";
+        }
     }
 }
---
+++ osu.Framework/Platform/Linux/SDL/SDLClipboard.cs
@@ -0,0 +1,36 @@
+// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
+
+// using System.Windows.Forms;
+
+using System.Runtime.InteropServices;
+
+namespace osu.Framework.Platform.SDL
+{
+    public class SDLClipboard : Clipboard
+    {
+        #if ANDROID
+        const string lib = "libSDL2.so";
+        #elif IPHONE
+        const string lib = "__Internal";
+        #else
+        private const string lib = "libSDL2-2.0.so.0";
+        #endif
+
+        [DllImport(lib, CallingConvention = CallingConvention.Cdecl, EntryPoint = "SDL_GetClipboardText", ExactSpelling = true)]
+        internal static extern string SDL_GetClipboardText();
+
+        [DllImport(lib, CallingConvention = CallingConvention.Cdecl, EntryPoint = "SDL_SetClipboardText", ExactSpelling = true)]
+        internal static extern int SDL_SetClipboardText(string text);
+
+        public override string GetText()
+        {
+            return SDL_GetClipboardText();
+        }
+
+        public override void SetText(string selectedText)
+        {
+            SDL_SetClipboardText(selectedText);
+        }
+    }
+}
