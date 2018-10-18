b37ec3b67d0810e0c1658357d007891e7545abaa
Dean Herbert <pe@ppy.sh>
2018-10-18 20:08:19 +0900

    Remove #ifdefs for now
    These should be implemented in their own platform classes

--- osu.Framework/Platform/Linux/Sdl/SdlClipboard.cs
+++ osu.Framework/Platform/Linux/Sdl/SdlClipboard.cs
@@ -7,13 +7,7 @@ namespace osu.Framework.Platform.Linux.Sdl
 {
     public class SdlClipboard : Clipboard
     {
-        #if ANDROID
-        const string lib = "libSDL2.so";
-        #elif IPHONE
-        const string lib = "__Internal";
-        #else
         private const string lib = "libSDL2-2.0.so.0";
-        #endif
 
         [DllImport(lib, CallingConvention = CallingConvention.Cdecl, EntryPoint = "SDL_GetClipboardText", ExactSpelling = true)]
         internal static extern string SDL_GetClipboardText();
