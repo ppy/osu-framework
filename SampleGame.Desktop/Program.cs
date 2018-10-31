907a4859b662883daa97de1780f7997415c6690a
Shane Woolcock <samahnub@gmail.com>
2018-10-31 21:21:59 +1030

    Split SampleGame to a library and desktop project

--- SampleGame.Desktop/Program.cs
+++ SampleGame.Desktop/Program.cs
@@ -4,15 +4,16 @@
 using System;
 using osu.Framework.Platform;
 using osu.Framework;
+using SampleGame;
 
-namespace SampleGame
+namespace SampleGame.Desktop
 {
     public static class Program
     {
         [STAThread]
         public static void Main()
         {
-            using (Game game = new SampleGame())
+            using (Game game = new SampleGameGame())
             using (GameHost host = Host.GetSuitableHost(@"sample-game"))
                 host.Run(game);
         }
