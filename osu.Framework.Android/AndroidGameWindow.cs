a4b63a6eaf625d2f74b9e04609ce9a62eae9230e
Shane Woolcock <samahnub@gmail.com>
2018-11-09 23:00:54 +1030

    Add Android projects

---
+++ osu.Framework.Android/AndroidGameWindow.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
+
+using System;
+using osu.Framework.Configuration;
+using osu.Framework.Platform;
+using osuTK.Graphics;
+
+namespace osu.Framework.Android
+{
+    public class AndroidGameWindow : GameWindow
+    {
+        public static AndroidGameView GameView;
+
+        public AndroidGameWindow() : base(null)//base(GameView)
+        {
+        }
+
+        public override void SetupWindow(FrameworkConfigManager config)
+        {
+            //throw new NotImplementedException();
+        }
+
+
+        public override IGraphicsContext Context => throw new NotImplementedException();// GameView.GraphicsContext;
+    }
+}
