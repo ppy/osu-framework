57a5d6571b7b08dc914198debd989751e3cac1b1
Dean Herbert <pe@ppy.sh>
2018-12-03 16:42:24 +0900

    Simplify function

--- osu.Framework/Lists/SortedList.cs
+++ osu.Framework/Lists/SortedList.cs
@@ -109,11 +109,7 @@ namespace osu.Framework.Lists
 
         public int BinarySearch(T value) => list.BinarySearch(value, Comparer);
 
-        public int IndexOf(T value)
-        {
-            int index = list.BinarySearch(value, Comparer);
-            return index;
-        }
+        public int IndexOf(T value) =>list.BinarySearch(value, Comparer);
 
         public void CopyTo(T[] array, int arrayIndex) => list.CopyTo(array, arrayIndex);
 
