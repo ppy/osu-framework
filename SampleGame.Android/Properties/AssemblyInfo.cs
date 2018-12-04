65eebd2ec496eaee9e5ec62d7eea57aa11f2f37a
Shane Woolcock <samahnub@gmail.com>
2018-12-04 22:22:57 +1030

    Clean up Android projects

--- SampleGame.Android/Properties/AssemblyInfo.cs
+++ SampleGame.Android/Properties/AssemblyInfo.cs
@@ -1,8 +1,11 @@
-﻿using System.Reflection;
+﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using Android.App;
 
-// Information about this assembly is defined by the following attributes. 
+// Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
 [assembly: AssemblyTitle("SampleGame.Android")]
@@ -20,7 +23,7 @@ using Android.App;
 
 [assembly: AssemblyVersion("1.0.0")]
 
-// The following attributes are used to specify the signing key for the assembly, 
+// The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
 
 //[assembly: AssemblyDelaySign(false)]
