3225551efc386d3de90bfeec635e98b09afdee5e
tangalbert919 <tangalbert919@gmail.com>
2018-12-10 14:42:46 -0600

    Revert "Revert Samah's changes to Android"
    This reverts commit 06475d333a1112bc92538d510458ade26213465f, as this did not fix the problem.

--- osu.Framework.Android/AndroidStorage.cs
+++ osu.Framework.Android/AndroidStorage.cs
@@ -1,7 +1,4 @@
-﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
-// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
