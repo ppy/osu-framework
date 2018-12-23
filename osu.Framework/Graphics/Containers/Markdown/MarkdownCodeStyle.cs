2147b9f8ccfa9ef579691f637a2ccc5150add472
andy840119 <andy840119@gmail.com>
2018-12-24 01:47:46 +0900

    makes codefactor happy

--- osu.Framework/Graphics/Containers/Markdown/MarkdownCodeStyle.cs
+++ osu.Framework/Graphics/Containers/Markdown/MarkdownCodeStyle.cs
@@ -14,40 +14,40 @@ namespace osu.Framework.Graphics.Containers.Markdown
             return item.ScopeName;
         }
 
-        protected virtual Color4 Blue => new Color4(0,0,255,255); //"#0000FF"
-        protected virtual Color4 White => new Color4(255,255,255,255); //"#FFFFFF"
-        protected virtual Color4 Black => new Color4(0,0,0,255); //"#000000"
-        protected virtual Color4 DullRed => new Color4(163, 21, 21, 255);//"#A31515"
-        protected virtual Color4 Yellow => new Color4(255, 255, 0,255);//"#FFFF00"
-        protected virtual Color4 Green => new Color4(0, 128, 0,255);//"#008000"
-        protected virtual Color4 PowderBlue => new Color4(176, 224, 230,255);//"#B0E0E6"
-        protected virtual Color4 Teal => new Color4(0, 128, 12,255);//"#008080"
-        protected virtual Color4 Gray => new Color4(128, 128, 128,255);//"#808080"
-        protected virtual Color4 Navy => new Color4(0, 0, 128,255);//"#000080"
-        protected virtual Color4 OrangeRed => new Color4(255, 69, 0,255);//"#FF4500"
-        protected virtual Color4 Purple => new Color4(128, 0, 128,255);//"#800080"
-        protected virtual Color4 Red => new Color4(255, 0, 0,255);//"#FF0000"
-        protected virtual Color4 MediumTurqoise => new Color4(72, 209, 204,255);//"48D1CC"
-        protected virtual Color4 Magenta => new Color4(255, 0, 255,255);//"FF00FF"
-        protected virtual Color4 OliveDrab =>new Color4(107, 142, 35,255);//"#6B8E23"
-        protected virtual Color4 DarkOliveGreen => new Color4(85, 107, 47,255);//"#556B2F"
-        protected virtual Color4 DarkCyan => new Color4(0, 139, 139,255);//"#008B8B"
+        protected virtual Color4 Blue => new Color4(0,0,255,255);
+        protected virtual Color4 White => new Color4(255,255,255,255);
+        protected virtual Color4 Black => new Color4(0,0,0,255);
+        protected virtual Color4 DullRed => new Color4(163, 21, 21, 255);
+        protected virtual Color4 Yellow => new Color4(255, 255, 0,255);
+        protected virtual Color4 Green => new Color4(0, 128, 0,255);
+        protected virtual Color4 PowderBlue => new Color4(176, 224, 230,255);
+        protected virtual Color4 Teal => new Color4(0, 128, 12,255);
+        protected virtual Color4 Gray => new Color4(128, 128, 128,255);
+        protected virtual Color4 Navy => new Color4(0, 0, 128,255);
+        protected virtual Color4 OrangeRed => new Color4(255, 69, 0,255);
+        protected virtual Color4 Purple => new Color4(128, 0, 128,255);
+        protected virtual Color4 Red => new Color4(255, 0, 0,255);
+        protected virtual Color4 MediumTurqoise => new Color4(72, 209, 204,255);
+        protected virtual Color4 Magenta => new Color4(255, 0, 255,255);
+        protected virtual Color4 OliveDrab =>new Color4(107, 142, 35,255);
+        protected virtual Color4 DarkOliveGreen => new Color4(85, 107, 47,255);
+        protected virtual Color4 DarkCyan => new Color4(0, 139, 139,255);
 
-        protected virtual Color4 DarkBackground => new Color4(30, 30, 30,255);//"#1E1E1E"
-        protected virtual Color4 DarkPlainText => new Color4(218, 218, 218,255);//"#DADADA"
+        protected virtual Color4 DarkBackground => new Color4(30, 30, 30,255);
+        protected virtual Color4 DarkPlainText => new Color4(218, 218, 218,255);
 
-        protected virtual Color4 DarkXmlDelimeter => new Color4(128, 128, 128,255);//"#808080"
-        protected virtual Color4 DarkXmlName => new Color4(230, 230, 230,255);//"#E6E6E6"
-        protected virtual Color4 DarkXmlAttribute => new Color4(146, 202, 244,255);//"#92CAF4"
-        protected virtual Color4 DarkXamlCData => new Color4(192, 208, 136,255);//"#C0D088"
-        protected virtual Color4 DarkXmlComment => new Color4(96, 139, 78,255);//"#608B4E"
+        protected virtual Color4 DarkXmlDelimeter => new Color4(128, 128, 128,255);
+        protected virtual Color4 DarkXmlName => new Color4(230, 230, 230,255);
+        protected virtual Color4 DarkXmlAttribute => new Color4(146, 202, 244,255);
+        protected virtual Color4 DarkXamlCData => new Color4(192, 208, 136,255);
+        protected virtual Color4 DarkXmlComment => new Color4(96, 139, 78,255);
 
-        protected virtual Color4 DarkComment => new Color4(87, 166, 74,255);//"#57A64A"
-        protected virtual Color4 DarkKeyword =>new Color4(86, 156, 214,255);//"#569CD6"
-        protected virtual Color4 DarkGray => new Color4(155, 155, 155,255);//"#9B9B9B"
-        protected virtual Color4 DarkNumber => new Color4(181, 206, 168,255);//"#B5CEA8"
-        protected virtual Color4 DarkClass =>new Color4(78, 201, 176,255);//"#4EC9B0"
-        protected virtual Color4 DarkString =>new Color4(214, 157, 133,255);//"#D69D85"
+        protected virtual Color4 DarkComment => new Color4(87, 166, 74,255);
+        protected virtual Color4 DarkKeyword =>new Color4(86, 156, 214,255);
+        protected virtual Color4 DarkGray => new Color4(155, 155, 155,255);
+        protected virtual Color4 DarkNumber => new Color4(181, 206, 168,255);
+        protected virtual Color4 DarkClass =>new Color4(78, 201, 176,255);
+        protected virtual Color4 DarkString =>new Color4(214, 157, 133,255);
 
         public virtual MarkdownCodeStyle CreateDefaultStyle()
         {
