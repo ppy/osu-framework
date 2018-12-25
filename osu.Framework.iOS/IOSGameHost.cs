2c8860f92e5f91628f7b36e6f19f7b98a6e7bc22
Dean Herbert <pe@ppy.sh>
2018-12-25 15:14:45 +0900

    Use UIImage for iOS image loading

--- osu.Framework.iOS/IOSGameHost.cs
+++ osu.Framework.iOS/IOSGameHost.cs
@@ -4,6 +4,7 @@
 using System;
 using System.Collections.Generic;
 using osu.Framework.Audio;
+using osu.Framework.Graphics.Textures;
 using osu.Framework.Input;
 using osu.Framework.Input.Handlers;
 using osu.Framework.IO.Stores;
@@ -41,6 +42,9 @@ namespace osu.Framework.iOS
         public override void OpenFileExternally(string filename) => throw new NotImplementedException();
 
         public override void OpenUrlExternally(string url) => throw new NotImplementedException();
+
+        public override IResourceStore<TextureUpload> CreateTextureLoaderStore(IResourceStore underlyingStore)
+            => new IOSTextureLoaderStore(underlyingStore);
 
         public override AudioManager CreateAudioManager(ResourceStore<byte[]> trackStore, ResourceStore<byte[]> sampleStore, Scheduler eventScheduler) =>
             new IOSAudioManager(trackStore, sampleStore) { EventScheduler = eventScheduler };
