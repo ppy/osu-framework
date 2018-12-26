eccfb2e04e1eeaf425c7085c814cecb0c9354d6d
tangalbert919 <tangalbert919@gmail.com>
2018-12-26 11:57:44 -0600

    Add osu.Framework.Tests.Android

---
+++ osu.Framework.Tests.Android/MainActivity.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
+
+using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace osu.Framework.Tests.Android
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+        }
+    }
+}
---
+++ osu.Framework.Tests.Android/TestGameView.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using osu.Framework.Android;
+
+namespace osu.Framework.Tests.Android
+{
+    public class TestGameView : AndroidGameView
+    {
+        public override Game CreateGame() => new VisualTestGame();
+    }
+}
