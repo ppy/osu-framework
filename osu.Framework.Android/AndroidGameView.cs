d2ea6c56960ef4d04d822b0cfaa4e8824fbac693
miterosan <miterosan@gmail.com>
2019-01-05 19:10:21 +0100

    Implement keyboard.

--- osu.Framework.Android/AndroidGameView.cs
+++ osu.Framework.Android/AndroidGameView.cs
@@ -5,6 +5,7 @@ using System;
 using Android.Content;
 using Android.Runtime;
 using Android.Util;
+using Android.Views;
 using osuTK.Graphics;
 
 namespace osu.Framework.Android
@@ -14,9 +15,14 @@ namespace osu.Framework.Android
         private AndroidGameHost host;
         private readonly Game game;
 
+        public event Action<Keycode> KeyDown;
+        public event Action<Keycode> KeyUp;
+        public event Action<Keycode> KeyLongPress;
+
         public AndroidGameView(Context context, Game game) : base(context)
         {
             this.game = game;
+
             init();
         }
 
@@ -34,6 +40,11 @@ namespace osu.Framework.Android
         {
             AutoSetContextOnRenderFrame = true;
             ContextRenderingApi = GLVersion.ES3;
+
+            // enable soft and hardware keyboard
+            // this needs to happen in the constructor
+            Focusable = true;
+            FocusableInTouchMode = true;
         }
 
         protected override void CreateFrameBuffer()
@@ -48,6 +59,24 @@ namespace osu.Framework.Android
                 Log.Verbose("AndroidGameView", "{0}", e);
                 throw new Exception("Can't load egl, aborting", e);
             }
+        }
+
+        public override bool OnKeyDown([GeneratedEnum] Keycode keyCode, KeyEvent e)
+        {
+            KeyDown?.Invoke(keyCode);
+            return true;
+        }
+
+        public override bool OnKeyLongPress([GeneratedEnum] Keycode keyCode, KeyEvent e)
+        {
+            KeyLongPress?.Invoke(keyCode);
+            return true;
+        }
+
+        public override bool OnKeyUp([GeneratedEnum] Keycode keyCode, KeyEvent e)
+        {
+            KeyUp?.Invoke(keyCode);
+            return true;
         }
 
         protected override void OnLoad(EventArgs e)
