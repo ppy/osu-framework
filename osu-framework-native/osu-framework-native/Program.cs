ac6d4db5b38d1f8a94559219f801f60f3f882461
David Zhao <nyquill@ppy.sh>
2019-01-07 14:05:41 +0900

    Fix resharper errors

--- osu-framework-native/osu-framework-native/Program.cs
+++ osu-framework-native/osu-framework-native/Program.cs
@@ -1,11 +1,5 @@
 ﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
 
 namespace osu_framework_native
 {
@@ -13,14 +7,7 @@ namespace osu_framework_native
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
-
-            host.Run();
+            return;
         }
     }
 }
