39efa13cb4c00adff6b65842f8f57113c5bca9d5
WebFreak001 <gh@webfreak.org>
2019-02-06 19:50:59 +0100

    Add barycentric polynomial interpolation calculation Includes PathApproximator implementation and visual testcase for them

--- osu.Framework.Tests/MathUtils/TestInterpolation.cs
+++ osu.Framework.Tests/MathUtils/TestInterpolation.cs
@@ -2,7 +2,6 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
-using System.Collections.Generic;
 using NUnit.Framework;
 using osu.Framework.MathUtils;
 using osuTK;
@@ -24,27 +23,29 @@ namespace osu.Framework.Tests.MathUtils
         public void TestLagrange()
         {
             // lagrange of (0,0) (0.5,0.35) (1,1) is equal to 0.6x*x + 0.4x
-            Vector2d[] points = { new Vector2d(0, 0), new Vector2d(0.5, 0.35), new Vector2d(1, 1) };
-            Vector2[] pointsf = { new Vector2(0, 0), new Vector2(0.5f, 0.35f), new Vector2(1, 1) };
+            Vector2[] points = { new Vector2(0, 0), new Vector2(0.5f, 0.35f), new Vector2(1, 1) };
 
             for (double t = -10; t <= 10; t += 0.01)
-            {
-                Assert.AreEqual(0.6 * t * t + 0.4 * t, Interpolation.Lagrange(points, t), 1e-6);
-                Assert.AreEqual(0.6 * t * t + 0.4 * t, Interpolation.Lagrange(pointsf, t), 1e-4);
-            }
+                Assert.AreEqual(0.6 * t * t + 0.4 * t, Interpolation.Lagrange(points, t), 1e-4);
 
             Assert.Throws<ArgumentException>(() =>
             {
-                Interpolation.Lagrange((List<Vector2>)null, 4);
+                Interpolation.Lagrange(null, 4);
             });
 
             Assert.Throws<ArgumentException>(() =>
             {
-                List<Vector2> list = new List<Vector2>();
+                ReadOnlySpan<Vector2> list = new Vector2[0];
                 Interpolation.Lagrange(list, 4);
             });
 
             Assert.AreEqual(Interpolation.Lagrange(new[] { new Vector2(3, 4) }, 12), 4);
+
+            double[] weights = Interpolation.BarycentricWeights(points);
+            Assert.AreEqual(3, weights.Length);
+            Assert.AreEqual(2, weights[0]);
+            Assert.AreEqual(-4, weights[1]);
+            Assert.AreEqual(2, weights[2]);
         }
     }
 }
--- osu.Framework/MathUtils/Interpolation.cs
+++ osu.Framework/MathUtils/Interpolation.cs
@@ -9,7 +9,6 @@ using osuTK.Graphics;
 using osu.Framework.Graphics.Colour;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Primitives;
-using System.Collections.Generic;
 
 namespace osu.Framework.MathUtils
 {
@@ -40,13 +39,13 @@ namespace osu.Framework.MathUtils
         /// </summary>
         /// <param name="points">An array of coordinates. No two x should be the same.</param>
         /// <param name="time">The x coordinate to calculate the y coordinate for.</param>
-        public static double Lagrange(IList<Vector2> points, double time)
+        public static double Lagrange(ReadOnlySpan<Vector2> points, double time)
         {
-            if (points == null || points.Count == 0)
+            if (points == null || points.Length == 0)
                 throw new ArgumentException($"{nameof(points)} must contain at least one point");
 
             double sum = 0;
-            for (int i = 0; i < points.Count; i++)
+            for (int i = 0; i < points.Length; i++)
                 sum += points[i].Y * LagrangeBasis(points, i, time);
             return sum;
         }
@@ -57,44 +56,62 @@ namespace osu.Framework.MathUtils
         /// <param name="points">An array of coordinates. No two x should be the same.</param>
         /// <param name="base">The index inside the coordinate array which polynomial to compute.</param>
         /// <param name="time">The x coordinate to calculate the basis polynomial for.</param>
-        public static double LagrangeBasis(IList<Vector2> points, int @base, double time)
+        public static double LagrangeBasis(ReadOnlySpan<Vector2> points, int @base, double time)
         {
             double product = 1;
-            for (int i = 0; i < points.Count; i++)
+            for (int i = 0; i < points.Length; i++)
                 if (i != @base)
                     product *= (time - points[i].X) / (points[@base].X - points[i].X);
             return product;
         }
 
         /// <summary>
-        /// Interpolates between a set of points using a lagrange polynomial.
+        /// Calculates the Barycentric weights for a Lagrange polynomial for a given set of coordinates. Can be used as a helper function to compute a Lagrange polynomial repeatedly.
         /// </summary>
         /// <param name="points">An array of coordinates. No two x should be the same.</param>
-        /// <param name="time">The x coordinate to calculate the y coordinate for.</param>
-        public static double Lagrange(IList<Vector2d> points, double time)
+        public static double[] BarycentricWeights(ReadOnlySpan<Vector2> points)
         {
-            if (points == null || points.Count == 0)
-                throw new ArgumentException($"{nameof(points)} must contain at least one point");
-
-            double sum = 0;
-            for (int i = 0; i < points.Count; i++)
-                sum += points[i].Y * LagrangeBasis(points, i, time);
-            return sum;
+            int n = points.Length;
+            double[] w = new double[n];
+            for (int i = 0; i < n; i++)
+            {
+                w[i] = 1;
+                for (int j = 0; j < n; j++)
+                    if (i != j)
+                        w[i] *= points[i].X - points[j].X;
+                w[i] = 1.0 / w[i];
+            }
+            return w;
         }
 
         /// <summary>
-        /// Calculates the Lagrange basis polynomial for a given set of x coordinates. Used as a helper function to compute Lagrange polynomials.
+        /// Calculates the Lagrange basis polynomial for a given set of x coordinates based on previously computed barycentric weights.
         /// </summary>
         /// <param name="points">An array of coordinates. No two x should be the same.</param>
-        /// <param name="base">The index inside the coordinate array which polynomial to compute.</param>
+        /// <param name="weights">An array of precomputed barycentric weights.</param>
         /// <param name="time">The x coordinate to calculate the basis polynomial for.</param>
-        public static double LagrangeBasis(IList<Vector2d> points, int @base, double time)
+        public static double BarycentricLagrange(ReadOnlySpan<Vector2> points, double[] weights, double time)
         {
-            double product = 1;
-            for (int i = 0; i < points.Count; i++)
-                if (i != @base)
-                    product *= (time - points[i].X) / (points[@base].X - points[i].X);
-            return product;
+            if (points == null || points.Length == 0)
+                throw new ArgumentException($"{nameof(points)} must contain at least one point");
+            if (points.Length != weights.Length)
+                throw new ArgumentException($"{nameof(points)} must contain exactly as many items as {nameof(weights)}");
+
+            double numerator = 0;
+            double denominator = 0;
+
+            for (int i = 0; i < points.Length; i++)
+            {
+                // while this is not great with branch prediction, it prevents NaN at control point X coordinates
+                if (time == points[i].X)
+                    return points[i].Y;
+
+                double li = weights[i] / (time - points[i].X);
+                numerator += li * points[i].Y;
+                denominator += li;
+            }
+
+            return numerator / denominator;
         }
 
         public static ColourInfo ValueAt(double time, ColourInfo startColour, ColourInfo endColour, double startTime, double endTime, Easing easing = Easing.None)
