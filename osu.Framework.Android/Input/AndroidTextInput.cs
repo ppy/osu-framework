465fc942efaa0d0d18c97484fe96b7b2f953fce1
tangalbert919 <tangalbert919@gmail.com>
2019-02-13 11:54:14 -0600

    Add initial shift key support
    Lower-case alphabet characters and symbols on number keys can now be entered.

--- osu.Framework.Android/Input/AndroidTextInput.cs
+++ osu.Framework.Android/Input/AndroidTextInput.cs
@@ -28,17 +28,52 @@ namespace osu.Framework.Android.Input
         {
             Key key = AndroidKeyboardHandler.GetKeyCodeAsKey(arg);
             string keynum = arg.ToString();
+            bool upper = e.IsShiftPressed;
 
             if (keynum.StartsWith(Keycode.Num.ToString()))
             {
-                pending = "" + keynum.Last();
-                return;
+                switch (keynum.Last())
+                {
+                    case '1':
+                        pending = upper ? "!" : "1";
+                        return;
+                    case '2':
+                        pending = upper ? "@" : "2";
+                        return;
+                    case '3':
+                        pending = upper ? "#" : "3";
+                        return;
+                    case '4':
+                        pending = upper ? "$" : "4";
+                        return;
+                    case '5':
+                        pending = upper ? "%" : "5";
+                        return;
+                    case '6':
+                        pending = upper ? "^" : "6";
+                        return;
+                    case '7':
+                        pending = upper ? "&" : "7";
+                        return;
+                    case '8':
+                        pending = upper ? "*" : "8";
+                        return;
+                    case '9':
+                        pending = upper ? "(" : "9";
+                        return;
+                    case '0':
+                        pending = upper ? ")" : "0";
+                        return;
+                    default:
+                        pending = "" + keynum.Last();
+                        return;
+                }
             }
 
             switch (arg)
             {
                 default:
-                    pending += e.DisplayLabel;
+                    pending += upper ? e.DisplayLabel.ToString() : e.DisplayLabel.ToString().ToLower();
                     break;
             }
         }
