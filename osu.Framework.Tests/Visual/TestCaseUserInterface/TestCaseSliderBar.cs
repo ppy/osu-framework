d876dcede259247da34a6dfd2297df0a9985f43a
Jamie Taylor <me@nekodex.net>
2019-02-21 16:25:31 +0900

    Fix SliderBar test case

--- osu.Framework.Tests/Visual/TestCaseUserInterface/TestCaseSliderBar.cs
+++ osu.Framework.Tests/Visual/TestCaseUserInterface/TestCaseSliderBar.cs
@@ -7,6 +7,7 @@ using NUnit.Framework;
 using osu.Framework.Configuration;
 using osu.Framework.Graphics.Sprites;
 using osu.Framework.Graphics.UserInterface;
+using osu.Framework.MathUtils;
 using osu.Framework.Testing;
 using osuTK;
 using osuTK.Graphics;
@@ -14,16 +15,16 @@ using osuTK.Input;
 
 namespace osu.Framework.Tests.Visual.TestCaseUserInterface
 {
-    public class TestCaseSliderbar : ManualInputManagerTestCase
+    public class TestCaseSliderBar : ManualInputManagerTestCase
     {
         public override IReadOnlyList<Type> RequiredTypes => new[] { typeof(BasicSliderBar<>), typeof(SliderBar<>) };
 
         // ReSharper disable once PrivateFieldCanBeConvertedToLocalVariable
         private readonly BindableDouble sliderBarValue; //keep a reference to avoid GC of the bindable
-        private readonly SpriteText sliderbarText;
+        private readonly SpriteText sliderBarText;
         private readonly SliderBar<double> sliderBar;
 
-        public TestCaseSliderbar()
+        public TestCaseSliderBar()
         {
             sliderBarValue = new BindableDouble(8)
             {
@@ -32,7 +33,7 @@ namespace osu.Framework.Tests.Visual.TestCaseUserInterface
             };
             sliderBarValue.ValueChanged += sliderBarValueChanged;
 
-            sliderbarText = new SpriteText
+            sliderBarText = new SpriteText
             {
                 Text = $"Selected value: {sliderBarValue.Value}",
                 Position = new Vector2(25, 0)
@@ -49,7 +50,7 @@ namespace osu.Framework.Tests.Visual.TestCaseUserInterface
             };
 
             Add(sliderBar);
-            Add(sliderbarText);
+            Add(sliderBarText);
 
             Add(sliderBar = new BasicSliderBar<double>
             {
@@ -76,16 +77,18 @@ namespace osu.Framework.Tests.Visual.TestCaseUserInterface
         }
 
         [Test]
-        public void Basic() {
-            AddSliderStep("Value", -10.0, 10.0, -10.0, v => sliderBarValue.Value = v);
+        public void Basic()
+        {
+            AddSliderStep("Value", sliderBarValue.MinValue, sliderBarValue.MaxValue, 0, v => sliderBarValue.Value = v);
 
-            AddStep("Click at x = 50", () =>
+            AddStep("Click at 25% mark", () =>
             {
-                InputManager.MoveMouseTo(sliderBar.ToScreenSpace(sliderBar.DrawSize / 4));
+                InputManager.MoveMouseTo(sliderBar.ToScreenSpace(new Vector2(sliderBar.DrawWidth * 0.25f, sliderBar.DrawHeight * 0.5f)));
                 InputManager.Click(MouseButton.Left);
             });
 
-            AddAssert("Value == -6,25", () => sliderBarValue == -6.25);
+            // We're translating to/from screen-space coordinates for click coordinates so we want to be more lenient with the value comparisons in this test
+            AddAssert("Value == -6.25", () => Precision.AlmostEquals(sliderBarValue, -6.25, Precision.FLOAT_EPSILON));
 
             AddStep("Press left arrow key", () =>
             {
@@ -96,24 +99,22 @@ namespace osu.Framework.Tests.Visual.TestCaseUserInterface
                 sliderBar.IsHovered = before;
             });
 
-            AddAssert("Value == -7,25", () => sliderBarValue == -7.25);
+            AddAssert("Value == -7.25", () => Precision.AlmostEquals(sliderBarValue, -7.25, Precision.FLOAT_EPSILON));
 
-            AddStep("Click at x = 150 with shift", () =>
+            AddStep("Click at 75% mark, holding shift", () =>
             {
-                var drawSize = sliderBar.DrawSize;
-                drawSize.X *= 0.75f;
                 InputManager.PressKey(Key.LShift);
-                InputManager.MoveMouseTo(sliderBar.ToScreenSpace(drawSize));
+                InputManager.MoveMouseTo(sliderBar.ToScreenSpace(new Vector2(sliderBar.DrawWidth * 0.75f, sliderBar.DrawHeight * 0.5f)));
                 InputManager.Click(MouseButton.Left);
                 InputManager.ReleaseKey(Key.LShift);
             });
 
-            AddAssert("Value == 6", () => sliderBarValue == 6);
+            AddAssert("Value == 6", () => Precision.AlmostEquals(sliderBarValue, 6, Precision.FLOAT_EPSILON));
         }
 
         private void sliderBarValueChanged(double newValue)
         {
-            sliderbarText.Text = $"Selected value: {newValue:N}";
+            sliderBarText.Text = $"Selected value: {newValue:N}";
         }
     }
 }
