7152cd5ebbab365ca05070e2570d62bca16424c0
Dean Herbert <pe@ppy.sh>
2019-03-14 14:40:46 +0900

    Add tests for second bad actor scenario

--- osu.Framework.Tests/Visual/Bindable/TestCaseBindableAutoUnbinding.cs
+++ osu.Framework.Tests/Visual/Bindable/TestCaseBindableAutoUnbinding.cs
@@ -1,6 +1,8 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using NUnit.Framework;
+using osu.Framework.Allocation;
 using osu.Framework.Bindables;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
@@ -14,20 +16,13 @@ namespace osu.Framework.Tests.Visual.Bindable
 {
     public class TestCaseBindableAutoUnbinding : TestCase
     {
-        public TestCaseBindableAutoUnbinding()
+        [Test]
+        public void TestCaseBindableAutoUnbindingAssign()
         {
-            var drawable1 = new TestExposedBindableDrawable();
-            var drawable2 = new TestExposedBindableDrawable();
-            var drawable3 = new TestExposedBindableDrawable(true);
-            var drawable4 = new TestExposedBindableDrawable();
+            TestExposedBindableDrawable drawable1 = null, drawable2 = null, drawable3 = null, drawable4 = null;
 
             AddStep("add drawables", () =>
             {
-                drawable1.Bindable = new Bindable<int>();
-                drawable2.Bindable = drawable1.Bindable.GetBoundCopy();
-                drawable3.Bindable = drawable1.Bindable; // an example of a bad usage of bindables
-                drawable4.Bindable = drawable1.Bindable.GetBoundCopy();
-
                 Child = new FillFlowContainer
                 {
                     RelativeSizeAxes = Axes.Both,
@@ -35,10 +30,10 @@ namespace osu.Framework.Tests.Visual.Bindable
                     Spacing = new Vector2(5),
                     Children = new Drawable[]
                     {
-                        drawable1,
-                        drawable2,
-                        drawable3,
-                        drawable4,
+                        drawable1 = new TestExposedBindableDrawable { Bindable = new Bindable<int>() },
+                        drawable2 = new TestExposedBindableDrawable { Bindable = drawable1.Bindable.GetBoundCopy() },
+                        drawable3 = new TestExposedBindableDrawable(true) { Bindable = drawable1.Bindable }, // an example of a bad usage of bindables
+                        drawable4 = new TestExposedBindableDrawable { Bindable = drawable1.Bindable.GetBoundCopy() },
                     }
                 };
             });
@@ -63,6 +58,80 @@ namespace osu.Framework.Tests.Visual.Bindable
 
             // fails due to drawable3 being expired/disposed with a direct reference to drawable1's bindable.
             AddAssert("transfer 1-2 fails", () => drawable1.Bindable.Value != drawable2.Bindable.Value);
+        }
+
+        [Test]
+        public void TestCaseBindableAutoUnbindingResolution()
+        {
+            TestResolvedBindableDrawable drawable1 = null, drawable2 = null, drawable3 = null, drawable4 = null;
+
+            AddStep("add drawables", () =>
+            {
+                Child = new BindableExposingFillFlowContainer
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    Direction = FillDirection.Vertical,
+                    Spacing = new Vector2(5),
+                    Children = new Drawable[]
+                    {
+                        drawable1 = new TestResolvedBindableDrawable(),
+                        drawable2 = new TestResolvedBindableDrawable(),
+                        drawable3 = new TestResolvedBindableDrawable(true), // an example of a bad usage of bindables
+                        drawable4 = new TestResolvedBindableDrawable(),
+                    }
+                };
+            });
+
+            AddStep("attempt value transfer", () => drawable1.Bindable.Value = 10);
+
+            AddAssert("transfer 1-2 completed", () => drawable1.Bindable.Value == drawable2.Bindable.Value);
+            AddAssert("transfer 1-3 completed", () => drawable1.Bindable.Value == drawable3.Bindable.Value);
+            AddAssert("transfer 1-4 completed", () => drawable1.Bindable.Value == drawable4.Bindable.Value);
+
+            AddStep("expire child 4", () => drawable4.Expire());
+
+            AddStep("attempt value transfer", () => drawable1.Bindable.Value = 20);
+
+            AddAssert("transfer 1-2 completed", () => drawable1.Bindable.Value == drawable2.Bindable.Value);
+            AddAssert("transfer 1-3 completed", () => drawable1.Bindable.Value == drawable3.Bindable.Value);
+            AddAssert("transfer 1-4 skipped", () => drawable1.Bindable.Value != drawable4.Bindable.Value);
+
+            AddStep("expire child 3", () => drawable3.Expire());
+
+            AddStep("attempt value transfer", () => drawable1.Bindable.Value = 10);
+
+            // fails due to drawable3 being expired/disposed with a direct reference to drawable1's bindable.
+            AddAssert("transfer 1-2 fails", () => drawable1.Bindable.Value != drawable2.Bindable.Value);
+        }
+
+        public class BindableExposingFillFlowContainer : FillFlowContainer
+        {
+            [Cached]
+            private Bindable<int> bindable = new Bindable<int>();
+        }
+
+        public class TestResolvedBindableDrawable : TestExposedBindableDrawable
+        {
+            private readonly bool badActor;
+
+            public TestResolvedBindableDrawable(bool badActor = false)
+                : base(badActor)
+            {
+                this.badActor = badActor;
+            }
+
+
+            [BackgroundDependencyLoader]
+            private void load(Bindable<int> parentBindable)
+            {
+                if (badActor)
+                    Bindable = parentBindable;
+                else
+                {
+                    Bindable = new Bindable<int>();
+                    Bindable.BindTo(parentBindable);
+                }
+            }
         }
 
         public class TestExposedBindableDrawable : CompositeDrawable
