b423a21e1b01ebcc8ac0710c7236840a60a4edd4
smoogipoo <smoogipoo@smgi.me>
2019-04-02 16:23:44 +0900

    Make BufferedContainer derive BufferedDrawNode

--- osu.Framework/Graphics/Containers/BufferedContainer.cs
+++ osu.Framework/Graphics/Containers/BufferedContainer.cs
@@ -35,7 +35,7 @@ namespace osu.Framework.Graphics.Containers
     /// appearance of the container at the cost of performance. Such effects include
     /// uniform fading of children, blur, and other post-processing effects.
     /// </summary>
-    public partial class BufferedContainer<T> : Container<T>, IBufferedContainer
+    public partial class BufferedContainer<T> : Container<T>, IBufferedContainer, IBufferedDrawable
         where T : Drawable
     {
         private bool drawOriginal;
@@ -108,14 +108,8 @@ namespace osu.Framework.Graphics.Containers
             get => pixelSnapping;
             set
             {
-                if (sharedData != null)
-                {
-                    for (int i = 0; i < sharedData.FrameBuffers.Length; i++)
-                    {
-                        if (sharedData.FrameBuffers[i].IsInitialized)
-                            throw new InvalidOperationException("May only set PixelSnapping before FrameBuffers are initialized (i.e. before the first draw).");
-                    }
-                }
+                if (sharedData?.GetMainBuffer().IsInitialized == true)
+                    throw new InvalidOperationException("May only set PixelSnapping before FrameBuffers are initialized (i.e. before the first draw).");
 
                 pixelSnapping = value;
             }
@@ -222,6 +216,10 @@ namespace osu.Framework.Graphics.Containers
 
         protected override bool CanBeFlattened => false;
 
+        public IShader TextureShader { get; private set; }
+
+        public IShader RoundedTextureShader { get; private set; }
+
         private IShader blurShader;
 
         /// <summary>
@@ -237,12 +235,14 @@ namespace osu.Framework.Graphics.Containers
         [BackgroundDependencyLoader]
         private void load(ShaderManager shaders)
         {
+            TextureShader = shaders.Load(VertexShaderDescriptor.TEXTURE_2, FragmentShaderDescriptor.TEXTURE);
+            RoundedTextureShader = shaders.Load(VertexShaderDescriptor.TEXTURE_2, FragmentShaderDescriptor.TEXTURE_ROUNDED);
             blurShader = shaders.Load(VertexShaderDescriptor.TEXTURE_2, FragmentShaderDescriptor.BLUR);
         }
 
         private readonly BufferedContainerDrawNodeSharedData sharedData = new BufferedContainerDrawNodeSharedData();
 
-        protected override DrawNode CreateDrawNode() => new BufferedContainerDrawNode(this, sharedData);
+        protected override DrawNode CreateDrawNode() => new BufferedContainerDrawNode(this, sharedData, attachedFormats.ToArray(), PixelSnapping);
 
         private readonly List<RenderbufferInternalFormat> attachedFormats = new List<RenderbufferInternalFormat>();
 
