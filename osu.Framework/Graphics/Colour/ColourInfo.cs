b5e79d57493b79be1c5ce3c48aade30b5b466ac2
smoogipoo <smoogipoo@smgi.me>
2019-04-03 20:27:44 +0900

    Add MinAlpha

--- osu.Framework/Graphics/Colour/ColourInfo.cs
+++ osu.Framework/Graphics/Colour/ColourInfo.cs
@@ -198,6 +198,22 @@ namespace osu.Framework.Graphics.Colour
             }
         }
 
+        /// <summary>
+        /// The minimum alpha value of all four corners.
+        /// </summary>
+        public float MinAlpha
+        {
+            get
+            {
+                float min = TopLeft.Linear.A;
+                if (TopRight.Linear.A < min) min = TopRight.Linear.A;
+                if (BottomLeft.Linear.A < min) min = BottomLeft.Linear.A;
+                if (BottomRight.Linear.A < min) min = BottomRight.Linear.A;
+
+                return min;
+            }
+        }
+
         public override string ToString() => HasSingleColour ? $@"{TopLeft} (Single)" : $@"{TopLeft}, {TopRight}, {BottomLeft}, {BottomRight}";
 
         public static implicit operator ColourInfo(SRGBColour colour) => SingleColour(colour);
