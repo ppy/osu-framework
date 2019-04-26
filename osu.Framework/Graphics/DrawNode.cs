f6a5b53d20fb078ed493a4486343babfdc1dc6fc
smoogipoo <smoogipoo@smgi.me>
2019-04-26 18:53:18 +0900

    Don't dispose drawnodes while being recorded

--- osu.Framework/Graphics/DrawNode.cs
+++ osu.Framework/Graphics/DrawNode.cs
@@ -4,6 +4,8 @@
 using osu.Framework.Graphics.OpenGL;
 using System;
 using osu.Framework.Graphics.OpenGL.Vertices;
+using osu.Framework.Logging;
+using osu.Framework.Threading;
 
 namespace osu.Framework.Graphics
 {
@@ -35,6 +37,8 @@ namespace osu.Framework.Graphics
         /// </summary>
         protected readonly IDrawable Source;
 
+        private readonly AtomicCounter referenceCount = new AtomicCounter();
+
         /// <summary>
         /// Creates a new <see cref="DrawNode"/>.
         /// </summary>
@@ -42,6 +46,8 @@ namespace osu.Framework.Graphics
         public DrawNode(IDrawable source)
         {
             Source = source;
+
+            Reference();
         }
 
         /// <summary>
@@ -66,6 +72,15 @@ namespace osu.Framework.Graphics
             GLWrapper.SetBlend(DrawColourInfo.Blending);
         }
 
+        /// <summary>
+        /// Increments the reference count of this <see cref="DrawNode"/>, blocking <see cref="Dispose"/> until the count reaches 0.
+        /// Invoke <see cref="Dispose"/> to remove the reference.
+        /// </summary>
+        /// <remarks>
+        /// All <see cref="DrawNode"/>s start with a reference count of 1.
+        /// </remarks>
+        internal void Reference() => referenceCount.Increment();
+
         ~DrawNode()
         {
             Dispose(false);
@@ -73,6 +88,9 @@ namespace osu.Framework.Graphics
 
         public void Dispose()
         {
+            if (referenceCount.Decrement() != 0)
+                return;
+
             Dispose(true);
             GC.SuppressFinalize(this);
         }
