d9a59b634d3125fd266db4049487a5cf8b31d6f7
Jamie Taylor <me@nekodex.net>
2019-05-08 13:35:44 +0900

    Add drag handle + remove button logic to BasicRearrangeableList and combine the tests with base class

--- osu.Framework.Tests/Visual/Containers/TestCaseRearrangeableListContainer.cs
+++ osu.Framework.Tests/Visual/Containers/TestCaseRearrangeableListContainer.cs
@@ -1,17 +1,14 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using NUnit.Framework;
 using osu.Framework.Allocation;
-using osu.Framework.Extensions.Color4Extensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
-using osu.Framework.Graphics.Sprites;
-using osu.Framework.Input.Events;
-using osu.Framework.MathUtils;
+using osu.Framework.Graphics.UserInterface;
 using osu.Framework.Testing;
 using osuTK;
 using osuTK.Graphics;
@@ -21,8 +18,13 @@ namespace osu.Framework.Tests.Visual.Containers
 {
     public class TestCaseRearrangeableListContainer : ManualInputManagerTestCase
     {
-        private TestRearrangeableListContainer list, listWithSpacing, listWithVariableSizes, listWithoutHandles, listWithoutHandlesWithSpacing, listWithoutHandlesWithVariableSizes;
+        private TestRearrangeableListContainer list, listWithSpacing, listWithoutHandles, listWithoutHandlesWithSpacing;
         private readonly List<TestRearrangeableListContainer> lists = new List<TestRearrangeableListContainer>();
+
+        public override IReadOnlyList<Type> RequiredTypes => new[]
+        {
+            typeof(BasicRearrangeableListContainer),
+        };
 
         [BackgroundDependencyLoader]
         private void load()
@@ -32,7 +34,7 @@ namespace osu.Framework.Tests.Visual.Containers
             {
                 RelativeSizeAxes = Axes.Both,
                 RowDimensions = new[] { new Dimension(GridSizeMode.Relative, 0.5f) },
-                ColumnDimensions = new[] { new Dimension(GridSizeMode.Relative, 1f / 3f) },
+                ColumnDimensions = new[] { new Dimension(GridSizeMode.Relative, 0.5f) },
                 Content = new[]
                 {
                     new Drawable[]
@@ -66,19 +68,6 @@ namespace osu.Framework.Tests.Visual.Containers
                                 },
                             }
                         },
-                        new Container
-                        {
-                            RelativeSizeAxes = Axes.Both,
-                            Children = new Drawable[]
-                            {
-                                new Box
-                                {
-                                    Colour = new Color4(1f, 1f, 1f, 0.05f),
-                                    RelativeSizeAxes = Axes.Both,
-                                },
-                                listWithVariableSizes = new TestRearrangeableListContainer(),
-                            }
-                        },
                     },
                     new Drawable[]
                     {
@@ -92,7 +81,10 @@ namespace osu.Framework.Tests.Visual.Containers
                                     Colour = new Color4(1f, 1f, 1f, 0.025f),
                                     RelativeSizeAxes = Axes.Both,
                                 },
-                                listWithoutHandles = new TestRearrangeableListContainer(),
+                                listWithoutHandles = new TestRearrangeableListContainer
+                                {
+                                    UseDragHandle = false
+                                },
                             }
                         },
                         new Container
@@ -107,21 +99,9 @@ namespace osu.Framework.Tests.Visual.Containers
                                 },
                                 listWithoutHandlesWithSpacing = new TestRearrangeableListContainer
                                 {
+                                    UseDragHandle = false,
                                     Spacing = new Vector2(20),
                                 },
-                            }
-                        },
-                        new Container
-                        {
-                            RelativeSizeAxes = Axes.Both,
-                            Children = new Drawable[]
-                            {
-                                new Box
-                                {
-                                    Colour = new Color4(1f, 1f, 1f, 0.025f),
-                                    RelativeSizeAxes = Axes.Both,
-                                },
-                                listWithoutHandlesWithVariableSizes = new TestRearrangeableListContainer(),
                             }
                         },
                     },
@@ -132,10 +112,8 @@ namespace osu.Framework.Tests.Visual.Containers
             {
                 list,
                 listWithSpacing,
-                listWithVariableSizes,
                 listWithoutHandles,
                 listWithoutHandlesWithSpacing,
-                listWithoutHandlesWithVariableSizes,
             });
         }
 
@@ -147,18 +125,9 @@ namespace osu.Framework.Tests.Visual.Containers
 
             for (int i = 0; i < 5; i++)
             {
-                var colour = randomColour();
                 foreach (var l in lists)
                 {
-                    var variableHeight = l == listWithVariableSizes || l == listWithoutHandlesWithVariableSizes;
-                    var hideHandles = l == listWithoutHandles || l == listWithoutHandlesWithSpacing || l == listWithoutHandlesWithVariableSizes;
-
-                    l.AddItem(new TestItem
-                    {
-                        VariableHeight = variableHeight,
-                        ShowHandles = !hideHandles,
-                        Colour = colour,
-                    });
+                    l.AddItem(new BasicRearrangeableItem($"Test {i}"));
                 }
             }
         });
@@ -205,135 +174,24 @@ namespace osu.Framework.Tests.Visual.Containers
                 AddAssert("Ensure correct child count", () => l.Count == 4);
                 AddStep("AddItem", () =>
                 {
-                    l.AddItem(new TestItem
-                    {
-                        ShowHandles = l == list || l == listWithSpacing || l == listWithVariableSizes,
-                    });
+                    l.AddItem(new BasicRearrangeableItem($"Test {l.Count + 1}"));
                 });
                 AddAssert("Ensure correct child count", () => l.Count == 5);
             }
         }
 
-        private Color4 randomColour() => new Color4(RNG.NextSingle(1), RNG.NextSingle(1), RNG.NextSingle(1), 1);
-
-        #region TestRearrangeableListContainer
-
-        private class TestRearrangeableListContainer : RearrangeableListContainer<TestItem>
+        private class TestRearrangeableListContainer : BasicRearrangeableListContainer
         {
             public int Count => ListContainer.Count;
 
             public IReadOnlyList<Drawable> Children => ListContainer.Children;
 
-            public float GetLayoutPosition(TestDrawable d) => ListContainer.GetLayoutPosition(d);
+            public float GetLayoutPosition(BasicDrawableRearrangeableListItem d) => ListContainer.GetLayoutPosition(d);
 
             // GetChild returns the children in the order of addition, not in their rearranged order.
-            public TestDrawable GetChild(int index) => (TestDrawable)Children[index];
+            public BasicDrawableRearrangeableListItem GetChild(int index) => (BasicDrawableRearrangeableListItem)Children[index];
 
             public Vector2 GetChildSize(int index) => GetChild(index).DrawSize;
-
-            protected override DrawableRearrangeableListItem CreateDrawable(TestItem item) => new TestDrawable(item);
-
-            public class TestDrawable : DrawableRearrangeableListItem
-            {
-                protected Button RemoveButton;
-
-                protected Drawable DragHandle;
-
-                protected override bool IsDraggableAt(Vector2 screenSpacePos) => !RemoveButton.IsHovered && (!Model.ShowHandles || DragHandle.ReceivePositionalInputAt(screenSpacePos));
-
-                protected override bool OnClick(ClickEvent e)
-                {
-                    if (RemoveButton.IsHovered)
-                    {
-                        OnRequestRemoval();
-
-                        return true;
-                    }
-
-                    return false;
-                }
-
-                public TestDrawable(TestItem item)
-                    : base(item)
-                {
-                    Height = item.VariableHeight ? RNG.NextSingle(10, 50) : 25;
-                    RelativeSizeAxes = Axes.X;
-                    CornerRadius = 5;
-                    Masking = true;
-                }
-
-                [BackgroundDependencyLoader]
-                private void load()
-                {
-                    var items = new Drawable[]
-                    {
-                        new Box
-                        {
-                            RelativeSizeAxes = Axes.Both,
-                            Width = Model.ShowHandles ? 0.9f : 0.95f,
-                            Colour = Model.Colour,
-                        },
-                        RemoveButton = new Button(FontAwesome.Solid.MinusSquare, Color4.DarkRed.Opacity(0.5f))
-                        {
-                            Width = 0.05f,
-                        },
-                    };
-
-                    if (Model.ShowHandles)
-                    {
-                        DragHandle = new Button(FontAwesome.Solid.Bars, Color4.Transparent)
-                        {
-                            Width = 0.05f,
-                            Anchor = Anchor.CentreLeft,
-                            Origin = Anchor.CentreLeft,
-                        };
-
-                        items = items.Prepend(DragHandle).ToArray();
-                    }
-
-                    InternalChild = new FillFlowContainer
-                    {
-                        RelativeSizeAxes = Axes.Both,
-                        Direction = FillDirection.Horizontal,
-                        Children = items,
-                    };
-                }
-
-                protected class Button : Container
-                {
-                    public override bool HandlePositionalInput => true;
-
-                    public Button(IconUsage icon, Color4 colour)
-                    {
-                        RelativeSizeAxes = Axes.Both;
-                        InternalChildren = new Drawable[]
-                        {
-                            new Box
-                            {
-                                RelativeSizeAxes = Axes.Both,
-                                Colour = colour
-                            },
-                            new SpriteIcon
-                            {
-                                RelativeSizeAxes = Axes.Both,
-                                Icon = icon,
-                                Scale = new Vector2(0.5f),
-                                Anchor = Anchor.Centre,
-                                Origin = Anchor.Centre,
-                            }
-                        };
-                    }
-                }
-            }
-        }
-
-        #endregion
-
-        private class TestItem : RearrangeableListItem
-        {
-            public Color4 Colour = new Color4(RNG.NextSingle(1), RNG.NextSingle(1), RNG.NextSingle(1), 1);
-            public bool VariableHeight;
-            public bool ShowHandles = true;
         }
     }
 }
--- osu.Framework/Graphics/UserInterface/BasicRearrangeableListContainer.cs
+++ osu.Framework/Graphics/UserInterface/BasicRearrangeableListContainer.cs
@@ -1,9 +1,12 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Linq;
+using osu.Framework.Allocation;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
 using osu.Framework.Graphics.Sprites;
+using osu.Framework.Input.Events;
 using osuTK;
 using osuTK.Graphics;
 
@@ -11,60 +14,127 @@ namespace osu.Framework.Graphics.UserInterface
 {
     public class BasicRearrangeableListContainer : RearrangeableListContainer<BasicRearrangeableItem>
     {
-        protected override DrawableRearrangeableListItem CreateDrawable(BasicRearrangeableItem item) => new BasicDrawableRearrangeableListItem(item);
+        public bool UseDragHandle = true;
+
+        public bool ShowRemoveButton = true;
+
+        protected override DrawableRearrangeableListItem CreateDrawable(BasicRearrangeableItem item) => new BasicDrawableRearrangeableListItem(item)
+        {
+            UseDragHandle = UseDragHandle,
+            ShowRemoveButton = ShowRemoveButton,
+        };
 
         public class BasicDrawableRearrangeableListItem : DrawableRearrangeableListItem
         {
-            protected readonly Drawable DragHandle;
+            public bool UseDragHandle = true;
 
-            protected override bool IsDraggableAt(Vector2 screenSpacePos) => DragHandle.ReceivePositionalInputAt(screenSpacePos);
+            public bool ShowRemoveButton = true;
+
+            protected override bool IsDraggableAt(Vector2 screenSpacePos) => (!ShowRemoveButton || !removeButton.IsHovered) && (!UseDragHandle || dragHandle.ReceivePositionalInputAt(screenSpacePos));
+
+            private Drawable dragHandle;
+            private Drawable removeButton;
 
             public BasicDrawableRearrangeableListItem(BasicRearrangeableItem item)
                 : base(item)
             {
+            }
+
+            [BackgroundDependencyLoader]
+            private void load()
+            {
+                var contentWidth = 1f;
+
+                if (UseDragHandle)
+                    contentWidth -= 0.05f;
+
+                if (ShowRemoveButton)
+                    contentWidth -= 0.05f;
+
+                var items = new Drawable[]
+                {
+                    new Container
+                    {
+                        Width = contentWidth,
+                        RelativeSizeAxes = Axes.Both,
+                        Anchor = Anchor.CentreLeft,
+                        Origin = Anchor.CentreLeft,
+                        CornerRadius = 2,
+                        Masking = true,
+                        Children = new Drawable[]
+                        {
+                            new Box
+                            {
+                                Anchor = Anchor.CentreLeft,
+                                Origin = Anchor.CentreLeft,
+                                RelativeSizeAxes = Axes.Both,
+                                Colour = Color4.DarkSlateGray,
+                            },
+                            new SpriteText
+                            {
+                                Anchor = Anchor.CentreLeft,
+                                Origin = Anchor.CentreLeft,
+
+                                AllowMultiline = false,
+                                Padding = new MarginPadding(5),
+                                Text = Model.Text,
+                            }
+                        },
+                    },
+                };
+
+                if (UseDragHandle)
+                {
+                    dragHandle = new Button(FontAwesome.Solid.Bars)
+                    {
+                        Width = 0.05f,
+                        Anchor = Anchor.CentreLeft,
+                        Origin = Anchor.CentreLeft,
+                    };
+
+                    items = items.Prepend(dragHandle).ToArray();
+                }
+
+                if (ShowRemoveButton)
+                {
+                    var drawable = new Container
+                    {
+                        RelativeSizeAxes = Axes.Both,
+                        Anchor = Anchor.CentreLeft,
+                        Origin = Anchor.CentreLeft,
+                        CornerRadius = 2,
+                        Masking = true,
+                        Width = 0.05f,
+                        Child = removeButton = new Button(FontAwesome.Solid.Times)
+                        {
+                            Colour = Color4.DarkRed,
+                            Anchor = Anchor.CentreLeft,
+                            Origin = Anchor.CentreLeft,
+                        },
+                    };
+
+                    items = items.Append(drawable).ToArray();
+                }
+
                 Height = 25;
                 RelativeSizeAxes = Axes.X;
                 InternalChild = new FillFlowContainer
                 {
                     RelativeSizeAxes = Axes.Both,
-                    Children = new[]
-                    {
-                        DragHandle = new Button(FontAwesome.Solid.Bars)
-                        {
-                            Width = 0.05f,
-                            Anchor = Anchor.CentreLeft,
-                            Origin = Anchor.CentreLeft,
-                        },
-                        new Container
-                        {
-                            Width = 0.95f,
-                            RelativeSizeAxes = Axes.Both,
-                            Anchor = Anchor.CentreLeft,
-                            Origin = Anchor.CentreLeft,
-                            CornerRadius = 2,
-                            Masking = true,
-                            Children = new Drawable[]
-                            {
-                                new Box
-                                {
-                                    Anchor = Anchor.CentreLeft,
-                                    Origin = Anchor.CentreLeft,
-                                    RelativeSizeAxes = Axes.Both,
-                                    Colour = Color4.DarkSlateGray,
-                                },
-                                new SpriteText
-                                {
-                                    Anchor = Anchor.CentreLeft,
-                                    Origin = Anchor.CentreLeft,
-
-                                    AllowMultiline = false,
-                                    Padding = new MarginPadding(5),
-                                    Text = item.Text,
-                                },
-                            }
-                        },
-                    }
+                    Children = items,
                 };
+            }
+
+            protected override bool OnClick(ClickEvent e)
+            {
+                if (removeButton.IsHovered)
+                {
+                    OnRequestRemoval();
+
+                    return true;
+                }
+
+                return false;
             }
 
             protected class Button : Container
