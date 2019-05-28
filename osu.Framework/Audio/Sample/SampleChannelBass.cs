93cf290425aee3da2063ca8494c6bd7f9e7d6db6
smoogipoo <smoogipoo@smgi.me>
2019-05-28 19:12:25 +0900

    Remove interface cast

--- osu.Framework/Audio/Sample/SampleChannelBass.cs
+++ osu.Framework/Audio/Sample/SampleChannelBass.cs
@@ -35,9 +35,9 @@ namespace osu.Framework.Audio.Sample
 
             if (channel != 0)
             {
-                Bass.ChannelSetAttribute(channel, ChannelAttribute.Volume, ((IAggregateAudioAdjustment)this).AggregateVolume.Value);
-                Bass.ChannelSetAttribute(channel, ChannelAttribute.Pan, ((IAggregateAudioAdjustment)this).AggregateBalance.Value);
-                Bass.ChannelSetAttribute(channel, ChannelAttribute.Frequency, initialFrequency * ((IAggregateAudioAdjustment)this).AggregateFrequency.Value);
+                Bass.ChannelSetAttribute(channel, ChannelAttribute.Volume, AggregateVolume.Value);
+                Bass.ChannelSetAttribute(channel, ChannelAttribute.Pan, AggregateBalance.Value);
+                Bass.ChannelSetAttribute(channel, ChannelAttribute.Frequency, initialFrequency * AggregateFrequency.Value);
             }
         }
 
