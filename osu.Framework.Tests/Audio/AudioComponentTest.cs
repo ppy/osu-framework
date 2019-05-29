6d825403c3b680672ff5579ae8105c142f9f1092
Dean Herbert <pe@ppy.sh>
2019-05-29 16:17:56 +0900

    Add basic VirtualTrack retrieval test
    Audio tests will be expanded on going forward.

---
+++ osu.Framework.Tests/Audio/AudioComponentTest.cs
@@ -0,0 +1,46 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Threading.Tasks;
+using NUnit.Framework;
+using osu.Framework.Audio;
+using osu.Framework.IO.Stores;
+using osu.Framework.Threading;
+
+namespace osu.Framework.Tests.Audio
+{
+    [TestFixture]
+    public class AudioComponentTest
+    {
+        [Test]
+        public void TestVirtualTrack()
+        {
+            var thread = new AudioThread();
+            var store = new NamespacedResourceStore<byte[]>(new DllResourceStore(@"osu.Framework.dll"), @"Resources");
+
+            var manager = new AudioManager(thread, store, store);
+
+            thread.Start();
+
+            var track = manager.Tracks.GetVirtual();
+
+            Assert.IsFalse(track.IsRunning);
+            Assert.AreEqual(0, track.CurrentTime);
+
+            track.Start();
+
+            Task.Delay(50);
+
+            Assert.Greater(track.CurrentTime, 0);
+
+            track.Stop();
+            Assert.IsFalse(track.IsRunning);
+
+            thread.Exit();
+
+            Task.Delay(500);
+
+            Assert.IsFalse(thread.Exited);
+        }
+    }
+}
