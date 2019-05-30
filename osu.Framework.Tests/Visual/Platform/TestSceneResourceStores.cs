2344651da506d14a6c74cd93fa043bd30fdb0697
Dean Herbert <pe@ppy.sh>
2019-05-30 14:20:01 +0900

    Rewrite tests

--- osu.Framework.Tests/Visual/Platform/TestSceneResourceStores.cs
+++ osu.Framework.Tests/Visual/Platform/TestSceneResourceStores.cs
@@ -1,12 +1,12 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
 using osu.Framework.Allocation;
 using osu.Framework.Audio;
-using osu.Framework.Audio.Track;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
@@ -15,17 +15,18 @@ using osu.Framework.Graphics.Textures;
 using osu.Framework.IO.Stores;
 using osu.Framework.Platform;
 using osuTK;
+using osuTK.Graphics;
 
 namespace osu.Framework.Tests.Visual.Platform
 {
     public class TestSceneResourceStores : FrameworkTestScene
     {
-        private FillFlowContainer textContainer;
+        private FillFlowContainer<ResourceDisplay> flow;
 
         private FontStore fontStore;
         private TextureStore textureStore;
-        private StorageBackedResourceStore storageStore;
-        private TrackStore trackStore;
+        private Storage storage;
+        private AudioManager audioManager;
 
         [BackgroundDependencyLoader]
         private void load(FontStore fontStore, Storage storage, TextureStore textureStore, AudioManager audioManager)
@@ -33,8 +34,10 @@ namespace osu.Framework.Tests.Visual.Platform
             Child = new ScrollContainer
             {
                 RelativeSizeAxes = Axes.Both,
-                Child = textContainer = new FillFlowContainer
+                Child = flow = new FillFlowContainer<ResourceDisplay>
                 {
+                    Margin = new MarginPadding(10),
+                    Spacing = new Vector2(3),
                     RelativeSizeAxes = Axes.X,
                     AutoSizeAxes = Axes.Y,
                     Direction = FillDirection.Vertical,
@@ -43,130 +46,87 @@ namespace osu.Framework.Tests.Visual.Platform
 
             this.fontStore = fontStore;
             this.textureStore = textureStore;
-            trackStore = (TrackStore)audioManager.GetTrackStore();
-            storageStore = new StorageBackedResourceStore(storage);
+            this.audioManager = audioManager;
+            this.storage = storage;
         }
 
         [Test]
-        public void TestGetFontStoreResources()
-        {
-            var glyphNames = fontStore.GetAvailableResources().ToList();
-            var glyphTextures = new List<Texture>();
-            var boxes = new List<Drawable>();
+        public void TestFontStore() => showResources(fontStore.GetAvailableResources(), fontStore.Get);
 
-            AddStep("Get all available resources", () =>
+        [Test]
+        public void TestStorageBackedResourceStore() => showResources(new StorageBackedResourceStore(storage));
+
+        [Test]
+        public void TestGetTextureStore() => showResources(textureStore.GetAvailableResources(), textureStore.Get);
+
+        [Test]
+        public void TestGetTrackManager() => showResources(audioManager.Tracks);
+
+        private void showResources<T>(IResourceStore<T> store) => showResources(store.GetAvailableResources(), store.Get);
+
+        private void showResources<T>(IEnumerable<string> resources, Func<string, T> fetch)
+        {
+            AddStep("list resources", () =>
             {
-                glyphTextures = new List<Texture>();
-                boxes = new List<Drawable>();
-                glyphTextures.AddRange(glyphNames.Select(glyph =>
+                flow.Clear();
+
+                foreach (var resourceName in resources)
+                    flow.Add(new ResourceDisplay(resourceName, fetch(resourceName)));
+            });
+
+            AddAssert("ensure all loaded", () => flow.Children.All(rd => rd.Resource != null));
+        }
+
+        private class ResourceDisplay : Container
+        {
+            public readonly object Resource;
+
+            public ResourceDisplay(string name, object resource)
+            {
+                Resource = resource;
+
+                AutoSizeAxes = Axes.Y;
+                RelativeSizeAxes = Axes.X;
+
+                Child = new FillFlowContainer
                 {
-                    var tex = fontStore.Get(glyph);
-                    boxes.Add(new Box
+                    AutoSizeAxes = Axes.Both,
+                    Direction = FillDirection.Horizontal,
+                    Spacing = new Vector2(10),
+                    Children = new Drawable[]
                     {
-                        Texture = tex,
-                        Size = new Vector2(20)
-                    });
-                    return tex;
-                }));
-            });
-            AddStep("Print resources in font store", () => populateText(glyphNames, boxes));
-            AddAssert("No glyphs were null", () => glyphTextures.All(tex => tex != null));
-        }
-
-        [Test]
-        public void TestGetStorageBackedResources()
-        {
-            var fileNames = storageStore.GetAvailableResources().ToList();
-            var fileList = new List<byte[]>();
-            var texts = new List<Drawable>();
-
-            AddStep("Get all available resources", () =>
-            {
-                texts = new List<Drawable>();
-                fileList = new List<byte[]>();
-                fileList.AddRange(fileNames.Select(file =>
-                {
-                    var tex = storageStore.Get(file);
-                    texts.Add(new SpriteText { Text = tex.ToString() });
-                    return tex;
-                }));
-            });
-            AddStep("Print storage backed resources", () => populateText(fileNames, texts));
-            AddAssert("No files were null", () => fileList.All(file => file != null));
-        }
-
-        [Test]
-        public void TestGetTextureStore()
-        {
-            var texNames = textureStore.GetAvailableResources().ToList();
-            var textures = new List<Texture>();
-            var drawables = new List<Drawable>();
-
-            AddStep("Get all available resources", () =>
-            {
-                textures = new List<Texture>();
-                drawables = new List<Drawable>();
-                textures.AddRange(texNames.Select(name =>
-                {
-                    var tex = textureStore.Get(name);
-                    drawables.Add(new Box
-                    {
-                        Texture = tex,
-                        Size = new Vector2(20)
-                    });
-                    return tex;
-                }));
-            });
-            AddStep("Print resources in texture store", () => populateText(texNames, drawables));
-            AddAssert("No textures were null", () => textures.All(tex => tex != null));
-        }
-
-        [Test]
-        public void TestGetTrackManager()
-        {
-            var trackNames = trackStore.GetAvailableResources().ToList();
-            var fileList = new List<Track>();
-            var texts = new List<Drawable>();
-
-            AddStep("Get all available resources", () =>
-            {
-                texts = new List<Drawable>();
-                fileList = new List<Track>();
-                fileList.AddRange(trackNames.Select(name =>
-                {
-                    var track = trackStore.Get(name);
-                    texts.Add(new SpriteText { Text = track.ToString() });
-                    return track;
-                }));
-            });
-            AddStep("Print resources in track store", () => populateText(trackNames, texts));
-            AddAssert("No tracks were null", () => fileList.All(file => file != null));
-        }
-
-        private void populateText(List<string> lines, List<Drawable> results)
-        {
-            textContainer.Clear();
-
-            for (int i = 0; i < lines.Count; i++)
-            {
-                var result = results[i];
-                result.Origin = Anchor.TopRight;
-                result.Anchor = Anchor.TopRight;
-                textContainer.Add(new Container
-                {
-                    RelativeSizeAxes = Axes.X,
-                    AutoSizeAxes = Axes.Y,
-                    Children = new[]
-                    {
-                        new SpriteText
+                        new Container
                         {
-                            Anchor = Anchor.TopLeft,
-                            Origin = Anchor.TopLeft,
-                            Text = lines[i],
+                            AutoSizeAxes = Axes.X,
+                            RelativeSizeAxes = Axes.Y,
+                            Children = new[]
+                            {
+                                new Box
+                                {
+                                    Colour = Color4.Navy,
+                                    RelativeSizeAxes = Axes.Both,
+                                },
+                                createDisplay(resource),
+                            }
                         },
-                        result
+                        new SpriteText { Text = name },
                     }
-                });
+                };
+            }
+
+            private Drawable createDisplay(object resource)
+            {
+                switch (resource)
+                {
+                    case Texture tex:
+                        return new Sprite
+                        {
+                            Size = new Vector2(20),
+                            Texture = tex
+                        };
+                    default:
+                        return new SpriteText { Text = resource.ToString() };
+                }
             }
         }
     }
