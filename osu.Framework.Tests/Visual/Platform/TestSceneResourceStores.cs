c396c204804009606c594fa2975787e78a710ecd
David Zhao <nyquill@ppy.sh>
2019-05-30 12:18:40 +0900

    Test getting all resources, and print results on right

--- osu.Framework.Tests/Visual/Platform/TestSceneResourceStores.cs
+++ osu.Framework.Tests/Visual/Platform/TestSceneResourceStores.cs
@@ -20,8 +20,9 @@ namespace osu.Framework.Tests.Visual.Platform
 {
     public class TestSceneResourceStores : FrameworkTestScene
     {
-        private FontStore fontStore;
         private FillFlowContainer textContainer;
+
+        private FontStore fontStore;
         private TextureStore textureStore;
         private StorageBackedResourceStore storageStore;
         private TrackStore trackStore;
@@ -49,92 +50,122 @@ namespace osu.Framework.Tests.Visual.Platform
         [Test]
         public void TestGetFontStoreResources()
         {
-            AddStep("Print resources in font store", () => populateText(fontStore.GetAvailableResources()));
+            var glyphNames = fontStore.GetAvailableResources().ToList();
+            var glyphTextures = new List<Texture>();
+            var boxes = new List<Drawable>();
+
+            AddStep("Get all available resources", () =>
+            {
+                glyphTextures = new List<Texture>();
+                boxes = new List<Drawable>();
+                glyphTextures.AddRange(glyphNames.Select(glyph =>
+                {
+                    var tex = fontStore.Get(glyph);
+                    boxes.Add(new Box
+                    {
+                        Texture = tex,
+                        Size = new Vector2(20)
+                    });
+                    return tex;
+                }));
+            });
+            AddStep("Print resources in font store", () => populateText(glyphNames, boxes));
+            AddAssert("No glyphs were null", () => glyphTextures.All(tex => tex != null));
         }
 
         [Test]
         public void TestGetStorageBackedResources()
         {
-            AddStep("Print storage backed resources", () => populateText(storageStore.GetAvailableResources()));
+            var fileNames = storageStore.GetAvailableResources().ToList();
+            var fileList = new List<byte[]>();
+            var texts = new List<Drawable>();
+
+            AddStep("Get all available resources", () =>
+            {
+                texts = new List<Drawable>();
+                fileList = new List<byte[]>();
+                fileList.AddRange(fileNames.Select(file =>
+                {
+                    var tex = storageStore.Get(file);
+                    texts.Add(new SpriteText { Text = tex.ToString() });
+                    return tex;
+                }));
+            });
+            AddStep("Print storage backed resources", () => populateText(fileNames, texts));
+            AddAssert("No files were null", () => fileList.All(file => file != null));
         }
 
         [Test]
         public void TestGetTextureStore()
         {
-            AddStep("Print texture store", () => populateText(textureStore.GetAvailableResources()));
+            var texNames = textureStore.GetAvailableResources().ToList();
+            var textures = new List<Texture>();
+            var drawables = new List<Drawable>();
+
+            AddStep("Get all available resources", () =>
+            {
+                textures = new List<Texture>();
+                drawables = new List<Drawable>();
+                textures.AddRange(texNames.Select(name =>
+                {
+                    var tex = textureStore.Get(name);
+                    drawables.Add(new Box
+                    {
+                        Texture = tex,
+                        Size = new Vector2(20)
+                    });
+                    return tex;
+                }));
+            });
+            AddStep("Print resources in texture store", () => populateText(texNames, drawables));
+            AddAssert("No textures were null", () => textures.All(tex => tex != null));
         }
 
         [Test]
         public void TestGetTrackManager()
         {
-            AddStep("Print track manager", () => populateText(trackStore.GetAvailableResources()));
-        }
+            var trackNames = trackStore.GetAvailableResources().ToList();
+            var fileList = new List<Track>();
+            var texts = new List<Drawable>();
 
-        [Test]
-        public void TestGetFromResources()
-        {
-            Texture glyph = null;
-            byte[] file = null;
-            Texture texture = null;
-            Track track = null;
-
-            AddStep("Get all resources", () =>
+            AddStep("Get all available resources", () =>
             {
-                glyph = null;
-                file = null;
-                texture = null;
-                track = null;
-
-                var glyphs = fontStore.GetAvailableResources();
-                var storage = storageStore.GetAvailableResources();
-                var textures = textureStore.GetAvailableResources();
-                var tracks = trackStore.GetAvailableResources();
-
-                glyph = fontStore.Get(glyphs.First());
-                file = storageStore.Get(storage.First());
-                texture = textureStore.Get(textures.First());
-                track = trackStore.Get(tracks.First());
-
-                textContainer.Clear();
-                textContainer.Add(new Box
+                texts = new List<Drawable>();
+                fileList = new List<Track>();
+                fileList.AddRange(trackNames.Select(name =>
                 {
-                    Texture = glyph,
-                    Size = new Vector2(20)
-                });
-
-                textContainer.Add(new SpriteText
-                {
-                    Text = file.ToString()
-                });
-
-                textContainer.Add(new Box
-                {
-                    Texture = texture,
-                    Size = new Vector2(20)
-                });
-
-                textContainer.Add(new SpriteText
-                {
-                    Text = track.ToString()
-                });
+                    var track = trackStore.Get(name);
+                    texts.Add(new SpriteText { Text = track.ToString() });
+                    return track;
+                }));
             });
-
-            AddAssert("glyph was not null", () => glyph != null);
-            AddAssert("file was not null", () => file != null);
-            AddAssert("texture was not null", () => texture != null);
-            AddAssert("track was not null", () => track != null);
+            AddStep("Print resources in track store", () => populateText(trackNames, texts));
+            AddAssert("No tracks were null", () => fileList.All(file => file != null));
         }
 
-        private void populateText(IEnumerable<string> lines)
+        private void populateText(List<string> lines, List<Drawable> results)
         {
             textContainer.Clear();
-            foreach (var text in lines)
+
+            for (int i = 0; i < lines.Count; i++)
             {
-                textContainer.Add(new SpriteText
+                var result = results[i];
+                result.Origin = Anchor.TopRight;
+                result.Anchor = Anchor.TopRight;
+                textContainer.Add(new Container
                 {
-                    Anchor = Anchor.TopLeft,
-                    Origin = Anchor.TopLeft,
-                    Text = text,
+                    RelativeSizeAxes = Axes.X,
+                    AutoSizeAxes = Axes.Y,
+                    Children = new[]
+                    {
+                        new SpriteText
+                        {
+                            Anchor = Anchor.TopLeft,
+                            Origin = Anchor.TopLeft,
+                            Text = lines[i],
+                        },
+                        result
+                    }
                 });
             }
         }
