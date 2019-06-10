097180fcf664adfc8f8746bb1623a1f8b13807d0
David Zhao <nyquill@ppy.sh>
2019-06-10 11:52:39 +0900

    Use string.Empty
    Co-Authored-By: miterosan <33435289+miterosan@users.noreply.github.com>

--- osu.Framework/IO/Stores/FontStore.cs
+++ osu.Framework/IO/Stores/FontStore.cs
@@ -64,7 +64,7 @@ namespace osu.Framework.IO.Stores
         /// <returns>The base height of the font.</returns>
         public float? GetBaseHeight(char c)
         {
-            var glyphStore = getGlyphStore("", c);
+            var glyphStore = getGlyphStore(string.Empty, c);
 
             return glyphStore?.GetBaseHeight() / ScaleAdjust;
         }
