e46143ced28bcecd11ed1a5f09a459cfa78eaeb7
Shane Woolcock <swoolcock@nextfaze.com>
2019-06-13 14:06:34 +0930

    Fix xmldoc

--- osu.Framework/Graphics/Containers/EdgeSnappingContainer.cs
+++ osu.Framework/Graphics/Containers/EdgeSnappingContainer.cs
@@ -12,7 +12,7 @@ namespace osu.Framework.Graphics.Containers
     /// <summary>
     /// A <see cref="Container{T}"/> that will apply negative <see cref="Container{T}.Padding"/> to snap the edges to
     /// the nearest cached <see cref="ISnapTargetContainer"/>.
-    /// Individual <see cref="Edges"/> may be snapped by setting the <see cref="snappedEdges"/> property.
+    /// Individual <see cref="Edges"/> may be snapped by setting the <see cref="SnappedEdges"/> property.
     /// This class is most useful when used in conjunction with <see cref="SafeAreaContainer{T}"/> to ensure UI elements are
     /// not hidden underneath inaccessible areas of the screen on mobile devices.
     /// Note that children within the negative padding will not be drawn if a parent <see cref="Drawable"/>
