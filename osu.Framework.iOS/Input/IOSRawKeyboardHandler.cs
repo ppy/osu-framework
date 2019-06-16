438cc0df2eb65e5f0c85d3fba7fa057a19ffe202
Shane Woolcock <samahnub@gmail.com>
2019-06-16 15:13:18 +0930

    Code sanity

--- osu.Framework.iOS/Input/IOSRawKeyboardHandler.cs
+++ osu.Framework.iOS/Input/IOSRawKeyboardHandler.cs
@@ -22,7 +22,7 @@ namespace osu.Framework.iOS.Input
             if (!(UIApplication.SharedApplication is GameUIApplication game))
                 return false;
 
-            game.KeyEvent += (int keyCode, bool isDown) =>
+            game.KeyEvent += (keyCode, isDown) =>
             {
                 if (IsActive && keyMap.ContainsKey(keyCode))
                     PendingInputs.Enqueue(new KeyboardKeyInput(keyMap[keyCode], isDown));
