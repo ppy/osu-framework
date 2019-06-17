dda6e6ad81c31710df764f41e3cf4ebdd7b7a4cd
Dean Herbert <pe@ppy.sh>
2019-06-17 18:03:47 +0900

    Fix UI controls implementing IHasCurrentValue in an unsafe way

--- osu.Framework/Graphics/UserInterface/Checkbox.cs
+++ osu.Framework/Graphics/UserInterface/Checkbox.cs
@@ -15,6 +15,8 @@ namespace osu.Framework.Graphics.UserInterface
     {
         private readonly Bindable<bool> current = new Bindable<bool>();
 
+        private Bindable<bool> currentBound;
+
         /// <summary>
         /// A bindable that holds the value if the checkbox is checked or not.
         /// </summary>
@@ -26,8 +28,8 @@ namespace osu.Framework.Graphics.UserInterface
                 if (value == null)
                     throw new ArgumentNullException(nameof(value));
 
-                current.UnbindBindings();
-                current.BindTo(value);
+                current.UnbindFrom(currentBound);
+                current.BindTo(currentBound = value);
             }
         }
 
