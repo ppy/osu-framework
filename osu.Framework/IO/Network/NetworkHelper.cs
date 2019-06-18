098e92cee6a032fbad716ec2d2e30f57990f99e3
Roman Kapustin <TocoToucanMS@gmail.com>
2019-06-18 21:44:42 +0300

    Add NetworkHelper

---
+++ osu.Framework/IO/Network/NetworkHelper.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Net.NetworkInformation;
+
+namespace osu.Framework.IO.Network
+{
+    public static class NetworkHelper
+    {
+        public static bool NetworkAvailable => NetworkInterface.GetIsNetworkAvailable();
+    }
+}
