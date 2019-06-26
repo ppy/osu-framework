e3ea5d8005296c993168b0d2c454777b019ef7e1
smoogipoo <smoogipoo@smgi.me>
2019-06-26 20:57:01 +0900

    Schedule appropriate finalisers

--- osu.Framework/Graphics/OpenGL/Buffers/FrameBuffer.cs
+++ osu.Framework/Graphics/OpenGL/Buffers/FrameBuffer.cs
@@ -22,7 +22,7 @@ namespace osu.Framework.Graphics.OpenGL.Buffers
 
         ~FrameBuffer()
         {
-            Dispose(false);
+            GLWrapper.ScheduleDisposal(() => Dispose(false));
         }
 
         public void Dispose()
--- osu.Framework/Graphics/OpenGL/Buffers/VertexBuffer.cs
+++ osu.Framework/Graphics/OpenGL/Buffers/VertexBuffer.cs
@@ -66,7 +66,7 @@ namespace osu.Framework.Graphics.OpenGL.Buffers
 
         ~VertexBuffer()
         {
-            Dispose(false);
+            GLWrapper.ScheduleDisposal(() => Dispose(false));
         }
 
         public void Dispose()
