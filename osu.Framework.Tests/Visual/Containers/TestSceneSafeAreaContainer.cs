26c0926646c5f162d1a1e0be8cb30601c6b3d6d1
Shane Woolcock <samahnub@gmail.com>
2019-06-28 12:41:34 +0930

    Remove unused usings

--- osu.Framework.Tests/Visual/Containers/TestSceneSafeAreaContainer.cs
+++ osu.Framework.Tests/Visual/Containers/TestSceneSafeAreaContainer.cs
@@ -4,14 +4,12 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using osu.Framework.Allocation;
 using osu.Framework.Bindables;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
 using osu.Framework.Graphics.Sprites;
 using osu.Framework.Graphics.UserInterface;
-using osu.Framework.Platform;
 using osuTK;
 using osuTK.Graphics;
 
