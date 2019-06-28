6dd149c3e368bdf9cefee7ca891ced6134be380b
Dean Herbert <pe@ppy.sh>
2019-06-28 15:21:15 +0900

    Revert "Pass through correct texture atlas size to TextureWhitePixel"
    This reverts commit 7616b9fea9ca30f3cd87befdfbcccf8f25fb0e1a.

--- osu.Framework/Graphics/Textures/TextureWhitePixel.cs
+++ osu.Framework/Graphics/Textures/TextureWhitePixel.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.Graphics.OpenGL;
 using osu.Framework.Graphics.OpenGL.Textures;
 using osu.Framework.Graphics.Primitives;
 
@@ -8,12 +9,9 @@ namespace osu.Framework.Graphics.Textures
 {
     internal class TextureWhitePixel : Texture
     {
-        private readonly int atlasTextureSize;
-
-        public TextureWhitePixel(TextureGL textureGl, int atlasTextureSize)
+        public TextureWhitePixel(TextureGL textureGl)
             : base(textureGl)
         {
-            this.atlasTextureSize = atlasTextureSize;
         }
 
         protected override RectangleF TextureBounds(RectangleF? textureRect = null)
@@ -21,7 +19,7 @@ namespace osu.Framework.Graphics.Textures
             // We need non-zero texture bounds for EdgeSmoothness to work correctly.
             // Let's be very conservative and use a tenth of the size of a pixel in the
             // largest possible texture.
-            float smallestPixelTenth = 0.1f / atlasTextureSize;
+            float smallestPixelTenth = 0.1f / GLWrapper.MaxTextureSize;
             return new RectangleF(0, 0, smallestPixelTenth, smallestPixelTenth);
         }
     }
