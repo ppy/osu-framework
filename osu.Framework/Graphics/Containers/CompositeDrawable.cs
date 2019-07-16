cfc1267f759305c0c1803a6cefe1a0aa7c3f95d6
smoogipoo <smoogipoo@smgi.me>
2019-07-16 13:08:26 +0900

    Refactor masked away checks

--- osu.Framework/Graphics/Containers/CompositeDrawable.cs
+++ osu.Framework/Graphics/Containers/CompositeDrawable.cs
@@ -999,18 +999,16 @@ namespace osu.Framework.Graphics.Containers
                     if (!drawable.IsPresent)
                         continue;
 
+                    if (drawable.IsMaskedAway)
+                        continue;
+
                     CompositeDrawable composite = drawable as CompositeDrawable;
 
                     if (composite?.CanBeFlattened == true)
                     {
-                        if (!composite.IsMaskedAway)
-                            addFromComposite(frame, treeIndex, forceNewDrawNode, ref j, composite, target);
-
+                        addFromComposite(frame, treeIndex, forceNewDrawNode, ref j, composite, target);
                         continue;
                     }
-
-                    if (drawable.IsMaskedAway)
-                        continue;
                 }
 
                 DrawNode next = drawable.GenerateDrawNodeSubtree(frame, treeIndex, forceNewDrawNode);
