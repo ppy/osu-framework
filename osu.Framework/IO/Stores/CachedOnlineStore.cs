a82e19839407633b0deb0e77b1942793084b2d40
Roman Kapustin <TocoToucanMS@gmail.com>
2019-08-03 20:48:58 +0300

    Add CachedOnlineStore class

---
+++ osu.Framework/IO/Stores/CachedOnlineStore.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace osu.Framework.IO.Stores
+{
+    public class CachedOnlineStore : OnlineStore
+    {
+        public override Task<byte[]> GetAsync(string url) => throw new NotImplementedException();
+
+        public override byte[] Get(string url) => throw new NotImplementedException();
+
+        public override Stream GetStream(string url) => throw new NotImplementedException();
+    }
+}
