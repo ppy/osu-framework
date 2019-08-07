53ffb2271c545837de71111eeb39339ae5f34814
Jacob <kozilord@gmail.com>
2019-08-07 15:27:56 +0200

    Add the licence header and remove unused usings

--- osu.Framework.Tests/Visual/Sprites/TestSceneTexturedTriangle.cs
+++ osu.Framework.Tests/Visual/Sprites/TestSceneTexturedTriangle.cs
@@ -1,9 +1,10 @@
-﻿using osu.Framework.Allocation;
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Allocation;
 using osu.Framework.Graphics.Shapes;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Textures;
-using System;
-using System.Collections.Generic;
 using osuTK;
 
 namespace osu.Framework.Tests.Visual.Sprites
