ea83492294ca1a03998c6bdc109cc58fbe3b626b
Roman Kapustin <TocoToucanMS@gmail.com>
2019-08-20 00:57:39 +0300

    Update tests

--- osu.Framework.Tests/IO/TestCachedOnlineStore.cs
+++ osu.Framework.Tests/IO/TestCachedOnlineStore.cs
@@ -3,8 +3,9 @@
 
 using System;
 using System.Threading.Tasks;
+using MonkeyCache;
+using MonkeyCache.FileStore;
 using NUnit.Framework;
-using osu.Framework.Extensions;
 using osu.Framework.IO.Stores;
 
 namespace osu.Framework.Tests.IO
@@ -15,83 +16,62 @@ namespace osu.Framework.Tests.IO
         private const string ru_flag_url = "https://osu.ppy.sh/images/flags/RU.png";
         private const string jp_flag_url = "https://osu.ppy.sh/images/flags/JP.png";
 
-        private CachedOnlineStore sut;
-        private TemporaryNativeStorage sutCache;
+        private CachedOnlineStore onlineStore;
+        private IBarrel onlineStoreCache;
 
         [SetUp]
         public void SetUp()
         {
-            sut = new CachedOnlineStore(sutCache = new TemporaryNativeStorage(Guid.NewGuid().ToString()), TimeSpan.FromHours(1));
+            onlineStore = new CachedOnlineStore(onlineStoreCache = Barrel.Create(new TemporaryNativeStorage(Guid.NewGuid().ToString()).GetFullPath(string.Empty)), TimeSpan.FromHours(1));
         }
 
         [TearDown]
         public void TearDown()
         {
-            sutCache.Dispose();
+            onlineStore.Dispose();
         }
 
         [Test]
         public void TestGet()
         {
-            Assert.That(sutCache.Exists(ru_flag_url.ComputeMD5Hash()), Is.False);
-            sut.Get(ru_flag_url);
-            Assert.That(sutCache.Exists(ru_flag_url.ComputeMD5Hash()), Is.True);
+            Assert.That(onlineStoreCache.Exists(ru_flag_url), Is.False);
+            onlineStore.Get(ru_flag_url);
+            Assert.That(onlineStoreCache.Exists(ru_flag_url), Is.True);
         }
 
         [Test]
         public void TestGetFromCache()
         {
-            sut.Get(ru_flag_url);
-            var accessTime = sutCache.GetLastAccessTime(ru_flag_url.ComputeMD5Hash());
-
-            sut.Get(ru_flag_url);
-            Assert.That(sutCache.GetLastAccessTime(ru_flag_url.ComputeMD5Hash()), Is.GreaterThan(accessTime));
+            throw new NotImplementedException();
         }
 
         [Test]
         public async Task TestGetAsync()
         {
-            Assert.That(sutCache.Exists(ru_flag_url.ComputeMD5Hash()), Is.False);
-            await sut.GetAsync(ru_flag_url);
-            Assert.That(sutCache.Exists(ru_flag_url.ComputeMD5Hash()), Is.True);
+            Assert.That(onlineStoreCache.Exists(ru_flag_url), Is.False);
+            await onlineStore.GetAsync(ru_flag_url);
+            Assert.That(onlineStoreCache.Exists(ru_flag_url), Is.True);
         }
 
         [Test]
         public async Task TestGetFromCacheAsync()
         {
-            await sut.GetAsync(ru_flag_url);
-            var accessTime = sutCache.GetLastAccessTime(ru_flag_url.ComputeMD5Hash());
-
-            await sut.GetAsync(ru_flag_url);
-            Assert.That(sutCache.GetLastAccessTime(ru_flag_url.ComputeMD5Hash()), Is.GreaterThan(accessTime));
+            throw new NotImplementedException();
         }
 
         [Test]
         public void TestGetStream()
         {
-            Assert.That(sutCache.Exists(ru_flag_url.ComputeMD5Hash()), Is.False);
-            var stream = sut.GetStream(ru_flag_url);
-
+            Assert.That(onlineStoreCache.Exists(ru_flag_url), Is.False);
+            var stream = onlineStore.GetStream(ru_flag_url);
             Assert.That(stream, Is.Not.Null);
-            Assert.That(sutCache.Exists(ru_flag_url.ComputeMD5Hash()), Is.True);
+            Assert.That(onlineStoreCache.Exists(ru_flag_url), Is.True);
         }
 
         [Test]
         public void TestClearExpired()
         {
-            sut.Get(ru_flag_url);
-            sut.Get(jp_flag_url);
-
-            Assert.That(sutCache.Exists(ru_flag_url.ComputeMD5Hash()), Is.True);
-            Assert.That(sutCache.Exists(jp_flag_url.ComputeMD5Hash()), Is.True);
-
-            Assert.That(sut.Clear(), Is.EqualTo(0));
-
-            sutCache.SetLastAccessTime(jp_flag_url.ComputeMD5Hash(), DateTime.Now.AddDays(-1));
-
-            Assert.That(sut.Clear(), Is.EqualTo(1));
-            Assert.That(sutCache.Exists(ru_flag_url.ComputeMD5Hash()), Is.True);
-            Assert.That(sutCache.Exists(jp_flag_url.ComputeMD5Hash()), Is.False);
+            throw new NotImplementedException();
         }
     }
 }
