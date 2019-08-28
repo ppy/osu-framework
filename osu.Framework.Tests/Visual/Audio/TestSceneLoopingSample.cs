2572fdd2a1ccd9c31b0b46a666680b24907a38c1
Dean Herbert <pe@ppy.sh>
2019-08-28 15:16:15 +0900

    Improve test coverage slightly
    Also ensures samples get stopped when exiting the test

--- osu.Framework.Tests/Visual/Audio/TestSceneLoopingSample.cs
+++ osu.Framework.Tests/Visual/Audio/TestSceneLoopingSample.cs
@@ -11,20 +11,18 @@ namespace osu.Framework.Tests.Visual.Audio
     public class TestSceneLoopingSample : FrameworkTestScene
     {
         private SampleChannel sampleChannel;
+        private ISampleStore samples;
 
         [BackgroundDependencyLoader]
         private void load(ISampleStore samples)
         {
-            sampleChannel = samples.Get("tone.wav");
-
-            // reduce volume of the tone due to how loud it normally is.
-            if (sampleChannel != null)
-                sampleChannel.Volume.Value = 0.05;
+            this.samples = samples;
         }
 
         [Test]
-        public void TestLooping()
+        public void TestLoopingToggle()
         {
+            AddStep("create sample", createSample);
             AddAssert("not looping", () => !sampleChannel.Looping);
 
             AddStep("enable looping", () => sampleChannel.Looping = true);
@@ -34,8 +32,39 @@ namespace osu.Framework.Tests.Visual.Audio
             AddWaitStep("wait", 1);
             AddAssert("is still playing", () => sampleChannel.Playing);
 
-            AddStep("stop sample", () => sampleChannel.Stop());
+            AddStep("disable looping", () => sampleChannel.Looping = false);
             AddAssert("not playing", () => !sampleChannel.Playing);
+        }
+
+        [Test]
+        public void TestStopWhileLooping()
+        {
+            AddStep("create sample", createSample);
+
+            AddStep("enable looping", () => sampleChannel.Looping = true);
+            AddStep("play sample", () => sampleChannel.Play());
+
+            AddWaitStep("wait", 1);
+            AddAssert("is playing", () => sampleChannel.Playing);
+
+            AddStep("stop playing", () => sampleChannel.Stop());
+            AddAssert("not playing", () => !sampleChannel.Playing);
+        }
+
+        private void createSample()
+        {
+            sampleChannel?.Dispose();
+            sampleChannel = samples.Get("tone.wav");
+
+            // reduce volume of the tone due to how loud it normally is.
+            if (sampleChannel != null)
+                sampleChannel.Volume.Value = 0.05;
+        }
+
+        protected override void Dispose(bool isDisposing)
+        {
+            sampleChannel?.Dispose();
+            base.Dispose(isDisposing);
         }
     }
 }
