4f019f0c3b3f31513a251bc516f3bf09c4665a00
Roman Kapustin <TocoToucanMS@gmail.com>
2019-09-14 01:08:25 +0300

    Use ManualResetEventSlim in TestRunner.RunTestBlocking

--- osu.Framework/Testing/TestSceneTestRunner.cs
+++ osu.Framework/Testing/TestSceneTestRunner.cs
@@ -75,44 +75,46 @@ namespace osu.Framework.Testing
             {
                 Trace.Assert(host != null, $"Ensure this runner has been loaded before calling {nameof(RunTestBlocking)}");
 
-                bool completed = false;
-                ExceptionDispatchInfo exception = null;
-
-                void complete()
+                using (var completionEvent = new ManualResetEventSlim(false))
                 {
-                    // We want to remove the TestScene from the hierarchy on completion as under nUnit, it may have operations run on it from a different thread.
-                    // This is because nUnit will reuse the same class multiple times, running a different [Test] method each time, while the GameHost
-                    // is run from its own asynchronous thread.
-                    RemoveInternal(test);
-                    completed = true;
-                }
+                    ExceptionDispatchInfo exception = null;
 
-                Schedule(() =>
-                {
-                    AddInternal(test);
-
-                    Console.WriteLine($@"{(int)Time.Current}: Running {test} visual test cases...");
-
-                    // Nunit will run the tests in the TestScene with the same TestScene instance so the TestScene
-                    // needs to be removed before the host is exited, otherwise it will end up disposed
-
-                    test.RunAllSteps(() =>
+                    void complete()
                     {
-                        Scheduler.AddDelayed(complete, time_between_tests);
-                    }, e =>
+                        // We want to remove the TestScene from the hierarchy on completion as under nUnit, it may have operations run on it from a different thread.
+                        // This is because nUnit will reuse the same class multiple times, running a different [Test] method each time, while the GameHost
+                        // is run from its own asynchronous thread.
+                        RemoveInternal(test);
+                        // ReSharper disable once AccessToDisposedClosure
+                        completionEvent.Set();
+                    }
+
+                    Schedule(() =>
                     {
-                        if (e is DependencyInjectionException die)
-                            exception = die.DispatchInfo;
-                        else
-                            exception = ExceptionDispatchInfo.Capture(e);
-                        complete();
+                        AddInternal(test);
+
+                        Console.WriteLine($@"{(int)Time.Current}: Running {test} visual test cases...");
+
+                        // Nunit will run the tests in the TestScene with the same TestScene instance so the TestScene
+                        // needs to be removed before the host is exited, otherwise it will end up disposed
+
+                        test.RunAllSteps(() =>
+                        {
+                            Scheduler.AddDelayed(complete, time_between_tests);
+                        }, e =>
+                        {
+                            if (e is DependencyInjectionException die)
+                                exception = die.DispatchInfo;
+                            else
+                                exception = ExceptionDispatchInfo.Capture(e);
+                            complete();
+                        });
                     });
-                });
 
-                while (!completed && host.ExecutionState == ExecutionState.Running)
-                    Thread.Sleep(10);
+                    completionEvent.Wait();
 
-                exception?.Throw();
+                    exception?.Throw();
+                }
             }
         }
     }
