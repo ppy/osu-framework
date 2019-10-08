afac84e0989ea491a06ca9e8dd8d03cc9403067b
miterosan <miterosan@gmail.com>
2019-10-08 19:40:06 +0200

    Avoid that the backbutton closes the app.

--- osu.Framework.Android/AndroidGameActivity.cs
+++ osu.Framework.Android/AndroidGameActivity.cs
@@ -5,6 +5,7 @@ using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Runtime;
+using Android.Views;
 
 namespace osu.Framework.Android
 {
@@ -31,6 +32,37 @@ namespace osu.Framework.Android
             base.OnPause();
             // Because Android is not playing nice with Background - we just kill it
             System.Diagnostics.Process.GetCurrentProcess().Kill();
+        }
+
+        /// <summary>
+        /// Avoid the default implementation that does close the app.
+        /// </summary>
+        public override void OnBackPressed()
+        {
+        }
+
+        /// <summary>
+        /// There is the rare situation that the view does not get the events. So the view just gets triggered directly.
+        /// </summary>
+        public override bool OnKeyDown([GeneratedEnum] Keycode keyCode, KeyEvent e)
+        {
+            return gameView.OnKeyDown(keyCode, e);
+        }
+
+        /// <summary>
+        /// There is the rare situation that the view does not get the events. So the view just gets triggered directly.
+        /// </summary>
+        public override bool OnKeyUp([GeneratedEnum] Keycode keyCode, KeyEvent e)
+        {
+            return gameView.OnKeyUp(keyCode, e);
+        }
+
+        /// <summary>
+        /// There is the rare situation that the view does not get the events. So the view just gets triggered directly.
+        /// </summary>
+        public override bool OnKeyLongPress([GeneratedEnum] Keycode keyCode, KeyEvent e)
+        {
+            return gameView.OnKeyLongPress(keyCode, e);
         }
     }
 }
