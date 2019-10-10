d46ee65d20fe026afb69aba01dbd4c3798743859
Dean Herbert <pe@ppy.sh>
2019-10-10 14:27:08 +0900

    Fix non RBGA32 PNGs decoding incorrectly on iOS

--- osu.Framework.iOS/Graphics/Textures/IOSTextureLoaderStore.cs
+++ osu.Framework.iOS/Graphics/Textures/IOSTextureLoaderStore.cs
@@ -28,7 +28,7 @@ namespace osu.Framework.iOS.Graphics.Textures
                 int height = (int)uiImage.Size.Height;
 
                 IntPtr data = Marshal.AllocHGlobal(width * height * 4);
-                using (CGBitmapContext textureContext = new CGBitmapContext(data, width, height, 8, width * 4, uiImage.CGImage.ColorSpace, CGImageAlphaInfo.PremultipliedLast))
+                using (CGBitmapContext textureContext = new CGBitmapContext(data, width, height, 8, width * 4, CGColorSpace.CreateDeviceRGB(), CGImageAlphaInfo.PremultipliedLast))
                     textureContext.DrawImage(new CGRect(0, 0, width, height), uiImage.CGImage);
 
                 // NOTE: this will probably only be correct for Rgba32, will need to look into other pixel formats
