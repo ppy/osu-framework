c2ac6fa5c8bc39b704f8d7e2c293e78c5ff4013a
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2019-10-16 23:00:27 +0800

    Don't use conditionless for loop. This is a problematic syntax from C++, and no formatter agreement on it.

--- osu.Framework/Extensions/ExtensionMethods.cs
+++ osu.Framework/Extensions/ExtensionMethods.cs
@@ -273,7 +273,7 @@ namespace osu.Framework.Extensions
         {
             if (display == null) return DisplayIndex.Default;
 
-            for (int i = 0;; i++)
+            for (int i = 0; true; i++)
             {
                 var device = DisplayDevice.GetDisplay((DisplayIndex)i);
                 if (device == null) return DisplayIndex.Default;
