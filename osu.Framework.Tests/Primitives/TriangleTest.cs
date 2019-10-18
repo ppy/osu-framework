06b61a488950e75e24014248e002800d4d08506b
Jacob <kozilord@gmail.com>
2019-10-18 19:19:37 +0200

    Add some basic Tests

---
+++ osu.Framework.Tests/Primitives/TriangleTest.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using osu.Framework.Graphics.Primitives;
+using osuTK;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace osu.Framework.Tests.Primitives
+{
+    [TestFixture]
+    class TriangleTest
+    {
+        [TestCaseSource(typeof(AreaTestData), nameof(AreaTestData.TestCases))]
+        [DefaultFloatingPointTolerance(0.1f)]
+        public float TestArea(Triangle testTriangle) => testTriangle.AreaNew;
+
+        private class AreaTestData
+        {
+            public static IEnumerable TestCases
+            {
+                get
+                {
+                    // Point
+                    yield return new TestCaseData(new Triangle(Vector2.Zero, Vector2.Zero, Vector2.Zero)).Returns(0);
+
+                    // Angled
+                    yield return new TestCaseData(new Triangle(Vector2.Zero, new Vector2(10, 0), new Vector2(10))).Returns(50);
+                    yield return new TestCaseData(new Triangle(Vector2.Zero, new Vector2(0, 10), new Vector2(10))).Returns(50);
+                    yield return new TestCaseData(new Triangle(Vector2.Zero, new Vector2(10, -10), new Vector2(10))).Returns(100);
+                    yield return new TestCaseData(new Triangle(Vector2.Zero, new Vector2(10, -10), new Vector2(10))).Returns(100);
+                }
+            }
+        }
+    }
+}
