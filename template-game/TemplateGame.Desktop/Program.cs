79e31e0bd0ad445de63344d4f564156299e93262
iiSaLMaN <slmanarendo1950@gmail.com>
2019-10-19 04:53:52 +0300

    Add desktop game template for framework

---
+++ template-game/TemplateGame.Desktop/Program.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using osu.Framework.Platform;
+using osu.Framework;
+using TemplateGame.Game;
+
+namespace TemplateGame.Desktop
+{
+    public static class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            using (GameHost host = Host.GetSuitableHost(@"TemplateGame"))
+            using (osu.Framework.Game game = new TemplateGameGame())
+                host.Run(game);
+        }
+    }
+}
