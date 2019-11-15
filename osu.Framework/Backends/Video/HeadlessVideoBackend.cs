6fb96d7519b2e96c59056ccef98a81030443b381
Shane Woolcock <samahnub@gmail.com>
2019-11-15 17:02:53 +0900

    Use real backends for headless testing

--- osu.Framework/Backends/Video/HeadlessVideoBackend.cs
+++ osu.Framework/Backends/Video/HeadlessVideoBackend.cs
@@ -1,7 +1,6 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System;
 using System.IO;
 using osu.Framework.Graphics.Video;
 using osu.Framework.Platform;
@@ -14,7 +13,7 @@ namespace osu.Framework.Backends.Video
     /// </summary>
     public class HeadlessVideoBackend : IVideoBackend
     {
-        public VideoDecoder CreateVideoDecoder(Stream stream, Scheduler scheduler) => throw new NotImplementedException();
+        public VideoDecoder CreateVideoDecoder(Stream stream, Scheduler scheduler) => new VideoDecoder(stream, scheduler);
 
         public void Dispose()
         {
