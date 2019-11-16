14f341a89c882991cd80d72b46ed1b0dbc29fccd
Shane Woolcock <samahnub@gmail.com>
2019-11-16 17:11:29 +0900

    Add headless backend provider

--- osu.Framework/Platform/HeadlessGameHost.cs
+++ osu.Framework/Platform/HeadlessGameHost.cs
@@ -28,10 +28,12 @@ namespace osu.Framework.Platform
         protected override Storage GetStorage(string baseName) => new DesktopStorage($"headless-{baseName}", this);
 
         public HeadlessGameHost(string gameName = @"", bool bindIPC = false, bool realtime = true, bool portableInstallation = false)
-            : base(gameName, bindIPC, portableInstallation: portableInstallation, backends: new HeadlessBackendProvider())
+            : base(gameName, bindIPC, portableInstallation: portableInstallation)
         {
             this.realtime = realtime;
         }
+
+        protected override IBackendProvider CreateBackendProvider() => new HeadlessBackendProvider();
 
         protected override void SetupForRun()
         {
