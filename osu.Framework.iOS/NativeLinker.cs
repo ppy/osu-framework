f67784ba40367d9fbf8c922b3c77ad1618865cd3
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2019-11-27 18:57:00 +0800

    Update iOS native packaging.

---
+++ osu.Framework.iOS/NativeLinker.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using ObjCRuntime;
+
+[assembly: LinkWith("libavcodec.a", SmartLink = false, ForceLoad = true)]
+[assembly: LinkWith("libavdevice.a", SmartLink = false, ForceLoad = true)]
+[assembly: LinkWith("libavfilter.a", SmartLink = false, ForceLoad = true)]
+[assembly: LinkWith("libavformat.a", SmartLink = false, ForceLoad = true)]
+[assembly: LinkWith("libavutil.a", SmartLink = false, ForceLoad = true)]
+[assembly: LinkWith("libbass.a", SmartLink = false, ForceLoad = true)]
+[assembly: LinkWith("libbass_fx.a", SmartLink = false, ForceLoad = true)]
+[assembly: LinkWith("libswresample.a", SmartLink = false, ForceLoad = true)]
+[assembly: LinkWith("libswscale.a", SmartLink = false, ForceLoad = true)]
