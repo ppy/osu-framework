1f56740685447f200faff1a4ef2f48a637c9f37e
Dean Herbert <pe@ppy.sh>
2019-12-09 15:54:46 +0900

    Combine BindableNumber implementations into base class

--- osu.Framework/Bindables/BindableDouble.cs
+++ osu.Framework/Bindables/BindableDouble.cs
@@ -1,19 +1,14 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using System.Globalization;
 
 namespace osu.Framework.Bindables
 {
+    [Obsolete("Use BindableNumber<int> instead")] // can be removed 20200609
     public class BindableDouble : BindableNumber<double>
     {
-        // Take 50% of the precision to ensure the value doesn't underflow and return true for non-default values.
-        public override bool IsDefault => MathUtils.Precision.AlmostEquals(Value, Default, Precision / 2);
-
-        protected override double DefaultMinValue => double.MinValue;
-        protected override double DefaultMaxValue => double.MaxValue;
-        protected override double DefaultPrecision => double.Epsilon;
-
         public BindableDouble(double value = 0)
             : base(value)
         {
--- osu.Framework/Bindables/BindableFloat.cs
+++ osu.Framework/Bindables/BindableFloat.cs
@@ -1,19 +1,14 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using System.Globalization;
 
 namespace osu.Framework.Bindables
 {
+    [Obsolete("Use BindableNumber<int> instead")] // can be removed 20200609
     public class BindableFloat : BindableNumber<float>
     {
-        // Take 50% of the precision to ensure the value doesn't underflow and return true for non-default values.
-        public override bool IsDefault => MathUtils.Precision.AlmostEquals(Value, Default, Precision / 2);
-
-        protected override float DefaultMinValue => float.MinValue;
-        protected override float DefaultMaxValue => float.MaxValue;
-        protected override float DefaultPrecision => float.Epsilon;
-
         public BindableFloat(float value = 0)
             : base(value)
         {
