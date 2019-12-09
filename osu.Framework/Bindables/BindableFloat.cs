ef82e717a08302b416c09b28bd36ced2f33cdc8a
Dean Herbert <pe@ppy.sh>
2019-12-09 17:49:16 +0900

    Remove obsoleted flags

--- osu.Framework/Bindables/BindableFloat.cs
+++ osu.Framework/Bindables/BindableFloat.cs
@@ -1,12 +1,10 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System;
 using System.Globalization;
 
 namespace osu.Framework.Bindables
 {
-    [Obsolete("Use BindableNumber<int> instead")] // can be removed 20200609
     public class BindableFloat : BindableNumber<float>
     {
         public BindableFloat(float value = 0)
--- osu.Framework/Bindables/BindableInt.cs
+++ osu.Framework/Bindables/BindableInt.cs
@@ -1,12 +1,10 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System;
 using System.Globalization;
 
 namespace osu.Framework.Bindables
 {
-    [Obsolete("Use BindableNumber<int> instead")] // can be removed 20200609
     public class BindableInt : BindableNumber<int>
     {
         public BindableInt(int value = 0)
