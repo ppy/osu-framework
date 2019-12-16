97a5f8f5cccb30a2c52ec981e0480487f3996020
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2019-12-16 13:18:11 +0800

    Make SRGBColour readonly.

--- osu.Framework/Graphics/Colour/SRGBColour.cs
+++ osu.Framework/Graphics/Colour/SRGBColour.cs
@@ -14,11 +14,13 @@ namespace osu.Framework.Graphics.Colour
     /// This struct implicitly converts to sRGB space Color4 values (i.e. it can be assigned and implicitly cast)
     /// to sRGB Color4.
     /// </summary>
-    public struct SRGBColour : IEquatable<SRGBColour>
+    public readonly struct SRGBColour : IEquatable<SRGBColour>
     {
-        public Color4 Linear;
+        public readonly Color4 Linear;
 
-        public static implicit operator SRGBColour(Color4 value) => new SRGBColour { Linear = value.ToLinear() };
+        public SRGBColour(Color4 linear) => Linear = linear;
+
+        public static implicit operator SRGBColour(Color4 value) => new SRGBColour(value.ToLinear());
         public static implicit operator Color4(SRGBColour value) => value.Linear.ToSRGB();
 
         /// <summary>
@@ -27,45 +29,45 @@ namespace osu.Framework.Graphics.Colour
         /// <param name="first">First factor.</param>
         /// <param name="second">Second factor.</param>
         /// <returns>Product of first and second.</returns>
-        public static SRGBColour operator *(SRGBColour first, SRGBColour second) => new SRGBColour
-        {
-            Linear = new Color4(
+        public static SRGBColour operator *(SRGBColour first, SRGBColour second) => new Color4(
                 first.Linear.R * second.Linear.R,
                 first.Linear.G * second.Linear.G,
                 first.Linear.B * second.Linear.B,
-                first.Linear.A * second.Linear.A),
-        };
+                first.Linear.A * second.Linear.A);
 
-        public static SRGBColour operator *(SRGBColour first, float second) => new SRGBColour
-        {
-            Linear = new Color4(
+        public static SRGBColour operator *(SRGBColour first, float second) => new Color4(
                 first.Linear.R * second,
                 first.Linear.G * second,
                 first.Linear.B * second,
-                first.Linear.A * second),
-        };
+                first.Linear.A * second);
 
         public static SRGBColour operator /(SRGBColour first, float second) => first * (1 / second);
 
-        public static SRGBColour operator +(SRGBColour first, SRGBColour second) => new SRGBColour
-        {
-            Linear = new Color4(
+        public static SRGBColour operator +(SRGBColour first, SRGBColour second) => new Color4(
                 first.Linear.R + second.Linear.R,
                 first.Linear.G + second.Linear.G,
                 first.Linear.B + second.Linear.B,
-                first.Linear.A + second.Linear.A),
-        };
+                first.Linear.A + second.Linear.A);
 
-        public readonly Vector4 ToVector() => new Vector4(Linear.R, Linear.G, Linear.B, Linear.A);
-        public static SRGBColour FromVector(Vector4 v) => new SRGBColour { Linear = new Color4(v.X, v.Y, v.Z, v.W) };
+        public Vector4 ToVector() => new Vector4(Linear.R, Linear.G, Linear.B, Linear.A);
+        public static SRGBColour FromVector(Vector4 v) => new Color4(v.X, v.Y, v.Z, v.W);
 
+        public bool Equals(SRGBColour other) => Linear.Equals(other.Linear);
+        public override string ToString() => Linear.ToString();
+    }
+
+    internal static class SRGBColorExtensions
+    {
         /// <summary>
         /// Multiplies the alpha value of this colour by the given alpha factor.
         /// </summary>
+        /// <param name="colour">The colour variable to multiply with.</param>
         /// <param name="alpha">The alpha factor to multiply with.</param>
-        public void MultiplyAlpha(float alpha) => Linear.A *= alpha;
-
-        public readonly bool Equals(SRGBColour other) => Linear.Equals(other.Linear);
-        public override readonly string ToString() => Linear.ToString();
+        public static void MultiplyAlpha(this ref SRGBColour colour, float alpha)
+        {
+            var linear = colour.Linear;
+            linear.A *= alpha;
+            colour = linear;
+        }
     }
 }
