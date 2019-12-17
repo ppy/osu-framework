0d6b4079a838e6ed7bba26a551ff987e73e27a39
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2019-12-17 12:40:57 +0800

    Revert 'Make SRGBColour readonly.'
    This reverts commit 97a5f8f5.

--- osu.Framework/Graphics/Colour/SRGBColour.cs
+++ osu.Framework/Graphics/Colour/SRGBColour.cs
@@ -14,13 +14,11 @@ namespace osu.Framework.Graphics.Colour
     /// This struct implicitly converts to sRGB space Color4 values (i.e. it can be assigned and implicitly cast)
     /// to sRGB Color4.
     /// </summary>
-    public readonly struct SRGBColour : IEquatable<SRGBColour>
+    public struct SRGBColour : IEquatable<SRGBColour>
     {
-        public readonly Color4 Linear;
+        public Color4 Linear;
 
-        public SRGBColour(Color4 linear) => Linear = linear;
-
-        public static implicit operator SRGBColour(Color4 value) => new SRGBColour(value.ToLinear());
+        public static implicit operator SRGBColour(Color4 value) => new SRGBColour { Linear = value.ToLinear() };
         public static implicit operator Color4(SRGBColour value) => value.Linear.ToSRGB();
 
         /// <summary>
@@ -29,45 +27,45 @@ namespace osu.Framework.Graphics.Colour
         /// <param name="first">First factor.</param>
         /// <param name="second">Second factor.</param>
         /// <returns>Product of first and second.</returns>
-        public static SRGBColour operator *(SRGBColour first, SRGBColour second) => new Color4(
-            first.Linear.R * second.Linear.R,
-            first.Linear.G * second.Linear.G,
-            first.Linear.B * second.Linear.B,
-            first.Linear.A * second.Linear.A);
+        public static SRGBColour operator *(SRGBColour first, SRGBColour second) => new SRGBColour
+        {
+            Linear = new Color4(
+                first.Linear.R * second.Linear.R,
+                first.Linear.G * second.Linear.G,
+                first.Linear.B * second.Linear.B,
+                first.Linear.A * second.Linear.A),
+        };
 
-        public static SRGBColour operator *(SRGBColour first, float second) => new Color4(
-            first.Linear.R * second,
-            first.Linear.G * second,
-            first.Linear.B * second,
-            first.Linear.A * second);
+        public static SRGBColour operator *(SRGBColour first, float second) => new SRGBColour
+        {
+            Linear = new Color4(
+                first.Linear.R * second,
+                first.Linear.G * second,
+                first.Linear.B * second,
+                first.Linear.A * second),
+        };
 
         public static SRGBColour operator /(SRGBColour first, float second) => first * (1 / second);
 
-        public static SRGBColour operator +(SRGBColour first, SRGBColour second) => new Color4(
-            first.Linear.R + second.Linear.R,
-            first.Linear.G + second.Linear.G,
-            first.Linear.B + second.Linear.B,
-            first.Linear.A + second.Linear.A);
+        public static SRGBColour operator +(SRGBColour first, SRGBColour second) => new SRGBColour
+        {
+            Linear = new Color4(
+                first.Linear.R + second.Linear.R,
+                first.Linear.G + second.Linear.G,
+                first.Linear.B + second.Linear.B,
+                first.Linear.A + second.Linear.A),
+        };
 
-        public Vector4 ToVector() => new Vector4(Linear.R, Linear.G, Linear.B, Linear.A);
-        public static SRGBColour FromVector(Vector4 v) => new Color4(v.X, v.Y, v.Z, v.W);
+        public readonly Vector4 ToVector() => new Vector4(Linear.R, Linear.G, Linear.B, Linear.A);
+        public static SRGBColour FromVector(Vector4 v) => new SRGBColour { Linear = new Color4(v.X, v.Y, v.Z, v.W) };
 
-        public bool Equals(SRGBColour other) => Linear.Equals(other.Linear);
-        public override string ToString() => Linear.ToString();
-    }
-
-    internal static class SRGBColorExtensions
-    {
         /// <summary>
         /// Multiplies the alpha value of this colour by the given alpha factor.
         /// </summary>
-        /// <param name="colour">The colour variable to multiply with.</param>
         /// <param name="alpha">The alpha factor to multiply with.</param>
-        public static void MultiplyAlpha(this ref SRGBColour colour, float alpha)
-        {
-            var linear = colour.Linear;
-            linear.A *= alpha;
-            colour = linear;
-        }
+        public void MultiplyAlpha(float alpha) => Linear.A *= alpha;
+
+        public readonly bool Equals(SRGBColour other) => Linear.Equals(other.Linear);
+        public override readonly string ToString() => Linear.ToString();
     }
 }
