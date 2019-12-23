4a1c22390882bcb0d5063e6219bf0890f34c390f
andy840119 <andy840119@gmail.com>
2019-12-23 12:21:50 +0800

    Add initial testcase

---
+++ osu.Framework.Tests/Visual/UserInterface/TestSceneColorPicker.cs
@@ -0,0 +1,179 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Bindables;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Colour;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
+using osu.Framework.Graphics.UserInterface;
+using osuTK;
+using osuTK.Graphics;
+
+namespace osu.Framework.Tests.Visual.UserInterface
+{
+    public class TestSceneColorPicker : FrameworkTestScene
+    {
+        public TestSceneColorPicker()
+        {
+            var colorPicker = new ColorPicker();
+
+            Add(colorPicker);
+        }
+    }
+
+    public class ColorPicker : Container, IHasCurrentValue<Color4>
+    {
+        private readonly BindableWithCurrent<Color4> current = new BindableWithCurrent<Color4>();
+
+        public Bindable<Color4> Current
+        {
+            get => current.Current;
+            set => current.Current = value;
+        }
+
+        private readonly Box background;
+        private readonly FillFlowContainer fillFlowContainer;
+        private readonly ColorCanvas colorCanvas;
+        private readonly ColorScroller colorScroller;
+        private readonly TextBox colorCodeTextBox;
+        private readonly Box previewColorBox;
+        
+
+        public ColorPicker()
+        {
+            AutoSizeAxes = Axes.Both;
+            Children = new Drawable[]
+            {
+                background = new Box
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    Colour = Color4.Gray
+                },
+                fillFlowContainer = new FillFlowContainer
+                {
+                    Margin = new MarginPadding(10),
+                    AutoSizeAxes = Axes.Both,
+                    Direction = FillDirection.Vertical,
+                    Spacing = new Vector2(0, 10),
+                    Children = new Drawable[]
+                    {
+                        colorCanvas = new ColorCanvas
+                        {
+                            Size = new Vector2(200),
+                        },
+                        colorScroller = new ColorScroller
+                        {
+                            RelativeSizeAxes = Axes.X,
+                            Height = 50,
+                        },
+                        new GridContainer
+                        {
+                            RelativeSizeAxes = Axes.X,
+                            Height = 50,
+                            Content = new Drawable[][]
+                            {
+                                new Drawable[]
+                                {
+                                    colorCodeTextBox = new TextBox
+                                    {
+                                        RelativeSizeAxes = Axes.Both
+                                    },
+                                    previewColorBox = new Box
+                                    {
+                                        RelativeSizeAxes = Axes.Both
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+
+            colorScroller.Current.BindTo(colorCanvas.Source);
+            colorCanvas.Current.BindTo(current);
+
+            //Testing bindable
+            colorScroller.Current.Value = Color4.Blue;
+        }
+
+        public class ColorCanvas : Container, IHasCurrentValue<Color4>
+        {
+            private readonly BindableWithCurrent<Color4> source = new BindableWithCurrent<Color4>();
+
+            public Bindable<Color4> Source
+            {
+                get => source.Current;
+                set => source.Current = value;
+            }
+
+            private readonly BindableWithCurrent<Color4> current = new BindableWithCurrent<Color4>();
+
+            public Bindable<Color4> Current
+            {
+                get => current.Current;
+                set => current.Current = value;
+            }
+
+            private readonly Box background;
+            private readonly Box background2;
+            private readonly Circle pickerStylus;
+
+            public ColorCanvas()
+            {
+                Children = new Drawable[]
+                {
+                    background = new Box
+                    {
+                        RelativeSizeAxes = Axes.Both,
+                    },
+                    background2 = new Box
+                    {
+                        RelativeSizeAxes = Axes.Both,
+                    },
+                    pickerStylus = new Circle
+                    {
+                        Size = new Vector2(10),
+                        Colour = Color4.Red,
+                    }
+                };
+
+                source.BindValueChanged(value =>
+                {
+                    background.Colour = ColourInfo.GradientHorizontal(Color4.White, value.NewValue);
+                    background2.Colour = ColourInfo.GradientVertical(Color4.White, Color4.Black);
+                });
+            }
+        }
+
+        public class ColorScroller : Container, IHasCurrentValue<Color4>
+        {
+            private readonly BindableWithCurrent<Color4> current = new BindableWithCurrent<Color4>();
+
+            public Bindable<Color4> Current
+            {
+                get => current.Current;
+                set => current.Current = value;
+            }
+
+            private readonly Box background;
+            private readonly Triangle picker;
+
+            public ColorScroller()
+            {
+                Children = new Drawable[]
+                {
+                    background = new Box
+                    {
+                        RelativeSizeAxes = Axes.Both,
+                    },
+                    picker = new Triangle
+                    {
+                        Size = new Vector2(10),
+                        Colour = Color4.Red,
+                    }
+                };
+            }
+        }
+    }
+}
