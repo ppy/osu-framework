2a7a9a545644854777155f0e0369278e89ac4ab3
andy840119 <andy840119@gmail.com>
2019-12-24 11:14:34 +0800

    Implement testcase with counter

--- osu.Framework.Tests/Visual/UserInterface/TestSceneColorPicker.cs
+++ osu.Framework.Tests/Visual/UserInterface/TestSceneColorPicker.cs
@@ -1,6 +1,10 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
+using osu.Framework.Graphics.Sprites;
 using osu.Framework.Graphics.UserInterface;
 using osuTK.Graphics;
 
@@ -8,11 +12,108 @@ namespace osu.Framework.Tests.Visual.UserInterface
 {
     public class TestSceneColorPicker : FrameworkTestScene
     {
+        private int count = 0;
+
+        private readonly ColorPicker colorPicker;
+        private readonly Box previewColorBox;
+        private readonly SpriteText counterText;
+
         public TestSceneColorPicker()
         {
-            var colorPicker = new ColorPicker();
-            colorPicker.Current.Value = Color4.Red;
-            Add(colorPicker);
+            Add(new GridContainer
+            {
+                Width = 500,
+                Height = 300,
+                Content = new Drawable[][]
+                {
+                    new Drawable[]
+                    {
+                        colorPicker = new ColorPicker(),
+                        new GridContainer
+                        {
+                            RelativeSizeAxes = Axes.Both,
+                            Content = new Drawable[][]
+                            {
+                                new Drawable[]
+                                {
+                                    new Container
+                                    {
+                                        RelativeSizeAxes = Axes.Both,
+                                        Padding = new MarginPadding(10),
+                                        Children = new Drawable[]
+                                        {
+                                            previewColorBox = new Box
+                                            {
+                                                RelativeSizeAxes = Axes.Both,
+                                            },
+                                            counterText = new SpriteText
+                                            {
+                                                Anchor = Anchor.Centre,
+                                                Origin = Anchor.Centre
+                                            }
+                                        }
+                                    }
+                                },
+                                new Drawable[]
+                                {
+                                    new GridContainer
+                                    {
+                                        RelativeSizeAxes = Axes.Both,
+                                        Content = new Drawable[][]
+                                        {
+                                            new Drawable[]
+                                            {
+                                                new ClickableColor(colorPicker,Color4.Red),
+                                                new ClickableColor(colorPicker,Color4.Blue),
+                                                new ClickableColor(colorPicker,Color4.White),
+                                            },
+                                            new Drawable[]
+                                            {
+                                                new ClickableColor(colorPicker,Color4.Green),
+                                                new ClickableColor(colorPicker,Color4.Yellow),
+                                                new ClickableColor(colorPicker,Color4.Purple),
+                                            },
+                                            new Drawable[]
+                                            {
+                                                new ClickableColor(colorPicker,Color4.Gray),
+                                                new ClickableColor(colorPicker,Color4.Orange),
+                                                new ClickableColor(colorPicker,Color4.Aqua),
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            });
+
+            colorPicker.Current.BindValueChanged(value =>
+            {
+                count++;
+                previewColorBox.Colour = value.NewValue;
+                counterText.Text = $"{count} changes!";
+            });
+        }
+
+        public class ClickableColor : ClickableContainer
+        {
+            private readonly Box background;
+
+            public ClickableColor(ColorPicker picker, Color4 color)
+            {
+                RelativeSizeAxes = Axes.Both;
+                Child = background = new Box
+                {
+                    RelativeSizeAxes= Axes.Both,
+                    Colour = color
+                };
+
+                Action += () =>
+                {
+                    picker.Current.Value = background.Colour;
+                };
+            }
         }
     }
 }
