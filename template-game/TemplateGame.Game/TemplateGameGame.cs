fa004377c4a4ec5a58211dbdf837c1d205e1aa04
Salman Ahmed <email@iisalman.me>
2020-01-01 18:29:31 +0300

    Add a rotating box for indication of a good game template state

--- template-game/TemplateGame.Game/TemplateGameGame.cs
+++ template-game/TemplateGame.Game/TemplateGameGame.cs
@@ -2,15 +2,32 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Shapes;
+using osuTK;
+using osuTK.Graphics;
 
 namespace TemplateGame.Game
 {
     public class TemplateGameGame : osu.Framework.Game
     {
+        private Box box;
+
         [BackgroundDependencyLoader]
         private void load()
         {
-            // Add your game components here
+            // Add your game components here.
+            // The rotating box can be removed.
+
+            Child = box = new Box
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                Colour = Color4.Orange,
+                Size = new Vector2(200),
+            };
+
+            box.Loop(b => b.RotateTo(0).RotateTo(360, 2500));
         }
     }
 }
