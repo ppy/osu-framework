912234f99dc7b98142fdde6ea32e8d16ee480487
smoogipoo <smoogipoo@smgi.me>
2020-01-09 17:17:20 +0900

    Replace linq query with manual loop

--- osu.Framework/Lists/WeakList.cs
+++ osu.Framework/Lists/WeakList.cs
@@ -49,9 +49,27 @@ namespace osu.Framework.Lists
             return found;
         }
 
-        public bool Contains(T item) => list.Any(t => !t.Invalid && t.Reference.TryGetTarget(out var obj) && obj == item);
+        public bool Contains(T item)
+        {
+            foreach (var t in list)
+            {
+                if (!t.Invalid && t.Reference.TryGetTarget(out var obj) && obj == item)
+                    return true;
+            }
 
-        public bool Contains(WeakReference<T> weakReference) => list.Any(t => !t.Invalid && t.Reference == weakReference);
+            return false;
+        }
+
+        public bool Contains(WeakReference<T> weakReference)
+        {
+            foreach (var t in list)
+            {
+                if (!t.Invalid && t.Reference == weakReference)
+                    return true;
+            }
+
+            return false;
+        }
 
         public void Clear()
         {
