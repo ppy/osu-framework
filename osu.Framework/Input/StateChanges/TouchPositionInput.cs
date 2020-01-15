214ef30e0bd3d984faebc58532dd0717bbc5c139
Salman Ahmed <email@iisalman.me>
2020-01-15 05:01:17 +0300

    Improve touch position input and split button input from modifying position

--- osu.Framework/Input/StateChanges/TouchPositionInput.cs
+++ osu.Framework/Input/StateChanges/TouchPositionInput.cs
@@ -1,59 +1,48 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using osu.Framework.Input.StateChanges.Events;
 using osu.Framework.Input.States;
+using osuTK;
 using osuTK.Input;
 
 namespace osu.Framework.Input.StateChanges
 {
     /// <summary>
-    /// Denotes an absolute change in touch position input.
-    /// When all provided pointers are not active, no changes will be made.
+    /// Denotes an absolute change of a provided touch <see cref="Source"/>'s position.
+    /// Any provided touch source should always be in the range <see cref="MouseButton.Touch1"/> - <see cref="MouseButton.Touch10"/>.
     /// </summary>
     public class TouchPositionInput : IInput
     {
-        public IEnumerable<PositionalPointer> Pointers;
+        /// <summary>
+        /// The touch source to be modified.
+        /// </summary>
+        public readonly MouseButton Source;
+
+        /// <summary>
+        /// The new position to move to.
+        /// </summary>
+        public readonly Vector2 Position;
+
+        public TouchPositionInput(MouseButton source, Vector2 newPosition)
+        {
+            Source = source;
+            Position = newPosition;
+        }
 
         public void Apply(InputState state, IInputStateChangeHandler handler)
         {
             var touch = state.Touch;
 
-            if (Pointers == null)
+            if (Source < MouseButton.Touch1)
                 return;
 
-            var activePointers = touch.Pointers.ToArray();
-            var buttonsState = state.Mouse.Buttons.Clone();
+            Vector2? lastPosition = touch.GetTouchPosition(Source);
+            if (lastPosition != null && lastPosition == Position)
+                return;
 
-            foreach (var pointer in Pointers)
-            {
-                foreach (ref var active in activePointers.AsSpan())
-                {
-                    if (!pointer.Equals(active) || !(pointer.Position != active.Position))
-                        continue;
-
-                    var buttons = new List<MouseButton>();
-                    if (touch.PrimaryPointer?.Equals(pointer) ?? false)
-                        buttons.Add(MouseButton.Left);
-                    else
-                        buttons.Add(pointer.Source);
-
-                    state.Mouse.Buttons.Set(buttons);
-                    state.Mouse.Position = pointer.Position;
-                    handler.HandleInputStateChange(new MousePositionChangeEvent(state, this, active.Position));
-
-                    active = pointer;
-                }
-            }
-
-            state.Mouse.Buttons.Set(buttonsState);
-            if (touch.PrimaryPointer != null)
-                state.Mouse.Position = touch.PrimaryPointer.Value.Position;
-
-            touch.Pointers.Set(activePointers);
+            touch.TouchPositions[Source] = Position;
+            handler.HandleInputStateChange(new TouchPositionChangeEvent(state, this, Source, lastPosition ?? Position));
         }
     }
 }
