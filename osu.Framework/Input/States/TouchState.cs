baade5136197e2c2294d458a7dd8c8a59d2e09de
Salman Ahmed <email@iisalman.me>
2020-01-15 05:03:06 +0300

    Improve touch state data

--- osu.Framework/Input/States/TouchState.cs
+++ osu.Framework/Input/States/TouchState.cs
@@ -1,21 +1,38 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System.Linq;
+using System.Collections.Generic;
+using osuTK;
+using osuTK.Input;
 
 namespace osu.Framework.Input.States
 {
     public class TouchState
     {
         /// <summary>
-        /// Represents a list of currently active pointers.
+        /// The list of currently active touch sources.
         /// </summary>
-        public readonly ButtonStates<PositionalPointer> Pointers = new ButtonStates<PositionalPointer>();
+        public readonly ButtonStates<MouseButton> ActiveSources = new ButtonStates<MouseButton>();
 
         /// <summary>
-        /// Retrieves the recent pointer of all active pointers.
-        /// Null is returned when there are no active pointers.
+        /// The dictionary to retrieve current touch positions from and save them.
+        /// The values in this dictionary remain the same regardless of any touch activity change, use <see cref="GetTouchPosition"/> instead.
         /// </summary>
-        public PositionalPointer? PrimaryPointer => Pointers.Any() ? (PositionalPointer?)Pointers.OrderByDescending(p => p.Source).First() : null;
+        public readonly Dictionary<MouseButton, Vector2> TouchPositions = new Dictionary<MouseButton, Vector2>();
+
+        /// <summary>
+        /// Retrieves the current touch position of a specified <paramref name="source"/>, or null if not active nor existing in the <see cref="TouchPositions"/> dictionary.
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public Vector2? GetTouchPosition(MouseButton source)
+        {
+            if (!IsActive(source) || !TouchPositions.TryGetValue(source, out var pos))
+                return null;
+
+            return pos;
+        }
+
+        public bool IsActive(MouseButton source) => ActiveSources.IsPressed(source);
     }
 }
