8df14193b55132193195faa7ec5655a50d993a88
Dean Herbert <pe@ppy.sh>
2020-02-05 14:29:16 +0900

    Remove licence headers

--- template-game/TemplateGame.Game.Tests/Program.cs
+++ template-game/TemplateGame.Game.Tests/Program.cs
@@ -1,7 +1,4 @@
-﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
-// See the LICENCE file in the repository root for full licence text.
-
-using osu.Framework;
+﻿using osu.Framework;
 using osu.Framework.Platform;
 
 namespace TemplateGame.Game.Tests
