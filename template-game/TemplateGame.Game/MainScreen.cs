c1f3bf124f06c73259b7f488e2899d3011179157
Dean Herbert <pe@ppy.sh>
2020-02-05 13:50:14 +0900

    Make main screen a bit more identifiable

--- template-game/TemplateGame.Game/MainScreen.cs
+++ template-game/TemplateGame.Game/MainScreen.cs
@@ -1,6 +1,9 @@
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
+using osu.Framework.Graphics.Shapes;
+using osu.Framework.Graphics.Sprites;
 using osu.Framework.Screens;
+using osuTK.Graphics;
 
 namespace TemplateGame.Game
 {
@@ -9,10 +12,26 @@ namespace TemplateGame.Game
         [BackgroundDependencyLoader]
         private void load()
         {
-            AddInternal(new SpinningBox
+            InternalChildren = new Drawable[]
             {
-                Anchor = Anchor.Centre,
-            });
+                new Box
+                {
+                    Colour = Color4.Violet,
+                    RelativeSizeAxes = Axes.Both,
+                },
+                new SpriteText
+                {
+                    Y = 20,
+                    Text = "Main Screen",
+                    Anchor = Anchor.TopCentre,
+                    Origin = Anchor.TopCentre,
+                    Font = FontUsage.Default.With(size: 40)
+                },
+                new SpinningBox
+                {
+                    Anchor = Anchor.Centre,
+                }
+            };
         }
     }
 }
