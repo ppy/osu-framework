475eb92347d59dc42498434257ef50e2c4c54134
Shane Woolcock <samahnub@gmail.com>
2020-02-12 19:36:29 +1030

    Add SDL2 native calls for querying display modes

--- osu.Framework/Platform/Sdl2Functions.cs
+++ osu.Framework/Platform/Sdl2Functions.cs
@@ -1,6 +1,8 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
+using System.Drawing;
 using System.Numerics;
 using System.Runtime.InteropServices;
 using Veldrid.Sdl2;
@@ -9,6 +11,8 @@ namespace osu.Framework.Platform
 {
     internal static unsafe class Sdl2Functions
     {
+        #region GL-specific Calls
+
         [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
         private delegate void SdlGLGetDrawableSizeDelegate(SDL_Window window, int* w, int* h);
 
@@ -27,5 +31,113 @@ namespace osu.Framework.Platform
         private static readonly SdlGLGetSwapIntervalDelegate sdl_gl_get_swap_interval = Sdl2Native.LoadFunction<SdlGLGetSwapIntervalDelegate>("SDL_GL_GetSwapInterval");
 
         public static int SDL_GL_GetSwapInterval() => sdl_gl_get_swap_interval();
+
+        #endregion
+
+        #region Display Modes
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        private delegate int SdlGetCurrentDisplayModeDelegate(int displayIndex, SDL_DisplayMode* mode);
+
+        private static readonly SdlGetCurrentDisplayModeDelegate sdl_get_current_display_mode = Sdl2Native.LoadFunction<SdlGetCurrentDisplayModeDelegate>("SDL_GetCurrentDisplayMode");
+
+        public static SDL_DisplayMode SDL_GetCurrentDisplayMode(int displayIndex)
+        {
+            SDL_DisplayMode mode;
+            sdl_get_current_display_mode(displayIndex, &mode);
+            return mode;
+        }
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        private delegate SDL_DisplayMode* SdlGetClosestDisplayModeDelegate(int displayIndex, SDL_DisplayMode* mode, SDL_DisplayMode* closest);
+
+        private static readonly SdlGetClosestDisplayModeDelegate sdl_get_closest_display_mode = Sdl2Native.LoadFunction<SdlGetClosestDisplayModeDelegate>("SDL_GetClosestDisplayMode");
+
+        public static SDL_DisplayMode SDL_GetClosestDisplayMode(int displayIndex, SDL_DisplayMode requested)
+        {
+            SDL_DisplayMode closest;
+            sdl_get_closest_display_mode(displayIndex, &requested, &closest);
+            return closest;
+        }
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        private delegate int SdlGetDisplayModeDelegate(int displayIndex, int modeIndex, SDL_DisplayMode* mode);
+
+        private static readonly SdlGetDisplayModeDelegate sdl_get_display_mode = Sdl2Native.LoadFunction<SdlGetDisplayModeDelegate>("SDL_GetDisplayMode");
+
+        public static SDL_DisplayMode SDL_GetDisplayMode(int displayIndex, int modeIndex)
+        {
+            SDL_DisplayMode mode;
+            sdl_get_display_mode(displayIndex, modeIndex, &mode);
+            return mode;
+        }
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        private delegate int SdlGetWindowDisplayModeDelegate(IntPtr window, SDL_DisplayMode* mode);
+
+        private static readonly SdlGetWindowDisplayModeDelegate sdl_get_window_display_mode = Sdl2Native.LoadFunction<SdlGetWindowDisplayModeDelegate>("SDL_GetWindowDisplayMode");
+
+        public static SDL_DisplayMode SDL_GetWindowDisplayMode(SDL_Window window)
+        {
+            SDL_DisplayMode mode;
+            sdl_get_window_display_mode(window.NativePointer, &mode);
+            return mode;
+        }
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        private delegate int SdlSetWindowDisplayModeDelegate(IntPtr window, SDL_DisplayMode* mode);
+
+        private static readonly SdlSetWindowDisplayModeDelegate sdl_set_window_display_mode = Sdl2Native.LoadFunction<SdlSetWindowDisplayModeDelegate>("SDL_SetWindowDisplayMode");
+
+        public static int SDL_SetWindowDisplayMode(SDL_Window window, SDL_DisplayMode mode) => sdl_set_window_display_mode(window.NativePointer, &mode);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        private delegate int SdlGetNumDisplayModesDelegate(int displayIndex);
+
+        private static readonly SdlGetNumDisplayModesDelegate sdl_get_num_display_modes = Sdl2Native.LoadFunction<SdlGetNumDisplayModesDelegate>("SDL_GetNumDisplayModes");
+
+        public static int SDL_GetNumDisplayModes(int displayIndex) => sdl_get_num_display_modes(displayIndex);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        private delegate int SdlGetNumVideoDisplaysDelegate();
+
+        private static readonly SdlGetNumVideoDisplaysDelegate sdl_get_num_video_displays = Sdl2Native.LoadFunction<SdlGetNumVideoDisplaysDelegate>("SDL_GetNumVideoDisplays");
+
+        public static int SDL_GetNumVideoDisplays() => sdl_get_num_video_displays();
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        private delegate int SdlGetDisplayBoundsDelegate(int displayIndex, SDL_Rect* rect);
+
+        private static readonly SdlGetDisplayBoundsDelegate sdl_get_display_bounds = Sdl2Native.LoadFunction<SdlGetDisplayBoundsDelegate>("SDL_GetDisplayBounds");
+
+        public static Rectangle SDL_GetDisplayBounds(int displayIndex)
+        {
+            SDL_Rect rect;
+            sdl_get_display_bounds(displayIndex, &rect);
+            return new Rectangle(rect.X, rect.Y, rect.Width, rect.Height);
+        }
+
+        #endregion
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    // ReSharper disable once InconsistentNaming
+    internal struct SDL_DisplayMode
+    {
+        public uint Format;
+        public int Width;
+        public int Height;
+        public int RefreshRate;
+        public IntPtr DriverData;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    // ReSharper disable once InconsistentNaming
+    internal struct SDL_Rect
+    {
+        public int X;
+        public int Y;
+        public int Width;
+        public int Height;
     }
 }
