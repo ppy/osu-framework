d1faf991873b72cdd2a8e537e25cfdf48cde6328
Dean Herbert <pe@ppy.sh>
2020-02-12 16:50:48 +0900

    Add xmldoc to enum

--- osu.Framework/Threading/ScheduledDelegate.cs
+++ osu.Framework/Threading/ScheduledDelegate.cs
@@ -131,11 +131,29 @@ namespace osu.Framework.Threading
             }
         }
 
+        /// <summary>
+        /// The current state of a scheduled delegate.
+        /// </summary>
         public enum RunState
         {
+            /// <summary>
+            /// Waiting to run. Potentially not the first run if on a repeating schedule.
+            /// </summary>
             Waiting,
+
+            /// <summary>
+            /// Currently running.
+            /// </summary>
             Running,
+
+            /// <summary>
+            /// Running completed for a final time.
+            /// </summary>
             Complete,
+
+            /// <summary>
+            /// Task manually cancelled.
+            /// </summary>
             Cancelled
         }
     }
