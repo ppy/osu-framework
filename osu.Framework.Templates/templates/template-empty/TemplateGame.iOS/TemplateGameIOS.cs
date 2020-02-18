34acb310337c20490db43cb69f2aab0bba8fce06
Tim Oliver <me@timoliver.com.au>
2020-02-18 17:30:10 +0900

    Added basic iOS project to GameTemplate solution

---
+++ osu.Framework.Templates/templates/template-empty/TemplateGame.iOS/TemplateGameIOS.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using TemplateGame.Game;
+
+namespace TemplateGame.iOS
+{
+    public class TemplateGameIOS : TemplateGameGame
+    {
+    }
+}
