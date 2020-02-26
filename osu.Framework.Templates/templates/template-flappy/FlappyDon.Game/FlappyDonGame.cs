41458ef878cc57a9fc1bcc2eac0cab85085dd3ac
Tim Oliver <me@timoliver.com.au>
2020-02-26 17:20:51 +0900

    Performed code cleanup

--- osu.Framework.Templates/templates/template-flappy/FlappyDon.Game/FlappyDonGame.cs
+++ osu.Framework.Templates/templates/template-flappy/FlappyDon.Game/FlappyDonGame.cs
@@ -1,9 +1,7 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System;
 using osu.Framework.Allocation;
-using osu.Framework.Audio.Track;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Audio;
 using osu.Framework.Graphics.Containers;
--- osu.Framework.Templates/templates/template-flappy/FlappyDon.Game/Testing/FlappyDonTestBrowser.cs
+++ osu.Framework.Templates/templates/template-flappy/FlappyDon.Game/Testing/FlappyDonTestBrowser.cs
@@ -36,4 +36,3 @@ namespace FlappyDon.Game.Testing
         }
     }
 }
-
