dcf4e0d36b37c1086d28833e799b03d50ebc5bf5
smoogipoo <smoogipoo@smgi.me>
2020-02-26 15:05:12 +0900

    0 is not a valid enum value for a flag

--- osu.Framework/Layout/InvalidationSource.cs
+++ osu.Framework/Layout/InvalidationSource.cs
@@ -12,17 +12,17 @@ namespace osu.Framework.Layout
         /// <summary>
         /// The invalidation originates from the current <see cref="Drawable"/>.
         /// </summary>
-        Self = 0,
+        Self = 1,
 
         /// <summary>
         /// The invalidation originates from a parent in the supertree of the current <see cref="Drawable"/>.
         /// </summary>
-        Parent = 1,
+        Parent = 2,
 
         /// <summary>
         /// The invalidation originates from a child in the subtree of the current <see cref="Drawable"/>.
         /// </summary>
-        Child = 2,
+        Child = 4,
 
         /// <summary>
         /// The default invalidation source. Deconstructs into <see cref="Self"/> <code>or</code> <see cref="Parent"/>.
