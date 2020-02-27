3bdaa3a313242d7a4230ceb5326d4126fff5e0ff
smoogipoo <smoogipoo@smgi.me>
2020-02-27 15:23:13 +0900

    Remove license headers

--- osu.Framework.Templates/templates/template-flappy/FlappyDon.Game/FlappyDonGameBase.cs
+++ osu.Framework.Templates/templates/template-flappy/FlappyDon.Game/FlappyDonGameBase.cs
@@ -1,7 +1,4 @@
-﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
-// See the LICENCE file in the repository root for full licence text.
-
-using FlappyDon.Resources;
+﻿using FlappyDon.Resources;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics.Textures;
 using osu.Framework.IO.Stores;
