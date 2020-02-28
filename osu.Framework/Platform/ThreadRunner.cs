a25892163e00809273a851cb4fac97a6d292e1a0
Dean Herbert <pe@ppy.sh>
2020-02-28 18:00:33 +0900

    Move ExecutionMode to own file

--- osu.Framework/Platform/ThreadRunner.cs
+++ osu.Framework/Platform/ThreadRunner.cs
@@ -6,7 +6,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
 using osu.Framework.Development;
@@ -199,14 +198,5 @@ namespace osu.Framework.Platform
                 mainThread.InactiveHz = GameThread.DEFAULT_INACTIVE_HZ;
             }
         }
-    }
-
-    public enum ExecutionMode
-    {
-        [Description("Single thread")]
-        SingleThread,
-
-        [Description("Multithreaded")]
-        MultiThreaded
     }
 }
