d50668f10617b23aeaef4afe71342f511c13b257
Tim Oliver <me@timoliver.com.au>
2020-03-12 01:50:06 +0800

    Refined thread logic to not null active mode right after setting

--- osu.Framework/Platform/ThreadRunner.cs
+++ osu.Framework/Platform/ThreadRunner.cs
@@ -122,9 +122,7 @@ namespace osu.Framework.Platform
 
         public void Suspend()
         {
-            // shut down threads in reverse to ensure audio stops last (other threads may be waiting on a queued event otherwise)
-            foreach (var t in Threads.Reverse())
-                t.Pause();
+            pauseAllThreads();
 
             // set the active execution mode back to null to set the state checking back to when it can be resumed.
             activeExecutionMode = null;
@@ -157,8 +155,7 @@ namespace osu.Framework.Platform
                 // in the case we have not yet got an execution mode, set this early to allow usage in GameThread.Initialize overrides.
                 activeExecutionMode = ThreadSafety.ExecutionMode = ExecutionMode;
 
-            // Suspend all threads
-            Suspend();
+            pauseAllThreads();
 
             switch (ExecutionMode)
             {
@@ -193,6 +190,13 @@ namespace osu.Framework.Platform
             activeExecutionMode = ThreadSafety.ExecutionMode = ExecutionMode;
 
             updateMainThreadRates();
+        }
+
+        private void pauseAllThreads()
+        {
+            // shut down threads in reverse to ensure audio stops last (other threads may be waiting on a queued event otherwise)
+            foreach (var t in Threads.Reverse())
+                t.Pause();
         }
 
         private void updateMainThreadRates()
