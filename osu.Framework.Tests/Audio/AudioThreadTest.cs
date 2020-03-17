018adf7ffcd815cda8d3230e87d0c66fdb9a14ff
Kelvin <2yangk23@gmail.com>
2020-03-17 13:37:15 -0700

    Call Bass.Free() after each test

--- osu.Framework.Tests/Audio/AudioThreadTest.cs
+++ osu.Framework.Tests/Audio/AudioThreadTest.cs
@@ -4,6 +4,7 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using ManagedBass;
 using NUnit.Framework;
 using osu.Framework.Audio.Track;
 using osu.Framework.IO.Stores;
@@ -37,6 +38,8 @@ namespace osu.Framework.Tests.Audio
             thread.Exit();
 
             WaitForOrAssert(() => thread.Exited, "Audio thread did not exit in time");
+
+            Bass.Free();
         }
 
         public void CheckTrackIsProgressing(Track track)
