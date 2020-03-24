b7f8340308391251ebd5dc65686f71ff61e18ec0
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2020-03-24 15:08:10 +0800

    Use ConcurrentDictionary in LargeTextureStore.

--- osu.Framework/Graphics/Textures/LargeTextureStore.cs
+++ osu.Framework/Graphics/Textures/LargeTextureStore.cs
@@ -1,7 +1,7 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System.Collections.Generic;
+using System.Collections.Concurrent;
 using System.Diagnostics;
 using System.Threading;
 using osu.Framework.IO.Stores;
@@ -15,7 +15,7 @@ namespace osu.Framework.Graphics.Textures
     public class LargeTextureStore : TextureStore
     {
         private readonly object referenceCountLock = new object();
-        private readonly Dictionary<string, TextureWithRefCount.ReferenceCount> referenceCounts = new Dictionary<string, TextureWithRefCount.ReferenceCount>();
+        private readonly ConcurrentDictionary<string, TextureWithRefCount.ReferenceCount> referenceCounts = new ConcurrentDictionary<string, TextureWithRefCount.ReferenceCount>();
 
         public LargeTextureStore(IResourceStore<TextureUpload> store = null)
             : base(store, false, All.Linear, true)
@@ -31,25 +31,21 @@ namespace osu.Framework.Graphics.Textures
         /// <returns>The texture.</returns>
         public override Texture Get(string name)
         {
-            lock (referenceCountLock)
-            {
-                var tex = base.Get(name);
+            var tex = base.Get(name);
 
-                if (tex?.TextureGL == null)
-                    return null;
+            if (tex?.TextureGL == null)
+                return null;
 
-                if (!referenceCounts.TryGetValue(name, out TextureWithRefCount.ReferenceCount count))
-                    referenceCounts[name] = count = new TextureWithRefCount.ReferenceCount(referenceCountLock, () => onAllReferencesLost(name));
+            var count = referenceCounts.GetOrAdd(name, n => new TextureWithRefCount.ReferenceCount(referenceCountLock, () => onAllReferencesLost(name)));
 
-                return new TextureWithRefCount(tex.TextureGL, count);
-            }
+            return new TextureWithRefCount(tex.TextureGL, count);
         }
 
         private void onAllReferencesLost(string name)
         {
             Debug.Assert(Monitor.IsEntered(referenceCountLock));
 
-            referenceCounts.Remove(name);
+            referenceCounts.TryRemove(name, out _);
             Purge(name);
         }
     }
