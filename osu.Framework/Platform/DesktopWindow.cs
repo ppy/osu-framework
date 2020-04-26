7872bb5bab7e604a3589c46e53e32047008dfcc3
Shane Woolcock <samahnub@gmail.com>
2020-04-26 22:01:31 +0930

    Initial SDL window backend

--- osu.Framework/Platform/DesktopWindow.cs
+++ osu.Framework/Platform/DesktopWindow.cs
@@ -1,6 +1,8 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.Platform.Sdl;
+
 namespace osu.Framework.Platform
 {
     public class DesktopWindow : Window
