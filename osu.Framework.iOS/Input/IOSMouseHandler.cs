0f57eca73f7655aa240870e07228e9b80bdbc2d3
Tim Oliver <me@timoliver.com.au>
2020-05-08 16:33:15 +0800

    Added delegate and callback to mouse handler class

--- osu.Framework.iOS/Input/IOSMouseHandler.cs
+++ osu.Framework.iOS/Input/IOSMouseHandler.cs
@@ -2,18 +2,22 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
+using CoreGraphics;
 using Foundation;
 using ObjCRuntime;
 using osu.Framework.Input.Handlers;
+using osu.Framework.Input.StateChanges;
 using osu.Framework.Platform;
+using osuTK;
 using UIKit;
 
 namespace osu.Framework.iOS.Input
 {
-    public class IOSMouseHandler : InputHandler, IUIPointerInteractionDelegate
+    public class IOSMouseHandler : InputHandler
     {
         private readonly IOSGameView view;
         private readonly UIPointerInteraction pointerInteraction;
+        private readonly IOSMouseDelegate mouseDelegate;
 
         public IOSMouseHandler(IOSGameView view)
         {
@@ -22,16 +26,40 @@ namespace osu.Framework.iOS.Input
                 return;
 
             this.view = view;
-            view.AddInteraction(pointerInteraction = new UIPointerInteraction(this));
+            view.AddInteraction(pointerInteraction = new UIPointerInteraction(mouseDelegate = new IOSMouseDelegate(view)));
+            mouseDelegate.LocationUpdated += locationUpdated;
         }
 
         public override bool IsActive => (pointerInteraction != null);
 
         public override int Priority => 1; // Touches always take priority
 
-        public IntPtr Handle => throw new NotImplementedException();
-
         public override bool Initialize(GameHost host) => true;
+
+        private void locationUpdated(CGPoint location)
+        {
+            PendingInputs.Enqueue(new MousePositionAbsoluteInput { Position = new Vector2((float)location.X * view.Scale,
+                (float)location.Y * view.Scale) });
+        }
+    }
+
+    public class IOSMouseDelegate: NSObject, IUIPointerInteractionDelegate
+    {
+        private readonly IOSGameView view;
+
+        public Action<CGPoint> LocationUpdated;
+
+        public IOSMouseDelegate(IOSGameView view)
+        {
+            this.view = view;
+        }
+
+        [Export("pointerInteraction:regionForRequest:defaultRegion:")]
+        public UIPointerRegion GetRegionForRequest(UIPointerInteraction interaction, UIPointerRegionRequest request, UIPointerRegion defaultRegion)
+        {
+            LocationUpdated(request.Location);
+            return defaultRegion;
+        }
 
         [Export("pointerInteraction:styleForRegion:")]
         public UIPointerStyle GetStyleForRegion(UIPointerInteraction interaction, UIPointerRegion region)
