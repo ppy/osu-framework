6dd4cdf57f9daaae881d0afe02e57d264cbe5d16
Shane Woolcock <samahnub@gmail.com>
2020-05-20 16:41:11 +0930

    Use Colour4 for SRGBColour backing

--- osu.Framework/Graphics/Colour/SRGBColour.cs
+++ osu.Framework/Graphics/Colour/SRGBColour.cs
@@ -9,17 +9,19 @@ using System;
 namespace osu.Framework.Graphics.Colour
 {
     /// <summary>
-    /// A wrapper struct around Color4 that takes care of converting between sRGB and linear colour spaces.
+    /// A wrapper struct around <see cref="Colour4"/> that takes care of converting between sRGB and linear colour spaces.
     /// Internally this struct stores the colour in linear space, which is exposed by the Linear member.
-    /// This struct implicitly converts to sRGB space Color4 values (i.e. it can be assigned and implicitly cast)
-    /// to sRGB Color4.
+    /// This struct implicitly converts to sRGB space <see cref="Colour4"/> values (i.e. it can be assigned and implicitly cast)
+    /// to sRGB <see cref="Colour4"/>.
     /// </summary>
     public struct SRGBColour : IEquatable<SRGBColour>
     {
-        public Color4 Linear;
+        public Colour4 Linear;
 
+#pragma warning disable CS0618
         public static implicit operator SRGBColour(Color4 value) => new SRGBColour { Linear = value.ToLinear() };
         public static implicit operator Color4(SRGBColour value) => value.Linear.ToSRGB();
+#pragma warning restore CS0618
 
         public static implicit operator SRGBColour(Colour4 value) => new SRGBColour { Linear = value.ToLinear() };
         public static implicit operator Colour4(SRGBColour value) => value.Linear.ToSRGB();
@@ -32,7 +34,7 @@ namespace osu.Framework.Graphics.Colour
         /// <returns>Product of first and second.</returns>
         public static SRGBColour operator *(SRGBColour first, SRGBColour second) => new SRGBColour
         {
-            Linear = new Color4(
+            Linear = new Colour4(
                 first.Linear.R * second.Linear.R,
                 first.Linear.G * second.Linear.G,
                 first.Linear.B * second.Linear.B,
@@ -41,7 +43,7 @@ namespace osu.Framework.Graphics.Colour
 
         public static SRGBColour operator *(SRGBColour first, float second) => new SRGBColour
         {
-            Linear = new Color4(
+            Linear = new Colour4(
                 first.Linear.R * second,
                 first.Linear.G * second,
                 first.Linear.B * second,
@@ -52,7 +54,7 @@ namespace osu.Framework.Graphics.Colour
 
         public static SRGBColour operator +(SRGBColour first, SRGBColour second) => new SRGBColour
         {
-            Linear = new Color4(
+            Linear = new Colour4(
                 first.Linear.R + second.Linear.R,
                 first.Linear.G + second.Linear.G,
                 first.Linear.B + second.Linear.B,
@@ -60,13 +62,13 @@ namespace osu.Framework.Graphics.Colour
         };
 
         public readonly Vector4 ToVector() => new Vector4(Linear.R, Linear.G, Linear.B, Linear.A);
-        public static SRGBColour FromVector(Vector4 v) => new SRGBColour { Linear = new Color4(v.X, v.Y, v.Z, v.W) };
+        public static SRGBColour FromVector(Vector4 v) => new SRGBColour { Linear = new Colour4(v.X, v.Y, v.Z, v.W) };
 
         /// <summary>
         /// Multiplies the alpha value of this colour by the given alpha factor.
         /// </summary>
         /// <param name="alpha">The alpha factor to multiply with.</param>
-        public void MultiplyAlpha(float alpha) => Linear.A *= alpha;
+        public void MultiplyAlpha(float alpha) => Linear = Linear.MultiplyAlpha(alpha);
 
         public readonly bool Equals(SRGBColour other) => Linear.Equals(other.Linear);
         public override readonly string ToString() => Linear.ToString();
