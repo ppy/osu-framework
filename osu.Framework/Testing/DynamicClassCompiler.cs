25efe13f39d040922f49ee12c45136010e889431
Fuewburvpoa <fuewburvpoa@yandex.ru>
2020-05-24 22:36:07 +0300

    Add pdb to DynamicClassCompiler to show more info in stack trace

--- osu.Framework/Testing/DynamicClassCompiler.cs
+++ osu.Framework/Testing/DynamicClassCompiler.cs
@@ -11,6 +11,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
+using System.Text;
 
 namespace osu.Framework.Testing
 {
@@ -147,22 +148,25 @@ namespace osu.Framework.Testing
 
                 var compilation = CSharpCompilation.Create(
                     dynamicNamespace,
-                    requiredFiles.Select(file => CSharpSyntaxTree.ParseText(File.ReadAllText(file), parseOptions, file))
+                    requiredFiles.Select(file => CSharpSyntaxTree.ParseText(File.ReadAllText(file, Encoding.UTF8), parseOptions, file, encoding: Encoding.UTF8))
                                  // Compile the assembly with a new version so that it replaces the existing one
                                  .Append(CSharpSyntaxTree.ParseText($"using System.Reflection; [assembly: AssemblyVersion(\"{assemblyVersion}\")]", parseOptions)),
                     references,
                     options
                 );
 
+                using (var symbols = new MemoryStream())
                 using (var ms = new MemoryStream())
                 {
-                    var compilationResult = compilation.Emit(ms);
+                    var compilationResult = compilation.Emit(ms, symbols);
 
                     if (compilationResult.Success)
                     {
                         ms.Seek(0, SeekOrigin.Begin);
+                        symbols.Seek(0, SeekOrigin.Begin);
+
                         CompilationFinished?.Invoke(
-                            Assembly.Load(ms.ToArray()).GetModules()[0].GetTypes().LastOrDefault(t => t.FullName == targetType.FullName)
+                            Assembly.Load(ms.ToArray(), symbols.ToArray()).GetModules()[0].GetTypes().LastOrDefault(t => t.FullName == targetType.FullName)
                         );
                     }
                     else
