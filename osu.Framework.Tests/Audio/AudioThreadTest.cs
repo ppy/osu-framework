dab50368021302b1857e6e3585d77e6bab01da62
smoogipoo <smoogipoo@smgi.me>
2020-06-10 23:11:42 +0900

    Remove unnecessary disposal

--- osu.Framework.Tests/Audio/AudioThreadTest.cs
+++ osu.Framework.Tests/Audio/AudioThreadTest.cs
@@ -36,8 +36,6 @@ namespace osu.Framework.Tests.Audio
 
             thread.Exit();
 
-            Manager?.Dispose();
-
             WaitForOrAssert(() => thread.Exited, "Audio thread did not exit in time");
         }
 
