9175b6a1d87950bdb89246c7889c0e6a7a298aa0
Dean Herbert <pe@ppy.sh>
2020-06-22 21:01:51 +0900

    Add basic test

---
+++ osu.Framework.Tests/Visual/Testing/TestSceneTestWithValues.cs
@@ -0,0 +1,16 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using NUnit.Framework;
+using osu.Framework.Tests.Bindables;
+
+namespace osu.Framework.Tests.Visual.Testing
+{
+    public class TestSceneTestWithValues : FrameworkTestScene
+    {
+        [Test]
+        public void TestValues([Values] BindableEnumTest.TestEnum vals1, [Values] BindableEnumTest.TestEnum vals2)
+        {
+        }
+    }
+}
