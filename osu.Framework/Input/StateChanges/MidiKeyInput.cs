c51505e80b998eed18893178732fec434aa78b49
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2020-07-05 12:02:20 +0200

    Add MIDI input handling to passthrough input manager

--- osu.Framework/Input/StateChanges/MidiKeyInput.cs
+++ osu.Framework/Input/StateChanges/MidiKeyInput.cs
@@ -1,29 +1,35 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Collections.Generic;
 using osu.Framework.Input.States;
 
 namespace osu.Framework.Input.StateChanges
 {
     public class MidiKeyInput : ButtonInput<MidiKey>
     {
-        public readonly MidiKey Button;
-        public readonly byte Velocity;
-        public readonly bool IsPressed;
+        public readonly IReadOnlyDictionary<MidiKey, byte> Velocities;
 
         public MidiKeyInput(MidiKey button, byte velocity, bool isPressed)
             : base(button, isPressed)
         {
-            Button = button;
-            Velocity = velocity;
-            IsPressed = isPressed;
+            Velocities = new Dictionary<MidiKey, byte> { [button] = velocity };
+        }
+
+        public MidiKeyInput(MidiState currentState, MidiState previousState)
+            : base(currentState.Keys, previousState.Keys)
+        {
+            // newer velocities always take precedence
+            // if the newer midi state doesn't specify a velocity for a key, it will be preserved after Apply()
+            Velocities = new Dictionary<MidiKey, byte>(currentState.Velocities);
         }
 
         protected override ButtonStates<MidiKey> GetButtonStates(InputState state) => state.Midi.Keys;
 
         public override void Apply(InputState state, IInputStateChangeHandler handler)
         {
-            state.Midi.Velocities[Button] = Velocity;
+            foreach (var (key, velocity) in Velocities)
+                state.Midi.Velocities[key] = velocity;
 
             base.Apply(state, handler);
         }
