ae5b17296f07b4b40ed1ee08df0a7c7761b201b1
Shane Woolcock <samahnub@gmail.com>
2020-08-13 14:48:56 +0930

    Remember window relative position

--- osu.Framework/Platform/DesktopWindow.cs
+++ osu.Framework/Platform/DesktopWindow.cs
@@ -11,8 +11,37 @@ namespace osu.Framework.Platform
     public class DesktopWindow : Window
     {
         private readonly BindableSize sizeWindowed = new BindableSize();
+        private readonly BindableDouble windowPositionX = new BindableDouble();
+        private readonly BindableDouble windowPositionY = new BindableDouble();
 
         public readonly Bindable<ConfineMouseMode> ConfineMouseMode = new Bindable<ConfineMouseMode>();
+
+        protected Vector2 RelativePosition
+        {
+            get
+            {
+                var displayBounds = CurrentDisplay.Value.Bounds;
+                var windowX = Position.Value.X - displayBounds.X;
+                var windowY = Position.Value.Y - displayBounds.Y;
+                var windowSize = sizeWindowed.Value;
+
+                return new Vector2(
+                    displayBounds.Width > windowSize.Width ? (float)windowX / (displayBounds.Width - windowSize.Width) : 0,
+                    displayBounds.Height > windowSize.Height ? (float)windowY / (displayBounds.Height - windowSize.Height) : 0);
+            }
+            set
+            {
+                if (WindowMode.Value != Configuration.WindowMode.Windowed)
+                    return;
+
+                var displayBounds = CurrentDisplay.Value.Bounds;
+                var windowSize = sizeWindowed.Value;
+                var windowX = (int)Math.Round((displayBounds.Width - windowSize.Width) * value.X);
+                var windowY = (int)Math.Round((displayBounds.Height - windowSize.Height) * value.Y);
+
+                Position.Value = new Point(windowX + displayBounds.X, windowY + displayBounds.Y);
+            }
+        }
 
         /// <summary>
         /// Initialises a window for desktop platforms.
@@ -35,16 +64,41 @@ namespace osu.Framework.Platform
 
             config.BindWith(FrameworkSetting.WindowedSize, sizeWindowed);
 
+            config.BindWith(FrameworkSetting.WindowedPositionX, windowPositionX);
+            config.BindWith(FrameworkSetting.WindowedPositionY, windowPositionY);
+
+            RelativePosition = new Vector2((float)windowPositionX.Value, (float)windowPositionY.Value);
             config.BindWith(FrameworkSetting.ConfineMouseMode, ConfineMouseMode);
             ConfineMouseMode.BindValueChanged(confineMouseModeChanged, true);
 
             Resized += onResized;
+            Moved += onMoved;
         }
 
         private void onResized()
         {
-            if (!Size.Value.IsEmpty && WindowMode.Value == Configuration.WindowMode.Windowed)
-                sizeWindowed.Value = Size.Value;
+            if (WindowState.Value == Platform.WindowState.Normal)
+            {
+                sizeWindowed.Value = WindowBackend.Size;
+                Size.Value = sizeWindowed.Value;
+                updateWindowPositionConfig();
+            }
+        }
+
+        private void onMoved(Point point)
+        {
+            if (WindowState.Value == Platform.WindowState.Normal)
+                updateWindowPositionConfig();
+        }
+
+        private void updateWindowPositionConfig()
+        {
+            if (WindowState.Value == Platform.WindowState.Normal)
+            {
+                var relativePosition = RelativePosition;
+                windowPositionX.Value = relativePosition.X;
+                windowPositionY.Value = relativePosition.Y;
+            }
         }
 
         private void confineMouseModeChanged(ValueChangedEvent<ConfineMouseMode> args)
