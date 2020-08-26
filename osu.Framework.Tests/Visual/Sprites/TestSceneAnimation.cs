bcfa722dfb884aa19f8183c3cf8c2b7406092258
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2020-08-26 15:34:13 +0200

    Move frame construction in animation tests to ctor

--- osu.Framework.Tests/Visual/Sprites/TestSceneAnimation.cs
+++ osu.Framework.Tests/Visual/Sprites/TestSceneAnimation.cs
@@ -24,6 +24,9 @@ namespace osu.Framework.Tests.Visual.Sprites
         private TestAnimation animation;
         private Container animationContainer;
 
+        [Resolved]
+        private FontStore fontStore { get; set; }
+
         [SetUpSteps]
         public void SetUpSteps()
         {
@@ -232,7 +235,7 @@ namespace osu.Framework.Tests.Visual.Sprites
         {
             AddStep("load animation", () =>
             {
-                animationContainer.Child = animation = new TestAnimation(startFromCurrent)
+                animationContainer.Child = animation = new TestAnimation(startFromCurrent, fontStore)
                 {
                     Loop = false,
                 };
@@ -260,27 +263,16 @@ namespace osu.Framework.Tests.Visual.Sprites
         {
             public const int LOADABLE_FRAMES = 72;
 
-            [Resolved]
-            private FontStore fontStore { get; set; }
-
             public int FramesProcessed;
 
-            public TestAnimation(bool startFromCurrent)
+            // fontStore passed in via ctor to be able to test scenarios where an animation
+            // already has frames before load
+            public TestAnimation(bool startFromCurrent, FontStore fontStore)
                 : base(startFromCurrent)
             {
                 Anchor = Anchor.Centre;
                 Origin = Anchor.Centre;
-            }
 
-            protected override void DisplayFrame(Texture content)
-            {
-                FramesProcessed++;
-                base.DisplayFrame(content);
-            }
-
-            [BackgroundDependencyLoader]
-            private void load()
-            {
                 for (int i = 0; i < LOADABLE_FRAMES; i++)
                 {
                     AddFrame(new Texture(fontStore.Get(null, (char)('0' + i))?.Texture.TextureGL)
@@ -288,6 +280,12 @@ namespace osu.Framework.Tests.Visual.Sprites
                         ScaleAdjust = 1 + i / 40f,
                     }, 250);
                 }
+            }
+
+            protected override void DisplayFrame(Texture content)
+            {
+                FramesProcessed++;
+                base.DisplayFrame(content);
             }
         }
     }
