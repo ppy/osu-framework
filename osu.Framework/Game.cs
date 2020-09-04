f40ac565606b734deef2b81d4663c31da5a0c6b8
Dean Herbert <pe@ppy.sh>
2020-09-04 13:59:04 +0900

    Fix broken comment
    Co-authored-by: Dan Balasescu <smoogipoo@smgi.me>

--- osu.Framework/Game.cs
+++ osu.Framework/Game.cs
@@ -352,7 +352,7 @@ namespace osu.Framework
         protected override void Dispose(bool isDisposing)
         {
             // ensure any async disposals are completed before we begin to rip components out.
-            // if we were to not wait, async disposals may through unexpected exceptions.
+            // if we were to not wait, async disposals may throw unexpected exceptions.
             AsyncDisposalQueue.WaitForEmpty();
 
             base.Dispose(isDisposing);
