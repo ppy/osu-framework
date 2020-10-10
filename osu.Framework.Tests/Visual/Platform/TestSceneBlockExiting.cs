901a64cc18bfec2851657950f97b176308aa4097
Dean Herbert <pe@ppy.sh>
2020-10-10 22:28:53 +0900

    Add test coverage for exit blocking

---
+++ osu.Framework.Tests/Visual/Platform/TestSceneBlockExiting.cs
@@ -0,0 +1,26 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Allocation;
+using osu.Framework.Bindables;
+
+namespace osu.Framework.Tests.Visual.Platform
+{
+    public class TestSceneBlockExiting : FrameworkTestScene
+    {
+        private Bindable<bool> blockingClose;
+
+        [BackgroundDependencyLoader]
+        private void load(TestGame game)
+        {
+            blockingClose = game.BlockExit.GetBoundCopy();
+        }
+
+        protected override void LoadComplete()
+        {
+            base.LoadComplete();
+
+            AddToggleStep("Block closing window", b => blockingClose.Value = b);
+        }
+    }
+}
