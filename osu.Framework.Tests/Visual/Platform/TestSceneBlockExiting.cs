51224b407fc91c2f1da9fffa955ef70ca93595cd
Dean Herbert <pe@ppy.sh>
2020-10-10 22:49:40 +0900

    Ignore added test for simplicity

--- osu.Framework.Tests/Visual/Platform/TestSceneBlockExiting.cs
+++ osu.Framework.Tests/Visual/Platform/TestSceneBlockExiting.cs
@@ -1,11 +1,13 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using NUnit.Framework;
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
 
 namespace osu.Framework.Tests.Visual.Platform
 {
+    [Ignore("for manual testing")]
     public class TestSceneBlockExiting : FrameworkTestScene
     {
         private Bindable<bool> blockingClose;
