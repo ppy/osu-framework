45da7088003ef603b83f8f7d8ca16ee0c7f34482
smoogipoo <smoogipoo@smgi.me>
2020-11-24 14:14:53 +0900

    Fix PoolableDrawable being returned from child invalidations

--- osu.Framework/Graphics/Pooling/PoolableDrawable.cs
+++ osu.Framework/Graphics/Pooling/PoolableDrawable.cs
@@ -115,7 +115,7 @@ namespace osu.Framework.Graphics.Pooling
 
         protected override bool OnInvalidate(Invalidation invalidation, InvalidationSource source)
         {
-            if (invalidation.HasFlag(Invalidation.Parent))
+            if (source != InvalidationSource.Child && invalidation.HasFlag(Invalidation.Parent))
             {
                 if (IsInUse && Parent == null)
                     Return();
