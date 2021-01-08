1fc0a43176ac259512e022bd5145fbae11fd8992
Dean Herbert <pe@ppy.sh>
2021-01-08 11:20:51 +0900

    Move ISampleStore to correct namespace

--- osu.Framework.Tests/Visual/Audio/TestSceneSamples.cs
+++ osu.Framework.Tests/Visual/Audio/TestSceneSamples.cs
@@ -4,6 +4,7 @@
 using System.Linq;
 using osu.Framework.Allocation;
 using osu.Framework.Audio;
+using osu.Framework.Audio.Sample;
 using osu.Framework.Audio.Track;
 using osu.Framework.Extensions.IEnumerableExtensions;
 using osu.Framework.Graphics;
