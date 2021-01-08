37b4bacd2265679f0655d935e07c4f1bdad7a47a
Dean Herbert <pe@ppy.sh>
2021-01-08 14:25:27 +0900

    Extract interface out of KeyBinding

---
+++ osu.Framework/Input/Bindings/IKeyBinding.cs
@@ -0,0 +1,31 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+
+namespace osu.Framework.Input.Bindings
+{
+    /// <summary>
+    /// A binding of a <see cref="Bindings.KeyCombination"/> to an action.
+    /// </summary>
+    public interface IKeyBinding
+    {
+        /// <summary>
+        /// The combination of keys which will trigger this binding.
+        /// </summary>
+        KeyCombination KeyCombination { get; set; }
+
+        /// <summary>
+        /// The resultant action which is triggered by this binding.
+        /// </summary>
+        object Action { get; set; }
+
+        /// <summary>
+        /// Get the action associated with this binding, cast to the required enum type.
+        /// </summary>
+        /// <typeparam name="T">The enum type.</typeparam>
+        /// <returns>A cast <typeparamref name="T"/> representation of <see cref="KeyBinding.Action"/>.</returns>
+        T GetAction<T>()
+            where T : Enum;
+    }
+}
--- osu.Framework/Input/Bindings/KeyBinding.cs
+++ osu.Framework/Input/Bindings/KeyBinding.cs
@@ -1,22 +1,15 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
+
 namespace osu.Framework.Input.Bindings
 {
-    /// <summary>
-    /// A binding of a <see cref="Bindings.KeyCombination"/> to an action.
-    /// </summary>
-    public class KeyBinding
+    public class KeyBinding : IKeyBinding
     {
-        /// <summary>
-        /// The combination of keys which will trigger this binding.
-        /// </summary>
-        public KeyCombination KeyCombination;
+        public KeyCombination KeyCombination { get; set; }
 
-        /// <summary>
-        /// The resultant action which is triggered by this binding.
-        /// </summary>
-        public object Action;
+        public object Action { get; set; }
 
         /// <summary>
         /// Construct a new instance.
@@ -47,12 +40,8 @@ namespace osu.Framework.Input.Bindings
         {
         }
 
-        /// <summary>
-        /// Get the action associated with this binding, cast to the required enum type.
-        /// </summary>
-        /// <typeparam name="T">The enum type.</typeparam>
-        /// <returns>A cast <typeparamref name="T"/> representation of <see cref="Action"/>.</returns>
-        public virtual T GetAction<T>() => (T)Action;
+        public virtual T GetAction<T>()
+            where T : Enum => (T)Action;
 
         public override string ToString() => $"{KeyCombination}=>{Action}";
     }
