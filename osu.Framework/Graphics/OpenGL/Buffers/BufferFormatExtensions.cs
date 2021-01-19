b753fd6a5f54e26e36e1aa3289891aa5f05da267
Flutterish <CharmindYT@gmail.com>
2021-01-19 10:20:46 +0100

    remove unused usings

--- osu.Framework/Graphics/OpenGL/Buffers/BufferFormatExtensions.cs
+++ osu.Framework/Graphics/OpenGL/Buffers/BufferFormatExtensions.cs
@@ -2,10 +2,6 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using osuTK.Graphics.ES30;
 
 namespace osu.Framework.Graphics.OpenGL.Buffers
