accf9c1c72162ad3a65edac75dbc8558f9d26eab
Thomas Maalem <vinaqqdev@gmail.com>
2021-01-22 18:06:02 +1000

    Add KeyCombination Test

---
+++ osu.Framework.Tests/Input/KeyCombinationTest.cs
@@ -0,0 +1,49 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using NUnit.Framework;
+using osu.Framework.Input.Bindings;
+using osu.Framework.Input.States;
+using osuTK.Input;
+using KeyboardState = osu.Framework.Input.States.KeyboardState;
+
+namespace osu.Framework.Tests.Input
+{
+    [TestFixture]
+    public class KeyCombinationTest
+    {
+        [Test]
+        public void TestKeyCombinationSortKeysWithTrueOrder()
+        {
+            var keyCombination = new KeyCombination(InputKey.R, InputKey.Shift, InputKey.Control);
+
+            Assert.AreEqual(keyCombination.Keys[0], InputKey.Control);
+            Assert.AreEqual(keyCombination.Keys[1], InputKey.Shift);
+            Assert.AreEqual(keyCombination.Keys[2], InputKey.R);
+        }
+
+        [Test]
+        public void TestKeyCombinationReadableStringDisplayTrueOrder()
+        {
+            var keyCombination1 = new KeyCombination(InputKey.Control, InputKey.Shift, InputKey.R);
+            var keyCombination2 = new KeyCombination(InputKey.R, InputKey.Shift, InputKey.Control);
+
+            Assert.AreEqual(keyCombination1.ReadableString(), keyCombination2.ReadableString());
+        }
+
+        [Test]
+        public void TestKeyCombinationFromKeyboardStateReadableStringDisplayTrueOrder()
+        {
+            var keyboardState = new KeyboardState();
+
+            keyboardState.Keys.Add(Key.R);
+            keyboardState.Keys.Add(Key.LShift);
+            keyboardState.Keys.Add(Key.LControl);
+
+            var keyCombination1 = KeyCombination.FromInputState(new InputState(keyboard: keyboardState));
+            var keyCombination2 = new KeyCombination(InputKey.Control, InputKey.Shift, InputKey.R);
+
+            Assert.AreEqual(keyCombination1.ReadableString(), keyCombination2.ReadableString());
+        }
+    }
+}
