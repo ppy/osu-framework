ad23a00ccda2a9f85ea499c96bff1be21b88bf6c
InfinityGhost <39218853+InfinityGhost@users.noreply.github.com>
2021-02-18 22:25:37 -0500

    Compile tablet driver benchmark only with .NET 5

--- osu.Framework.Benchmarks/BenchmarkTabletDriver.cs
+++ osu.Framework.Benchmarks/BenchmarkTabletDriver.cs
@@ -1,6 +1,7 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+#if NET5_0
 using BenchmarkDotNet.Attributes;
 using osu.Framework.Input.Handlers.Tablet;
 
@@ -22,3 +23,4 @@ namespace osu.Framework.Benchmarks
         }
     }
 }
+#endif
