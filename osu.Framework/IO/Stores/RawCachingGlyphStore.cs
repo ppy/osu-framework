0953554201f7d78bfd24f56a14bea814bad3782a
Dean Herbert <pe@ppy.sh>
2021-02-19 12:14:51 +0900

    Fix RawCachingGlyphStore performing operations on same files from multiple threads
    As seen in
    https://ci.appveyor.com/project/peppy/osu/builds/37847069/tests.
    
    To test this, modifying `TestSceneSpriteText` to [load asynchronously](https://gist.github.com/peppy/7d48f0fdb6cb138306f7769969447d0c) can
    work quite well (will result in corruption of characters if not working
    correctly)

--- osu.Framework/IO/Stores/RawCachingGlyphStore.cs
+++ osu.Framework/IO/Stores/RawCachingGlyphStore.cs
@@ -35,7 +35,16 @@ namespace osu.Framework.IO.Stores
         protected override TextureUpload LoadCharacter(Character character)
         {
             if (!pageLookup.TryGetValue(character.Page, out var pageInfo))
-                pageInfo = createCachedPageInfo(character.Page);
+            {
+                // Use simple global locking for the time being.
+                // If necessary, a per-lookup-key (page number) locking mechanism could be implemented similar to TextureStore.
+                lock (pageLookup)
+                {
+                    // second lookup within lock for safety
+                    if (!pageLookup.TryGetValue(character.Page, out pageInfo))
+                        pageInfo = createCachedPageInfo(character.Page);
+                }
+            }
 
             return createTextureUpload(character, pageInfo);
         }
