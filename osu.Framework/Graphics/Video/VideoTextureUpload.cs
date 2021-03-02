73b98b8f7072848626a7cb594b1e5be777ade4cd
Dean Herbert <pe@ppy.sh>
2021-03-02 14:55:36 +0900

    Make VideoTextureUpload's constructor internal

--- osu.Framework/Graphics/Video/VideoTextureUpload.cs
+++ osu.Framework/Graphics/Video/VideoTextureUpload.cs
@@ -29,7 +29,7 @@ namespace osu.Framework.Graphics.Video
         /// </summary>
         /// <param name="frame">The frame to upload.</param>
         /// <param name="freeFrameDelegate">A function to free the frame on disposal.</param>
-        public VideoTextureUpload(AVFrame* frame, FFmpegFuncs.AvFrameFreeDelegate freeFrameDelegate)
+        internal VideoTextureUpload(AVFrame* frame, FFmpegFuncs.AvFrameFreeDelegate freeFrameDelegate)
         {
             Frame = frame;
             this.freeFrameDelegate = freeFrameDelegate;
