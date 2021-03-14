63756c2b22b1e1d1cdb431841b22081ac12c9d21
Nathan Alo <n20gaming2000@gmail.com>
2021-03-14 14:56:31 +0800

    add test scene

---
+++ osu.Framework.Tests/Visual/Platform/TestSceneResizableWindow.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Allocation;
+using osu.Framework.Platform;
+
+namespace osu.Framework.Tests.Visual.Platform
+{
+    public class TestSceneResizableWindow : FrameworkTestScene
+    {
+        [BackgroundDependencyLoader]
+        private void load(GameHost host)
+        {
+            AddToggleStep("toggle resizable", (state) => ((SDL2DesktopWindow)host.Window).Resizable = state);
+        }
+    }
+}
