6dc601fce70791bc3931165579d0c053fc8b2aad
Nathan Alo <n20gaming2000@gmail.com>
2021-03-15 10:28:27 +0800

    fix appveyor errors

--- osu.Framework/Platform/SDL2DesktopWindow.cs
+++ osu.Framework/Platform/SDL2DesktopWindow.cs
@@ -93,7 +93,7 @@ namespace osu.Framework.Platform
         private bool resizable = true;
 
         /// <summary>
-        /// Returns or sets whether the window is resizable or not. Only valid when in <see cref="WindowState.Normal"/>.
+        /// Returns or sets whether the window is resizable or not. Only valid when in <see cref="osu.Framework.Platform.WindowState.Normal"/>.
         /// </summary>
         public bool Resizable
         {
