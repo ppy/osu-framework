dd3077e7ad5db37d7d0513103636b0903ee47f50
Nathan Alo <n20gaming2000@gmail.com>
2021-03-15 10:58:54 +0800

    ignore test when running in headless tests

--- osu.Framework.Tests/Visual/Platform/TestSceneResizableWindow.cs
+++ osu.Framework.Tests/Visual/Platform/TestSceneResizableWindow.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using NUnit.Framework;
 using osu.Framework.Allocation;
 using osu.Framework.Platform;
 
@@ -11,6 +12,12 @@ namespace osu.Framework.Tests.Visual.Platform
         [BackgroundDependencyLoader]
         private void load(GameHost host)
         {
+            if (host.Window == null)
+            {
+                Assert.Ignore("This test cannot be run in headless mode (a window instance is required).");
+                return;
+            }
+
             AddToggleStep("toggle resizable", state => ((SDL2DesktopWindow)host.Window).Resizable = state);
         }
     }
