18ec83838f000517599c82abdb7f411b7fc26ec7
smoogipoo <smoogipoo@smgi.me>
2021-04-26 18:10:28 +0900

    Expose ThreadSafety

--- osu.Framework/Development/ThreadSafety.cs
+++ osu.Framework/Development/ThreadSafety.cs
@@ -7,17 +7,67 @@ using osu.Framework.Platform;
 
 namespace osu.Framework.Development
 {
-    internal static class ThreadSafety
+    /// <summary>
+    /// Utilities to ensure correct thread usage throughout a game.
+    /// </summary>
+    public static class ThreadSafety
     {
+        /// <summary>
+        /// Asserts that the current code is executing on the update thread. Debug only.
+        /// </summary>
         [Conditional("DEBUG")]
-        internal static void EnsureUpdateThread() => Debug.Assert(IsUpdateThread);
+        public static void EnsureUpdateThread() => Debug.Assert(IsUpdateThread);
 
+        /// <summary>
+        /// Asserts that the current code is not executing on the update thread. Debug only.
+        /// </summary>
         [Conditional("DEBUG")]
-        internal static void EnsureNotUpdateThread() => Debug.Assert(!IsUpdateThread);
+        public static void EnsureNotUpdateThread() => Debug.Assert(!IsUpdateThread);
 
+        /// <summary>
+        /// Asserts that the current code is executing on the draw thread. Debug only.
+        /// </summary>
         [Conditional("DEBUG")]
-        internal static void EnsureDrawThread() => Debug.Assert(IsDrawThread);
+        public static void EnsureDrawThread() => Debug.Assert(IsDrawThread);
 
+        /// <summary>
+        /// Asserts that the current code is executing on the audio thread. Debug only.
+        /// </summary>
+        [Conditional("DEBUG")]
+        public static void EnsureAudioThread() => Debug.Assert(IsAudioThread);
+
+        /// <summary>
+        /// Whether the current code is executing on the input thread.
+        /// </summary>
+        [field: ThreadStatic]
+        public static bool IsInputThread { get; internal set; }
+
+        /// <summary>
+        /// Whether the current code is executing on the update thread.
+        /// </summary>
+        [field: ThreadStatic]
+        public static bool IsUpdateThread { get; internal set; }
+
+        /// <summary>
+        /// Whether the current code is executing on the draw thread.
+        /// </summary>
+        [field: ThreadStatic]
+        public static bool IsDrawThread { get; internal set; }
+
+        /// <summary>
+        /// Whether the current code is executing on the audio thread.
+        /// </summary>
+        [field: ThreadStatic]
+        public static bool IsAudioThread { get; internal set; }
+
+        /// <summary>
+        /// The current execution mode.
+        /// </summary>
+        internal static ExecutionMode ExecutionMode;
+
+        /// <summary>
+        /// Resets all statics for the current thread.
+        /// </summary>
         internal static void ResetAllForCurrentThread()
         {
             IsInputThread = false;
@@ -25,19 +75,5 @@ namespace osu.Framework.Development
             IsDrawThread = false;
             IsAudioThread = false;
         }
-
-        public static ExecutionMode ExecutionMode;
-
-        [ThreadStatic]
-        public static bool IsInputThread;
-
-        [ThreadStatic]
-        public static bool IsUpdateThread;
-
-        [ThreadStatic]
-        public static bool IsDrawThread;
-
-        [ThreadStatic]
-        public static bool IsAudioThread;
     }
 }
