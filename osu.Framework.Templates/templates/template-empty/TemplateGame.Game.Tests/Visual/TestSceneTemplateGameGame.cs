4dd1c013686ce13198e10403a75ea66ac908c42d
Salman Ahmed <frenzibyte@gmail.com>
2021-04-27 04:08:35 +0300

    Add headless test runner for empty template test scenes

--- osu.Framework.Templates/templates/template-empty/TemplateGame.Game.Tests/Visual/TestSceneTemplateGameGame.cs
+++ osu.Framework.Templates/templates/template-empty/TemplateGame.Game.Tests/Visual/TestSceneTemplateGameGame.cs
@@ -1,10 +1,9 @@
 using osu.Framework.Allocation;
 using osu.Framework.Platform;
-using osu.Framework.Testing;
 
 namespace TemplateGame.Game.Tests.Visual
 {
-    public class TestSceneTemplateGameGame : TestScene
+    public class TestSceneTemplateGameGame : TemplateGameTestScene
     {
         // Add visual tests to ensure correct behaviour of your game: https://github.com/ppy/osu-framework/wiki/Development-and-Testing
         // You can make changes to classes associated with the tests and they will recompile and update immediately.
