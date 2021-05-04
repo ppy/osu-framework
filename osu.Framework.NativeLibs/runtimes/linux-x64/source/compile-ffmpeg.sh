5ba690472a57cb108116e0079e061bce0f829984
MATRIX-feather <midnightcarnival@outlook.com>
2021-05-05 05:07:19 +0800

    Fetch and extract tarball from ffmpeg.org; Remove pkgconfig after build finish; Move target ffmpeg version to "ffmpeg_version" variable;

--- osu.Framework.NativeLibs/runtimes/linux-x64/source/compile-ffmpeg.sh
+++ osu.Framework.NativeLibs/runtimes/linux-x64/source/compile-ffmpeg.sh
@@ -3,18 +3,23 @@
 #Also, GCC 5.4.0 or Clang 3.8.0 is recommended.
 #Tested on Ubuntu 16.04 and Ubuntu 20.10, works fine.
 
-#Extract tarball
-xz -d ffmpeg-4.3.1.tar.xz
-tar -xvf ffmpeg-4.3.1.tar
+ffmpeg_version="4.3.2"
+
+#Fetch tarball from ffmpeg.org and extract
+curl https://ffmpeg.org/releases/ffmpeg-"${ffmpeg_version}".tar.xz | tar xfJ -
 
 #Switch to `ffmpeg-4.3.1` directory and make `build` directory
-cd ffmpeg-4.3.1 || exit 1;
+cd ffmpeg-"${ffmpeg_version}" || exit 1;
 mkdir build
 
 #Configure
 ./configure --disable-ffplay --disable-ffprobe --disable-avdevice --disable-swresample \
 --disable-static --enable-shared --prefix=build --libdir=../../native
 
-#Compile
+#Build
 make -j
 make install
+
+#Remove unused pkgconfig
+cd ../../native
+rm -r pkgconfig
