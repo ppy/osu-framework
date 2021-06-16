a9f32c1a0fa8bfb95255b3404f456be014f09052
smoogipoo <smoogipoo@smgi.me>
2021-06-16 15:32:39 +0900

    Add xmldocs and exception on invalid type

--- osu.Framework/Localisation/EnumLocalisationMapper.cs
+++ osu.Framework/Localisation/EnumLocalisationMapper.cs
@@ -5,9 +5,25 @@ using System;
 
 namespace osu.Framework.Localisation
 {
-    public abstract class EnumLocalisationMapper<T>
+    /// <summary>
+    /// Describes the values of an <see cref="Enum"/> type by <see cref="LocalisableString"/>s.
+    /// </summary>
+    /// <typeparam name="T">The <see cref="Enum"/> type.</typeparam>
+    public abstract class EnumLocalisationMapper<T> : IEnumLocalisationMapper
         where T : Enum
     {
+        /// <summary>
+        /// Describes a <typeparamref name="T"/> value by a <see cref="LocalisableString"/>.
+        /// </summary>
+        /// <param name="value">The value to map.</param>
+        /// <returns>The <see cref="LocalisableString"/> describing <paramref name="value"/>.</returns>
         public abstract LocalisableString Map(T value);
+    }
+
+    /// <summary>
+    /// Marker class for <see cref="EnumLocalisationMapper{T}"/>. Do not use.
+    /// </summary>
+    internal interface IEnumLocalisationMapper
+    {
     }
 }
--- osu.Framework/Localisation/LocalisableEnumAttribute.cs
+++ osu.Framework/Localisation/LocalisableEnumAttribute.cs
@@ -5,14 +5,27 @@ using System;
 
 namespace osu.Framework.Localisation
 {
+    /// <summary>
+    /// Indicates that the members of an enum can be localised.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Enum)]
     public sealed class LocalisableEnumAttribute : Attribute
     {
+        /// <summary>
+        /// The <see cref="EnumLocalisationMapper{T}"/> type that maps enum values to <see cref="LocalisableString"/>s.
+        /// </summary>
         public readonly Type MapperType;
 
+        /// <summary>
+        /// Creates a new <see cref="LocalisableEnumAttribute"/>.
+        /// </summary>
+        /// <param name="mapperType">The <see cref="EnumLocalisationMapper{T}"/> type that maps enum values to <see cref="LocalisableString"/>s.</param>
         public LocalisableEnumAttribute(Type mapperType)
         {
             MapperType = mapperType;
+
+            if (!typeof(IEnumLocalisationMapper).IsAssignableFrom(mapperType))
+                throw new ArgumentException($"Mapper type must inherit from {nameof(EnumLocalisationMapper<Enum>)}.");
         }
     }
 }
