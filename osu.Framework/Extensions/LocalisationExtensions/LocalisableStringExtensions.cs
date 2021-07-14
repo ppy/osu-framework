53ffe4794f863cf26529a9581bfc556dc1865b45
Lucas A <game4allyt@gmail.com>
2021-07-14 20:24:20 +0200

    Add `LocalisableString` overloads for extension methods.

--- osu.Framework/Extensions/LocalisationExtensions/LocalisableStringExtensions.cs
+++ osu.Framework/Extensions/LocalisationExtensions/LocalisableStringExtensions.cs
@@ -8,24 +8,45 @@ namespace osu.Framework.Extensions.LocalisationExtensions
     public static class LocalisableStringExtensions
     {
         /// <summary>
+        /// Returns a <see cref="TransformableString"/> with the specified underlying localisable string uppercased.
+        /// </summary>
+        /// <param name="str">The localisable string.</param>
+        /// <returns>A transformable string with its localisable string uppercased.</returns>
+        public static TransformableString ToUpper(this LocalisableString str) => new TransformableString(str, Casing.UpperCase);
+
+        /// <summary>
         /// Returns a <see cref="TransformableString"/> with the specified underlying string data uppercased.
         /// </summary>
         /// <param name="data">The string data.</param>
         /// <returns>A transformable string with its string data uppercased.</returns>
-        public static TransformableString ToUpper(this ILocalisableStringData data) => new TransformableString(new LocalisableString(data), Casing.UpperCase);
+        public static TransformableString ToUpper(this ILocalisableStringData data) => new LocalisableString(data).ToUpper();
+
+        /// <summary>
+        /// Returns a <see cref="LocalisableString"/> with the specified underlying localisable string transformed to title case.
+        /// </summary>
+        /// <param name="str">The localisable string.</param>
+        /// <returns>A transformable string with its localisable string transformed to title case.</returns>
+        public static TransformableString ToTitle(this LocalisableString str) => new TransformableString(str, Casing.TitleCase);
 
         /// <summary>
         /// Returns a <see cref="LocalisableString"/> with the specified underlying string data transformed to title case.
         /// </summary>
         /// <param name="data">The string data.</param>
         /// <returns>A transformable string with its string data transformed to title case.</returns>
-        public static TransformableString ToTitle(this ILocalisableStringData data) => new TransformableString(new LocalisableString(data), Casing.TitleCase);
+        public static TransformableString ToTitle(this ILocalisableStringData data) => new LocalisableString(data).ToTitle();
+
+        /// <summary>
+        /// Returns a <see cref="LocalisableString"/> with the specified underlying localisable string lowercased.
+        /// </summary>
+        /// <param name="str">The localisable string.</param>
+        /// <returns>A transformable string with its localisable string lowercased.</returns>
+        public static TransformableString ToLower(this LocalisableString str) => new TransformableString(str, Casing.LowerCase);
 
         /// <summary>
         /// Returns a <see cref="LocalisableString"/> with the specified underlying string data lowercased.
         /// </summary>
         /// <param name="data">The string data.</param>
         /// <returns>A transformable string with its string data lowercased.</returns>
-        public static TransformableString ToLower(this ILocalisableStringData data) => new TransformableString(new LocalisableString(data), Casing.LowerCase);
+        public static TransformableString ToLower(this ILocalisableStringData data) => new LocalisableString(data).ToLower();
     }
 }
