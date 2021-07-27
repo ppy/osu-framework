e4783946924c1de9a40105bbfa68245d554a1c5a
Roman Kapustin <TocoToucanMS@gmail.com>
2021-07-27 21:22:05 +0300

    Introduce InputQueue class hierarchy

---
+++ osu.Framework/Input/InputQueue.cs
@@ -0,0 +1,57 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using System.Collections.Generic;
+using osu.Framework.Graphics;
+using osu.Framework.Input.Bindings;
+
+namespace osu.Framework.Input
+{
+    public class InputQueue : ReadOnlyInputQueue
+    {
+        public List<Drawable> AllList => AllInner;
+        public List<Drawable> RegularList => RegularInner;
+        public List<KeyBindingContainer> KeyBingingContainersList => KeyBingingContainersInner;
+
+        public InputQueue(Func<Drawable> getFocusedDrawable = null)
+            : base(getFocusedDrawable)
+        {
+        }
+
+        public void Add(Drawable drawable)
+        {
+            if (drawable is KeyBindingContainer kbc)
+                KeyBingingContainersList.Add(kbc);
+            else
+                RegularList.Add(drawable);
+
+            AllList.Add(drawable);
+        }
+
+        /// <summary>
+        /// Clears underlying input queues.
+        /// </summary>
+        public void Clear()
+        {
+            AllList.Clear();
+            RegularList.Clear();
+            KeyBingingContainersList.Clear();
+        }
+
+        /// <summary>
+        /// Reverses underlying input queues.
+        /// </summary>
+        public void Reverse()
+        {
+            AllList.Reverse();
+            RegularList.Reverse();
+            KeyBingingContainersList.Reverse();
+        }
+
+        public void RemoveAll(Predicate<Drawable> func)
+        {
+            RegularList.RemoveAll(func);
+        }
+    }
+}
---
+++ osu.Framework/Input/ReadOnlyInputQueue.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using System.Collections.Generic;
+using osu.Framework.Extensions.ListExtensions;
+using osu.Framework.Graphics;
+using osu.Framework.Input.Bindings;
+using osu.Framework.Lists;
+
+namespace osu.Framework.Input
+{
+    /// <summary>
+    /// Holds grouped drawables for input event propagation.
+    /// </summary>
+    public class ReadOnlyInputQueue
+    {
+        protected readonly List<Drawable> AllInner;
+        protected readonly List<Drawable> RegularInner;
+        protected readonly List<KeyBindingContainer> KeyBingingContainersInner;
+
+        protected ReadOnlyInputQueue(Func<Drawable> getFocusedDrawable = null)
+        {
+            GetFocusedDrawable = getFocusedDrawable;
+
+            AllInner = new List<Drawable>();
+            RegularInner = new List<Drawable>();
+            KeyBingingContainersInner = new List<KeyBindingContainer>();
+
+            All = AllInner.AsSlimReadOnly();
+            Regular = RegularInner.AsSlimReadOnly();
+            KeyBingingContainers = KeyBingingContainersInner.AsSlimReadOnly();
+        }
+
+        /// <summary>
+        /// The currently focused <see cref="Drawable"/>. Null if there is no current focus.
+        /// </summary>
+        public readonly Func<Drawable> GetFocusedDrawable;
+
+        /// <summary>
+        /// Holds drawables that handles regular input.
+        /// </summary>
+        public readonly SlimReadOnlyListWrapper<Drawable> Regular;
+
+        /// <summary>
+        /// Holds drawables that handles key binding input.
+        /// </summary>
+        public readonly SlimReadOnlyListWrapper<KeyBindingContainer> KeyBingingContainers;
+
+        /// <summary>
+        /// Holds all drawables that handles any input
+        /// </summary>
+        public readonly SlimReadOnlyListWrapper<Drawable> All;
+    }
+}
