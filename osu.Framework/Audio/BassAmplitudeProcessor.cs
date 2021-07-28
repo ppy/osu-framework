09bd0b8ede3891bd75bf44d918b2d4aeb8014974
smoogipoo <smoogipoo@smgi.me>
2021-07-28 23:20:40 +0900

    Make amplitude processor public again

--- osu.Framework/Audio/BassAmplitudeProcessor.cs
+++ osu.Framework/Audio/BassAmplitudeProcessor.cs
@@ -12,7 +12,7 @@ namespace osu.Framework.Audio
     /// <summary>
     /// Computes and caches amplitudes for a bass channel.
     /// </summary>
-    internal class BassAmplitudeProcessor
+    public class BassAmplitudeProcessor
     {
         /// <summary>
         /// The most recent amplitude data. Note that this is updated on an ongoing basis and there is no guarantee it is in a consistent (single sample) state.
