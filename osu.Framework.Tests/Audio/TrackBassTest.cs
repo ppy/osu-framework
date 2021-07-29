4f61c459ee9abe8173082754e9068d2893dc54f3
smoogipoo <smoogipoo@smgi.me>
2021-07-29 22:03:24 +0900

    Increase assertion range

--- osu.Framework.Tests/Audio/TrackBassTest.cs
+++ osu.Framework.Tests/Audio/TrackBassTest.cs
@@ -374,9 +374,7 @@ namespace osu.Framework.Tests.Audio
             bass.Update();
 
             bass.RunOnAudioThread(() => track.Seek(20000));
-            takeEffectsAndUpdateAfter(50);
-
-            Assert.That(track.CurrentTime, Is.EqualTo(20000).Within(100));
+            Assert.That(track.CurrentTime, Is.EqualTo(20000).Within(500));
         }
 
         private void takeEffectsAndUpdateAfter(int after)
