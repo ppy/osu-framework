17289ff1f18224e015782746f07670228a4d0d6e
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-08-02 11:14:50 +1200

    Use `set -e` instead of `|| exit 1`

--- osu.Framework.NativeLibs/runtimes/win-arm64/source/compile-ffmpeg.sh
+++ osu.Framework.NativeLibs/runtimes/win-arm64/source/compile-ffmpeg.sh
@@ -2,6 +2,8 @@
 # This script expects you to have llvm-mingw set up beforehand.
 # You can get it here: https://github.com/mstorsjo/llvm-mingw
 
+set -e
+
 ffmpeg_version="4.3.2"
 
 echo "Preparing FFMPEG source"
@@ -9,15 +11,15 @@ if [ ! -d "ffmpeg-${ffmpeg_version}" ]
 then
     curl https://ffmpeg.org/releases/ffmpeg-"${ffmpeg_version}".tar.xz | tar xfJ -
 fi
-cd ffmpeg-"${ffmpeg_version}" || exit 1;
+cd ffmpeg-"${ffmpeg_version}"
 
 echo "Configuring FFMPEG build"
 ./configure --arch=aarch64 --target-os=mingw32 --cross-prefix=aarch64-w64-mingw32- \
---enable-shared --disable-static --prefix=build --disable-programs --disable-doc || exit 1
+--enable-shared --disable-static --prefix=build --disable-programs --disable-doc
 
 echo "Compiling FFMPEG binaries"
-make -j || exit 1
-make install || exit 1
+make -j
+make install
 
 echo "Moving binaries to native folder"
-cp build/bin/*.dll ../../native || exit 1
+cp build/bin/*.dll ../../native
