ee7ea882c6bb3cd9afca4fb6ba7b1a196810d2fa
Salman Ahmed <frenzibyte@gmail.com>
2021-08-06 05:39:32 +0300

    Add `ColourInfo.GradientHorizontal` case for visual cross-comparison

--- osu.Framework.Tests/Visual/Drawables/TestSceneColourInterpolation.cs
+++ osu.Framework.Tests/Visual/Drawables/TestSceneColourInterpolation.cs
@@ -4,9 +4,12 @@
 using System.Linq;
 using NUnit.Framework;
 using osu.Framework.Graphics;
+using osu.Framework.Graphics.Colour;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
+using osu.Framework.Graphics.Sprites;
 using osu.Framework.Utils;
+using osuTK;
 using osuTK.Graphics;
 
 namespace osu.Framework.Tests.Visual.Drawables
@@ -16,23 +19,60 @@ namespace osu.Framework.Tests.Visual.Drawables
         [Test]
         public void TestColourInterpolatesInLinearSpace()
         {
-            FillFlowContainer lines = null;
+            FillFlowContainer interpolatingLines = null;
 
-            AddStep("load interpolated colours", () => Child = lines = new FillFlowContainer
+            AddStep("load interpolated colours", () =>
             {
-                Anchor = Anchor.Centre,
-                Origin = Anchor.Centre,
-                AutoSizeAxes = Axes.X,
-                Height = 50f,
-                ChildrenEnumerable = Enumerable.Range(0, 750).Select(i => new Box
+                Child = new FillFlowContainer
                 {
-                    Width = 1f,
-                    RelativeSizeAxes = Axes.Y,
-                    Colour = Interpolation.ValueAt(i, Color4.Blue, Color4.Red, 0, 750),
-                }),
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    AutoSizeAxes = Axes.Both,
+                    Direction = FillDirection.Vertical,
+                    Children = new Drawable[]
+                    {
+                        new SpriteText
+                        {
+                            Anchor = Anchor.CentreLeft,
+                            Origin = Anchor.CentreLeft,
+                            Text = $"{nameof(ColourInfo)}.{nameof(ColourInfo.GradientHorizontal)}(Blue, Red)"
+                        },
+                        new Box
+                        {
+                            Anchor = Anchor.CentreLeft,
+                            Origin = Anchor.CentreLeft,
+                            Size = new Vector2(750f, 50f),
+                            Colour = ColourInfo.GradientHorizontal(Color4.Blue, Color4.Red),
+                        },
+                        new SpriteText
+                        {
+                            Margin = new MarginPadding { Top = 20f },
+                            Anchor = Anchor.CentreLeft,
+                            Origin = Anchor.CentreLeft,
+                            Text = $"{nameof(Interpolation)}.{nameof(Interpolation.ValueAt)}(Blue, Red) with 1px boxes",
+                        },
+                        interpolatingLines = new FillFlowContainer
+                        {
+                            Anchor = Anchor.CentreLeft,
+                            Origin = Anchor.CentreLeft,
+                            AutoSizeAxes = Axes.X,
+                            Height = 50f,
+                            ChildrenEnumerable = Enumerable.Range(0, 750).Select(i => new Box
+                            {
+                                Width = 1f,
+                                RelativeSizeAxes = Axes.Y,
+                                Colour = Interpolation.ValueAt(i, Color4.Blue, Color4.Red, 0, 750),
+                            }),
+                        },
+                    }
+                };
             });
 
-            AddAssert("interpolation in linear space", () => lines.Children[lines.Children.Count / 2].Colour.AverageColour.Linear == new Color4(0.5f, 0f, 0.5f, 1f));
+            AddAssert("interpolation in linear space", () =>
+            {
+                var middle = interpolatingLines.Children[interpolatingLines.Children.Count / 2];
+                return middle.Colour.AverageColour.Linear == new Color4(0.5f, 0f, 0.5f, 1f);
+            });
         }
     }
 }
