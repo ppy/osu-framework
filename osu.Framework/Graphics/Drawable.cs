2e6374b1f24af20cab85c35f569c78a15b5494a1
Salman Ahmed <frenzibyte@gmail.com>
2021-08-26 16:00:31 +0300

    Log drawable auto-unbind failure on a verbose level

--- osu.Framework/Graphics/Drawable.cs
+++ osu.Framework/Graphics/Drawable.cs
@@ -151,9 +151,9 @@ namespace osu.Framework.Graphics
                         {
                             a(target);
                         }
-                        catch
+                        catch (Exception e)
                         {
-                            // Execution should continue regardless of whether an unbind failed
+                            Logger.Log($"Failed to unbind a local bindable in {type.ReadableName()}: {e}");
                         }
                     }
                 };
