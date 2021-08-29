35e1b8d8e35a6a2e76937f1ee75ccd9b5778644f
Salman Ahmed <frenzibyte@gmail.com>
2021-08-29 13:32:25 +0300

    Allow using `ToLocalisableString` without a format string
    Defaults to `null` which is what the number structs (`Double`, `Int32`,
    etc.) use to format values without a format string.

--- osu.Framework/Localisation/LocalisableStringExtensions.cs
+++ osu.Framework/Localisation/LocalisableStringExtensions.cs
@@ -10,11 +10,11 @@ namespace osu.Framework.Localisation
     public static class LocalisableStringExtensions
     {
         /// <summary>
-        /// Returns a <see cref="LocalisableFormattableString"/> formatting the given <paramref name="value"/> with the specified <paramref name="format"/>.
+        /// Returns a <see cref="LocalisableFormattableString"/> formatting the given <paramref name="value"/> to a string, along with an optional <paramref name="format"/> string.
         /// </summary>
         /// <param name="value">The value to format.</param>
         /// <param name="format">The format string.</param>
-        public static LocalisableFormattableString ToLocalisableString(this IFormattable value, string? format)
+        public static LocalisableFormattableString ToLocalisableString(this IFormattable value, string? format = null)
             => new LocalisableFormattableString(value, format);
     }
 }
