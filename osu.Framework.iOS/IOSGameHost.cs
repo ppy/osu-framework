bf0a870fc35278929759d8586942bcd9ebfa67ef
Opelkuh <25430283+Opelkuh@users.noreply.github.com>
2021-10-22 23:58:40 +0200

    Make VideoToolbox the default HW decoder on iOS

--- osu.Framework.iOS/IOSGameHost.cs
+++ osu.Framework.iOS/IOSGameHost.cs
@@ -77,6 +77,9 @@ namespace osu.Framework.iOS
             if (!defaultOverrides.ContainsKey(FrameworkSetting.ExecutionMode))
                 defaultOverrides.Add(FrameworkSetting.ExecutionMode, ExecutionMode.SingleThread);
 
+            if (!defaultOverrides.ContainsKey(FrameworkSetting.HardwareVideoDecoder))
+                defaultOverrides.Add(FrameworkSetting.HardwareVideoDecoder, HardwareVideoDecoder.VideoToolbox);
+
             base.SetupConfig(defaultOverrides);
 
             DebugConfig.SetValue(DebugSetting.BypassFrontToBackPass, true);
