3cd110af4da4c9bd625b43cb2e6b9721d9669a8a
Dean Herbert <pe@ppy.sh>
2021-10-25 13:36:23 +0900

    Remove use of `case` pattern matching in `MidiHandler` altogether

--- osu.Framework/Input/Handlers/Midi/MidiHandler.cs
+++ osu.Framework/Input/Handlers/Midi/MidiHandler.cs
@@ -213,18 +213,30 @@ namespace osu.Framework.Input.Handlers.Midi
             // Ignore to receive messages from all channels
             switch (eventType & 0xF0)
             {
-                case MidiEvent.NoteOn when velocity != 0:
-                    Logger.Log($"NoteOn: {(MidiKey)key}/{velocity / 128f:P}");
-                    PendingInputs.Enqueue(new MidiKeyInput((MidiKey)key, velocity, true));
-                    FrameStatistics.Increment(StatisticsCounterType.MidiEvents);
+                case MidiEvent.NoteOn:
+                    if (velocity != 0)
+                        noteOn();
+                    else
+                        noteOff();
                     break;
 
                 case MidiEvent.NoteOff:
-                case MidiEvent.NoteOn:
-                    Logger.Log($"NoteOff: {(MidiKey)key}/{velocity / 128f:P}");
-                    PendingInputs.Enqueue(new MidiKeyInput((MidiKey)key, 0, false));
-                    FrameStatistics.Increment(StatisticsCounterType.MidiEvents);
+                    noteOff();
                     break;
+            }
+
+            void noteOff()
+            {
+                Logger.Log($"NoteOff: {(MidiKey)key}/{velocity / 128f:P}");
+                PendingInputs.Enqueue(new MidiKeyInput((MidiKey)key, 0, false));
+                FrameStatistics.Increment(StatisticsCounterType.MidiEvents);
+            }
+
+            void noteOn()
+            {
+                Logger.Log($"NoteOn: {(MidiKey)key}/{velocity / 128f:P}");
+                PendingInputs.Enqueue(new MidiKeyInput((MidiKey)key, velocity, true));
+                FrameStatistics.Increment(StatisticsCounterType.MidiEvents);
             }
         }
     }
