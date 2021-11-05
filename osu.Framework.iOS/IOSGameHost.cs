f6f7d058a8bdae751737e2186c295cff89348e9b
Susko3 <16479013+Susko3@users.noreply.github.com>
2021-11-05 12:30:45 +0100

    Fix missed NullReferenceException inspections

--- osu.Framework.iOS/IOSGameHost.cs
+++ osu.Framework.iOS/IOSGameHost.cs
@@ -46,6 +46,8 @@ namespace osu.Framework.iOS
         /// </summary>
         private void handleKeyboardNotification(NSNotification notification)
         {
+            if (notification.UserInfo == null) return;
+
             NSValue nsKeyboardFrame = (NSValue)notification.UserInfo[UIKeyboard.FrameEndUserInfoKey];
             RectangleF keyboardFrame = nsKeyboardFrame.RectangleFValue;
 
