25ba3e0a2b1041f42eb9fb91d7509326587d8c1c
Opelkuh <25430283+Opelkuh@users.noreply.github.com>
2021-11-13 01:30:03 +0100

    Remove duplicate video set-up steps

--- osu.Framework.Tests/Visual/Sprites/TestSceneVideo.cs
+++ osu.Framework.Tests/Visual/Sprites/TestSceneVideo.cs
@@ -9,7 +9,6 @@ using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Video;
 using osu.Framework.IO.Stores;
-using osu.Framework.Testing;
 using osu.Framework.Timing;
 
 namespace osu.Framework.Tests.Visual.Sprites
@@ -50,22 +49,14 @@ namespace osu.Framework.Tests.Visual.Sprites
             };
         }
 
-        [SetUpSteps]
-        public void SetUpSteps()
+        private void loadNewVideo(string format = "mp4")
         {
             AddStep("Reset clock", () =>
             {
                 clock.CurrentTime = 0;
                 didDecode = false;
             });
-            loadNewVideo();
-            AddUntilStep("Wait for video to load", () => video.IsLoaded);
-            AddStep("Reset clock", () => clock.CurrentTime = 0);
-        }
-
-        private void loadNewVideo(string format = "mp4")
-        {
-            AddStep("load video", () =>
+            AddStep($"load .{format} video", () =>
             {
                 videoContainer.Child = video = new TestVideo(videoStore.GetStream($"sample-video.{format}"))
                 {
@@ -74,11 +65,15 @@ namespace osu.Framework.Tests.Visual.Sprites
                     Anchor = Anchor.Centre,
                 };
             });
+            AddUntilStep("Wait for video to load", () => video.IsLoaded);
+            AddStep("Reset clock", () => clock.CurrentTime = 0);
         }
 
         [Test]
         public void TestHardwareDecode()
         {
+            loadNewVideo();
+
             AddStep("disable hardware decoding", () => config.SetValue(FrameworkSetting.HardwareVideoDecoder, HardwareVideoDecoder.None));
             AddWaitStep("Wait some", 20);
             AddStep("enable hardware decoding", () => config.SetValue(FrameworkSetting.HardwareVideoDecoder, HardwareVideoDecoder.Any));
@@ -87,6 +82,8 @@ namespace osu.Framework.Tests.Visual.Sprites
         [Test]
         public void TestStartFromCurrentTime()
         {
+            loadNewVideo();
+
             AddAssert("Video is near start", () => video.PlaybackPosition < 1000);
 
             AddWaitStep("Wait some", 20);
@@ -99,6 +96,8 @@ namespace osu.Framework.Tests.Visual.Sprites
         [Test]
         public void TestDecodingStopsWhenNotPresent()
         {
+            loadNewVideo();
+
             AddStep("make video hidden", () => video.Hide());
 
             AddWaitStep("wait a bit", 10);
@@ -118,6 +117,8 @@ namespace osu.Framework.Tests.Visual.Sprites
         [TestCase(true)]
         public void TestDecodingStopsBeforeStartTime(bool looping)
         {
+            loadNewVideo();
+
             AddStep("Set looping", () => video.Loop = looping);
 
             AddStep("Jump back to before start time", () => clock.CurrentTime = -30000);
@@ -177,6 +178,8 @@ namespace osu.Framework.Tests.Visual.Sprites
         [Test]
         public void TestVideoDoesNotLoopIfDisabled()
         {
+            loadNewVideo();
+
             AddStep("Seek to end", () => clock.CurrentTime = video.Duration);
             AddUntilStep("Video seeked", () => video.PlaybackPosition >= video.Duration - 1000);
             AddWaitStep("Wait for playback", 10);
@@ -186,6 +189,8 @@ namespace osu.Framework.Tests.Visual.Sprites
         [Test]
         public void TestVideoLoopsIfEnabled()
         {
+            loadNewVideo();
+
             AddStep("Set looping", () => video.Loop = true);
             AddStep("Seek to end", () => clock.CurrentTime = video.Duration);
             AddWaitStep("Wait for playback", 10);
@@ -195,6 +200,8 @@ namespace osu.Framework.Tests.Visual.Sprites
         [Test]
         public void TestShader()
         {
+            loadNewVideo();
+
             AddStep("Set colour", () => video.Colour = Color4Extensions.FromHex("#ea7948").Opacity(0.75f));
             AddStep("Use normal shader", () => video.UseRoundedShader = false);
             AddStep("Use rounded shader", () => video.UseRoundedShader = true);
