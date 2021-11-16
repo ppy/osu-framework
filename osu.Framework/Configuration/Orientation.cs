1401a31b44bb867a8d26c941b446f86977d34f8a
Hồ Nguyên Minh <honguyenminh@outlook.com>
2021-11-16 19:06:08 +0700

    Add Orientation setting

---
+++ osu.Framework/Configuration/Orientation.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+namespace osu.Framework.Configuration
+{
+    /// <summary>
+    /// Screen orientation
+    /// </summary>
+    public enum Orientation
+    {
+        Landscape = 0,
+        Portrait = 1,
+        Auto = 2
+    }
+}
