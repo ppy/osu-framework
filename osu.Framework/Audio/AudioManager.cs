422317b9c39b46cae258390acec957babd9e63d4
Jamie Taylor <me@nekodex.net>
2021-11-26 22:00:09 +0900

    ActiveMixers not really useful anymore

--- osu.Framework/Audio/AudioManager.cs
+++ osu.Framework/Audio/AudioManager.cs
@@ -130,9 +130,6 @@ namespace osu.Framework.Audio
         /// </summary>
         public Scheduler EventScheduler;
 
-        internal IBindableList<AudioMixer> ActiveMixers => activeMixers;
-        private readonly BindableList<AudioMixer> activeMixers = new BindableList<AudioMixer>();
-
         private readonly Lazy<TrackStore> globalTrackStore;
         private readonly Lazy<SampleStore> globalSampleStore;
 
@@ -239,20 +236,6 @@ namespace osu.Framework.Audio
             parentMixer.Add(mixer);
 
             return mixer;
-        }
-
-        protected override void ItemAdded(AudioComponent item)
-        {
-            base.ItemAdded(item);
-            if (item is AudioMixer mixer)
-                activeMixers.Add(mixer);
-        }
-
-        protected override void ItemRemoved(AudioComponent item)
-        {
-            base.ItemRemoved(item);
-            if (item is AudioMixer mixer)
-                activeMixers.Remove(mixer);
         }
 
         /// <summary>
