66e01c4aa76302110802e3ea17c7db0362aa2287
Jamie Taylor <me@nekodex.net>
2021-12-01 13:56:32 +0900

    Should be continue not return

--- osu.Framework/Audio/Mixing/Bass/BassAudioMixer.cs
+++ osu.Framework/Audio/Mixing/Bass/BassAudioMixer.cs
@@ -275,7 +275,7 @@ namespace osu.Framework.Audio.Mixing.Bass
                 ActiveChannels.RemoveAt(i--);
 
                 if (channel is BassAudioMixer bassAudioMixer && bassAudioMixer.IsDisposed)
-                    return;
+                    continue;
 
                 removeChannelFromBassMix(channel);
             }
