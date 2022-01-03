994826b575b8ffbd2095798a92950f15ac34c5f1
Speykious <speykious@gmail.com>
2022-01-03 18:48:45 +0100

    Add XML documentation for `HostConfig`

--- osu.Framework/HostConfig.cs
+++ osu.Framework/HostConfig.cs
@@ -3,11 +3,35 @@
 
 namespace osu.Framework
 {
+    /// <summary>
+    /// Various configuration properties for a <see cref="Host"/>.
+    /// </summary>
     public struct HostConfig
     {
+        /// <summary>
+        /// Name of the game or application.
+        /// </summary>
         public string GameName { get; set; }
+
+        /// <summary>
+        /// Whether to bind the IPC port.
+        /// </summary>
         public bool BindIPC { get; set; }
+
+        /// <summary>
+        /// Whether this is a portable installation.
+        /// </summary>
         public bool PortableInstallation { get; set; }
+
+        /// <summary>
+        /// Whether to bypass the compositor.
+        /// </summary>
+        /// <remarks>
+        /// On Linux, the compositor re-buffers the application to apply various window effects,
+        /// increasing latency in the process. Thus it is a good idea for games to bypass it,
+        /// though normal applications would generally benefit from letting the window effects untouched. <br/>
+        /// If the SDL_VIDEO_X11_NET_WM_BYPASS_COMPOSITOR environment variable is set, this property will have no effect.
+        /// </remarks>
         public bool BypassCompositor { get; set; }
 
         public static HostConfig GameConfig(string gameName, bool bindIPC = false, bool portableInstallation = false)
