305ac931f61b7ccc7ddb991e49333baa0e7b291e
Susko3 <16479013+Susko3@users.noreply.github.com>
2022-01-04 21:48:12 +0100

    Move SDL event handling into private helper

--- osu.Framework/Platform/Windows/WindowsWindow.cs
+++ osu.Framework/Platform/Windows/WindowsWindow.cs
@@ -42,22 +42,24 @@ namespace osu.Framework.Platform.Windows
             // enable window message events to use with `OnSDLEvent` below.
             SDL.SDL_EventState(SDL.SDL_EventType.SDL_SYSWMEVENT, SDL.SDL_ENABLE);
 
-            OnSDLEvent += e =>
+            OnSDLEvent += handleSDLEvent;
+        }
+
+        private void handleSDLEvent(SDL.SDL_Event e)
+        {
+            if (e.type != SDL.SDL_EventType.SDL_SYSWMEVENT) return;
+
+            var wmMsg = Marshal.PtrToStructure<SDL2Structs.SDL_SysWMmsg>(e.syswm.msg);
+            var m = wmMsg.msg.win;
+
+            switch (m.msg)
             {
-                if (e.type != SDL.SDL_EventType.SDL_SYSWMEVENT) return;
-
-                var wmMsg = Marshal.PtrToStructure<SDL2Structs.SDL_SysWMmsg>(e.syswm.msg);
-                var m = wmMsg.msg.win;
-
-                switch (m.msg)
-                {
-                    case Imm.WM_IME_STARTCOMPOSITION:
-                    case Imm.WM_IME_COMPOSITION:
-                    case Imm.WM_IME_ENDCOMPOSITION:
-                        handleImeMessage(m.hwnd, m.msg, m.lParam);
-                        break;
-                }
-            };
+                case Imm.WM_IME_STARTCOMPOSITION:
+                case Imm.WM_IME_COMPOSITION:
+                case Imm.WM_IME_ENDCOMPOSITION:
+                    handleImeMessage(m.hwnd, m.msg, m.lParam);
+                    break;
+            }
         }
 
         public override void ResetIme() => ScheduleCommand(() => Imm.CancelComposition(WindowHandle));
