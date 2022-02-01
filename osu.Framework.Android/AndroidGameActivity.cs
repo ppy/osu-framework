6fd2c111be8c410cde23a3defefb66d4abea14cf
Dean Herbert <pe@ppy.sh>
2022-02-01 20:08:44 +0900

    Remove unused using statement

--- osu.Framework.Android/AndroidGameActivity.cs
+++ osu.Framework.Android/AndroidGameActivity.cs
@@ -2,7 +2,6 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
-using System.Threading;
 using Android.App;
 using Android.Content;
 using Android.Content.PM;
