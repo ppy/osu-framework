e83d97c449de5f6ccffb4c3d0946a0f28c722e89
Dean Herbert <pe@ppy.sh>
2022-02-10 17:48:59 +0900

    Fix new inspections on async calls

--- osu.Framework.Tests/IO/TestWebRequest.cs
+++ osu.Framework.Tests/IO/TestWebRequest.cs
@@ -415,7 +415,7 @@ namespace osu.Framework.Tests.IO
             request.Failed += exception => hasThrown = exception != null;
 
             cancellationSource.Cancel();
-            request.PerformAsync(cancellationSource.Token);
+            request.PerformAsync(cancellationSource.Token).WaitSafely();
 
             Assert.ThrowsAsync<InvalidOperationException>(() => request.PerformAsync(cancellationSource.Token));
 
