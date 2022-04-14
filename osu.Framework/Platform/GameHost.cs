3f707df24d2efbc1ff44ed80c55d445911df8091
Dean Herbert <pe@ppy.sh>
2022-04-14 20:49:19 +0900

    Add xmldoc for `GameHost.Exiting`

--- osu.Framework/Platform/GameHost.cs
+++ osu.Framework/Platform/GameHost.cs
@@ -84,6 +84,9 @@ namespace osu.Framework.Platform
         /// </summary>
         public event Action Deactivated;
 
+        /// <summary>
+        /// Called when the host is requesting to exit. Return <c>false</c> to block the exit process.
+        /// </summary>
         public event Func<bool> Exiting;
 
         public event Action Exited;
