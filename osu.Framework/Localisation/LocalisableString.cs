05b9cba521b64105c9d68a620b639b39df43de70
Salman Ahmed <frenzibyte@gmail.com>
2022-04-24 21:12:05 +0300

    Add `LocalisableString.Format` method
    Analogue to `string.Format`.

--- osu.Framework/Localisation/LocalisableString.cs
+++ osu.Framework/Localisation/LocalisableString.cs
@@ -33,6 +33,13 @@ namespace osu.Framework.Localisation
             Data = data;
         }
 
+        /// <summary>
+        /// Replaces one or more format items in a specified string with a localised string representation of a corresponding object in <paramref name="args"/>.
+        /// </summary>
+        /// <param name="format">The format string.</param>
+        /// <param name="args">The objects to format.</param>
+        public static LocalisableString Format(string format, params object?[] args) => new LocalisableFormattableString(format, args);
+
         // it's somehow common to call default(LocalisableString), and we should return empty string then.
         public override string ToString() => Data?.ToString() ?? string.Empty;
 
@@ -40,13 +47,13 @@ namespace osu.Framework.Localisation
         public override bool Equals(object? obj) => obj is LocalisableString other && Equals(other);
         public override int GetHashCode() => LocalisableStringEqualityComparer.Default.GetHashCode(this);
 
+        public static bool operator ==(LocalisableString left, LocalisableString right) => left.Equals(right);
+        public static bool operator !=(LocalisableString left, LocalisableString right) => !left.Equals(right);
+
         public static implicit operator LocalisableString(string text) => new LocalisableString(text);
         public static implicit operator LocalisableString(TranslatableString translatable) => new LocalisableString(translatable);
         public static implicit operator LocalisableString(RomanisableString romanisable) => new LocalisableString(romanisable);
         public static implicit operator LocalisableString(LocalisableFormattableString formattable) => new LocalisableString(formattable);
         public static implicit operator LocalisableString(CaseTransformableString transformable) => new LocalisableString(transformable);
-
-        public static bool operator ==(LocalisableString left, LocalisableString right) => left.Equals(right);
-        public static bool operator !=(LocalisableString left, LocalisableString right) => !left.Equals(right);
     }
 }
