4eae2682eedb8ec2e9f7496a7f666f5d7cf24c17
Dan Balasescu <smoogipoo@smgi.me>
2022-04-28 18:53:19 +0900

    Remove no longer used libraries to prevent errors

--- osu.Framework.NativeLibs/scripts/ffmpeg/macOS/combine_universal.sh
+++ osu.Framework.NativeLibs/scripts/ffmpeg/macOS/combine_universal.sh
@@ -3,9 +3,7 @@
 mkdir -p macOS-universal
 
 lipo -create macOS-arm64/libavcodec.58.dylib macOS-x86_64/libavcodec.58.dylib -output macOS-universal/libavcodec.58.dylib
-lipo -create macOS-arm64/libavdevice.58.dylib macOS-x86_64/libavdevice.58.dylib -output macOS-universal/libavdevice.58.dylib
 lipo -create macOS-arm64/libavfilter.7.dylib macOS-x86_64/libavfilter.7.dylib -output macOS-universal/libavfilter.7.dylib
 lipo -create macOS-arm64/libavformat.58.dylib macOS-x86_64/libavformat.58.dylib -output macOS-universal/libavformat.58.dylib
 lipo -create macOS-arm64/libavutil.56.dylib macOS-x86_64/libavutil.56.dylib -output macOS-universal/libavutil.56.dylib
-lipo -create macOS-arm64/libswresample.3.dylib macOS-x86_64/libswresample.3.dylib -output macOS-universal/libswresample.3.dylib
 lipo -create macOS-arm64/libswscale.5.dylib macOS-x86_64/libswscale.5.dylib -output macOS-universal/libswscale.5.dylib
--- osu.Framework.NativeLibs/scripts/ffmpeg/macOS/fetch_and_build.sh
+++ osu.Framework.NativeLibs/scripts/ffmpeg/macOS/fetch_and_build.sh
@@ -32,11 +32,9 @@ FFMPEG_FLAGS+=(
 build_ffmpeg
 
 mv build-$arch/lib/libavcodec.58.91.100.dylib build-$arch/lib/libavcodec.58.dylib
-mv build-$arch/lib/libavdevice.58.10.100.dylib build-$arch/lib/libavdevice.58.dylib
 mv build-$arch/lib/libavfilter.7.85.100.dylib build-$arch/lib/libavfilter.7.dylib
 mv build-$arch/lib/libavformat.58.45.100.dylib build-$arch/lib/libavformat.58.dylib
 mv build-$arch/lib/libavutil.56.51.100.dylib build-$arch/lib/libavutil.56.dylib
-mv build-$arch/lib/libswresample.3.7.100.dylib build-$arch/lib/libswresample.3.dylib
 mv build-$arch/lib/libswscale.5.7.100.dylib build-$arch/lib/libswscale.5.dylib
 
 echo "-> Fixing dylibs paths..."
@@ -45,11 +43,9 @@ LIBS="libavcodec.58.dylib libavdevice.58.dylib libavfilter.7.dylib libavformat.5
 for f in $LIBS; do
     install_name_tool $BUILDPATH/$f -id @loader_path/$f \
         -change $BUILDPATH/libavcodec.58.dylib @loader_path/libavcodec.58.dylib \
-        -change $BUILDPATH/libavdevice.58.dylib @loader_path/libavdevice.58.dylib \
         -change $BUILDPATH/libavfilter.7.dylib @loader_path/libavfilter.7.dylib \
         -change $BUILDPATH/libavformat.58.dylib @loader_path/libavformat.58.dylib \
         -change $BUILDPATH/libavutil.56.dylib @loader_path/libavutil.56.dylib \
-        -change $BUILDPATH/libswresample.3.dylib @loader_path/libswresample.3.dylib \
         -change $BUILDPATH/libswscale.5.dylib @loader_path/libswscale.5.dylib
 
     mkdir -p ../build-$arch
