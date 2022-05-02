3bbb6aaad108eb4bd515b288455db1ef451c2933
Dean Herbert <pe@ppy.sh>
2022-05-02 17:06:23 +0900

    Move caching specifications of markdown interfaces to be attached to the interfaces
    As seen in https://github.com/ppy/osu/pull/18049, it can be easy to
    forget adding the caching of these interfaces. I believe all usages of
    the interfaces intend to also cache (as that is the main purpose of the
    interface existing).

--- osu.Framework/Graphics/Containers/Markdown/IMarkdownTextComponent.cs
+++ osu.Framework/Graphics/Containers/Markdown/IMarkdownTextComponent.cs
@@ -1,10 +1,12 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.Allocation;
 using osu.Framework.Graphics.Sprites;
 
 namespace osu.Framework.Graphics.Containers.Markdown
 {
+    [Cached(typeof(IMarkdownTextComponent))]
     public interface IMarkdownTextComponent
     {
         /// <summary>
--- osu.Framework/Graphics/Containers/Markdown/IMarkdownTextFlowComponent.cs
+++ osu.Framework/Graphics/Containers/Markdown/IMarkdownTextFlowComponent.cs
@@ -1,8 +1,11 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.Allocation;
+
 namespace osu.Framework.Graphics.Containers.Markdown
 {
+    [Cached(Type = typeof(IMarkdownTextFlowComponent))]
     public interface IMarkdownTextFlowComponent
     {
         /// <summary>
