252a680b32a0eb97acc7c56f72564700d178fd69
Salman Ahmed <frenzibyte@gmail.com>
2022-05-03 10:59:56 +0300

    Add note about noto font usage

--- osu.Framework.Tests/Visual/FrameworkTestSceneTestRunner.cs
+++ osu.Framework.Tests/Visual/FrameworkTestSceneTestRunner.cs
@@ -20,6 +20,7 @@ namespace osu.Framework.Tests.Visual
             // nested store for framework test fonts to not interfere with framework's default (roboto) in test scenes.
             Fonts.AddStore(testFonts = new FontStore(useAtlas: false));
 
+            // note that only a few of the noto sprite sheets have been included with this font, not the full set.
             AddFont(Resources, "Fonts/Noto/Noto-Basic", metrics: new FontMetrics(880, 120, 1000), target: testFonts);
         }
     }
