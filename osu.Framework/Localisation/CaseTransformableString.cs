2a1fc26fed61e4c5809f96dbabdc3104e5d6d684
Joseph Madamba <madamba.joehu@outlook.com>
2022-05-03 12:19:53 -0700

    Fix removed null operator

--- osu.Framework/Localisation/CaseTransformableString.cs
+++ osu.Framework/Localisation/CaseTransformableString.cs
@@ -37,7 +37,7 @@ namespace osu.Framework.Localisation
         public string GetLocalised(LocalisationParameters parameters)
         {
             string stringData = getStringData(parameters);
-            var cultureText = parameters.Store?.EffectiveCulture.TextInfo ?? CultureInfo.InvariantCulture.TextInfo;
+            var cultureText = parameters.Store?.EffectiveCulture?.TextInfo ?? CultureInfo.InvariantCulture.TextInfo;
 
             switch (Casing)
             {
