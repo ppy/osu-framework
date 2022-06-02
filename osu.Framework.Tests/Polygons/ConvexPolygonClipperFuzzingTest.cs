9dd011dd5c44d243e64e726ff571aa96980e9fa0
Dan Balasescu <smoogipoo@smgi.me>
2022-06-02 13:27:04 +0900

    Unused using...

--- osu.Framework.Tests/Polygons/ConvexPolygonClipperFuzzingTest.cs
+++ osu.Framework.Tests/Polygons/ConvexPolygonClipperFuzzingTest.cs
@@ -4,7 +4,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using osu.Framework.Graphics.Primitives;
