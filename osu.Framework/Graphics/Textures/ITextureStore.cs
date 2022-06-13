1d03b6d4b71c3e44c8b610a674313c30c2479211
Salman Ahmed <frenzibyte@gmail.com>
2022-06-14 01:39:22 +0300

    Add `#nullable enable` to texture store interface

--- osu.Framework/Graphics/Textures/ITextureStore.cs
+++ osu.Framework/Graphics/Textures/ITextureStore.cs
@@ -1,6 +1,8 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+#nullable enable
+
 using System.Threading;
 using System.Threading.Tasks;
 using osu.Framework.Graphics.OpenGL.Textures;
@@ -17,7 +19,7 @@ namespace osu.Framework.Graphics.Textures
         /// <param name="wrapModeS">The texture wrap mode in horizontal direction.</param>
         /// <param name="wrapModeT">The texture wrap mode in vertical direction.</param>
         /// <returns>The texture.</returns>
-        Texture Get(string name, WrapMode wrapModeS, WrapMode wrapModeT);
+        Texture? Get(string name, WrapMode wrapModeS, WrapMode wrapModeT);
 
         /// <summary>
         /// Retrieves a texture from the store asynchronously.
@@ -27,6 +29,6 @@ namespace osu.Framework.Graphics.Textures
         /// <param name="wrapModeT">The texture wrap mode in vertical direction.</param>
         /// <param name="cancellationToken">A cancellation token.</param>
         /// <returns>The texture.</returns>
-        Task<Texture> GetAsync(string name, WrapMode wrapModeS, WrapMode wrapModeT, CancellationToken cancellationToken = default);
+        Task<Texture?> GetAsync(string name, WrapMode wrapModeS, WrapMode wrapModeT, CancellationToken cancellationToken = default);
     }
 }
