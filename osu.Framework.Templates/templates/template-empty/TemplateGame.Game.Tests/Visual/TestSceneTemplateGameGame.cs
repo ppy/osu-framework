d133f7c066e800e01d4d2ede89c8bd13eb512fcd
Dan Balasescu <smoogipoo@smgi.me>
2022-06-16 20:54:23 +0900

    Add [TestFixture] attributes to tests

--- osu.Framework.Templates/templates/template-empty/TemplateGame.Game.Tests/Visual/TestSceneTemplateGameGame.cs
+++ osu.Framework.Templates/templates/template-empty/TemplateGame.Game.Tests/Visual/TestSceneTemplateGameGame.cs
@@ -1,8 +1,10 @@
 using osu.Framework.Allocation;
 using osu.Framework.Platform;
+using NUnit.Framework;
 
 namespace TemplateGame.Game.Tests.Visual
 {
+    [TestFixture]
     public class TestSceneTemplateGameGame : TemplateGameTestScene
     {
         // Add visual tests to ensure correct behaviour of your game: https://github.com/ppy/osu-framework/wiki/Development-and-Testing
