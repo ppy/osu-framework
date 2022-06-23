5532c5be77a22ea9788192911a3fdf7b372672e2
Salman Ahmed <frenzibyte@gmail.com>
2022-06-24 01:37:45 +0300

    Add failing test case

--- osu.Framework.Tests/Localisation/LocalisableDescriptionAttributeTest.cs
+++ osu.Framework.Tests/Localisation/LocalisableDescriptionAttributeTest.cs
@@ -51,6 +51,13 @@ namespace osu.Framework.Tests.Localisation
             Assert.Throws<InvalidOperationException>(() => EnumD.Item1.GetLocalisableDescription());
         }
 
+        [Test]
+        public void TestLocalisableStringDescription()
+        {
+            object description = TestStrings.Romanisable;
+            Assert.AreEqual(description, description.GetLocalisableDescription());
+        }
+
         public enum EnumA
         {
             Item1,
@@ -65,7 +72,7 @@ namespace osu.Framework.Tests.Localisation
             Item1,
 
             [LocalisableDescription(typeof(TestStrings), nameof(TestStrings.B))]
-            Item2
+            Item2,
         }
 
         public enum EnumC
@@ -90,6 +97,8 @@ namespace osu.Framework.Tests.Localisation
             public static LocalisableString A => "Localised A";
 
             public static readonly LocalisableString B = "Localised B";
+
+            public static LocalisableString Romanisable => new RomanisableString("Original", "Romanised");
 
             public LocalisableString Instance => string.Empty;
         }
