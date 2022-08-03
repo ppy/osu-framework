13c9978ba13ed785b5517eb77bdefcfd3898a2c9
Dean Herbert <pe@ppy.sh>
2022-08-03 17:01:52 +0900

    Add test with origin offset for good measure

--- osu.Framework.Tests/Graphics/TextureRegionTest.cs
+++ osu.Framework.Tests/Graphics/TextureRegionTest.cs
@@ -31,6 +31,26 @@ namespace osu.Framework.Tests.Graphics
         }
 
         [Test]
+        public void TestRegionHasCorrectSizeWithOffset()
+        {
+            var tex = new DummyRenderer().CreateTexture(100, 100);
+
+            Assert.That(tex.Width, Is.EqualTo(100));
+            Assert.That(tex.Height, Is.EqualTo(100));
+            Assert.That(tex.DisplayWidth, Is.EqualTo(100));
+            Assert.That(tex.DisplayHeight, Is.EqualTo(100));
+            Assert.That(tex.GetTextureRect(), Is.EqualTo(new RectangleF(0, 0, 1, 1)));
+
+            var subTex = tex.Crop(new RectangleF(25, 25, 50, 50));
+
+            Assert.That(subTex.Width, Is.EqualTo(50));
+            Assert.That(subTex.Height, Is.EqualTo(50));
+            Assert.That(subTex.DisplayWidth, Is.EqualTo(50));
+            Assert.That(subTex.DisplayHeight, Is.EqualTo(50));
+            Assert.That(subTex.GetTextureRect(), Is.EqualTo(new RectangleF(0.25f, 0.25f, 0.5f, 0.5f)));
+        }
+
+        [Test]
         public void TestScaleAdjustOnlyAffectsDisplaySize()
         {
             var tex = new DummyRenderer().CreateTexture(100, 100);
