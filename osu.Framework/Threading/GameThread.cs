069d3a716a6d6e550d41c957f07d92b7682978ca
andy840119 <andy840119@gmail.com>
2022-08-07 16:42:40 +0800

    Add the null check.

--- osu.Framework/Threading/GameThread.cs
+++ osu.Framework/Threading/GameThread.cs
@@ -265,7 +265,7 @@ namespace osu.Framework.Threading
 
                 Clock.Throttling = withThrottling;
 
-                Monitor.MakeCurrent();
+                Monitor?.MakeCurrent();
 
                 updateCulture();
 
