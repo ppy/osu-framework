b1fdfe7b4623e8c6ab20afe8160783b5a0efa5fa
Susko3 <Susko3@protonmail.com>
2022-08-08 18:13:48 +0200

    Move `CultureInfo` parsing logic from `GameHost` into a separate helper
    The new `CultureInfoHelper.TryGetCultureInfo` is slightly different from the old logic;
    returning the current culture instead of the invariant for an empty string. This is to
    align with `LocalisationManager`.

---
+++ osu.Framework/Localisation/CultureInfoHelper.cs
@@ -0,0 +1,48 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Globalization;
+
+namespace osu.Framework.Localisation
+{
+    public static class CultureInfoHelper
+    {
+        /// <summary>
+        /// Wrapper around <see cref="CultureInfo.GetCultureInfo(string)"/> providing common behaviour and exception handling.
+        /// </summary>
+        /// <param name="name">Name of the culture, or empty string for <see cref="CultureInfo.CurrentCulture"/>.</param>
+        /// <param name="culture">The <see cref="CultureInfo"/> with the specified <paramref name="name"/>, or <see cref="CultureInfo.InvariantCulture"/> on failure.</param>
+        /// <returns>Whether the culture was successfully retrieved and a is .NET/OS predefined culture.</returns>
+        public static bool TryGetCultureInfo(string name, out CultureInfo culture)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                culture = CultureInfo.CurrentCulture;
+                return true;
+            }
+
+            try
+            {
+#if NET6_0_OR_GREATER
+                culture = CultureInfo.GetCultureInfo(name, predefinedOnly: true);
+#else
+                culture = CultureInfo.GetCultureInfo(name);
+
+                // This is best-effort for now to catch cases where dotnet is creating cultures.
+                // See https://github.com/dotnet/runtime/blob/5877e8b713742b6d80bd1aa9819094be029e3e1f/src/libraries/System.Private.CoreLib/src/System/Globalization/CultureData.Icu.cs#L341-L345
+                if (culture.ThreeLetterWindowsLanguageName == "ZZZ")
+                {
+                    culture = CultureInfo.InvariantCulture;
+                    return false;
+                }
+#endif
+                return true;
+            }
+            catch (CultureNotFoundException)
+            {
+                culture = CultureInfo.InvariantCulture;
+                return false;
+            }
+        }
+    }
+}
