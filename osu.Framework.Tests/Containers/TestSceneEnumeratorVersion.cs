99fe6b6463d5e27608ca28a1bb91145ea0c2baf4
Dan Balasescu <smoogipoo@smgi.me>
2022-09-09 08:00:45 +0900

    Remove nullable disable

--- osu.Framework.Tests/Containers/TestSceneEnumeratorVersion.cs
+++ osu.Framework.Tests/Containers/TestSceneEnumeratorVersion.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-#nullable disable
-
 using System;
 using NUnit.Framework;
 using osu.Framework.Graphics.Containers;
@@ -12,7 +10,7 @@ namespace osu.Framework.Tests.Containers
 {
     public class TestSceneEnumeratorVersion : FrameworkTestScene
     {
-        private Container parent;
+        private Container parent = null!;
 
         [SetUp]
         public void SetUp() => Schedule(() =>
