5ed6448f80db046941e493a08a9021316343f49a
Andrei Zavatski <megaman9919@gmail.com>
2022-10-12 04:07:37 +0300

    Refactor CircularProgressUtils
    Added more comments, improved readability and getting prepared for aa implementation

--- osu.Framework/Resources/Shaders/sh_CircularProgressUtils.h
+++ osu.Framework/Resources/Shaders/sh_CircularProgressUtils.h
@@ -1,10 +1,7 @@
 ﻿#define PI 3.1415926536
 
-bool insideProgressSector(highp vec2 pixelPos, mediump float progress, mediump float innerRadius, bool roundedCaps)
+bool insideProgress(highp vec2 pixelPos, mediump float progress, mediump float innerRadius, bool roundedCaps)
 {
-    if (progress >= 1.0)
-        return true;
-
     // Compute angle of the current pixel in the (0, 2*PI) range
     mediump float pixelAngle = atan(0.5 - pixelPos.y, 0.5 - pixelPos.x) - PI / 2.0;
     if (pixelAngle < 0.0)
@@ -12,28 +9,27 @@ bool insideProgressSector(highp vec2 pixelPos, mediump float progress, mediump f
 
     mediump float progressAngle = 2.0 * PI * progress;
 
-    if (pixelAngle < progressAngle)
-        return true;
+    if (progress >= 1.0 || pixelAngle < progressAngle) // Pixel inside the sector
+    {
+        highp float dstFromCentre = distance(pixelPos, vec2(0.5));
+        highp float innerBorder = 0.5 * (1.0 - innerRadius);
 
-    if (!roundedCaps)
-        return false;
+        // Pixel within a ring
+        return dstFromCentre < 0.5 && dstFromCentre > innerBorder;
+    }
 
-    mediump float pathRadius = 0.25 * innerRadius;
+    if (roundedCaps) // Pixel outside the sector with rounded caps enabled
+    {
+        mediump float pathRadius = 0.25 * innerRadius;
 
-    mediump vec2 arcStart = vec2(0.5, pathRadius);
-    mediump vec2 arcEnd = vec2(0.5) + vec2(cos(progressAngle - PI / 2.0), sin(progressAngle - PI / 2.0)) * vec2(0.25 * (2.0 - innerRadius));
+        highp vec2 arcStart = vec2(0.5, pathRadius);
+        highp vec2 arcEnd = vec2(0.5) + vec2(cos(progressAngle - PI / 2.0), sin(progressAngle - PI / 2.0)) * vec2(0.5 - pathRadius);
 
-    mediump float dstToArc = min(distance(pixelPos, arcStart), distance(pixelPos, arcEnd));
+        highp float dstToArc = min(distance(pixelPos, arcStart), distance(pixelPos, arcEnd));
 
-    return dstToArc < pathRadius;
-}
+        // Pixel within a cap
+        return dstToArc < pathRadius;
+    }
 
-bool insideProgress(highp vec2 pixelPos, mediump float progress, mediump float innerRadius, bool roundedCaps)
-{
-    mediump float innerBorder = 0.5 * (1.0 - innerRadius);
-
-    mediump float dstFromCentre = distance(pixelPos, vec2(0.5));
-    bool insideRing = dstFromCentre > innerBorder && dstFromCentre < 0.5;
-
-    return insideRing && insideProgressSector(pixelPos, progress, innerRadius, roundedCaps);
+    return false;
 }
