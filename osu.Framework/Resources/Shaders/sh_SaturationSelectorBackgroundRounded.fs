4192493ab7a407533bd6f627404730b6e4511bdc
Salman Ahmed <frenzibyte@gmail.com>
2022-10-14 01:47:07 +0300

    Use swizzle to improve readability

--- osu.Framework/Resources/Shaders/sh_SaturationSelectorBackgroundRounded.fs
+++ osu.Framework/Resources/Shaders/sh_SaturationSelectorBackgroundRounded.fs
@@ -9,7 +9,7 @@ uniform mediump float hue;
 
 void main(void)
 {
-    highp vec2 resolution = vec2(v_TexRect[2] - v_TexRect[0], v_TexRect[3] - v_TexRect[1]);
+    highp vec2 resolution = v_TexRect.zw - v_TexRect.xy;
     highp vec2 pixelPos = v_TexCoord / resolution;
     gl_FragColor = getRoundedColor(hsv2rgb(vec4(hue, pixelPos.x, 1.0 - pixelPos.y, 1.0)), v_TexCoord);
 }
