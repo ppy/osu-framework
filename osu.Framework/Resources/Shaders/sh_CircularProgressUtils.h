752ace361ef6c3346104603f71f4d36d7ea936fc
Andrei Zavatski <megaman9919@gmail.com>
2022-10-18 03:21:29 +0300

    Expose distanceToProgress function for future use

--- osu.Framework/Resources/Shaders/sh_CircularProgressUtils.h
+++ osu.Framework/Resources/Shaders/sh_CircularProgressUtils.h
@@ -10,21 +10,13 @@ highp float dstToLine(highp vec2 start, highp vec2 end, highp vec2 pixelPos)
         return distance(pixelPos, start);
 
     highp vec2 a = (end - start) / lineLength;
-    highp vec2 closest = clamp(dot(a, pixelPos - start), 0.0, distance(end, start)) * a + start; // closest point on a line to from given position
+    highp vec2 closest = clamp(dot(a, pixelPos - start), 0.0, distance(end, start)) * a + start; // closest point on a line from given position
     return distance(closest, pixelPos);
 }
 
-lowp float progressAlphaAt(highp vec2 pixelPos, mediump float progress, mediump float innerRadius, bool roundedCaps, highp float texelSize)
+// Returns distance to the progress shape (to closest pixel on it's border)
+highp float distanceToProgress(highp vec2 pixelPos, mediump float progress, mediump float innerRadius, bool roundedCaps, highp float texelSize)
 {
-    // This is a bit of a hack to make progress appear smooth if it's radius < texelSize by making it more transparent while leaving thickness the same
-    lowp float subAAMultiplier = 1.0;
-
-    if (innerRadius < texelSize * 2.0)
-    {
-        subAAMultiplier = max(innerRadius / (texelSize * 2.0), 0.1);
-        innerRadius = texelSize * 2.0;
-    }
-
     // Compute angle of the current pixel in the (0, 2*PI) range
     mediump float pixelAngle = atan(0.5 - pixelPos.y, 0.5 - pixelPos.x) - HALF_PI;
     if (pixelAngle < 0.0)
@@ -37,11 +29,11 @@ lowp float progressAlphaAt(highp vec2 pixelPos, mediump float progress, mediump 
         highp float dstFromCentre = distance(pixelPos, vec2(0.5));
 
         if (dstFromCentre > 0.5 - texelSize) // on the outer side of the ring
-            return smoothstep(0.5, 0.5 - texelSize, dstFromCentre) * subAAMultiplier;
+            return dstFromCentre - (0.5 - texelSize);
 
         highp float innerBorder = 0.5 * (1.0 - innerRadius);
 
-        return smoothstep(innerBorder - texelSize, innerBorder, dstFromCentre) * subAAMultiplier;
+        return innerBorder - dstFromCentre;
     }
 
     progressAngle = progressAngle - HALF_PI;
@@ -55,9 +47,7 @@ lowp float progressAlphaAt(highp vec2 pixelPos, mediump float progress, mediump 
         highp vec2 arcStart = vec2(0.5, pathRadius + halfTexel);
         highp vec2 arcEnd = vec2(0.5) + cs * vec2(0.5 - pathRadius - halfTexel);
 
-        highp float dstToArc = min(distance(pixelPos, arcStart), distance(pixelPos, arcEnd));
-
-        return smoothstep(pathRadius + halfTexel, pathRadius - halfTexel, dstToArc) * subAAMultiplier;
+        return min(distance(pixelPos, arcStart), distance(pixelPos, arcEnd)) + halfTexel - pathRadius;
     }
 
     highp float dstToIdleEdge = dstToLine(vec2(0.5, texelSize), vec2(0.5, 0.5 * innerRadius), pixelPos);
@@ -66,5 +56,19 @@ lowp float progressAlphaAt(highp vec2 pixelPos, mediump float progress, mediump 
     highp vec2 rotatingEdgeBottom = vec2(0.5) + cs * vec2(0.5 - 0.5 * innerRadius);
     highp float dstToRotatingEdge = dstToLine(rotatingEdgeTop, rotatingEdgeBottom, pixelPos);
 
-    return smoothstep(texelSize, 0.0, min(dstToIdleEdge, dstToRotatingEdge)) * subAAMultiplier;
+    return min(dstToIdleEdge, dstToRotatingEdge);
+}
+
+lowp float progressAlphaAt(highp vec2 pixelPos, mediump float progress, mediump float innerRadius, bool roundedCaps, highp float texelSize)
+{
+    // This is a bit of a hack to make progress appear smooth if it's radius < texelSize by making it more transparent while leaving thickness the same
+    lowp float subAAMultiplier = 1.0;
+
+    if (innerRadius < texelSize * 2.0)
+    {
+        subAAMultiplier = max(innerRadius / (texelSize * 2.0), 0.1);
+        innerRadius = texelSize * 2.0;
+    }
+    
+    return smoothstep(texelSize, 0.0, distanceToProgress(pixelPos, progress, innerRadius, roundedCaps, texelSize)) * subAAMultiplier;
 }
