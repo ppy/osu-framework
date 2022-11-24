ppy/osu-framework

commit 9faae09581d9c4aa24175ed78197720fd983e40e

Add `Bindable<T>.CopyTo` and use in `GetUnboundCopy` implementation

osu.Framework/Bindables/RangeConstrainedBindable.cs
@@ -158,6 +158,17 @@ namespace osu.Framework.Bindables
                 MaxValueChanged?.Invoke(maxValue);
         }
 
+        public override void CopyTo(Bindable<T> them)
+        {
+            base.CopyTo(them);
+
+            if (them is RangeConstrainedBindable<T> other)
+            {
+                other.MinValue = MinValue;
+                other.MaxValue = MaxValue;
+            }
+        }
+
         public override void BindTo(Bindable<T> them)
         {
             if (them is RangeConstrainedBindable<T> other)
@@ -167,9 +178,6 @@ namespace osu.Framework.Bindables
                     throw new ArgumentOutOfRangeException(
                         nameof(them), $"The target bindable has specified an invalid range of [{other.MinValue} - {other.MaxValue}].");
                 }
-
-                MinValue = other.MinValue;
-                MaxValue = other.MaxValue;
             }
 
             base.BindTo(them);

osu.Framework/Bindables/Bindable.cs
@@ -181,6 +181,17 @@ namespace osu.Framework.Bindables
         }
 
         /// <summary>
+        /// Copies all values and value limitations of this bindable to another such.
+        /// </summary>
+        /// <param name="them">The target to copy to</param>
+        public virtual void CopyTo(Bindable<T> them)
+        {
+            them.Value = Value;
+            them.Default = Default;
+            them.Disabled = Disabled;
+        }
+
+        /// <summary>
         /// Binds this bindable to another such that bi-directional updates are propagated.
         /// This will adopt any values and value limitations of the bindable bound to.
         /// </summary>
@@ -191,9 +202,7 @@ namespace osu.Framework.Bindables
             if (Bindings?.Contains(them) == true)
                 throw new InvalidOperationException($"This bindable is already bound to the requested bindable ({them}).");
 
-            Value = them.Value;
-            Default = them.Default;
-            Disabled = them.Disabled;
+            them.CopyTo(this);
 
             addWeakReference(them.weakReference);
             them.addWeakReference(weakReference);
@@ -389,9 +398,9 @@ namespace osu.Framework.Bindables
         /// </summary>
         public Bindable<T> GetUnboundCopy()
         {
-            var clone = GetBoundCopy();
-            clone.UnbindAll();
-            return clone;
+            var newBindable = CreateInstance();
+            CopyTo(newBindable);
+            return newBindable;
         }
 
         IBindable IBindable.CreateInstance() => CreateInstance();

osu.Framework/Bindables/BindableNumber.cs
@@ -199,12 +199,12 @@ namespace osu.Framework.Bindables
                 PrecisionChanged?.Invoke(precision);
         }
 
-        public override void BindTo(Bindable<T> them)
+        public override void CopyTo(Bindable<T> them)
         {
             if (them is BindableNumber<T> other)
-                Precision = other.Precision;
+                other.Precision = Precision;
 
-            base.BindTo(them);
+            base.CopyTo(them);
         }
 
         public override void UnbindEvents()

commit 5ce4bc8cb9e6e83d52f65d3238a87018a525c8fe

Fix incorrect transferal order in `RangeConstrainedBindable.CopyTo()`
Regressed in 9faae09581d9c4aa24175ed78197720fd983e40e.

When copying over values of a range-constrained bindable, the range
bounds need to be transferred first, because assignment to `.Value` of a
range-constrained bindable will automatically clamp the value to
[`MinValue`, `MaxValue`], leading to a scenario where a new value being
transferred across would get clamped to stale bounds.

This affected both `.CopyTo()` and `.BindTo()`, as the latter calls the
former.

osu.Framework/Bindables/RangeConstrainedBindable.cs
@@ -160,13 +160,15 @@ namespace osu.Framework.Bindables
 
         public override void CopyTo(Bindable<T> them)
         {
-            base.CopyTo(them);
-
+            // if the other bindable is range-constrained, the bounds need to be copied across first,
+            // as Value assignment (in the base call below) automatically clamps to [MinValue, MaxValue].
             if (them is RangeConstrainedBindable<T> other)
             {
                 other.MinValue = MinValue;
                 other.MaxValue = MaxValue;
             }
+
+            base.CopyTo(them);
         }
 
         public override void BindTo(Bindable<T> them)