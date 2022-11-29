9cbdf6babc116e279ee6792b0633b18cb61dbc90
Dan Balasescu <smoogipoo@smgi.me>
2022-11-29 19:49:22 +0900

    Rework structure

--- osu.Framework.SourceGeneration/pack.sh
+++ osu.Framework.SourceGeneration/pack.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-dotnet pack osu.Framework.SourceGeneration.Roslyn3.11.csproj $@
-dotnet pack osu.Framework.SourceGeneration.Roslyn4.0.csproj $@
+dotnet pack ./Roslyn3.11/osu.Framework.SourceGeneration.Roslyn3.11.csproj $@
+dotnet pack ./Roslyn4.0/osu.Framework.SourceGeneration.Roslyn4.0.csproj $@
