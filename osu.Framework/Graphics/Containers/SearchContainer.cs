ppy/osu-framework

commit 99915f9286d10b12b00479346c30005832ee8823

Rewrite `SearchContainer` to not rely on `IFilterableChildren`

osu.Framework/Graphics/Containers/SearchContainer.cs
@@ -8,9 +8,9 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using osu.Framework.Allocation;
+using osu.Framework.Bindables;
 using osu.Framework.Caching;
 using osu.Framework.Localisation;
-using osu.Framework.Extensions.IEnumerableExtensions;
 
 namespace osu.Framework.Graphics.Containers
 {
@@ -19,10 +19,17 @@ namespace osu.Framework.Graphics.Containers
     }
 
     /// <summary>
-    /// A container which filters children based on a search term.
+    /// A container which automatically filters <see cref="IFilterable"/> children based on a search term.
     /// Re-filtering will only be performed when the <see cref="SearchTerm"/> changes, or
-    /// new items are added as direct children of this container.
+    /// the layout of the container is invalidated.
     /// </summary>
+    /// <remarks>
+    /// <list type="bullet">
+    /// <item>Children which are searchable should be marked with the <see cref="IFilterable"/> interface. They do not need to be direct children to work (filtering will traverse the full drawable subtree).</item>
+    /// <item>Marking a container (ie. a "group" or "section" that contains nested <see cref="IFilterable"/>s) as <see cref="IFilterable"/> will automatically keep it non-filtered as long as at least one nested item is not filtered away.</item>
+    /// <item>Any <see cref="IFilterable"/>s which are contained in a <see cref="IConditionalFilterable"/> which has <see cref="IConditionalFilterable.CanBeShown"/> set to <see langword="false"/> will be excluded from filtering. This can be used to exclude certain items from consideration (ie. items which are hidden from display), allowing group/sections to be correctly filtered away.</item>
+    /// </list>
+    /// </remarks>
     /// <typeparam name="T"></typeparam>
     public class SearchContainer<T> : FillFlowContainer<T> where T : Drawable
     {
@@ -71,13 +78,14 @@ namespace osu.Framework.Graphics.Containers
         [Resolved]
         private LocalisationManager localisation { get; set; }
 
-        protected override void AddInternal(Drawable drawable)
+        private readonly Cached filterValid = new Cached();
+        private readonly ICollection<IBindable<bool>> canBeShownBindables = new List<IBindable<bool>>();
+
+        protected override void InvalidateLayout()
         {
-            base.AddInternal(drawable);
+            base.InvalidateLayout();
             filterValid.Invalidate();
         }
-
-        private readonly Cached filterValid = new Cached();
 
         protected override void Update()
         {
@@ -85,6 +93,7 @@ namespace osu.Framework.Graphics.Containers
 
             if (!filterValid.IsValid)
             {
+                canBeShownBindables.Clear();
                 performFilter();
                 filterValid.Validate();
                 FilterCompleted?.Invoke();
@@ -94,30 +103,54 @@ namespace osu.Framework.Graphics.Containers
         private void performFilter()
         {
             string[] terms = (searchTerm ?? string.Empty).Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-            Children.OfType<IFilterable>().ForEach(child => match(child, terms, terms.Length > 0, allowNonContiguousMatching));
+            matchSubTree(this, terms, terms.Any(), allowNonContiguousMatching);
         }
 
-        private bool match(IFilterable filterable, IEnumerable<string> searchTerms, bool searchActive, bool nonContiguousMatching)
+        private bool matchSubTree(Drawable drawable, IReadOnlyList<string> searchTerms, bool searchActive, bool nonContiguousMatching)
         {
-            IEnumerable<string> filterTerms = filterable.FilterTerms.SelectMany(localisedStr =>
-                new[] { localisedStr.ToString(), localisation.GetLocalisedString(localisedStr) });
+            bool matching = match(drawable, searchTerms, nonContiguousMatching, out var nonMatchingTerms);
 
-            //Words matched by parent is not needed to match children
-            string[] childTerms = searchTerms.Where(term =>
-                !filterTerms.Any(filterTerm =>
-                    checkTerm(filterTerm, term, nonContiguousMatching))).ToArray();
-
-            bool matching = childTerms.Length == 0;
-
-            //We need to check the children and should any child match this matches as well
-            if (filterable is IHasFilterableChildren hasFilterableChildren)
+            if (drawable is IContainerEnumerable<Drawable> container)
             {
-                foreach (IFilterable child in hasFilterableChildren.FilterableChildren)
-                    matching |= match(child, childTerms, searchActive, nonContiguousMatching);
+                foreach (var child in container.Children)
+                    matching |= matchSubTree(child, nonMatchingTerms, searchActive, nonContiguousMatching);
             }
 
-            filterable.FilteringActive = searchActive;
-            return filterable.MatchingFilter = matching;
+            if (drawable is IFilterable filterable)
+            {
+                filterable.FilteringActive = searchActive;
+                filterable.MatchingFilter = matching;
+            }
+
+            return matching;
+        }
+
+        private bool match(Drawable drawable, IReadOnlyList<string> searchTerms, bool nonContiguousMatching, out IReadOnlyList<string> nonMatchingTerms)
+        {
+            nonMatchingTerms = searchTerms;
+
+            if (drawable is IConditionalFilterable conditionalFilterable)
+            {
+                var canBeShownBindable = conditionalFilterable.CanBeShown.GetBoundCopy();
+                canBeShownBindables.Add(canBeShownBindable);
+                canBeShownBindable.BindValueChanged(_ => filterValid.Invalidate());
+
+                if (!conditionalFilterable.CanBeShown.Value)
+                    return false;
+            }
+
+            if (drawable is IFilterable filterable)
+            {
+                IEnumerable<string> filterTerms = filterable.FilterTerms.SelectMany(localisedStr =>
+                    new[] { localisedStr.ToString(), localisation.GetLocalisedString(localisedStr) });
+
+                //Words matched by parent is not needed to match children
+                nonMatchingTerms = searchTerms.Where(term =>
+                    !filterTerms.Any(filterTerm =>
+                        checkTerm(filterTerm, term, nonContiguousMatching))).ToArray();
+            }
+
+            return nonMatchingTerms.Count == 0;
         }
 
         /// <summary>

osu.Framework/Graphics/Containers/FlowContainer.cs
@@ -219,7 +219,7 @@ namespace osu.Framework.Graphics.Containers
 
             if (!childLayout.IsValid)
             {
-                layout.Invalidate();
+                InvalidateLayout();
                 childLayout.Validate();
             }
 

osu.Framework/Graphics/Containers/IFilterable.cs
@@ -3,6 +3,10 @@
 
 namespace osu.Framework.Graphics.Containers
 {
+    /// <summary>
+    /// Implementing this interface in a <see cref="Drawable"/> allows it to be filtered basing on provided search terms
+    /// when placed inside a <see cref="SearchContainer{T}"/>.
+    /// </summary>
     public interface IFilterable : IHasFilterTerms
     {
         /// <summary>

osu.Framework/Graphics/Containers/IHasFilterableChildren.cs
@@ -1,10 +1,12 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using System.Collections.Generic;
 
 namespace osu.Framework.Graphics.Containers
 {
+    [Obsolete("IFilterable children are now looked up automatically. Implementors of this interface should implement IFilterable directly instead.")] // can be removed 20230512
     public interface IHasFilterableChildren : IFilterable
     {
         /// <summary>

commit e0d3132d19715b4225878cc6129fe69049685b74

Fix search container filtering too much
As it turns out, 99915f9286d10b12b00479346c30005832ee8823 introduced a
performance regression in `SearchContainer`. Due to borrowing from #5154
too much, the search logic was changed in order to hook into
`InvalidateLayout()` to perform search, rather than just do it when a
new internal child was added, as was the case previously.

For the sake of completeness, let's review the possible causes for
`InvalidateLayout()` firing with respect to filtering:

- `AddInternal()`: Valid, as the new child needs to be examined against
  the current filter, and preserved by reverting to the old code.
- `RemoveInternal()`: Mostly irrelevant, as long as the presence of the
  removed drawable in the container doesn't impact the filter state of
  any of its siblings. Let's not support that case for now.
- `ClearInternal()`: Totally irrelevant, as anything that could be
  filtered is now gone.
- `SetLayoutPosition()`: Hopefully irrelevant.
- `UpdateChildrenLife()`: Not relevant. Nothing in `SearchContainer`
  considers lifetime explicitly.
- `FlowContainer.childLayout` becoming invalid: The invalidation flags
  there are `RequiredParentSizeToFit | Presence`. Neither should be
  relevant for filtering, given the direction taken with
  `IConditionalFilterable`.
- `Direction` and `Spacing`: 100% irrelevant.

osu.Framework/Graphics/Containers/SearchContainer.cs
@@ -81,9 +81,9 @@ namespace osu.Framework.Graphics.Containers
         private readonly Cached filterValid = new Cached();
         private readonly ICollection<IBindable<bool>> canBeShownBindables = new List<IBindable<bool>>();
 
-        protected override void InvalidateLayout()
+        protected override void AddInternal(Drawable drawable)
         {
-            base.InvalidateLayout();
+            base.AddInternal(drawable);
             filterValid.Invalidate();
         }
 