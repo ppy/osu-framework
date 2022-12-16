e766b8432190b617cdfd2a461ae48bb49ca81722
Andrei Zavatski <megaman9919@gmail.com>
2022-12-17 01:19:27 +0300

    Rename variables to make more sense

--- osu.Framework/Resources/Shaders/sh_CircularBlobUtils.h
+++ osu.Framework/Resources/Shaders/sh_CircularBlobUtils.h
@@ -10,7 +10,7 @@ highp float random(highp vec2 st)
 }
 
 // we could use exact box calculation, however this implementation is way faster but still good enough
-highp bool insideBezierBoundingBox(highp vec2 point, highp vec2 p0, highp vec2 p1, highp vec2 p2, highp float inflation)
+highp bool inBoundingBox(highp vec2 point, highp vec2 p0, highp vec2 p1, highp vec2 p2, highp float inflation)
 {
     highp vec2 topLeft = min(min(p0, p1), p2) - vec2(inflation);
     highp vec2 bottomRight = max(max(p0, p1), p2) + vec2(inflation);
@@ -25,7 +25,7 @@ highp float dot2(highp vec2 v)
     return dot(v, v);
 }
 
-highp float cro(highp vec2 a, highp vec2 b)
+highp float det(highp vec2 a, highp vec2 b)
 {
     return a.x * b.y - a.y * b.x;
 }
@@ -52,7 +52,7 @@ highp float dstToBezier(highp vec2 pos, highp vec2 A, highp vec2 B, highp vec2 C
         highp vec2 uv = sign(x) * pow(abs(x), vec2(1.0 / 3.0));
         highp float t = clamp(uv.x + uv.y - kx, 0.0, 1.0);
         highp vec2 q1 = d + (c + b * t) * t;
-        return sqrt(dot2(q1)) * sign(cro(c + 2.0 * b * t, q1));
+        return sqrt(dot2(q1)) * sign(det(c + 2.0 * b * t, q1));
     }
     
     // 3 roots
@@ -67,9 +67,9 @@ highp float dstToBezier(highp vec2 pos, highp vec2 A, highp vec2 B, highp vec2 C
     highp float dx = dot2(qx);
     highp float dy = dot2(qy);
     highp float dz = dot2(qz);
-    highp float sx = cro(c + 2.0 * b * t.x, qx);
-    highp float sy = cro(c + 2.0 * b * t.y, qy);
-    highp float sz = cro(c + 2.0 * b * t.z, qz);
+    highp float sx = det(c + 2.0 * b * t.x, qx);
+    highp float sy = det(c + 2.0 * b * t.y, qy);
+    highp float sz = det(c + 2.0 * b * t.z, qz);
 
     return sqrt(min(dx, dy)) * sign(dx < dy ? (dx < dz ? sx : sz) : sy);
 }
@@ -85,32 +85,32 @@ lowp float blobAlphaAt(highp vec2 pixelPos, mediump float pathRadius, highp floa
 {
     // distances to the closest curve
     highp float absDst = 10.0;
-    highp float dst = -10.0;
+    highp float signedDst = -10.0;
 
     mediump float vertexAngleOffset = TWO_PI / float(pointCount);
 
     highp vec2 lastVertex = getVertexPosByAngle(-vertexAngleOffset, noisePosition, amplitude, texelSize);
     highp vec2 currentVertex = getVertexPosByAngle(0.0, noisePosition, amplitude, texelSize);
-    highp vec2 midLeft = lerp(lastVertex, currentVertex, 0.5);
+    highp vec2 curveStart = lerp(lastVertex, currentVertex, 0.5);
 
     for (int i = 0; i < pointCount; i++)
     {
         highp vec2 nextVertex = getVertexPosByAngle(float(i + 1) * vertexAngleOffset, noisePosition, amplitude, texelSize);
-        highp vec2 midRight = lerp(currentVertex, nextVertex, 0.5);
+        highp vec2 curveEnd = lerp(currentVertex, nextVertex, 0.5);
 
-        if (insideBezierBoundingBox(pixelPos, midLeft, currentVertex, midRight, pathRadius))
+        if (inBoundingBox(pixelPos, curveStart, currentVertex, curveEnd, pathRadius))
         {
-            highp float dstToCurve = dstToBezier(pixelPos, midLeft, currentVertex, midRight);
+            highp float dstToCurve = dstToBezier(pixelPos, curveStart, currentVertex, curveEnd);
             highp float absDstToCurve = abs(dstToCurve);
 
             // save distances to current curve if it's the closest one
-            dst = mix(dst, dstToCurve, float(absDstToCurve < absDst));
+            signedDst = mix(signedDst, dstToCurve, float(absDstToCurve < absDst));
             absDst = min(absDst, absDstToCurve);
         }
         
         currentVertex = nextVertex;
-        midLeft = midRight;
+        curveStart = curveEnd;
     }
 
-    return smoothstep(texelSize, 0.0, absDst + float(dst < 0.0) * (texelSize - pathRadius));
+    return smoothstep(texelSize, 0.0, absDst + float(signedDst < 0.0) * (texelSize - pathRadius));
 }
