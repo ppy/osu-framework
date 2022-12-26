55bd7a66da3e9f2dff3409d18e3e871f5ffe8ed6
Dan Balasescu <smoogipoo@smgi.me>
2022-12-26 18:40:46 +0900

    Add precision qualifiers

--- osu.Framework/Resources/Shaders/sh_mipmap.fs
+++ osu.Framework/Resources/Shaders/sh_mipmap.fs
@@ -1,10 +1,9 @@
 #include "sh_Utils.h"
 
 varying highp vec2 uv;
-uniform highp sampler2D tex;
+uniform lowp sampler2D tex;
 
 void main()
 {
-    highp vec2 tex_coords = uv;
-    gl_FragColor = toSRGB(texture2D(tex, tex_coords, 0.0));
+    gl_FragColor = toSRGB(texture2D(tex, uv, 0.0));
 }
