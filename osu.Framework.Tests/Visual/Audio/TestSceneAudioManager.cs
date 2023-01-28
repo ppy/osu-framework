6c9d9dbbe08d78f5938c46121824e6a120baef48
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2023-01-28 20:41:15 +0100

    Add test coverage for correct extension lookups

--- osu.Framework.Tests/Visual/Audio/TestSceneAudioManager.cs
+++ osu.Framework.Tests/Visual/Audio/TestSceneAudioManager.cs
@@ -3,9 +3,12 @@
 
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using NUnit.Framework;
 using osu.Framework.Allocation;
 using osu.Framework.Audio;
+using osu.Framework.Audio.Sample;
 using osu.Framework.IO.Stores;
 using osu.Framework.Testing;
 
@@ -21,18 +24,37 @@ namespace osu.Framework.Tests.Visual.Audio
         public void TestSampleStoreCreationDoesNotMutateOriginalResourceStore()
         {
             TestResourceStore resourceStore = null!;
+            ISampleStore sampleStore = null!;
             List<string> filenames = null!;
 
             AddStep("create resource store", () => resourceStore = new TestResourceStore());
             AddStep("store lookups for sample file", () => filenames = resourceStore.GetFilenames("test").ToList());
 
-            AddStep("create sample store", () => audioManager.GetSampleStore(resourceStore));
+            AddStep("create sample store", () => sampleStore = audioManager.GetSampleStore(resourceStore));
             AddAssert("resource store lookups unchanged", () => resourceStore.GetFilenames("test"), () => Is.EquivalentTo(filenames));
+
+            AddStep("attempt to look up sample", () => sampleStore.Get("sample"));
+            AddAssert("extension lookups attempted", () => resourceStore.AttemptedLookups, () => Is.EquivalentTo(new[] { "sample", "sample.wav", "sample.mp3" }));
         }
 
         private class TestResourceStore : ResourceStore<byte[]>
         {
             public new IEnumerable<string> GetFilenames(string lookup) => base.GetFilenames(lookup);
+
+            private readonly List<string> attemptedLookups = new List<string>();
+            public IEnumerable<string> AttemptedLookups => attemptedLookups;
+
+            public override byte[] Get(string name)
+            {
+                attemptedLookups.Add(name);
+                return base.Get(name);
+            }
+
+            public override Task<byte[]> GetAsync(string name, CancellationToken cancellationToken = default)
+            {
+                attemptedLookups.Add(name);
+                return base.GetAsync(name, cancellationToken);
+            }
         }
     }
 }
