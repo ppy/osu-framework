97485bafdd1fdc14daa91f8a5f8e6d31f4adc010
Dan Balasescu <smoogipoo@smgi.me>
2023-03-10 18:05:25 +0900

    Improve test to cover more cases

--- osu.Framework.Tests/Dependencies/TestSceneAsyncBackgroundDependencyLoader.cs
+++ osu.Framework.Tests/Dependencies/TestSceneAsyncBackgroundDependencyLoader.cs
@@ -5,44 +5,76 @@ using System.Threading.Tasks;
 using NUnit.Framework;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
-using osu.Framework.Graphics.Containers;
-using osu.Framework.Graphics.Shapes;
 using osu.Framework.Testing;
 using osu.Framework.Tests.Visual;
-using osuTK;
 
 namespace osu.Framework.Tests.Dependencies
 {
     [HeadlessTest]
     public partial class TestSceneAsyncBackgroundDependencyLoader : FrameworkTestScene
     {
-        public TestSceneAsyncBackgroundDependencyLoader()
-        {
-            Add(new MutatingDrawable
-            {
-                Anchor = Anchor.Centre,
-                Origin = Anchor.Centre,
-                Size = new Vector2(100)
-            });
-        }
-
         [Test]
-        public void TestMutationAfterAsyncOperation()
+        public void TestLoadOnUpdateThread()
         {
             MutatingDrawable? drawable = null;
 
             AddStep("add drawable", () => Child = drawable = new MutatingDrawable());
-            AddUntilStep("wait for load", () => drawable!.IsLoaded);
+            AddUntilStep("wait to be ready", () => drawable!.IsReadyToVerify);
+            AddStep("verify", () => drawable!.Verify());
+        }
+
+        [Test]
+        public void TestLoadOnAsyncThread()
+        {
+            MutatingDrawable? drawable = null;
+
+            AddStep("add drawable", () => LoadComponentAsync(drawable = new MutatingDrawable(), Add));
+            AddUntilStep("wait to be ready", () => drawable!.IsReadyToVerify);
+            AddStep("verify", () => drawable!.Verify());
         }
 
 #pragma warning disable OFSG001
-        private partial class MutatingDrawable : CompositeDrawable
+        private partial class MutatingDrawable : Drawable
         {
+            private int asyncLoadStarted;
+            private int asyncTaskFinished;
+            private int asyncLoadFinished;
+            private int loadCompleteStarted;
+            private int counter;
+
             [BackgroundDependencyLoader]
             private async Task load()
             {
-                await Task.Delay(1000).ConfigureAwait(true);
-                InternalChild = new Box();
+                asyncLoadStarted = ++counter;
+
+                await Task.Run(async () =>
+                {
+                    await Task.Delay(1000).ConfigureAwait(true);
+                    asyncTaskFinished = ++counter;
+                }).ConfigureAwait(true);
+
+                EnsureMutationAllowed("Mutate");
+
+                asyncLoadFinished = ++counter;
+            }
+
+            protected override void LoadComplete()
+            {
+                base.LoadComplete();
+                loadCompleteStarted = ++counter;
+            }
+
+            public bool IsReadyToVerify => asyncLoadStarted > 0 && asyncTaskFinished > 0 && asyncLoadFinished > 0 && loadCompleteStarted > 0;
+
+            public void Verify()
+            {
+                Assert.Multiple(() =>
+                {
+                    Assert.That(asyncLoadStarted, Is.EqualTo(1));
+                    Assert.That(asyncTaskFinished, Is.EqualTo(2));
+                    Assert.That(asyncLoadFinished, Is.EqualTo(3));
+                    Assert.That(loadCompleteStarted, Is.EqualTo(4));
+                });
             }
         }
 #pragma warning restore OFSG001
