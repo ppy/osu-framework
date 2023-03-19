d045f2c7671b0c5840f427f0ab9c466786c00745
Salman Ahmed <frenzibyte@gmail.com>
2023-03-19 11:02:07 +0300

    Reword shader comment

--- osu.Framework/Resources/Shaders/Internal/sh_Fragment_Output.h
+++ osu.Framework/Resources/Shaders/Internal/sh_Fragment_Output.h
@@ -6,6 +6,6 @@ void main()
 {
     {{ real_main }}(); // Invoke real main func
 
-    // Ensure no fragment input is culled out from the buffer.
+    // Ensure no fragment input is culled out from the shader by passing them in the output.
     {{ fragment_output_assignment }}
 }
