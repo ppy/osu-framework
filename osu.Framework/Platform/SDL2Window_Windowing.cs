bf323716a8e2f16a15a5019c7e59129e76d9fc52
Dean Herbert <pe@ppy.sh>
2023-03-24 19:28:22 +0900

    Move desktop-only hacks out of base method

--- osu.Framework/Platform/SDL2Window_Windowing.cs
+++ osu.Framework/Platform/SDL2Window_Windowing.cs
@@ -577,13 +577,6 @@ namespace osu.Framework.Platform
         /// </summary>
         protected virtual void UpdateWindowStateAndSize(WindowState state, Display display, DisplayMode displayMode)
         {
-            // this reset is required even on changing from one fullscreen resolution to another.
-            // if it is not included, the GL context will not get the correct size.
-            // this is mentioned by multiple sources as an SDL issue, which seems to resolve by similar means (see https://discourse.libsdl.org/t/sdl-setwindowsize-does-not-work-in-fullscreen/20711/4).
-            SDL.SDL_SetWindowBordered(SDLWindowHandle, SDL.SDL_bool.SDL_TRUE);
-            SDL.SDL_SetWindowFullscreen(SDLWindowHandle, (uint)SDL.SDL_bool.SDL_FALSE);
-            SDL.SDL_RestoreWindow(SDLWindowHandle);
-
             switch (state)
             {
                 case WindowState.Normal:
