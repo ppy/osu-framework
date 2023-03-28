cb7eb834e2f59e10978b5e8178cd4e63cc527f68
Salman Ahmed <frenzibyte@gmail.com>
2023-03-28 21:45:43 +0300

    Disable GPU-level colour space conversion

--- osu.Framework/Graphics/OpenGL/GLRenderer.cs
+++ osu.Framework/Graphics/OpenGL/GLRenderer.cs
@@ -76,12 +76,6 @@ namespace osu.Framework.Graphics.OpenGL
             GL.Disable(EnableCap.StencilTest);
             GL.Enable(EnableCap.Blend);
 
-            // For whatever reason, changing this value breaks colour rendering inside BufferedContainers only on android.
-            // We're going to eventually have to figure out why this is the case, but for now this workaround fixes the issue very locally.
-            // See https://github.com/ppy/osu-framework/issues/5694.
-            if (RuntimeInfo.OS != RuntimeInfo.Platform.Android)
-                GL.Disable((EnableCap)36281); // GL_FRAMEBUFFER_SRGB
-
             Logger.Log($@"GL Initialized
                         GL Version:                 {GL.GetString(StringName.Version)}
                         GL Renderer:                {GL.GetString(StringName.Renderer)}
