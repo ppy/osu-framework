7beaf62b6f0435b31203f2217a1d8127bc29bf2a
Dean Herbert <pe@ppy.sh>
2023-05-06 19:47:01 +0900

    Move `ActiveCursor` initial populator to `ctor` to allow non-null spec

--- osu.Framework/Graphics/Cursor/CursorContainer.cs
+++ osu.Framework/Graphics/Cursor/CursorContainer.cs
@@ -15,7 +15,7 @@ namespace osu.Framework.Graphics.Cursor
 {
     public partial class CursorContainer : VisibilityContainer, IRequireHighFrequencyMousePosition
     {
-        public Drawable? ActiveCursor { get; protected set; }
+        public Drawable ActiveCursor { get; protected set; }
 
         private TouchLongPressFeedback longPressFeedback = null!;
 
@@ -27,12 +27,14 @@ namespace osu.Framework.Graphics.Cursor
             RelativeSizeAxes = Axes.Both;
 
             State.Value = Visibility.Visible;
+
+            ActiveCursor = CreateCursor();
         }
 
         [BackgroundDependencyLoader]
         private void load()
         {
-            Add(ActiveCursor = CreateCursor());
+            Add(ActiveCursor);
             Add(longPressFeedback = CreateLongPressFeedback());
         }
 
@@ -63,8 +65,7 @@ namespace osu.Framework.Graphics.Cursor
 
         protected override bool OnMouseMove(MouseMoveEvent e)
         {
-            if (ActiveCursor != null)
-                ActiveCursor.Position = e.MousePosition;
+            ActiveCursor.Position = e.MousePosition;
             return base.OnMouseMove(e);
         }
 
