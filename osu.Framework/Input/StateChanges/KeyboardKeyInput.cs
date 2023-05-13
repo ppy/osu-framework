28dd02fc3d07dca217658effecc4eef1615a367c
timiimit <timi.korda@gmail.com>
2023-05-13 15:20:48 +0200

    Make KeyboardKeyInput a standalone IInput

--- osu.Framework/Input/StateChanges/KeyboardKeyInput.cs
+++ osu.Framework/Input/StateChanges/KeyboardKeyInput.cs
@@ -4,28 +4,92 @@
 #nullable disable
 
 using System.Collections.Generic;
+using System.Collections.Immutable;
+using osu.Framework.Input.StateChanges.Events;
 using osu.Framework.Input.States;
 using osuTK.Input;
+using KeyboardState = osu.Framework.Input.States.KeyboardState;
 
 namespace osu.Framework.Input.StateChanges
 {
-    public class KeyboardKeyInput : ButtonInput<Key>
+    /// <summary>
+    /// An abstract base class of an <see cref="IInput"/> which denotes a list of button state changes (pressed or released).
+    /// </summary>
+    /// <typeparam name="Key">The type of button.</typeparam>
+    public class KeyboardKeyInput : IInput
     {
-        public KeyboardKeyInput(IEnumerable<ButtonInputEntry<Key>> entries)
-            : base(entries)
+        public ImmutableArray<KeyboardKeyInputEntry> Entries;
+
+        public KeyboardKeyInput(IEnumerable<KeyboardKeyInputEntry> entries)
         {
+            Entries = entries.ToImmutableArray();
         }
 
-        public KeyboardKeyInput(Key button, bool isPressed, bool isRepeated = false)
-            : base(button, isPressed, isRepeated)
+        /// <summary>
+        /// Creates a <see cref="KeyboardKeyInput{Key}"/> with a single <typeparamref name="Key"/> state.
+        /// </summary>
+        /// <param name="button">The <typeparamref name="Key"/> to add.</param>
+        /// <param name="isPressed">The state of <paramref name="button"/>.</param>
+        public KeyboardKeyInput(Key button, bool isPressed, bool isRepeated)
         {
+            Entries = ImmutableArray.Create(new KeyboardKeyInputEntry(button, isPressed, isRepeated));
         }
 
-        public KeyboardKeyInput(ButtonStates<Key> current, ButtonStates<Key> previous)
-            : base(current, previous)
+        /// <summary>
+        /// Creates a <see cref="KeyboardKeyInput{Key}"/> from the difference of two <see cref="ButtonStates{Key}"/>.
+        /// </summary>
+        /// <remarks>
+        /// Buttons that are pressed in <paramref name="previous"/> and not pressed in <paramref name="current"/> will be listed as <see cref="ButtonStateChangeKind.Released"/>.
+        /// Buttons that are not pressed in <paramref name="previous"/> and pressed in <paramref name="current"/> will be listed as <see cref="ButtonStateChangeKind.Pressed"/>.
+        /// </remarks>
+        /// <param name="current">The newer <see cref="ButtonStates{Key}"/>.</param>
+        /// <param name="previous">The older <see cref="ButtonStates{Key}"/>.</param>
+        public KeyboardKeyInput(KeyboardState current, KeyboardState previous)
         {
+            var difference = (current.Keys ?? new ButtonStates<Key>()).EnumerateDifference(previous.Keys ?? new ButtonStates<Key>());
+
+            var builder = ImmutableArray.CreateBuilder<KeyboardKeyInputEntry>(difference.Released.Length + difference.Pressed.Length);
+
+            foreach (var button in difference.Released)
+                builder.Add(new KeyboardKeyInputEntry(button, false, false));
+            foreach (var button in difference.Pressed)
+                builder.Add(new KeyboardKeyInputEntry(button, true, false));
+
+            Entries = builder.MoveToImmutable();
         }
 
-        protected override ButtonStates<Key> GetButtonStates(InputState state) => state.Keyboard.Keys;
+        /// <summary>
+        /// Retrieves the <see cref="Key"/> from an <see cref="InputState"/>.
+        /// </summary>
+        protected ButtonStates<Key> GetKeyStates(InputState state)
+        {
+            return state.Keyboard.Keys;
+        }
+
+        /// <summary>
+        /// Create a <typeparamref name="Key"/> state change event.
+        /// </summary>
+        /// <param name="state">The <see cref="InputState"/> which changed.</param>
+        /// <param name="button">The <typeparamref name="Key"/> that changed.</param>
+        /// <param name="kind">The type of change that occurred on <paramref name="button"/>.</param>
+        /// <param name="isRepeated">Whether this event is being repeated.</param>
+        protected virtual KeyboardKeyStateChangeEvent CreateEvent(InputState state, Key button, ButtonStateChangeKind kind, bool isRepeated) => new KeyboardKeyStateChangeEvent(state, this, button, kind, isRepeated);
+
+        public virtual void Apply(InputState state, IInputStateChangeHandler handler)
+        {
+            if (Entries.Length == 0)
+                return;
+
+            var keyStates = GetKeyStates(state);
+
+            foreach (var entry in Entries)
+            {
+                if (keyStates.SetPressed(entry.Key, entry.IsPressed))
+                {
+                    var buttonStateChange = CreateEvent(state, entry.Key, entry.IsPressed ? ButtonStateChangeKind.Pressed : ButtonStateChangeKind.Released, entry.IsRepeated);
+                    handler.HandleInputStateChange(buttonStateChange);
+                }
+            }
+        }
     }
 }
