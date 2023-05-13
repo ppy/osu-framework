c8b6317a30b4f0abfd00f9d02f81158e1077c53e
timiimit <timi.korda@gmail.com>
2023-05-13 14:05:01 +0200

    Create KeyboardKeyStateChangeEvent

---
+++ osu.Framework/Input/StateChanges/Events/KeyboardKeyStateChangeEvent.cs
@@ -0,0 +1,24 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+#nullable disable
+
+using osu.Framework.Input.States;
+using osuTK.Input;
+
+namespace osu.Framework.Input.StateChanges.Events
+{
+    public class KeyboardKeyStateChangeEvent : ButtonStateChangeEvent<Key>
+    {
+        /// <summary>
+        /// Whether this event was sent in the past and is now being repeated.
+        /// </summary>
+        public readonly bool IsRepeated;
+
+        public KeyboardKeyStateChangeEvent(InputState state, IInput input, Key key, ButtonStateChangeKind kind, bool isRepeated)
+            : base(state, input, key, kind)
+        {
+            IsRepeated = isRepeated;
+        }
+    }
+}
