bc372500b922991b314794ef5f4572ddfbccdb1d
Dan Balasescu <smoogipoo@smgi.me>
2023-06-15 16:56:59 +0900

    Add some xmldocs

--- osu.Framework/Input/ISourceGeneratedHandleInputCache.cs
+++ osu.Framework/Input/ISourceGeneratedHandleInputCache.cs
@@ -7,8 +7,20 @@ namespace osu.Framework.Input
 {
     public interface ISourceGeneratedHandleInputCache
     {
+        /// <summary>
+        /// The most-derived type that implements this interface.
+        /// This is used to handle non-partial leaf types that can't rely on the source-generated values and must fall back to reflection.
+        /// </summary>
         protected internal Type KnownType => typeof(object);
+
+        /// <summary>
+        /// Whether this type or any of its base types request positional input.
+        /// </summary>
         protected internal bool RequestsPositionalInput => false;
+
+        /// <summary>
+        /// Whether this type or any of its base types request non-positional input.
+        /// </summary>
         protected internal bool RequestsNonPositionalInput => false;
     }
 }
