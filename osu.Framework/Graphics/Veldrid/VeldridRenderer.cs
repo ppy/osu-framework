934e226f1817a7f86134b1b00569c15d3f879e1e
Salman Ahmed <frenzibyte@gmail.com>
2023-06-23 16:03:37 +0300

    Remove unused using directive

--- osu.Framework/Graphics/Veldrid/VeldridRenderer.cs
+++ osu.Framework/Graphics/Veldrid/VeldridRenderer.cs
@@ -28,7 +28,6 @@ using Veldrid;
 using Veldrid.OpenGL;
 using Veldrid.OpenGLBinding;
 using Image = SixLabors.ImageSharp.Image;
-using PixelFormat = Veldrid.PixelFormat;
 using PrimitiveTopology = Veldrid.PrimitiveTopology;
 
 namespace osu.Framework.Graphics.Veldrid
