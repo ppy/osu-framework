488da2e64dc517aa836c283972a49554033d38a9
Salman Ahmed <frenzibyte@gmail.com>
2023-07-07 04:40:25 +0300

    Allow hiding performance test scenes content

---
+++ osu.Framework.Tests/Visual/Performance/PerformanceTestScene.cs
@@ -0,0 +1,16 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+namespace osu.Framework.Tests.Visual.Performance
+{
+    public partial class PerformanceTestScene : FrameworkTestScene
+    {
+        protected override void LoadComplete()
+        {
+            base.LoadComplete();
+
+            AddLabel("General");
+            AddToggleStep("hide content", v => Content.Alpha = v ? 0 : 1);
+        }
+    }
+}
