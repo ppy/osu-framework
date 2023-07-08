41646ec71a69043be0ba5265940c9f6267967bff
Salman Ahmed <frenzibyte@gmail.com>
2023-07-08 03:22:21 +0300

    Make render scaling local to test scene content

--- osu.Framework.Tests/Visual/Performance/PerformanceTestScene.cs
+++ osu.Framework.Tests/Visual/Performance/PerformanceTestScene.cs
@@ -4,11 +4,18 @@
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
 using osu.Framework.Configuration;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osuTK;
 
 namespace osu.Framework.Tests.Visual.Performance
 {
     public abstract partial class PerformanceTestScene : FrameworkTestScene
     {
+        protected override Container<Drawable> Content => content;
+
+        private BufferedContainer content = null!;
+
         [Resolved]
         private FrameworkDebugConfigManager debugConfig { get; set; } = null!;
 
@@ -20,9 +27,15 @@ namespace osu.Framework.Tests.Visual.Performance
 
             bypassFrontToBack = debugConfig.GetBindable<bool>(DebugSetting.BypassFrontToBackPass);
 
+            base.Content.Child = content = new BufferedContainer
+            {
+                RelativeSizeAxes = Axes.Both,
+            };
+
             AddLabel("General");
             AddToggleStep("hide content", v => Content.Alpha = v ? 0 : 1);
             AddToggleStep("bypass front to back", v => bypassFrontToBack.Value = v);
+            AddSliderStep("render scale", 0f, 1f, 1f, v => content.FrameBufferScale = new Vector2(v));
         }
     }
 }
