5616fc8c3cbdf5a28235e98522687467c5a91e0a
Susko3 <Susko3@protonmail.com>
2023-07-10 20:38:30 +0200

    Update SDL2 clipboard to return `null` when there's no text
    Tested on windows by changing the host to use the SDL clipboard.

--- osu.Framework/Platform/SDL2/SDL2Clipboard.cs
+++ osu.Framework/Platform/SDL2/SDL2Clipboard.cs
@@ -8,7 +8,10 @@ namespace osu.Framework.Platform.SDL2
 {
     public class SDL2Clipboard : Clipboard
     {
-        public override string? GetText() => SDL.SDL_GetClipboardText();
+        // SDL cannot differentiate between string.Empty and no text (eg. empty clipboard or an image)
+        // doesn't matter as text editors don't really allow copying empty strings.
+        // assume that empty text means no text.
+        public override string? GetText() => SDL.SDL_HasClipboardText() == SDL.SDL_bool.SDL_TRUE ? SDL.SDL_GetClipboardText() : null;
 
         public override void SetText(string text) => SDL.SDL_SetClipboardText(text);
 
