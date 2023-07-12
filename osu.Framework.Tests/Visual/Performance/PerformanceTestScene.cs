c86b12b7fd5884e8a019027c01de0ca2e857e125
Dean Herbert <pe@ppy.sh>
2023-07-12 15:51:02 +0900

    Move colour cycling logic to base class

--- osu.Framework.Tests/Visual/Performance/PerformanceTestScene.cs
+++ osu.Framework.Tests/Visual/Performance/PerformanceTestScene.cs
@@ -1,12 +1,15 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
 using osu.Framework.Configuration;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
+using osu.Framework.Utils;
 using osuTK;
+using osuTK.Graphics;
 
 namespace osu.Framework.Tests.Visual.Performance
 {
@@ -17,6 +20,7 @@ namespace osu.Framework.Tests.Visual.Performance
         private Container content = null!;
 
         private bool rotation;
+        private bool cycleColour;
 
         [Resolved]
         private FrameworkDebugConfigManager debugConfig { get; set; } = null!;
@@ -50,6 +54,7 @@ namespace osu.Framework.Tests.Visual.Performance
             AddToggleStep("enable front to back", v => bypassFrontToBack.Value = !v);
             AddSliderStep("render scale", 0.01f, 1f, 1f, v => buffer.FrameBufferScale = new Vector2(v));
             AddToggleStep("rotate everything", v => rotation = v);
+            AddToggleStep("cycle colour", v => cycleColour = v);
         }
 
         protected override void Update()
@@ -65,6 +70,17 @@ namespace osu.Framework.Tests.Visual.Performance
             {
                 content.Scale = Vector2.One;
                 content.Rotation = 0;
+            }
+
+            if (cycleColour)
+            {
+                var col = Interpolation.ValueAt((MathF.Sin((float)Time.Current / 1000) + 1) / 2, Color4.Red, Color4.SkyBlue, 0f, 1f);
+
+                content.Colour = col;
+            }
+            else
+            {
+                content.Colour = Color4.White;
             }
         }
     }
--- osu.Framework.Tests/Visual/Performance/TestSceneVertexUploadPerformance.cs
+++ osu.Framework.Tests/Visual/Performance/TestSceneVertexUploadPerformance.cs
@@ -1,31 +1,23 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System;
+using System.Linq;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.UserInterface;
 using osu.Framework.Utils;
-using osuTK;
-using osuTK.Graphics;
 
 namespace osu.Framework.Tests.Visual.Performance
 {
     public partial class TestSceneVertexUploadPerformance : TestSceneBoxPerformance
     {
-        protected override Drawable CreateBox() =>
-            new CircularProgress
-            {
-                Size = new Vector2(10),
-                Current = { Value = 1 }
-            };
+        protected override Drawable CreateBox() => new CircularProgress();
 
         protected override void Update()
         {
             base.Update();
 
-            var col = Interpolation.ValueAt((MathF.Sin((float)Time.Current / 1000) + 1) / 2, Color4.Red, Color4.SkyBlue, 0f, 1f);
-
-            Flow.Colour = col;
+            foreach (var p in Flow.OfType<CircularProgress>())
+                p.Current.Value = (p.Current.Value + (Time.Elapsed * RNG.NextSingle()) / 1000) % 1;
         }
     }
 }
