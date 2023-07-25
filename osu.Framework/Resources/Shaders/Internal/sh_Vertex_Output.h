368ba59897b3c1abd5eb0da4776a4bb0c0b95a16
Dan Balasescu <smoogipoo@smgi.me>
2023-07-25 18:57:51 +0900

    Remove leftover comment

--- osu.Framework/Resources/Shaders/Internal/sh_Vertex_Output.h
+++ osu.Framework/Resources/Shaders/Internal/sh_Vertex_Output.h
@@ -3,9 +3,6 @@
 #ifndef INTERNAL_VERTEX_OUTPUT_H
 #define INTERNAL_VERTEX_OUTPUT_H
 
-// The -1 is a placeholder value to offset all vertex input members
-// of the actual vertex shader during inclusion of this header.
-
 void main()
 {
     {{ real_main }}(); // Invoke real main func
