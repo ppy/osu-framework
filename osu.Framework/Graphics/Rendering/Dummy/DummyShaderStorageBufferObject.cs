3b1bf4b0deaf9db661858a5f6e80a6bfb7c7d2b2
Dan Balasescu <smoogipoo@smgi.me>
2023-07-27 17:06:24 +0900

    Add unit test

--- osu.Framework/Graphics/Rendering/Dummy/DummyShaderStorageBufferObject.cs
+++ osu.Framework/Graphics/Rendering/Dummy/DummyShaderStorageBufferObject.cs
@@ -8,17 +8,20 @@ namespace osu.Framework.Graphics.Rendering.Dummy
     public class DummyShaderStorageBufferObject<T> : IShaderStorageBufferObject<T>
         where T : unmanaged, IEquatable<T>
     {
+        private readonly T[] data;
+
         public DummyShaderStorageBufferObject(int size)
         {
             Size = size;
+            data = new T[size];
         }
 
         public int Size { get; }
 
         public T this[int index]
         {
-            get => default;
-            set { }
+            get => data[index];
+            set => data[index] = value;
         }
 
         public void Dispose()
