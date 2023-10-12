4a4ed7df28d0d27d1b8260a2adf62c9a199e4225
FreezyLemon <contact@freezylemon.com>
2023-10-12 14:43:17 +0200

    FFmpeg: Enable MPEG-4 parser and decoder

--- osu.Framework.NativeLibs/scripts/ffmpeg/common.sh
+++ osu.Framework.NativeLibs/scripts/ffmpeg/common.sh
@@ -18,8 +18,8 @@ FFMPEG_FLAGS=(
 
     # File and video formats
     --enable-demuxer='mov,matroska,flv,avi' # mov = mp4, matroska = mkv & webm
-    --enable-parser='h264,hevc,vp8,vp9'
-    --enable-decoder='h264,hevc,vp8,vp9'
+    --enable-parser='mpeg4video,h264,hevc,vp8,vp9'
+    --enable-decoder='mpeg4,h264,hevc,vp8,vp9'
     --enable-protocol=pipe
 )
 
