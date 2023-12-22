41ad291581d72ce77c1671de9748f80e0ee41c73
Lena <lena@nihil.gay>
2023-12-22 13:57:01 +0100

    create notification tray icon abstraction

---
+++ osu.Framework/Platform/NotificationTrayIcon.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace osu.Framework.Platform
+{
+    /// <summary>
+    /// Represents an icon located in the OS notification tray.
+    /// </summary>
+    public abstract class NotificationTrayIcon : IDisposable
+    {
+        /// <summary>
+        /// The hint text shown when hovering over the icon with the cursor
+        /// </summary>
+        public string Text { get; init; }
+
+        /// <summary>
+        /// The action to perform when the icon gets clicked
+        /// </summary>
+        public Action? OnClick { get; init; }
+
+        public static NotificationTrayIcon Create(string text, Action? onClick, IWindow window)
+        {
+            throw new PlatformNotSupportedException();
+        }
+
+        public abstract void Dispose();
+    }
+}
