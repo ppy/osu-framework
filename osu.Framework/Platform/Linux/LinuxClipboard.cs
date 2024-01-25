b8a02f0c4fda8f32b1e2151f45a9d9644c10054a
Speykious <speykious@gmail.com>
2024-01-26 00:38:34 +0100

    Implement a Linux clipboard using `xclip`
    (thus only works on X11)

---
+++ osu.Framework/Platform/Linux/LinuxClipboard.cs
@@ -0,0 +1,132 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using NuGet.Packaging;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Png;
+
+namespace osu.Framework.Platform.Linux
+{
+    public class LinuxClipboard : Clipboard
+    {
+        protected Process Xclip(IEnumerable<string> args)
+        {
+            Process xclip = new Process();
+            xclip.StartInfo.FileName = "xclip";
+            xclip.StartInfo.UseShellExecute = false;
+            xclip.StartInfo.RedirectStandardInput = true;
+            xclip.StartInfo.RedirectStandardError = true;
+            xclip.StartInfo.RedirectStandardOutput = true;
+            xclip.StartInfo.ArgumentList.AddRange(args);
+            return xclip;
+        }
+
+        protected bool HasTarget(string target)
+        {
+            using Process xclip = Xclip(new[] { "-selection", "clipboard", "-t", "TARGETS", "-o" });
+            xclip.Start();
+
+            bool hasTarget = false;
+
+            try
+            {
+                string? line;
+                while ((line = xclip.StandardOutput.ReadLine()) != null)
+                {
+                    if (line == target)
+                    {
+                        hasTarget = true;
+                        break;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+            }
+
+            xclip.StandardInput.Close();
+            xclip.WaitForExit();
+
+            return hasTarget;
+        }
+
+        public override Image<TPixel>? GetImage<TPixel>()
+        {
+            if (!HasTarget("image/png"))
+                return null;
+
+            using Process xclip = Xclip(new[] { "-selection", "clipboard", "-t", "image/png", "-o" });
+            xclip.Start();
+
+            // reserve 5 MB because it's a bit above a typical png size
+            using MemoryStream xclipStdout = new MemoryStream(5 * 1024 * 1024);
+
+            // If we don't read stdout before waiting, the process might hang because some internal buffer got full.
+            // I know, weird...
+            xclip.StandardOutput.BaseStream.CopyTo(xclipStdout);
+            xclipStdout.Position = 0;
+
+            xclip.StandardInput.Close();
+            xclip.WaitForExit();
+
+            if (xclip.ExitCode == 0)
+                return Image.Load<TPixel>(xclipStdout);
+            else
+                return null;
+        }
+
+        public override string? GetText()
+        {
+            if (!HasTarget("text/plain"))
+                return null;
+
+            using Process xclip = Xclip(new[] { "-selection", "clipboard", "-t", "text/plain", "-o" });
+            xclip.Start();
+
+            string? stdout = null;
+            try
+            {
+                stdout = xclip.StandardOutput.ReadToEnd();
+            }
+            catch (Exception)
+            {
+            }
+
+            xclip.StandardInput.Close();
+            xclip.WaitForExit();
+
+            if (xclip.ExitCode == 0)
+                return stdout;
+            else
+                return null;
+        }
+
+        public override bool SetImage(Image image)
+        {
+            using Process xclip = Xclip(new[] { "-selection", "clipboard", "-t", "image/png", "-i" });
+            xclip.Start();
+
+            image.Save(xclip.StandardInput.BaseStream, PngFormat.Instance);
+
+            xclip.StandardInput.Close();
+            xclip.WaitForExit();
+
+            return xclip.ExitCode == 0;
+        }
+
+        public override void SetText(string text)
+        {
+            using Process xclip = Xclip(new[] { "-selection", "clipboard", "-t", "text/plain", "-i" });
+            xclip.Start();
+
+            xclip.StandardInput.Write(text);
+
+            xclip.StandardInput.Close();
+            xclip.WaitForExit();
+        }
+    }
+}
