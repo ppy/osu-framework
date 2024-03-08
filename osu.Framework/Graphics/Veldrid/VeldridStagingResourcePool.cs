d79104def2efe9aab8d767cf24b4e6ae718e36a8
Dan Balasescu <smoogipoo@smgi.me>
2024-03-08 23:15:18 +0900

    Track staging resources per-execution

--- osu.Framework/Graphics/Veldrid/VeldridStagingResourcePool.cs
+++ osu.Framework/Graphics/Veldrid/VeldridStagingResourcePool.cs
@@ -65,34 +65,15 @@ namespace osu.Framework.Graphics.Veldrid
             updateStats();
         }
 
+        /// <summary>
+        /// Updates the current execution index and frees any resources that have not been used for
+        /// <see cref="Rendering.Renderer.RESOURCE_FREE_NO_USAGE_LENGTH"/> executions.
+        /// </summary>
+        /// <param name="executionIndex">The current execution index.</param>
         private void executionStarted(ulong executionIndex)
         {
             currentExecutionIndex = executionIndex;
-        }
 
-        /// <summary>
-        /// Updates the state of the resources in this pool in two steps:
-        /// <list type="bullet">
-        /// <item>Returns all textures that the GPU has finished using back to the pool.</item>
-        /// <item>Frees any textures that have not been used for <see cref="Rendering.Renderer.RESOURCE_FREE_NO_USAGE_LENGTH"/> frames.</item>
-        /// </list>
-        /// </summary>
-        private void executionFinished(ulong executionIndex)
-        {
-            // return any resource that the GPU has finished using in the last frame.
-            for (int i = 0; i < used.Count; i++)
-            {
-                var item = used[i];
-
-                // Usages are sequential so we can stop checking after the first non-completed usage.
-                if (item.FrameUsageIndex > executionIndex)
-                    break;
-
-                available.Add(item);
-                used.RemoveAt(i--);
-            }
-
-            // free any resource that hasn't been used for a while.
             for (int i = 0; i < available.Count; i++)
             {
                 var item = available[i];
@@ -105,6 +86,30 @@ namespace osu.Framework.Graphics.Veldrid
                     available.Remove(item);
                     break;
                 }
+            }
+
+            updateStats();
+        }
+
+        /// <summary>
+        /// Returns all resources that the GPU has finished using back to the pool.
+        /// </summary>
+        /// <param name="executionIndex">The finished execution index.</param>
+        private void executionFinished(ulong executionIndex)
+        {
+            for (int i = 0; i < used.Count; i++)
+            {
+                var item = used[i];
+
+                // Usages are sequential so we can stop checking after the usage exceeding the index.
+                if (item.FrameUsageIndex > executionIndex)
+                    break;
+
+                if (item.FrameUsageIndex != executionIndex)
+                    continue;
+
+                available.Add(item);
+                used.RemoveAt(i--);
             }
 
             updateStats();
