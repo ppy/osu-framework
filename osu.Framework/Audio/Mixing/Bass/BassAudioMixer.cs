242941409e9e93f0690f5563a2fbd98041d4af27
Dan Balasescu <smoogipoo@smgi.me>
2024-03-13 22:36:42 +0900

    Remove effect prioritisation to fix segfaults on Linux

--- osu.Framework/Audio/Mixing/Bass/BassAudioMixer.cs
+++ osu.Framework/Audio/Mixing/Bass/BassAudioMixer.cs
@@ -10,7 +10,6 @@ using System.Runtime.InteropServices;
 using ManagedBass;
 using ManagedBass.Mix;
 using osu.Framework.Bindables;
-using osu.Framework.Development;
 using osu.Framework.Extensions.EnumExtensions;
 using osu.Framework.Extensions.ObjectExtensions;
 using osu.Framework.Statistics;
@@ -421,13 +420,7 @@ namespace osu.Framework.Audio.Mixing.Bass
                     // Effects with greatest priority are stored at the front of the list.
                     effect.Priority = -i;
 
-                    if (effect.Handle != 0)
-                    {
-                        // Todo: Temporary bypass to attempt to fix failing test runs.
-                        if (!DebugUtils.IsNUnitRunning)
-                            ManagedBass.Bass.FXSetPriority(effect.Handle, effect.Priority);
-                    }
-                    else
+                    if (effect.Handle == 0)
                         effect.Handle = ManagedBass.Bass.ChannelSetFX(Handle, effect.Effect.FXType, effect.Priority);
 
                     ManagedBass.Bass.FXSetParameters(effect.Handle, effect.Effect);
