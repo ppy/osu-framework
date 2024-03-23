95116ea871464e421248c030308dff4fd280e3a1
Marvin Schürz <minetoblend@gmail.com>
2024-03-23 23:40:56 +0100

    Add custom format support to `HeadlessClipboard`

--- osu.Framework/Platform/HeadlessClipboard.cs
+++ osu.Framework/Platform/HeadlessClipboard.cs
@@ -1,6 +1,7 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Collections.Generic;
 using SixLabors.ImageSharp;
 
 namespace osu.Framework.Platform
@@ -15,21 +16,41 @@ namespace osu.Framework.Platform
     {
         private string? clipboardText;
         private Image? clipboardImage;
+        private readonly Dictionary<string, string> customValues = new Dictionary<string, string>();
 
         public override string? GetText() => clipboardText;
 
-        public override void SetText(string text)
-        {
-            clipboardImage = null;
-            clipboardText = text;
-        }
-
         public override Image<TPixel>? GetImage<TPixel>() => clipboardImage?.CloneAs<TPixel>();
 
-        public override bool SetImage(Image image)
+        public override string? GetCustom(string format)
+        {
+            return customValues[format];
+        }
+
+        public override bool SetData(params ClipboardEntry[] entries)
         {
             clipboardText = null;
-            clipboardImage = image;
+            clipboardImage = null;
+            customValues.Clear();
+
+            foreach (var entry in entries)
+            {
+                switch (entry)
+                {
+                    case ClipboardTextEntry textEntry:
+                        clipboardText = textEntry.Value;
+                        break;
+
+                    case ClipboardImageEntry imageEntry:
+                        clipboardImage = imageEntry.Value;
+                        break;
+
+                    case ClipboardCustomEntry customEntry:
+                        customValues[customEntry.Format] = customEntry.Value;
+                        break;
+                }
+            }
+
             return true;
         }
     }
