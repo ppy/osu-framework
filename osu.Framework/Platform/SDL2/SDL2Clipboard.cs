16b00d90cf307d205a9c7bff9cf196d38b3660c7
Marvin Schürz <minetoblend@gmail.com>
2024-03-23 23:45:30 +0100

    Add in-memory fallback for custom clipboard format to `SDL2Clipboard`

--- osu.Framework/Platform/SDL2/SDL2Clipboard.cs
+++ osu.Framework/Platform/SDL2/SDL2Clipboard.cs
@@ -1,6 +1,8 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Collections.Generic;
+using System.Linq;
 using SDL2;
 using SixLabors.ImageSharp;
 
@@ -8,21 +10,47 @@ namespace osu.Framework.Platform.SDL2
 {
     public class SDL2Clipboard : Clipboard
     {
+        private readonly Dictionary<string, string> customFormatValues = new Dictionary<string, string>();
+
         // SDL cannot differentiate between string.Empty and no text (eg. empty clipboard or an image)
         // doesn't matter as text editors don't really allow copying empty strings.
         // assume that empty text means no text.
         public override string? GetText() => SDL.SDL_HasClipboardText() == SDL.SDL_bool.SDL_TRUE ? SDL.SDL_GetClipboardText() : null;
-
-        public override void SetText(string text) => SDL.SDL_SetClipboardText(text);
 
         public override Image<TPixel>? GetImage<TPixel>()
         {
             return null;
         }
 
-        public override bool SetImage(Image image)
+        public override string? GetCustom(string format)
         {
-            return false;
+            return customFormatValues[format];
+        }
+
+        public override bool SetData(params ClipboardEntry[] entries)
+        {
+            customFormatValues.Clear();
+
+            if (entries.Any(e => e is ClipboardImageEntry))
+            {
+                return false;
+            }
+
+            foreach (var entry in entries)
+            {
+                switch (entry)
+                {
+                    case ClipboardTextEntry textEntry:
+                        SDL.SDL_SetClipboardText(textEntry.Value);
+                        break;
+
+                    case ClipboardCustomEntry customEntry:
+                        customFormatValues[customEntry.Format] = customEntry.Value;
+                        break;
+                }
+            }
+
+            return true;
         }
     }
 }
