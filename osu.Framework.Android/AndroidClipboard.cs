99179b3dd14adfc4be53574dba317d9a740d6e80
Marvin Schürz <minetoblend@gmail.com>
2024-03-24 00:30:02 +0100

    Add in-memory fallback for custom clipboard format to `AndroidClipboard`

--- osu.Framework.Android/AndroidClipboard.cs
+++ osu.Framework.Android/AndroidClipboard.cs
@@ -1,6 +1,7 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Collections.Generic;
 using Android.Content;
 using osu.Framework.Platform;
 using SixLabors.ImageSharp;
@@ -10,6 +11,7 @@ namespace osu.Framework.Android
     public class AndroidClipboard : Clipboard
     {
         private readonly ClipboardManager? clipboardManager;
+        private readonly Dictionary<string, string> customFormatValues = new Dictionary<string, string>();
 
         public AndroidClipboard(AndroidGameView view)
         {
@@ -18,14 +20,45 @@ namespace osu.Framework.Android
 
         public override string? GetText() => clipboardManager?.PrimaryClip?.GetItemAt(0)?.Text;
 
-        public override void SetText(string text)
-        {
-            if (clipboardManager != null)
-                clipboardManager.PrimaryClip = ClipData.NewPlainText(null, text);
-        }
-
         public override Image<TPixel>? GetImage<TPixel>() => null;
 
-        public override bool SetImage(Image image) => false;
+        public override bool SetData(params ClipboardEntry[] entries)
+        {
+            if (clipboardManager == null)
+                return false;
+
+            customFormatValues.Clear();
+            clipboardManager.PrimaryClip = null;
+
+            if (entries.Length == 0)
+                return false;
+
+            bool success = true;
+
+            foreach (var entry in entries)
+            {
+                switch (entry)
+                {
+                    case ClipboardTextEntry textEntry:
+                        clipboardManager.PrimaryClip = ClipData.NewPlainText(null, textEntry.Value);
+                        break;
+
+                    case ClipboardCustomEntry customEntry:
+                        customFormatValues[customEntry.Format] = customFormatValues[customEntry.Value];
+                        break;
+
+                    default:
+                        success = false;
+                        break;
+                }
+            }
+
+            return success;
+        }
+
+        public override string? GetCustom(string format)
+        {
+            return customFormatValues[format];
+        }
     }
 }
