d19ceacfda77ff7170d8f70a1e56c83da48fb9dd
Dean Herbert <pe@ppy.sh>
2024-03-29 21:32:22 +0800

    Use nicer japanese for test

--- osu.Framework.Tests/Visual/Containers/TestSceneTextFlowContainer.cs
+++ osu.Framework.Tests/Visual/Containers/TestSceneTextFlowContainer.cs
@@ -126,7 +126,7 @@ namespace osu.Framework.Tests.Visual.Containers
         {
             AddStep("set latin text", () => textContainer.Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer mattis eu turpis vitae posuere. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam mauris nibh, faucibus maximus ornare eu, ultrices ut ipsum. Proin rhoncus, nunc et faucibus pretium, nisl nunc dapibus massa, et scelerisque nibh ligula id odio. Praesent dapibus ex sed nunc egestas, in placerat risus mattis. Nulla sed ligula velit. Vestibulum auctor porta eros et condimentum. Etiam laoreet nunc nec lacinia pulvinar. Mauris hendrerit, mi at aliquet condimentum, ex ex cursus dolor, non porta erat eros id justo. Cras malesuada tincidunt nunc, at tincidunt risus eleifend id. Maecenas hendrerit venenatis mi et lobortis. Etiam sem tortor, elementum eget lacus non, porta tristique quam. Morbi sed lacinia odio. Phasellus ut pretium nunc. Fusce vitae mollis magna, vel scelerisque dui. ");
             AddStep("set url", () => textContainer.Text = "https://osu.ppy.sh/home/news/2024-03-27-osutaiko-world-cup-2024-round-of-32-recap");
-            AddStep("set cjk text", () => textContainer.Text = "語自うし志景ル産倍びょ頭携エタシ声児スせざ最3連居ほあ曽望をよ津葉ゅ首新る知偏啓ぞにへ。稿ネ奈座最クつ肉表ス幸更へ書整鈴話ア済検ヌナヲオ観体ヨワノホ導6凶ケツヌ食折テヤ国地ね右米東ろとふ方悩岳犠眠担よみ。5高おゆ括待チオ見1野ハワホシ作政すぞちも素著タ本球ヤサユ勝覚っおはう憂夫ふに離論資キ補彦リ浮68並茨りご。");
+            AddStep("set cjk text", () => textContainer.Text = "日本の桜は世界中から観光客を引きつけています。寿司は美味しい伝統的な日本食です。東京タワーは景色が美しいです。速い新幹線は、便利な交通手段です。富士山は、その美しさと完全な形状で知られています。日本文化は、優雅さと繊細さを象徴しています。抹茶は特別な日本の茶です。着物は、伝統的な日本の衣装で、特別な場面でよく着用されます。");
         }
 
         private void assertSpriteTextCount(int count)
