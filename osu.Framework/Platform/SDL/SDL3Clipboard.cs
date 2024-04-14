61183171be5fa460982ccdf6d22f5cc2d9d835b6
Speykious <speykious@gmail.com>
2024-04-14 13:50:27 +0200

    `EntryPoint` specification is not needed actually

--- osu.Framework/Platform/SDL/SDL3Clipboard.cs
+++ osu.Framework/Platform/SDL/SDL3Clipboard.cs
@@ -78,7 +78,7 @@ namespace osu.Framework.Platform.SDL
             }
         }
 
-        [UnmanagedCallersOnly(EntryPoint = "clipboardDataCallback", CallConvs = [typeof(CallConvCdecl)])]
+        [UnmanagedCallersOnly(CallConvs = [typeof(CallConvCdecl)])]
         private static unsafe nint clipboardDataCallback(nint userdata, byte* mimeType, nuint* length)
         {
             ClipboardImageData* pngData = (ClipboardImageData*)userdata;
@@ -95,7 +95,7 @@ namespace osu.Framework.Platform.SDL
             return 0;
         }
 
-        [UnmanagedCallersOnly(EntryPoint = "clipboardCleanupCallback", CallConvs = [typeof(CallConvCdecl)])]
+        [UnmanagedCallersOnly(CallConvs = [typeof(CallConvCdecl)])]
         private static unsafe void clipboardCleanupCallback(nint userdata)
         {
             ClipboardImageData* pngData = (ClipboardImageData*)userdata;
