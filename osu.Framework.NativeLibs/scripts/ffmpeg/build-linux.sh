5b9ae47eb72bb746df4644cc9c681c57235d8709
Kevin Pfeifer <kevin.pfeifer8@gmail.com>
2024-07-06 02:52:01 +0000

    move arch to the top

--- osu.Framework.NativeLibs/scripts/ffmpeg/build-linux.sh
+++ osu.Framework.NativeLibs/scripts/ffmpeg/build-linux.sh
@@ -6,36 +6,31 @@ SCRIPT_PATH=$(pwd)
 popd > /dev/null
 source "$SCRIPT_PATH/common.sh"
 
+if [ "$(dpkg --print-architecture)" = "amd64" ]; then
+    arch="x64"
+elif [ "$(dpkg --print-architecture)" = "i386" ]; then
+    arch="x86"
+elif [ "$(dpkg --print-architecture)" = "arm64" ]; then
+    arch="arm64"
+elif [ "$(dpkg --print-architecture)" = "armhf" ]; then
+    arch="arm"
+else
+    echo "Unsupported architecture: $(dpkg --print-architecture)"
+    exit 1
+fi
+
 FFMPEG_FLAGS+=(
     --target-os=linux
 )
 
 pushd . > /dev/null
-
-if [ $(uname -m) == "x86_64" ]; then
-    ARCH="x64"
-elif [ $(uname -m) == "i686" ]; then
-    ARCH="x86"
-elif [ $(uname -m) == "aarch64" ]; then
-    ARCH="arm64"
-elif [ $(uname -m) == "armv7l" ]; then
-    ARCH="arm"
-else
-    echo "Unsupported architecture: $(uname -m)"
-    exit 1
-fi
-
-prep_ffmpeg linux-$ARCH
-# Apply patch from upstream to fix errors with new binutils versions:
-# Ticket: https://fftrac-bg.ffmpeg.org/ticket/10405
-# This patch should be removed when FFmpeg is updated to >=6.1
-patch -p1 < "$SCRIPT_PATH/fix-binutils-2.41.patch"
+prep_ffmpeg linux-$arch
 build_ffmpeg
 popd > /dev/null
 
 # gcc creates multiple symlinks per .so file for versioning.
 # We delete the symlinks and rename the real files to include the major library version
-rm linux-$ARCH/*.so
-for f in linux-$ARCH/*.so.*.*.*; do
+rm linux-$arch/*.so
+for f in linux-$arch/*.so.*.*.*; do
     mv -vf "$f" "${f%.*.*}"
 done
