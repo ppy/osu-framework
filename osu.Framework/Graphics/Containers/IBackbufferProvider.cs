d8c64da29d0a0a517007ceaaaab32a681774ff26
Marvin Schürz <m.schuerz@hautzy.com>
2024-10-19 01:07:10 +0200

    Add doc-comment for IBackbufferProvider

--- osu.Framework/Graphics/Containers/IBackbufferProvider.cs
+++ osu.Framework/Graphics/Containers/IBackbufferProvider.cs
@@ -5,6 +5,9 @@ using osu.Framework.Allocation;
 
 namespace osu.Framework.Graphics.Containers
 {
+    /// <summary>
+    /// A container which ensures that its children are drawn to a framebuffer.
+    /// </summary>
     [Cached]
     public interface IBackbufferProvider : IContainer
     {
