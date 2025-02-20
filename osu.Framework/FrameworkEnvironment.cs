2f57a591612e1754f04c9f0d936a0453eeef48cd
Dean Herbert <pe@ppy.sh>
2025-02-20 13:29:05 +0900

    Disable SDL3 desktop default for now
    Pending upstream fixes for external tablet driver support.

--- osu.Framework/FrameworkEnvironment.cs
+++ osu.Framework/FrameworkEnvironment.cs
@@ -53,7 +53,7 @@ namespace osu.Framework
             if (DebugUtils.IsDebugBuild)
                 AllowInsecureRequests = parseBool(Environment.GetEnvironmentVariable("OSU_INSECURE_REQUESTS")) ?? false;
 
-            UseSDL3 = RuntimeInfo.IsMobile || (parseBool(Environment.GetEnvironmentVariable("OSU_SDL3")) ?? true);
+            UseSDL3 = RuntimeInfo.IsMobile || (parseBool(Environment.GetEnvironmentVariable("OSU_SDL3")) ?? false);
         }
 
         private static bool? parseBool(string? value)
