92eb2d05c0ceb60add9ffca0cf4b5e3cf6ac2c68
Dean Herbert <pe@ppy.sh>
2025-02-28 13:42:58 +0900

    Fix tablet buttons being handled when window is not active
    Closes https://github.com/ppy/osu/issues/32132.

--- osu.Framework/Input/Handlers/Tablet/OpenTabletDriverHandler.cs
+++ osu.Framework/Input/Handlers/Tablet/OpenTabletDriverHandler.cs
@@ -53,6 +53,8 @@ namespace osu.Framework.Input.Handlers.Tablet
 
         private Task? lastInitTask;
 
+        private IBindable<bool> isActive;
+
         public override bool Initialize(GameHost host)
         {
             this.host = host;
@@ -60,6 +62,7 @@ namespace osu.Framework.Input.Handlers.Tablet
             outputMode = new AbsoluteTabletMode(this);
 
             host.Window.Resized += () => updateOutputArea(host.Window);
+            isActive = host.Window.IsActive.GetBoundCopy();
 
             AreaOffset.BindValueChanged(_ => updateTabletAndInputArea(device));
             AreaSize.BindValueChanged(_ => updateTabletAndInputArea(device));
@@ -203,6 +206,9 @@ namespace osu.Framework.Input.Handlers.Tablet
 
         private void handleTabletReport(ITabletReport tabletReport)
         {
+            if (!isActive.Value)
+                return;
+
             int buttonCount = tabletReport.PenButtons.Length;
             var buttons = new ButtonInputEntry<TabletPenButton>[buttonCount];
             for (int i = 0; i < buttonCount; i++)
@@ -213,6 +219,9 @@ namespace osu.Framework.Input.Handlers.Tablet
 
         private void handleAuxiliaryReport(IAuxReport auxiliaryReport)
         {
+            if (!isActive.Value)
+                return;
+
             int buttonCount = auxiliaryReport.AuxButtons.Length;
             var buttons = new ButtonInputEntry<TabletAuxiliaryButton>[buttonCount];
             for (int i = 0; i < buttonCount; i++)
