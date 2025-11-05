704f95e9dd0e053f7fbe29108e83aadd4a61b501
Du Yijie <nathandu@outlook.com>
2025-11-05 22:47:31 +0800

    Add outline font benchmarks

---
+++ osu.Framework.Benchmarks/BenchmarkOutlineFontLoading.cs
@@ -0,0 +1,62 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Diagnostics;
+using System.Reflection;
+using BenchmarkDotNet.Attributes;
+using osu.Framework.Extensions;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.IO.Stores;
+using SixLabors.ImageSharp.Memory;
+
+namespace osu.Framework.Benchmarks
+{
+    public class BenchmarkOutlineFontLoading : BenchmarkTest
+    {
+        private NamespacedResourceStore<byte[]> baseResources = null!;
+
+        public override void SetUp()
+        {
+            SixLabors.ImageSharp.Configuration.Default.MemoryAllocator = MemoryAllocator.Default;
+
+            baseResources = new NamespacedResourceStore<byte[]>(new DllResourceStore(@"osu.Framework.Benchmarks.dll"), @"Resources");
+        }
+
+        [Params(1, 10, 100, 1000, 10000)]
+        public int FetchCount;
+
+        private const string font_name = @"Fonts/FontAwesome5/FontAwesome-Solid";
+
+        [Benchmark(Baseline = true)]
+        public void BenchmarkNoCache()
+        {
+            using (var store = new SelfContainedOutlineGlyphStore(baseResources, font_name))
+                runFor(store);
+        }
+
+        private void runFor(OutlineGlyphStore store)
+        {
+            store.LoadFontAsync().WaitSafely();
+
+            var props = typeof(FontAwesome.Solid).GetProperties(BindingFlags.Public | BindingFlags.Static);
+
+            int remainingCount = FetchCount;
+
+            while (true)
+            {
+                foreach (var p in props)
+                {
+                    object? propValue = p.GetValue(null);
+                    Debug.Assert(propValue != null);
+
+                    var icon = (IconUsage)propValue;
+                    using (var upload = store.Get(icon.Icon.ToString()))
+                        Trace.Assert(upload.Data != null);
+
+                    if (remainingCount-- == 0)
+                        return;
+                }
+            }
+        }
+    }
+}
