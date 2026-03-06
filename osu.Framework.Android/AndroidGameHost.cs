c576b0f93be5929c8abe1b8d7743ae920d7bb248
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2026-03-06 15:14:32 +0100

    Add support for presenting files on Android

--- osu.Framework.Android/AndroidGameHost.cs
+++ osu.Framework.Android/AndroidGameHost.cs
@@ -3,9 +3,9 @@
 
 using System;
 using System.Collections.Generic;
-using System.IO;
 using Android.App;
 using Android.Content;
+using AndroidX.Core.Content;
 using osu.Framework.Android.Graphics.Textures;
 using osu.Framework.Android.Graphics.Video;
 using osu.Framework.Configuration;
@@ -17,6 +17,7 @@ using osu.Framework.Graphics.Video;
 using osu.Framework.IO.Stores;
 using osu.Framework.Logging;
 using osu.Framework.Platform;
+using Stream = System.IO.Stream;
 using Uri = Android.Net.Uri;
 
 namespace osu.Framework.Android
@@ -62,9 +63,85 @@ namespace osu.Framework.Android
         public override ISystemFileSelector CreateSystemFileSelector(string[] allowedExtensions)
             => new AndroidFileSelector(activity, allowedExtensions);
 
-        public override bool OpenFileExternally(string filename) => false;
+        /// <inheritdoc />
+        /// <remarks>
+        /// <para>
+        /// On Android, this method and <see cref="PresentFileExternally"/> have the same behaviour.
+        /// </para>
+        /// <para>
+        /// Because of Android's stringent restrictions on accessing files on the device,
+        /// this method will pretty much only work on files that the game directly controls or creates in its dedicated storages,
+        /// and even then, only if they are explicitly allowlisted as accessible via a <c>FileProvider</c>.
+        /// See provided example below for how to set up sharing.
+        /// </para>
+        /// <para>
+        /// If this method is prompted to open a file that is not in this game's storages, or the file path is not whitelisted, this method will return <see langword="false"/>
+        /// and log the appropriate error.
+        /// </para>
+        /// </remarks>
+        /// <example>
+        /// <para>
+        /// In <c>AndroidManifest.xml</c>:
+        /// <code>
+        /// &lt;manifest xmlns:android="http://schemas.android.com/apk/res/android" package="com.example.MyGame" android:installLocation="auto"&gt;
+        ///     &lt;application android:label="osu!framework test"&gt;
+        ///          &lt;provider android:name="androidx.core.content.FileProvider"
+        ///                    android:authorities="com.example.MyGame.fileprovider"
+        ///                    android:grantUriPermissions="true"
+        ///                    android:exported="false"&gt;
+        ///               &lt;meta-data android:name="android.support.FILE_PROVIDER_PATHS"
+        ///                          android:resource="@xml/filepaths" /&gt;
+        ///          &lt;/provider&gt;
+        ///     &lt;/application&gt;
+        /// &lt;/manifest&gt;
+        /// </code>
+        /// Note that the authority of the file provider MUST be the package name suffixed with <c>.fileprovider</c>.
+        /// </para>
+        /// <para>
+        /// In <c>Resources/xml/filepaths.xml</c>:
+        /// <code>
+        /// &lt;?xml version="1.0" encoding="utf-8"?&gt;
+        /// &lt;paths&gt;
+        ///      &lt;external-files-path path="logs" name="logs" /&gt;
+        /// &lt;/paths&gt;
+        /// </code>
+        /// Paths in <c>&lt;external-files-path&gt;</c> tags are relative to the only path in <see cref="UserStoragePaths"/>.
+        /// </para>
+        /// </example>
+        public override bool OpenFileExternally(string filename)
+        {
+            var context = activity.ApplicationContext!;
+            Java.IO.File file = new Java.IO.File(filename);
+            Uri? contentUri;
 
-        public override bool PresentFileExternally(string filename) => false;
+            try
+            {
+                contentUri = FileProvider.GetUriForFile(context, $"{context.PackageName}.fileprovider", file);
+            }
+            catch (Exception ex)
+            {
+                Logger.Log($"Failed to create content URI for file: {filename}.\nError: {ex}");
+                return false;
+            }
+
+            if (contentUri == null)
+                return false;
+
+            // https://developer.android.com/training/sharing/send#send-binary-content
+            // https://developer.android.com/reference/android/content/Intent#ACTION_SEND
+            var shareIntent = new Intent(Intent.ActionSend);
+            shareIntent.PutExtra(Intent.ExtraStream, contentUri);
+            shareIntent.SetType(activity.ContentResolver?.GetType(contentUri));
+            activity.StartActivity(Intent.CreateChooser(shareIntent, "Share"));
+            return true;
+        }
+
+        /// <inheritdoc/>
+        /// <remarks>
+        /// On Android, this method and <see cref="OpenFileExternally"/> have the same behaviour.
+        /// See remarks of that method for instructions how to set up file sharing on Android.
+        /// </remarks>
+        public override bool PresentFileExternally(string filename) => OpenFileExternally(filename);
 
         public override void OpenUrlExternally(string url)
         {
