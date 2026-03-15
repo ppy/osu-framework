15e3c8ad0b71294172d7919165ac0a28bbcae07f
Susko3 <Susko3@protonmail.com>
2026-03-15 14:59:01 +0100

    Fix mouse not being able to leave window across top and left edge on Wayland

--- osu.Framework/Input/UserInputManager.cs
+++ osu.Framework/Input/UserInputManager.cs
@@ -10,6 +10,7 @@ using osu.Framework.Input.Handlers;
 using osu.Framework.Input.StateChanges;
 using osu.Framework.Input.StateChanges.Events;
 using osu.Framework.Platform;
+using osu.Framework.Platform.SDL3;
 using osuTK;
 using osuTK.Input;
 using RectangleF = osu.Framework.Graphics.Primitives.RectangleF;
@@ -90,6 +91,12 @@ namespace osu.Framework.Input
             switch (Host.Window.WindowMode.Value)
             {
                 case WindowMode.Windowed:
+                    // The window location on wayland is always reported as the top-left corner of the current display,
+                    // which would erroniously prevent the mouse from exiting over the top and left edge.
+                    // In windowed, always consider the mouse as inside the displays to fix this.
+                    if (Host.Window is SDL3Window sdlWindow && sdlWindow.IsWayland)
+                        return false;
+
                     windowLocation = Host.Window.Position;
                     break;
 
