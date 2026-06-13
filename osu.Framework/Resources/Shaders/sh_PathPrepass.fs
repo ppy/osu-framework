73a1466319e0312923af0649a149af516d74cc97
Andrei Zavatski <megaman9919@gmail.com>
2026-06-14 02:20:36 +0300

    Improve path sdf encoding

--- osu.Framework/Resources/Shaders/sh_PathPrepass.fs
+++ osu.Framework/Resources/Shaders/sh_PathPrepass.fs
@@ -2,6 +2,7 @@
 #define PATH_PREPASS_FS
 
 #include "sh_CircularProgressUtils.h"
+#include "sh_Utils.h"
 
 layout(location = 0) in highp vec2 v_Position;
 layout(location = 1) in highp vec2 v_StartPos;
@@ -12,17 +13,9 @@ layout(location = 0) out vec4 o_Colour;
 
 void main(void) 
 {    
-    highp float dst = clamp(dstToLine(v_StartPos, v_EndPos, v_Position), 0.0, v_Radius);
-    highp float result = 1.0 - dst / v_Radius; // [0, 1] 24 bit
-
-    // encode 24-bit float as 3 8-bit floats
-    highp float v = result * 16777215.0;
-
-    highp float r = floor(v / 65536.0) / 255.0;
-    highp float g = floor(mod(v, 65536.0) / 256.0) / 255.0;
-    highp float b = mod(v, 256.0) / 255.0;
-
-    o_Colour = vec4(r, g, b, float(dst < v_Radius));
+    highp float dst = clamp(dstToLine(v_StartPos, v_EndPos, v_Position) / v_Radius, 0.0, 1.0);
+    highp float dstFromEdge = clamp(1.0 - dst, 0.0, 0.9999999);
+    o_Colour = encodeFloat(dstFromEdge);
 }
 
 #endif
