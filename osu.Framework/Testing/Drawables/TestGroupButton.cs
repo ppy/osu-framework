c118364b0d6f114db68c6f0f09f7a2be8984eeaf
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2026-09-10 11:08:26 +0200

    Remove unused usings
    Bumping the `LangVersion` seems to have unlocked *something* that allows
    `.Contains()` to be used on arrays without a LINQ using.

--- osu.Framework/Testing/Drawables/TestGroupButton.cs
+++ osu.Framework/Testing/Drawables/TestGroupButton.cs
@@ -6,7 +6,6 @@ using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Localisation;
 using System.Collections.Generic;
-using System.Linq;
 using osu.Framework.Extensions.IEnumerableExtensions;
 
 namespace osu.Framework.Testing.Drawables
